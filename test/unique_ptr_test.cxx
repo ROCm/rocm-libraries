3df340d43413cc22a725dc1573941b96fc8efc47
Daniel McIntosh <Daniel.McIntosh@amd.com>
2023-03-28 17:36:21 -0400

    Update test/unique_ptr_test.cxx to test host construction of gpu::unique_ptr

--- test/unique_ptr_test.cxx
+++ test/unique_ptr_test.cxx
@@ -1,6 +1,7 @@
 #include "gpu/memory"
 #include "hip/hip_runtime.h"
 #include <iostream>
+#include <cassert>
 
 #define CHECK(cmd)                                                                                 \
     {                                                                                              \
@@ -12,11 +13,40 @@
         }                                                                                          \
     }
 
-struct A {
-    __device__ ~A() {
-        printf("Destroying A: %d\n", b);
+struct PrintingDeleter {
+    __device__ ~PrintingDeleter() {
+        printf("Destroying PrintingDeleter: %d\n", b);
     }
     int b = 0;
+};
+
+struct Trivial {
+    char c;
+    int i;
+};
+
+// Trivially copyable, but not trivial
+struct TriviallyCopyable {
+    char c = 5;
+    int i = 7;
+};
+
+// Trivially Moveable, but not copy constructible (even non-trivially)
+struct TriviallyMoveable {
+    char c = 11;
+    int i = 13;
+    TriviallyMoveable(TriviallyMoveable &&) = default;
+};
+
+// Trivially Moveable, but no public constructor and not copy constructible (even non-trivially)
+struct NotConstructible {
+    char c = 23;
+    int i = 29;
+  private:
+    NotConstructible() {};
+  public:
+    static NotConstructible make() { return NotConstructible(); }
+    NotConstructible(NotConstructible &&) = default;
 };
 
 __global__ void gmain() {
@@ -24,12 +54,12 @@ __global__ void gmain() {
         gpu::unique_ptr<int[]> x(new int[32]);
     }
     {   
-        gpu::unique_ptr<A> y(new A{5});
+        gpu::unique_ptr<PrintingDeleter> y(new PrintingDeleter{5});
         printf("Before destruction\n");
     }
-    A *ptr;
+    PrintingDeleter *ptr;
     {   
-        gpu::unique_ptr<A> z(new A{7});
+        gpu::unique_ptr<PrintingDeleter> z(new PrintingDeleter{7});
         printf("Inside braces\n");
         ptr = z.release();
         printf("After release\n");
@@ -39,17 +69,39 @@ __global__ void gmain() {
     printf("After delete\n");
 }
 
+__global__ void test_value(int *ptr, int expected_value, int new_value) {
+    assert(*ptr == expected_value);
+    *ptr = new_value;
+}
+
 int main() {
-    /*
-    gpu::unique_ptr<int> x(0);
-    ++x;
+    {
+        Trivial tt{};
+        TriviallyCopyable tc{};
+        TriviallyMoveable tm{};
+        NotConstructible nc = NotConstructible::make();
+        auto a = gpu::make_unique<int>();
+        auto b = gpu::make_unique<float[]>(3);
+        auto c = gpu::make_unique(tt);
+        auto d = gpu::make_unique<Trivial>();
+        auto e = gpu::make_unique<Trivial[]>(5);
+        auto f = gpu::make_unique(tc);
+        auto g = gpu::make_unique(TriviallyCopyable{});
+        auto h = gpu::make_unique(std::move(tm));
+        auto i = gpu::make_unique(TriviallyMoveable{});
+        auto j = gpu::make_unique(std::move(nc));
+        auto k = gpu::make_unique(NotConstructible::make());
+    }
 
-    gpu::atomic<A> y;
-    y.store(A{.b = 6});
-
-    std::cout << x << std::endl;
-    std::cout << static_cast<A>(y).b << std::endl;
-    //*/
+    {
+        int x_h = 3;
+        auto x_d = gpu::make_unique(x_h);
+        hipLaunchKernelGGL(test_value, dim3(1), dim3(1), 0, nullptr, x_d.get(), 3, 5);
+        CHECK(hipMemcpy(&x_h, x_d.get(), sizeof(x_h), hipMemcpyDeviceToHost));
+        assert(x_h == 5);
+        CHECK(hipGetLastError());
+        CHECK(hipDeviceSynchronize());
+    }
 
     hipLaunchKernelGGL(gmain, dim3(1), dim3(1), 0, nullptr);
     CHECK(hipGetLastError());
