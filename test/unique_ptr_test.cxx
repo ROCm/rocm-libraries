d63abc5beae182437c3e791e67bdbc337c833758
Daniel McIntosh <Daniel.McIntosh@amd.com>
2023-03-29 20:26:15 -0400

    Update test/unique_ptr_test.cxx to test conversion between std::unique_ptr and gpu::unique_ptr

--- test/unique_ptr_test.cxx
+++ test/unique_ptr_test.cxx
@@ -74,6 +74,15 @@ __global__ void test_value(int *ptr, int expected_value, int new_value) {
     *ptr = new_value;
 }
 
+template <class T>
+gpu::unique_ptr<T, gpu::host_delete<T>> test_auto_conversion(gpu::unique_ptr<T, gpu::host_delete<T>> ptr) {
+    return ptr;
+}
+template <class T>
+std::unique_ptr<T> test_auto_conversion2(std::unique_ptr<T> ptr) {
+    return ptr;
+}
+
 int main() {
     {
         Trivial tt{};
@@ -101,6 +110,69 @@ int main() {
         assert(x_h == 5);
         CHECK(hipGetLastError());
         CHECK(hipDeviceSynchronize());
+    }
+
+    {
+        auto x_h = std::make_unique<int>(17);
+        gpu::unique_ptr<int, gpu::host_delete<int>> x_d = std::move(x_h);
+        hipLaunchKernelGGL(test_value, dim3(1), dim3(1), 0, nullptr, x_d.get(), 17, 19);
+        int result;
+        CHECK(hipMemcpy(&result, x_d.get(), sizeof(result), hipMemcpyDeviceToHost));
+        assert(result == 19);
+        CHECK(hipGetLastError());
+        CHECK(hipDeviceSynchronize());
+    }
+
+    {
+        // convert from std::unique_ptr to gpu::unique_ptr before passing to test_auto_conversion
+        // then convert the returned gpu::unique_ptr back to std::unique_ptr
+        auto w_h = std::make_unique<int>(17);
+        std::unique_ptr<int> w2_h = test_auto_conversion<int>(std::move(w_h));
+        assert(*w2_h == 17);
+
+        // convert from gpu::unique_ptr to std::unique_ptr before passing to test_auto_conversion
+        // then convert the returned std::unique_ptr back to gpu::unique_ptr
+        auto x_d = gpu::make_unique<int>(5);
+        gpu::unique_ptr<int, gpu::host_delete<int>> x2_d = test_auto_conversion2<int>(std::move(x_d));
+
+        // convert from std::unique_ptr to gpu::unique_ptr before passing to test_auto_conversion
+        // then convert the returned gpu::unique_ptr back to std::unique_ptr
+        auto y_h = std::make_unique<NotConstructible>(NotConstructible::make());
+        std::unique_ptr<NotConstructible> y2_h = test_auto_conversion<NotConstructible>(std::move(y_h));
+
+        // convert from gpu::unique_ptr to std::unique_ptr before passing to test_auto_conversion
+        // then convert the returned std::unique_ptr back to gpu::unique_ptr
+        auto z_d = gpu::make_unique<NotConstructible>(NotConstructible::make());
+        gpu::unique_ptr<NotConstructible, gpu::host_delete<NotConstructible>> z2_d = test_auto_conversion2<NotConstructible>(std::move(z_d));
+    }
+
+    {
+        auto x_h = std::make_unique<int[]>(2);
+        x_h[0] = 3;
+        x_h[1] = 7;
+        std::unique_ptr<int[]> x2_h = test_auto_conversion<int[]>({std::move(x_h), 2}).move_to_host(2);
+        assert(x2_h[0] == 3);
+        assert(x2_h[1] == 7);
+
+        auto y_h = std::make_unique<int[]>(2);
+        y_h[0] = 11;
+        y_h[1] = 13;
+        gpu::unique_ptr<int[], gpu::host_delete<int[]>> y_d(std::move(y_h), 2);
+        hipLaunchKernelGGL(test_value, dim3(1), dim3(1), 0, nullptr, y_d.get(),     11, 23);
+        hipLaunchKernelGGL(test_value, dim3(1), dim3(1), 0, nullptr, y_d.get() + 1, 13, 29);
+        CHECK(hipGetLastError());
+        CHECK(hipDeviceSynchronize());
+        std::unique_ptr<int[]> y2_h = std::move(y_d).move_to_host(2);
+        assert(y2_h[0] == 23);
+        assert(y2_h[1] == 29);
+
+        auto z_h = std::unique_ptr<NotConstructible[]>(new NotConstructible[2]{NotConstructible::make(), NotConstructible::make()});
+        z_h[0].i = 17;
+        z_h[1].i = 19;
+        gpu::unique_ptr<NotConstructible[], gpu::host_delete<NotConstructible[]>> z_d(std::move(z_h), 2);
+        std::unique_ptr<NotConstructible[]> z2_h = std::move(z_d).move_to_host(2);
+        assert(z2_h[0].i == 17);
+        assert(z2_h[1].i == 19);
     }
 
     hipLaunchKernelGGL(gmain, dim3(1), dim3(1), 0, nullptr);
