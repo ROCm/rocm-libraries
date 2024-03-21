11b630ae2b5964b3ad279d19aa2f2bd6f631bf7b
Daniel McIntosh <Daniel.McIntosh@amd.com>
2024-03-21 13:50:55 -0400

    Replace getActiveVcoreCount with a counter that is kept up to date

--- src/gpu/thread.cxx
+++ src/gpu/thread.cxx
@@ -8,6 +8,7 @@ __device__ WorkNode_Header *cpuWorkQueue[CPU_WORK_QUEUE_SIZE] = {};
 std::atomic<uint32_t> cpuWorkQueueIndex_push = 0;
 __device__ uint32_t cpuWorkQueueIndex_pop = 0;
 
+__device__ uint32_t activeVcoreCount = 0;
 __device__ WorkNode_Header *workListHead = nullptr;
 __device__ WorkNode_Header **workListTail = &workListHead;
 __device__ WorkNode_Header *volatile currentWorkNode[NUM_VCORES] = {};
