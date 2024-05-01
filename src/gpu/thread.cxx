e951fef56c1b52a40060e8bb880e00c70dfa9965
Daniel McIntosh <Daniel.McIntosh@amd.com>
2024-04-30 20:27:58 -0400

    Redesign gpu::threads to remove the global lock and instead use per-workitem locks

--- src/gpu/thread.cxx
+++ src/gpu/thread.cxx
@@ -8,10 +8,12 @@ __device__ WorkNode_Header *cpuWorkQueue[CPU_WORK_QUEUE_SIZE] = {};
 std::atomic<uint32_t> cpuWorkQueueIndex_push = 0;
 __device__ uint32_t cpuWorkQueueIndex_pop = 0;
 
+__device__ WorkNode_Header *mainWorkQueue[MAIN_WORK_QUEUE_SIZE] = {};
+__device__ uint32_t mainWorkQueueIndex_pop = 0;
+__device__ uint32_t mainWorkQueueIndex_push = 0;
+
 __device__ uint32_t activeVcoreCount = 0;
-__device__ WorkNode_Header *workListHead = nullptr;
-__device__ WorkNode_Header **workListTail = &workListHead;
-__device__ WorkNode_Header *volatile currentWorkNode[NUM_VCORES] = {};
+__device__ WorkNode_Header *currentWorkNode[NUM_VCORES] = {};
 
 hipStream_t mainStream;
 bool started = false;
