43450fd31bc9d035a1ac546829bd70b326b2ce30
Daniel McIntosh <Daniel.McIntosh@amd.com>
2024-06-04 14:27:45 -0400

    Initial work to facilitate spawning gpu::threads from host without a kernel launch

--- src/gpu/thread.cxx
+++ src/gpu/thread.cxx
@@ -4,6 +4,10 @@ namespace gpu::internal {
 
 __device__ uint32_t numVcores = NUM_VCORES;
 
+__device__ WorkNode_Header *cpuWorkQueue[CPU_WORK_QUEUE_SIZE] = {};
+std::atomic<uint32_t> cpuWorkQueueIndex_push = 0;
+__device__ uint32_t cpuWorkQueueIndex_pop = 0;
+
 __device__ WorkNode_Header *workListHead = nullptr;
 __device__ WorkNode_Header **workListTail = &workListHead;
 __device__ WorkNode_Header *volatile currentWorkNode[NUM_VCORES] = {};
