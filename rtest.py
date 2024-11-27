d6d6e3a8b27c3662d661ebbb588f8faf99e141ec
NguyenNhuDi <Zee.nguyen@amd.com>
2024-11-27 15:23:56 -0700

    implemented for windows and added --test_dir option

--- rtest.py
+++ rtest.py
@@ -34,6 +34,8 @@ class TestRunner():
                             help='Test output file (optional, default: None [output to stdout])')
         self.parser.add_argument(      '--install_dir', type=str, required=False, default="build", 
                             help='Installation directory where build or release folders are (optional, default: build)')
+        self.parser.add_argument(      '--test_dir', type=str, required=False, default=None,
+                            help='Test directory where rocprim tests are (optinal, default=None)')
         self.parser.add_argument(      '--fail_test', default=False, required=False, action='store_true',
                             help='Return as if test failed (optional, default: false)')
         self.args = self.parser.parse_args()
@@ -80,8 +82,12 @@ class TestRunner():
         self.lib_dir = os.path.dirname(os.path.abspath(__file__)) 
         self.xml_path = os.path.join(self.lib_dir, r'rtest.xml')
 
-        if self.OS_info['System'] == 'Linux':
-            
+        if self.args.test_dir:
+            if not os.path.isdir(self.args.test_dir):
+                raise ValueError(f'Value {self.args.test_dir} is not a directory!')
+
+            self.test_dir = self.args.test_dir
+        else:
             # find the test dir with default install dir
             if self.args.install_dir == 'build':
                 # if its debug mode
@@ -90,10 +96,10 @@ class TestRunner():
                 # if its release mode
                 elif os.path.isdir(os.path.join(self.lib_dir, f'build/release/test')): 
                     self.test_dir = os.path.join(self.lib_dir, f'build/release/test')
-                else:
+                elif os.path.isdir(os.path.join(self.lib_dir, f'build/test')):
                     self.test_dir = os.path.join(self.lib_dir, f'build/test')
-
-
+                else:
+                    raise FileNotFoundError(f'Did not find test directory')
             else:
                 # if its an actual directory AND it has test directory
                 if os.path.isdir(os.path.join(self.args.install_dir, f'test')):
@@ -107,6 +113,16 @@ class TestRunner():
         else:
             self.output = None
             self.output_path = None
+        
+        if self.OS_info['System'] == 'Windows':
+            self.lib_dir = self.lib_dir.replace('\\', '/')
+            self.xml_path = self.xml_path.replace('\\', '/')
+            self.test_dir = self.test_dir.replace('\\', '/')
+
+            if self.output_path:
+                self.output_path = self.output_path.replace('\\', '/')
+
+                self.output = open(os.path.abspath(self.output_path), 'w')
 
         m = ' Current Paths'
         print(f'{m:-^100}')
@@ -116,8 +132,6 @@ class TestRunner():
         print(f'Output File:       {self.output_path}')
         
         print()
-    
-
 
     def __call__(self):
         xml_file = minidom.parse(self.xml_path)
