3d0c296600ba17cecfbf2f29fe5437185ab6585c
Daniel McIntosh <Daniel.McIntosh@amd.com>
2025-05-08 17:14:50 -0400

    [thread] Add gpu::this_thread::sleep_for
    [ROCm/hipthreads commit: 067a18ec8926baf89dfa04bf532cf58b052efd84]

--- projects/hipthreads/inc/gpu/__thread/this_thread.h
+++ projects/hipthreads/inc/gpu/__thread/this_thread.h
@@ -27,9 +27,32 @@
 #ifndef __GPU___THREAD_THIS_THREAD_H__
 #define __GPU___THREAD_THIS_THREAD_H__
 
+#include <hip/std/chrono>
+
 namespace gpu {
 
 namespace this_thread {
+
+_LIBGPU_EXPORTED_FROM_ABI __device__ void sleep_for(cuda::std::chrono::nanoseconds __ns);
+
+// TODO: Should we also provide an implementation that accepts std::chrono::duration (and not just cuda::std::chrono::duration)?
+template <class _Rep, class _Period>
+__device__ _LIBGPU_HIDE_FROM_ABI void sleep_for(const cuda::std::chrono::duration<_Rep, _Period>& __d) {
+  if (__d > cuda::std::chrono::duration<_Rep, _Period>::zero()) {
+    // The standard guarantees a 64bit signed integer resolution for nanoseconds,
+    // so use INT64_MAX / 1e9 as cut-off point. Use a constant to avoid <climits>
+    // and issues with long double folding on PowerPC with GCC.
+    constexpr cuda::std::chrono::duration<long double> __max{9223372036.0L};
+    cuda::std::chrono::nanoseconds __ns;
+    if (__d < __max) {
+      __ns = cuda::std::chrono::duration_cast<cuda::std::chrono::nanoseconds>(__d);
+      if (__ns < __d)
+        ++__ns;
+    } else
+      __ns = cuda::std::chrono::nanoseconds::max();
+    gpu::this_thread::sleep_for(__ns);
+  }
+}
 
 __device__ gpu::thread::id get_id() noexcept;
 __device__ void pseudo_yield();
