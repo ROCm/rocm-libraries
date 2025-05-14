49a3193790e76eda8ceab06f7f167bb6b3bcdfd2
Cenxuan <cenxuan@streamhpc.com>
2025-05-14 13:24:16 +0000

    WIP: add configs for MIOpenBatchNormFwdTrainSpatial and add macro to read env config in batchnorm_functions.hpp

--- src/kernels/batchnorm_functions.hpp
+++ src/kernels/batchnorm_functions.hpp
@@ -91,7 +91,8 @@
 #endif
 
 #if MIOPEN_USE_BFPMIX == 1
-    #define FP_TYPE ushort
+    // Enables bfloat16, stored in ushort
+    #define FP_TYPE uint16_t
 
     #ifdef MIO_BN_NODPP
     #undef MIO_BN_NODPP
@@ -108,8 +109,10 @@
     #endif
     #define EPSILON static_cast<FP_TYPE_PREC>(0.000001)
 
+    // TODO bfloat16_to_float and float_to_bfloat16 is not directly supported in HIP
     #define FLOAT2FLOATPREC(x) (bfloat16_to_float(x))
     #define FLOATPREC2FLOAT(x) (float_to_bfloat16(x))
+
     #define FLOAT2ACCUM(x) (FLOAT2FLOATPREC(x))
     #define ACCUM2FLOAT(x) (FLOATPREC2FLOAT(x))
 
@@ -119,3 +122,82 @@
     #define FLOAT2ACCUM(x) (static_cast<_FLOAT_ACCUM>(x))
     #define ACCUM2FLOAT(x) (static_cast<FP_TYPE>(x))
 #endif
+
+#define _FLOAT2 PPCAT(FP_TYPE, TWO)
+#define _FLOAT4 PPCAT(FP_TYPE, FOUR)
+#define _FLOAT8 PPCAT(FP_TYPE, EIGHT)
+#define _AS_FLOAT PPCAT(as_, FP_TYPE)
+
+#define _FLOAT_PREC4 PPCAT(FP_TYPE_PREC, FOUR)
+#define _FLOAT_ACCUM4 PPCAT(_FLOAT_ACCUM, FOUR)
+
+
+// Env Configs
+#ifndef MIO_BN_LDSGCN_SIZE
+#define MIO_BN_LDSGCN_SIZE 16
+#endif
+
+#ifndef MIO_BN_LDS_SIZE
+#define MIO_BN_LDS_SIZE 256
+#endif
+
+#ifndef MIO_BN_C
+#define MIO_BN_C 1
+#endif
+
+#ifndef MIO_BN_N
+#define MIO_BN_N 1
+#endif
+
+#ifndef MIO_BN_NHW
+#define MIO_BN_NHW 1
+#endif
+
+#ifndef MIO_BN_INHW
+#define MIO_BN_INHW 1
+#endif
+
+#ifndef MIO_BN_CHW
+#define MIO_BN_CHW 1
+#endif
+
+#ifndef MIO_BN_HW
+#define MIO_BN_HW 1
+#endif
+
+#ifndef MIO_BN_GRP0
+#define MIO_BN_GRP0 1
+#endif
+
+#ifndef MIO_BN_GRP1
+#define MIO_BN_GRP1 1
+#endif
+
+#ifndef MIO_BN_GRP2
+#define MIO_BN_GRP2 1
+#endif
+
+#ifndef MIO_BN_NGRPS
+#define MIO_BN_NGRPS 1
+#endif
+
+#ifndef MIO_BN_LOOP_UNROLL_MAXN
+#define MIO_BN_LOOP_UNROLL_MAXN 768
+#endif
+
+#ifndef MIO_BN_LOOP_UNROLL_MAXHW
+#define MIO_BN_LOOP_UNROLL_MAXHW 2500
+#endif
+
+#ifndef MIO_BN_NCHW
+#define MIO_BN_NCHW 1
+#endif
+
+#ifndef MIO_BN_VARIANT
+#define MIO_BN_VARIANT 255
+#endif
+
+#ifndef MIO_BN_MAXN
+#define MIO_BN_MAXN 65
+#endif
+
