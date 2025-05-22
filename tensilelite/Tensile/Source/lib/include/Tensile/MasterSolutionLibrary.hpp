ROCm/rocm-libraries

commit 0d994713d40d995a5bb886444d77805615777c4e

streamk - adding memory fallbacks (#1682)
This PR adds in the fallback cases when not enough or null ptr workspace
is provided.

Notes
- correctly initialises inputs workspacesize
- updates requiredWorkspaceSize helper for data parallel and persistent
kernel cases
- using .githooks/pre-commit has introduced some additional formatting
independent of my PR

---------

Co-authored-by: mahmoodw <wmahmood@amd.com>

tensilelite/Tensile/Source/lib/include/Tensile/SingleSolutionLibrary.hpp
@@ -92,13 +92,14 @@ namespace TensileLite
 
             if(solution)
             {
+                Task task(hardware, problem, *(solution));
                 if(debug)
                 {
-                    std::cout << "hardwarePredicate:" << std::endl;
                     solution->hardwarePredicate->debugEval(hardware, std::cout);
                     std::cout << std::endl;
-                    std::cout << "problemPredicate:" << std::endl;
                     solution->problemPredicate->debugEval(problem, std::cout);
+                    std::cout << std::endl;
+                    solution->taskPredicate->debugEval(task, std::cout);
                     std::cout << std::endl;
                 }
 
@@ -129,7 +130,9 @@ namespace TensileLite
                     for(int idx = 0; idx < problems.size(); idx++)
                     {
                         auto problem = problems[idx];
+                        Task task(hardware, problem, *(solution));
                         solution->problemPredicate->debugEval(problem, std::cout);
+                        solution->taskPredicate->debugEval(task, std::cout);
                     }
                 }
 
@@ -141,9 +144,10 @@ namespace TensileLite
                 for(int idx = 0; idx < problems.size(); idx++)
                 {
                     auto problem = problems[idx];
+                    Task task(hardware, problem, *(solution));
                     problem.setWorkspaceSizeGroupedGemm(ws);
                     problem.setGroupedGemmCount(problems.size());
-                    if(!(*solution->problemPredicate)(problem))
+                    if(!(*solution->problemPredicate)(problem)  || !(*solution->taskPredicate)(task) )
                         return std::shared_ptr<MySolution>();
                 }
 
@@ -174,15 +178,19 @@ namespace TensileLite
             bool useSolution = false;
             if(solution)
             {
+                Task task(hardware, problem, (*solution));
                 if(debug)
                 {
                     solution->hardwarePredicate->debugEval(hardware, std::cout);
                     if(searchType == SolutionLibrarySearchType::DEFAULT)
+                    {
                         solution->problemPredicate->debugEval(problem, std::cout);
+                        solution->taskPredicate->debugEval(task, std::cout);
+                    }
                 }
 
                 if((*solution->hardwarePredicate)(hardware)
-                   && softwarePredicate(searchType, (*solution), problem))
+                   && softwarePredicate(searchType, task, hardware, (*solution), problem)) 
                     useSolution = true;
             }
             else if(debug)
@@ -225,9 +233,10 @@ namespace TensileLite
                     for(int idx = 0; idx < problems.size(); idx++)
                     {
                         auto problem = problems[idx];
+                        Task task(hardware, problem, (*solution));
                         problem.setWorkspaceSizeGroupedGemm(ws);
                         problem.setGroupedGemmCount(problems.size());
-                        if(!(*solution->problemPredicate)(problem))
+                        if(!(*solution->problemPredicate)(problem) || !(*solution->taskPredicate)(task))
                             useSolution = false;
                     }
                 }
@@ -244,7 +253,9 @@ namespace TensileLite
                         for(int idx = 0; idx < problems.size(); idx++)
                         {
                             auto problem = problems[idx];
+                            Task task(hardware, problem, (*solution));
                             solution->problemPredicate->debugEval(problem, std::cout);
+                            solution->taskPredicate->debugEval(task, std::cout);
                         }
                 }
             }

library/src/amd_detail/rocblaslt/src/tensile_host.cpp
@@ -1672,7 +1672,8 @@ namespace
         inputs.batchD = reinterpret_cast<void* const*>(prob.batch_D);
 
         // Set the GSU workspace
-        inputs.ws = prob.workspace;
+        inputs.ws            = prob.workspace;
+        inputs.workspaceSize = prob.workspaceSize;
 
         inputs.Synchronizer = prob.Synchronizer;
 
@@ -3531,7 +3532,8 @@ rocblaslt_status isSolutionSupported(rocblaslt_handle       handle,
         {
             tensile_prob.setParams().resetInternalArgs();
         }
-
+        
+        TensileLite::Task task(*hardware, tensile_prob, *solution);
         tensile_prob.setWorkspaceSize(algo->max_workspace_bytes);
         if(!(*solution->hardwarePredicate)(*hardware))
         {
@@ -3553,6 +3555,20 @@ rocblaslt_status isSolutionSupported(rocblaslt_handle       handle,
                 std::ostringstream msg;
                 msg << "Software match: " << solution->description();
                 solution->problemPredicate->debugEval(tensile_prob, msg);
+                msg << std::endl;
+                log_info(__func__, msg.str());
+            }
+
+            log_error(__func__, "Solution is not supported");
+            return rocblaslt_status_invalid_value;
+        }
+        if(!(*solution->taskPredicate)(task))
+        {
+            if(get_logger_layer_mode() & rocblaslt_layer_mode_log_info)
+            {
+                std::ostringstream msg;
+                msg << "Software match: " << solution->description();
+                solution->taskPredicate->debugEval(task, msg);
                 msg << std::endl;
                 log_info(__func__, msg.str());
             }
@@ -3612,8 +3628,7 @@ rocblaslt_status isSolutionSupported(rocblaslt_handle       handle,
                 if(get_logger_layer_mode() & rocblaslt_layer_mode_log_info)
                 {
                     std::ostringstream msg;
-                    msg << "Match "
-                        << "[" << i << "]: " << solution->description();
+                    msg << "Match " << "[" << i << "]: " << solution->description();
                     solution->problemPredicate->debugEval(tensile_prob.gemms[i], msg);
                     msg << std::endl;
                     log_info(__func__, msg.str());

tensilelite/Tensile/Contractions.py
@@ -405,6 +405,19 @@ def extractDimPredicate(cls, key, value, predicateName):
     elif len(predicates) > 1:
         return cls.And(predicates)
 
+class TaskPredicate(Properties.Predicate):
+    @classmethod
+    def FromOriginalKeyPair(cls, pair):
+        (key, value) = pair
+        if key == "_WorkspaceSizePerElemC" and value > 0:
+            return cls("WorkspaceCheck")
+        return None
+
+    @classmethod
+    def FromOriginalState(cls, d, problemType, morePreds=[]):
+        predicates = [p for p in map(cls.FromOriginalKeyPair, d.items()) if p is not None]
+        return cls.And(predicates)
+
 class ProblemPredicate(Properties.Predicate):
     @classmethod
     def FromOriginalKeyPair(cls, pair):
@@ -432,9 +445,6 @@ class ProblemPredicate(Properties.Predicate):
                 raise RuntimeError("Unknown Multiple Value: {}".format(key))
 
             return cls(tag, index=index, value=value)
-
-        if key == "WorkspaceCheck" and (not all(val == 0 for val in value)):
-            return cls("WorkspaceCheck", index=0, value=value)
 
         if key.startswith('Assert'):
             raise RuntimeError("Unknown assertion key: {}".format(key))
@@ -663,6 +673,7 @@ class Solution:
                 'problemType',
                 'hardwarePredicate',
                 'problemPredicate',
+                'taskPredicate',
                 'sizeMapping',
                 'internalArgsSupport',
                 'debugKernel',
@@ -719,6 +730,7 @@ class Solution:
         rv.problemType = ProblemType.FromOriginalState(d['ProblemType'])
 
         rv.problemPredicate = ProblemPredicate.FromOriginalState(d, rv.problemType)
+        rv.taskPredicate = TaskPredicate.FromOriginalState(d, rv.problemType)
 
         if 'DebugKernel' in d:
             rv.debugKernel = d['DebugKernel']
@@ -774,6 +786,7 @@ class Solution:
         self.problemType = None
         self.hardwarePredicate = Hardware.HardwarePredicate('TruePred')
         self.problemPredicate = ProblemPredicate('TruePred')
+        self.taskPredicate = TaskPredicate('TruePred')
         self.sizeMapping = None
         self.debugKernel = False
         self.libraryLogicIndex = {}

tensilelite/Tensile/Source/client/source/SolutionIterator.cpp
@@ -96,13 +96,16 @@ namespace TensileLite
 
             // Test if the persistent kernel is eligible for the current hw and solution
             problem.checkPersistentKernelEligibility(solution, *m_hardware);
-            if(!(*solution.problemPredicate)(problem))
+            Task task(*m_hardware, problem, solution);
+            if(!(*solution.problemPredicate)(problem) || !(*solution.taskPredicate)(task))
             {
                 m_reporter->report(ResultKey::Validation, "DID_NOT_SATISFY_ASSERTS");
                 if(m_reporter->logAtLevel(LogLevel::Verbose) && !m_printWinnerOnly)
                 {
                     std::ostringstream msg;
                     solution.problemPredicate->debugEval(problem, msg);
+                    msg << std::endl;
+                    solution.taskPredicate->debugEval(task, msg);
                     msg << std::endl;
                     m_reporter->log(LogLevel::Verbose, msg.str());
                 }

tensilelite/Tensile/Source/lib/include/Tensile/ContractionProblemPredicates.hpp
@@ -1506,119 +1506,6 @@ namespace TensileLite
                 }
             };
 
-            struct WorkspaceCheck : public Predicate_CRTP<WorkspaceCheck, ContractionProblemGemm>
-            {
-                enum
-                {
-                    HasIndex = true,
-                    HasValue = true
-                };
-#define MAX_GSU_WORKSPACE_SIZE 128 * 1024 * 1024
-                size_t             index;
-                std::array<int, 3> value;
-
-                WorkspaceCheck() = default;
-                WorkspaceCheck(size_t index, std::array<int, 3> value)
-                    : index(index)
-                    , value(value)
-                {
-                }
-
-                static std::string Type()
-                {
-                    return "WorkspaceCheck";
-                }
-
-                static size_t
-                    reductionSize(ContractionProblemGemm const& problem, int& elemC, int& elemBias)
-                {
-                    size_t reductionSize = 0;
-                    // 2d reduction
-                    if(problem.useGradient() && problem.useBias()
-                       && problem.getParams().biasEnum() != rocisa::DataType::None)
-                    {
-                        if(problem.biasSrc() == ContractionProblemGemm::TENSOR::D && (elemC == 0))
-                            reductionSize += problem.d().totalLogicalElements()
-                                             * problem.computeTypeElementSize();
-                        else if(problem.biasSrc() == ContractionProblemGemm::TENSOR::A)
-                        {
-                            reductionSize += problem.freeSizeA(0) * elemBias;
-                        }
-                        else if(problem.biasSrc() == ContractionProblemGemm::TENSOR::B)
-                        {
-                            reductionSize += problem.freeSizeB(0) * elemBias;
-                        }
-                    }
-                    return reductionSize;
-                }
-
-                virtual bool operator()(ContractionProblemGemm const& problem) const override
-                {
-                    int16_t gsu = problem.getParams().gsu() != 0 ? problem.getParams().gsu() : value[2];
-                    // auto gsu will consider workspace size, so bypassed
-                    if (gsu == -1)
-                        return 1;
-
-                    int gsuMultiplier = gsu > 1 ? gsu : 0;
-                    int elemC         = value[0] * gsuMultiplier;
-                    int elemBias      = value[1] * gsuMultiplier;
-                    size_t rs         = reductionSize(problem, elemC, elemBias);
-                    if(problem.d().totalLogicalElements() * elemC > MAX_GSU_WORKSPACE_SIZE)
-                        return 0;
-
-                    if(problem.groupedGemm())
-                        return problem.workspaceSizeGroupedGemm() <= problem.workspaceSize();
-                    else
-                        return problem.d().totalLogicalElements() * elemC + rs
-                               <= problem.workspaceSize();
-                }
-
-                virtual bool debugEval(ContractionProblemGemm const& problem,
-                                       std::ostream&                 stream) const override
-                {
-                    int16_t gsu = problem.getParams().gsu() != 0 ? problem.getParams().gsu() : value[2];
-                    if (gsu == -1)
-                    {
-                        bool rv = (*this)(problem);
-
-                        stream << *this << ": (" << "auto gsu will consider workspace size, so bypassed"
-                               << ") == " << rv;
-
-                        return rv;
-                    }
-
-                    int gsuMultiplier = gsu > 1 ? gsu : 0;
-                    int elemC         = value[0] * gsuMultiplier;
-                    int elemBias      = value[1] * gsuMultiplier;
-                    size_t rs         = reductionSize(problem, elemC, elemBias);
-
-                    if(problem.d().totalLogicalElements() * elemC > MAX_GSU_WORKSPACE_SIZE)
-                        return debugEvalCmp(problem,
-                                            stream,
-                                            "prob",
-                                            problem.d().totalLogicalElements() * elemC,
-                                            "<=",
-                                            "max gsu workspace size",
-                                            MAX_GSU_WORKSPACE_SIZE);
-
-                    if(problem.groupedGemm())
-                        return debugEvalCmp(problem,
-                                            stream,
-                                            "prob",
-                                            problem.workspaceSizeGroupedGemm(),
-                                            "<=",
-                                            "max",
-                                            problem.workspaceSize());
-                    return debugEvalCmp(problem,
-                                        stream,
-                                        "prob",
-                                        problem.d().totalLogicalElements() * value[0] + rs,
-                                        "<=",
-                                        "max",
-                                        problem.workspaceSize());
-                }
-            };
-
             struct WorkgroupNumberCheck
                 : public Predicate_CRTP<WorkgroupNumberCheck, ContractionProblemGemm>
             {

tensilelite/Tensile/Source/lib/include/Tensile/ContractionSolution.hpp
@@ -38,6 +38,7 @@
 #include <Tensile/ContractionProblem_fwd.hpp>
 #include <Tensile/DataTypes.hpp>
 #include <Tensile/Predicates.hpp>
+#include <Tensile/Task.hpp>
 #include <Tensile/Utils.hpp>
 
 #define TENSILE_COMMON_KERNEL_ARGS_SIZE 16
@@ -538,7 +539,8 @@ namespace TensileLite
         ThreadSafeValue<std::string> codeObjectFilename;
         bool                         debugKernel   = false;
         bool                         kernelArgsLog = false;
-
+        std::shared_ptr<Predicates::Predicate<Task>> taskPredicate
+            = std::make_shared<Predicates::True<Task>>();
         std::shared_ptr<Predicates::Predicate<Problem>> problemPredicate
             = std::make_shared<Predicates::True<Problem>>();
         std::shared_ptr<Predicates::Predicate<Hardware>> hardwarePredicate

tensilelite/Tensile/Source/lib/include/Tensile/ContractionTaskPredicates.hpp
@@ -1,0 +1,116 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#pragma once
+
+#include <Tensile/AMDGPU.hpp>
+#include <Tensile/ContractionProblem.hpp>
+#include <Tensile/ContractionSolution.hpp>
+#include <Tensile/Predicates.hpp>
+#include <Tensile/Task.hpp>
+
+#include <vector>
+
+namespace TensileLite
+{
+    namespace Predicates
+    {
+        /**
+ * \addtogroup Predicates
+ * @{
+ */
+        /**
+ * @brief Complex Predicates
+ */
+        namespace Contraction
+        {
+            struct WorkspaceCheck : public Predicate_CRTP<WorkspaceCheck, Task>
+            {
+                enum
+                {
+                    HasIndex = false,
+                    HasValue = false
+                };
+
+#define MAX_GSU_WORKSPACE_SIZE 128 * 1024 * 1024
+
+                WorkspaceCheck() = default;
+
+                static std::string Type()
+                {
+                    return "WorkspaceCheck";
+                }
+
+                virtual bool operator()(Task const& task) const override
+                {
+                    if(task.problem.groupedGemm())
+                        return task.problem.workspaceSizeGroupedGemm() <= task.problem.workspaceSize();
+                    else
+                        return task.solution.requiredWorkspaceSize(task.problem, task.hardware) <= task.problem.workspaceSize();
+                }
+
+                virtual bool debugEval(Task const& task, std::ostream& stream) const override
+                {
+
+                    size_t gsu           = task.problem.getParams().gsu() > 0 ? task.problem.getParams().gsu()
+                                                                        : task.solution.sizeMapping.globalSplitU;
+                    size_t gsuMultiplier = gsu > 1 ? gsu : 0;
+
+                    if(task.problem.d().totalLogicalElements() * gsuMultiplier > MAX_GSU_WORKSPACE_SIZE)
+                        return debugEvalCmp(task,
+                                            stream,
+                                            "prob",
+                                            task.problem.d().totalLogicalElements() * gsuMultiplier,
+                                            "<=",
+                                            "max gsu workspace size",
+                                            MAX_GSU_WORKSPACE_SIZE);
+
+                    if(task.problem.groupedGemm())
+                        return debugEvalCmp(task,
+                                            stream,
+                                            "prob",
+                                            task.problem.workspaceSizeGroupedGemm(),
+                                            "<=",
+                                            "max",
+                                            task.problem.workspaceSize());
+
+                    return debugEvalCmp(task,
+                                        stream,
+                                        "prob",
+                                        task.solution.requiredWorkspaceSize(task.problem, task.hardware),
+                                        "<=",
+                                        "max",
+                                        task.problem.workspaceSize());
+
+                }
+            };
+        } // namespace Contraction
+
+        /**
+ * @}
+ */
+    } // namespace Predicates
+} // namespace TensileLite

tensilelite/Tensile/Source/lib/include/Tensile/Debug.hpp
@@ -73,6 +73,8 @@ namespace TensileLite
 
         bool enableDebugSelection() const;
 
+        bool useStreamKDataParrallel() const;
+
         int useExperimentalSelection() const;
 
         std::string getMetric() const;
@@ -130,6 +132,7 @@ namespace TensileLite
         int         m_value2;
         bool        m_naivePropertySearch = false;
         bool        m_debugSelection      = false;
+        bool        m_dataParallel      = false;
         int         m_experimentSelection = 0;
         int         m_solution_index      = -1;
         bool        m_solselTrace         = false;

commit 2ec4269a23db9ac9668cbe034230337fe4a0954e

Fix: return solution which require workspace even given workspace siz… (#2113)
…e is zero
The issue (SWDEV-533891) is caused by #1682

tensilelite/Tensile/Source/lib/include/Tensile/MasterSolutionLibrary.hpp
@@ -115,7 +115,7 @@ namespace TensileLite
             {
                 return std::shared_ptr<MySolution>();
             }
-            auto       solution = solutions.at(index);
+            auto solution = solutions.at(index);
 
             if(solution->requiredHostWorkspaceSizePerProblem == static_cast<size_t>(-1))
             {
@@ -185,8 +185,9 @@ namespace TensileLite
                 {
                     std::lock_guard<std::mutex> guard(solutionsGuard);
                     auto                        selected_solution = solutions.at(solution_index);
-
+                    Task                        task(hardware, problem, *(selected_solution));
                     if((*selected_solution->problemPredicate)(problem)
+                       && (*selected_solution->taskPredicate)(task)
                        && (*selected_solution->hardwarePredicate)(hardware))
                         rv = selected_solution;
                     else
@@ -230,10 +231,10 @@ namespace TensileLite
         {
             if(Debug::Instance().printSolutionSelectionTime())
             {
-                auto start  = std::chrono::steady_clock::now();
-                auto result = library->findTopSolutions(problem, hardware, numSolutions);
-                auto end    = std::chrono::steady_clock::now();
-                double time = std::chrono::duration<double, std::micro>(end - start).count();
+                auto   start  = std::chrono::steady_clock::now();
+                auto   result = library->findTopSolutions(problem, hardware, numSolutions);
+                auto   end    = std::chrono::steady_clock::now();
+                double time   = std::chrono::duration<double, std::micro>(end - start).count();
                 std::cout << "Solution selection time: " << time << " us" << std::endl;
                 return result;
             }

tensilelite/Tensile/Source/lib/include/Tensile/SingleSolutionLibrary.hpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (C) 2022-2023 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2022-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -104,7 +104,8 @@ namespace TensileLite
                 }
 
                 if((*solution->hardwarePredicate)(hardware)
-                   && (*solution->problemPredicate)(problem))
+                   && softwarePredicate(
+                       SolutionLibrarySearchType::DEFAULT, task, hardware, (*solution), problem))
                     return solution;
             }
             else if(debug)
@@ -147,14 +148,14 @@ namespace TensileLite
                     Task task(hardware, problem, *(solution));
                     problem.setWorkspaceSizeGroupedGemm(ws);
                     problem.setGroupedGemmCount(problems.size());
-                    if(!(*solution->problemPredicate)(problem)  || !(*solution->taskPredicate)(task) )
+                    if(!(*solution->problemPredicate)(problem) || !(*solution->taskPredicate)(task))
                         return std::shared_ptr<MySolution>();
                 }
 
                 if(solution->requiredHostWorkspaceSizePerProblem == static_cast<size_t>(-1))
                 {
                     solution->requiredHostWorkspaceSizePerProblem
-                        = solution->requiredHostSizeGroupedGemmSingle(problems[0],hardware);
+                        = solution->requiredHostSizeGroupedGemmSingle(problems[0], hardware);
                 }
 
                 return solution;
@@ -190,7 +191,7 @@ namespace TensileLite
                 }
 
                 if((*solution->hardwarePredicate)(hardware)
-                   && softwarePredicate(searchType, task, hardware, (*solution), problem)) 
+                   && softwarePredicate(searchType, task, hardware, (*solution), problem))
                     useSolution = true;
             }
             else if(debug)
@@ -236,7 +237,8 @@ namespace TensileLite
                         Task task(hardware, problem, (*solution));
                         problem.setWorkspaceSizeGroupedGemm(ws);
                         problem.setGroupedGemmCount(problems.size());
-                        if(!(*solution->problemPredicate)(problem) || !(*solution->taskPredicate)(task))
+                        if(!(*solution->problemPredicate)(problem)
+                           || !(*solution->taskPredicate)(task))
                             useSolution = false;
                     }
                 }
@@ -277,7 +279,7 @@ namespace TensileLite
                 if(solution->requiredHostWorkspaceSizePerProblem == static_cast<size_t>(-1))
                 {
                     solution->requiredHostWorkspaceSizePerProblem
-                        = solution->requiredHostSizeGroupedGemmSingle(problems[0],hardware);
+                        = solution->requiredHostSizeGroupedGemmSingle(problems[0], hardware);
                 }
                 return SolutionSet<MySolution>({solution});
             }