b2d57c6c5fbe97f7813784abb9d3dba7f60ab2c0
Yung-sheng Tu <yung-sheng@streamhpc.com>
2025-06-17 16:24:53 +0000

    Deprecate thrust::null_type

--- projects/rocthrust/test/test_tuple.cpp
+++ projects/rocthrust/test/test_tuple.cpp
@@ -564,6 +564,7 @@ TEST(TupleTests, TestTupleCTAD)
 #endif // THRUST_CPP_DIALECT >= 2017
 
 // Ensure that we are backwards compatible with the old thrust::tuple implementation
+THRUST_SUPPRESS_DEPRECATED_PUSH
 static_assert(
   thrust::tuple_size<thrust::tuple<thrust::null_type,
                                    thrust::null_type,
@@ -660,3 +661,4 @@ static_assert(
 static_assert(
   thrust::tuple_size<thrust::tuple<int, int, int, int, int, int, int, int, int, thrust::null_type>>::value == 9, "");
 static_assert(thrust::tuple_size<thrust::tuple<int, int, int, int, int, int, int, int, int, int>>::value == 10, "");
+THRUST_SUPPRESS_DEPRECATED_POP
