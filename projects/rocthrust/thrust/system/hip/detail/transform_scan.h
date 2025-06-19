a4987d7805e8680788a8ac31c893d8d152dd9ad0
Yung-sheng Tu <yung-sheng@streamhpc.com>
2025-06-19 13:13:26 +0000

    Deprecate and replace thrust::cuda_cub iterators

--- projects/rocthrust/thrust/system/hip/detail/transform_scan.h
+++ projects/rocthrust/thrust/system/hip/detail/transform_scan.h
@@ -40,6 +40,7 @@
 #if THRUST_DEVICE_COMPILER == THRUST_DEVICE_COMPILER_HIP
 #  include <thrust/detail/type_traits.h>
 #  include <thrust/distance.h>
+#  include <thrust/iterator/transform_iterator.h>
 #  include <thrust/system/hip/detail/scan.h>
 
 #  include <iterator> // IWYU pragma: export
@@ -66,7 +67,7 @@ OutputIt THRUST_HOST_DEVICE transform_inclusive_scan(
 
   using size_type              = typename iterator_traits<InputIt>::difference_type;
   size_type num_items          = static_cast<size_type>(thrust::distance(first, last));
-  using transformed_iterator_t = transform_input_iterator_t<value_type, InputIt, TransformOp>;
+  using transformed_iterator_t = transform_iterator<TransformOp, InputIt, value_type, value_type>;
 
   return hip_rocprim::inclusive_scan_n(policy, transformed_iterator_t(first, transform_op), num_items, result, scan_op);
 }
@@ -87,7 +88,7 @@ OutputIt THRUST_HOST_DEVICE transform_inclusive_scan(
 
   using size_type              = typename iterator_traits<InputIt>::difference_type;
   size_type num_items          = static_cast<size_type>(thrust::distance(first, last));
-  using transformed_iterator_t = transform_input_iterator_t<value_type, InputIt, TransformOp>;
+  using transformed_iterator_t = transform_iterator<TransformOp, InputIt, value_type, value_type>;
 
   return hip_rocprim::inclusive_scan_n(
     policy, transformed_iterator_t(first, transform_op), num_items, result, init, scan_op);
@@ -108,7 +109,7 @@ OutputIt THRUST_HOST_DEVICE transform_exclusive_scan(
 
   using size_type              = typename iterator_traits<InputIt>::difference_type;
   size_type num_items          = static_cast<size_type>(thrust::distance(first, last));
-  using transformed_iterator_t = transform_input_iterator_t<result_type, InputIt, TransformOp>;
+  using transformed_iterator_t = transform_iterator<TransformOp, InputIt, result_type, result_type>;
 
   return hip_rocprim::exclusive_scan_n(
     policy, transformed_iterator_t(first, transform_op), num_items, result, init, scan_op);
