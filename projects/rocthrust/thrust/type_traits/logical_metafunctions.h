6318d45f0d09ce49a70bf1023c7b563a29cd0aa8
Yung-sheng Tu <yung-sheng@streamhpc.com>
2025-06-20 11:48:30 +0000

    Deprecate thrust logical meta functions

--- projects/rocthrust/thrust/type_traits/logical_metafunctions.h
+++ projects/rocthrust/thrust/type_traits/logical_metafunctions.h
@@ -30,70 +30,35 @@
 
 THRUST_NAMESPACE_BEGIN
 
-//! \addtogroup utility
-//! \{
-//! \addtogroup type_traits Type Traits
-//! \{
-
+#ifndef THRUST_DOXYGEN_INVOKED
 using ::internal::conjunction;
 using ::internal::conjunction_v;
 using ::internal::disjunction;
 using ::internal::disjunction_v;
 using ::internal::negation;
 using ::internal::negation_v;
+#endif
 
-//! \brief <a href="https://en.cppreference.com/w/cpp/types/integral_constant"><tt>std::integral_constant</tt></a>
-//! whose value is <tt>(... && Bs)</tt>.
-//!
-//! \see conjunction_value_v
-//! \see conjunction
-//! \see <a href="https://en.cppreference.com/w/cpp/types/conjunction"><tt>std::conjunction</tt></a>
 template <bool... Bs>
-using conjunction_value = conjunction<::internal::bool_constant<Bs>...>;
+using conjunction_value THRUST_DEPRECATED_BECAUSE("Use: internal::bool_constant<(Bs && ...)>") =
+  conjunction<::internal::bool_constant<Bs>...>;
 
-//! \brief <tt>constexpr bool</tt> whose value is <tt>(... && Bs)</tt>.
-//!
-//! \see conjunction_value
-//! \see conjunction
-//! \see <a href="https://en.cppreference.com/w/cpp/types/conjunction"><tt>std::conjunction</tt></a>
 template <bool... Bs>
-constexpr bool conjunction_value_v = conjunction_value<Bs...>::value;
+constexpr bool
+  conjunction_value_v THRUST_DEPRECATED_BECAUSE("Use a fold expression: Bs && ...") = conjunction_value<Bs...>::value;
 
-//! \brief <a href="https://en.cppreference.com/w/cpp/types/integral_constant"><tt>std::integral_constant</tt></a>
-//! whose value is <tt>(... || Bs)</tt>.
-//!
-//! \see disjunction_value_v
-//! \see disjunction
-//! \see <a href="https://en.cppreference.com/w/cpp/types/disjunction"><tt>std::disjunction</tt></a>
 template <bool... Bs>
-using disjunction_value = disjunction<::internal::bool_constant<Bs>...>;
+using disjunction_value THRUST_DEPRECATED_BECAUSE("Use: internal::bool_constant<(Bs || ...)>") =
+  disjunction<::internal::bool_constant<Bs>...>;
 
-//! \brief <tt>constexpr bool</tt> whose value is <tt>(... || Bs)</tt>.
-//!
-//! \see disjunction_value
-//! \see disjunction
-//! \see <a href="https://en.cppreference.com/w/cpp/types/disjunction"><tt>std::disjunction</tt></a>
 template <bool... Bs>
-constexpr bool disjunction_value_v = disjunction_value<Bs...>::value;
+constexpr bool
+  disjunction_value_v THRUST_DEPRECATED_BECAUSE("Use a fold expression: Bs || ...") = disjunction_value<Bs...>::value;
 
-//! \brief <a href="https://en.cppreference.com/w/cpp/types/integral_constant"><tt>std::integral_constant</tt></a>
-//! whose value is <tt>!Bs</tt>.
-//!
-//! \see negation_value_v
-//! \see negation
-//! \see <a href="https://en.cppreference.com/w/cpp/types/negation"><tt>std::negation</tt></a>
 template <bool B>
-using negation_value = ::internal::bool_constant<!B>;
+using negation_value THRUST_DEPRECATED_BECAUSE("Use internal::bool_constant<!B>") = ::internal::bool_constant<!B>;
 
-//! \brief <tt>constexpr bool</tt> whose value is <tt>!Bs</tt>.
-//!
-//! \see negation_value
-//! \see negation
-//! \see <a href="https://en.cppreference.com/w/cpp/types/negation"><tt>std::negation</tt></a>
 template <bool B>
-constexpr bool negation_value_v = negation_value<B>::value;
-
-//! \} // type traits
-//! \} // utility
+constexpr bool negation_value_v THRUST_DEPRECATED_BECAUSE("Use a plain negation !B") = negation_value<B>::value;
 
 THRUST_NAMESPACE_END
