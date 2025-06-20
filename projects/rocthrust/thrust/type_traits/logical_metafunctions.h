79eaed2c7003a3fa9aae6363e6e8258e2eba1eb9
Yung-sheng Tu <yung-sheng@streamhpc.com>
2025-06-20 11:48:30 +0000

    Deprecate thrust logical meta functions

--- projects/rocthrust/thrust/type_traits/logical_metafunctions.h
+++ projects/rocthrust/thrust/type_traits/logical_metafunctions.h
@@ -36,80 +36,56 @@
 
 THRUST_NAMESPACE_BEGIN
 
-//! \addtogroup utility
-//! \{
-//! \addtogroup type_traits Type Traits
-//! \{
-#if THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CUDA
-using ::cuda::std::bool_constant;
+#ifndef THRUST_DOXYGEN_INVOKED
+#  if THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CUDA
 using ::cuda::std::conjunction;
 using ::cuda::std::conjunction_v;
 using ::cuda::std::disjunction;
 using ::cuda::std::disjunction_v;
 using ::cuda::std::negation;
 using ::cuda::std::negation_v;
-#else
-using ::std::bool_constant;
+#  else
 using ::std::conjunction;
 using ::std::conjunction_v;
 using ::std::disjunction;
 using ::std::disjunction_v;
 using ::std::negation;
 using ::std::negation_v;
+#  endif
 #endif
 
-//! \brief <a href="https://en.cppreference.com/w/cpp/types/integral_constant"><tt>std::integral_constant</tt></a>
-//! whose value is <tt>(... && Bs)</tt>.
-//!
-//! \see conjunction_value_v
-//! \see conjunction
-//! \see <a href="https://en.cppreference.com/w/cpp/types/conjunction"><tt>std::conjunction</tt></a>
 template <bool... Bs>
-using conjunction_value = conjunction<bool_constant<Bs>...>;
+using conjunction_value THRUST_DEPRECATED_BECAUSE("Use: cuda::std::bool_constant<(Bs && ...)>") =
+#if THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CUDA
+  conjunction<::cuda::std::bool_constant<Bs>...>;
+#else
+  conjunction<::std::bool_constant<Bs>...>;
+#endif
 
-//! \brief <tt>constexpr bool</tt> whose value is <tt>(... && Bs)</tt>.
-//!
-//! \see conjunction_value
-//! \see conjunction
-//! \see <a href="https://en.cppreference.com/w/cpp/types/conjunction"><tt>std::conjunction</tt></a>
 template <bool... Bs>
-constexpr bool conjunction_value_v = conjunction_value<Bs...>::value;
+constexpr bool
+  conjunction_value_v THRUST_DEPRECATED_BECAUSE("Use a fold expression: Bs && ...") = conjunction_value<Bs...>::value;
 
-//! \brief <a href="https://en.cppreference.com/w/cpp/types/integral_constant"><tt>std::integral_constant</tt></a>
-//! whose value is <tt>(... || Bs)</tt>.
-//!
-//! \see disjunction_value_v
-//! \see disjunction
-//! \see <a href="https://en.cppreference.com/w/cpp/types/disjunction"><tt>std::disjunction</tt></a>
 template <bool... Bs>
-using disjunction_value = disjunction<bool_constant<Bs>...>;
+using disjunction_value THRUST_DEPRECATED_BECAUSE("Use: cuda::std::bool_constant<(Bs || ...)>") =
+#if THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CUDA
+  disjunction<::cuda::std::bool_constant<Bs>...>;
+#else
+  disjunction<::std::bool_constant<Bs>...>;
+#endif
 
-//! \brief <tt>constexpr bool</tt> whose value is <tt>(... || Bs)</tt>.
-//!
-//! \see disjunction_value
-//! \see disjunction
-//! \see <a href="https://en.cppreference.com/w/cpp/types/disjunction"><tt>std::disjunction</tt></a>
 template <bool... Bs>
-constexpr bool disjunction_value_v = disjunction_value<Bs...>::value;
+constexpr bool
+  disjunction_value_v THRUST_DEPRECATED_BECAUSE("Use a fold expression: Bs || ...") = disjunction_value<Bs...>::value;
 
-//! \brief <a href="https://en.cppreference.com/w/cpp/types/integral_constant"><tt>std::integral_constant</tt></a>
-//! whose value is <tt>!Bs</tt>.
-//!
-//! \see negation_value_v
-//! \see negation
-//! \see <a href="https://en.cppreference.com/w/cpp/types/negation"><tt>std::negation</tt></a>
 template <bool B>
-using negation_value = bool_constant<!B>;
+#if THRUST_DEVICE_SYSTEM == THRUST_DEVICE_SYSTEM_CUDA
+using negation_value THRUST_DEPRECATED_BECAUSE("Use cuda::std::bool_constant<!B>") = ::cuda::std::bool_constant<!B>;
+#else
+using negation_value THRUST_DEPRECATED_BECAUSE("Use cuda::std::bool_constant<!B>") = ::std::bool_constant<!B>;
+#endif
 
-//! \brief <tt>constexpr bool</tt> whose value is <tt>!Bs</tt>.
-//!
-//! \see negation_value
-//! \see negation
-//! \see <a href="https://en.cppreference.com/w/cpp/types/negation"><tt>std::negation</tt></a>
 template <bool B>
-constexpr bool negation_value_v = negation_value<B>::value;
-
-//! \} // type traits
-//! \} // utility
+constexpr bool negation_value_v THRUST_DEPRECATED_BECAUSE("Use a plain negation !B") = negation_value<B>::value;
 
 THRUST_NAMESPACE_END
