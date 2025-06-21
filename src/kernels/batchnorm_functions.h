6a0d4380cf54bd825bfc81d60f1c4174078543ee
ivandobskygithub <84400859+ivandobskygithub@users.noreply.github.com>
2025-06-21 21:54:17 +0100

    Update batchnorm_functions.h for ordering of gfx architectures

--- src/kernels/batchnorm_functions.h
+++ src/kernels/batchnorm_functions.h
@@ -176,8 +176,9 @@
 #ifndef MIOPEN_USE_AMDGCN
 #if defined(__AMDGCN__) &&                           \
     !((defined(MIO_BN_GFX103X) && MIO_BN_GFX103X) || \
-      (defined(MIO_BN_GFX110X) && MIO_BN_GFX110X) || (defined(MIO_BN_GFX120X) && MIO_BN_GFX120X) || \
-      (defined(MIO_BN_GFX115X) && MIO_BN_GFX115X))
+      (defined(MIO_BN_GFX110X) && MIO_BN_GFX110X) || \
+      (defined(MIO_BN_GFX115X) && MIO_BN_GFX115X) || \
+      (defined(MIO_BN_GFX120X) && MIO_BN_GFX120X))
 #define MIOPEN_USE_AMDGCN 1
 #else
 #define MIOPEN_USE_AMDGCN 0
