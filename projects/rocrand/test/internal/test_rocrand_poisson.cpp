8c956477588be4e868bd4dfd5b862dc2be797099
NguyenNhuDi <dinguyennhu@gmail.com>
2025-06-23 13:29:31 -0600

    added poisson host tests

--- projects/rocrand/test/internal/test_rocrand_poisson.cpp
+++ projects/rocrand/test/internal/test_rocrand_poisson.cpp
@@ -25,32 +25,34 @@
 #include <random>
 #include <vector>
 
-#include <rocrand/rocrand_poisson.h>
 #include <rocrand/rocrand_mtgp32_11213.h>
+#include <rocrand/rocrand_poisson.h>
 
-#define HIP_CHECK(cmd)                                                                          \
-    do                                                                                          \
-    {                                                                                           \
-        auto error = (cmd);                                                                     \
-        if(error != hipSuccess)                                                                 \
-        {                                                                                       \
-            std::cerr << "Encountered HIP error (" << hipGetErrorString(error) << ") at line "  \
-                      << __LINE__ << " in file " << __FILE__ << "\n";                           \
-            exit(-1);                                                                           \
-        }                                                                                       \
-    } while(0)                                                                                  \
+#define HIP_CHECK(cmd)                                                                         \
+    do                                                                                         \
+    {                                                                                          \
+        auto error = (cmd);                                                                    \
+        if(error != hipSuccess)                                                                \
+        {                                                                                      \
+            std::cerr << "Encountered HIP error (" << hipGetErrorString(error) << ") at line " \
+                      << __LINE__ << " in file " << __FILE__ << "\n";                          \
+            exit(-1);                                                                          \
+        }                                                                                      \
+    }                                                                                          \
+    while(0)
 
-#define ROCRAND_CHECK(cmd)                                                                      \
-    do                                                                                          \
-    {                                                                                           \
-        auto status = cmd;                                                                      \
-        if(status != 0){                                                                        \
-            std::cerr << "Encountered ROCRAND error: " << status << "at line"                   \
-            << __LINE__ << " in file " << __FILE__ << "\n";                                     \
-            exit(-1);                                                                           \
-        }                                                                                       \
-    } while(0)                                                                                  \
-
+#define ROCRAND_CHECK(cmd)                                                                \
+    do                                                                                    \
+    {                                                                                     \
+        auto status = cmd;                                                                \
+        if(status != 0)                                                                   \
+        {                                                                                 \
+            std::cerr << "Encountered ROCRAND error: " << status << "at line" << __LINE__ \
+                      << " in file " << __FILE__ << "\n";                                 \
+            exit(-1);                                                                     \
+        }                                                                                 \
+    }                                                                                     \
+    while(0)
 
 struct GlobalSizes {
     static constexpr size_t items_per_thread = 10000;
@@ -65,7 +67,7 @@ template<class RocrandPRNGType>
 inline void GetRocrandState(RocrandPRNGType * device_state){
 
     RocrandPRNGType * host_state = new RocrandPRNGType[GlobalSizes::block_size * GlobalSizes::grid_size];
-    
+
     for(size_t i = 0; i < GlobalSizes::block_size * GlobalSizes::grid_size; i++){
         if constexpr(std::is_same_v<RocrandPRNGType, rocrand_state_sobol32>){
             const unsigned int* directions;
@@ -143,11 +145,11 @@ template<typename ReturnType, class RocRandPrngType, class PoissonFunc>
 __global__ void poisson_kernel(RocRandPrngType * states, ReturnType * device_output, const double lambda, const PoissonFunc & f){
     const size_t offset = (GlobalSizes::items_per_block * blockIdx.x) + (GlobalSizes::items_per_thread * threadIdx.x);
     const size_t state_offset = (GlobalSizes::block_size * blockIdx.x) + threadIdx.x;
-    
+
     auto state = states + state_offset;
     for(size_t i = 0; i < GlobalSizes::items_per_thread; i++)
         device_output[offset + i] = f(state, lambda);
-    
+
     states[state_offset] = *state;
 }
 
@@ -155,15 +157,15 @@ __global__ void poisson_kernel(RocRandPrngType * states, ReturnType * device_out
 // size_multiplier is needed for special cases like uint4 where each element is actually 4
 template<typename RocRandPrngType, typename ReturnType, class PoissonFunc, class ReadFunc>
 void run_poisson_test(
-    std::vector<double> & all_lambdas, 
-    const PoissonFunc & f, 
+    std::vector<double> & all_lambdas,
+    const PoissonFunc & f,
     const ReadFunc & read_func,
     const size_t size_multiplier = 1)
-    { 
+    {
 
-    constexpr bool is_sobol  =  std::is_same_v<RocRandPrngType, rocrand_state_sobol32> || 
-                                std::is_same_v<RocRandPrngType, rocrand_state_sobol64> || 
-                                std::is_same_v<RocRandPrngType, rocrand_state_scrambled_sobol32> || 
+    constexpr bool is_sobol  =  std::is_same_v<RocRandPrngType, rocrand_state_sobol32> ||
+                                std::is_same_v<RocRandPrngType, rocrand_state_sobol64> ||
+                                std::is_same_v<RocRandPrngType, rocrand_state_scrambled_sobol32> ||
                                 std::is_same_v<RocRandPrngType, rocrand_state_scrambled_sobol64>;
 
     ReturnType * host_output = new ReturnType[GlobalSizes::size];
@@ -189,14 +191,14 @@ void run_poisson_test(
         for(size_t block_idx = 0; block_idx < GlobalSizes::grid_size; block_idx++){
             for(size_t thread_idx = 0; thread_idx < GlobalSizes::block_size; thread_idx++){
 
-                size_t offset = (block_idx * GlobalSizes::items_per_block) + (thread_idx * GlobalSizes::items_per_thread); 
+                size_t offset = (block_idx * GlobalSizes::items_per_block) + (thread_idx * GlobalSizes::items_per_thread);
 
                 double actual_mean = std::accumulate(
                     host_output + offset, host_output + offset + GlobalSizes::items_per_thread, (double) 0,
                     [=] (double acc, ReturnType x){
                         return acc + read_func(x);
                     }
-                ) 
+                )
                     / static_cast<double>(GlobalSizes::items_per_thread * size_multiplier);
                 double actual_std_dev = std::accumulate(
                     host_output + offset, host_output + offset + GlobalSizes::items_per_thread, (double) 0,
@@ -205,7 +207,7 @@ void run_poisson_test(
                     }
                 );
                 actual_std_dev = std::sqrt(actual_std_dev / static_cast<double>((GlobalSizes::items_per_thread * size_multiplier) - 1));
-                
+
                 ASSERT_NEAR(expected_mean, actual_mean, mean_tol);
                 if(!is_sobol)
                     ASSERT_NEAR(expected_std_dev, actual_std_dev, std_tol);
@@ -320,9 +322,8 @@ TYPED_TEST(PoissonTest, external_rocrand_poisson){
     );
 }
 
-// Special Tests 
+// Special Tests
 TEST(PoissonTest, philox4x32_10_uint4_output){
-    // Its a TEST so we do not have access to TestFixture
     std::vector<double> small_poisson_lambdas = {1, 2, 4, 8, 16, 32, 64};
 
     run_poisson_test<rocrand_state_philox4x32_10, uint4>(
@@ -336,4 +337,128 @@ TEST(PoissonTest, philox4x32_10_uint4_output){
         4
     );
 }
-// TODO Figure out MTGP32 tests
+
+/* #################################################
+
+                TEST HOST SIDE
+
+   ###############################################*/
+
+template<class RocrandPRNGType>
+inline void GetHostRocrandState(RocrandPRNGType* host_state)
+{
+
+    if constexpr(std::is_same_v<RocrandPRNGType, rocrand_state_sobol32>)
+    {
+        const unsigned int* directions;
+        ROCRAND_CHECK(
+            rocrand_get_direction_vectors32(&directions, ROCRAND_DIRECTION_VECTORS_32_JOEKUO6));
+        rocrand_init(directions, 123456, host_state);
+    }
+    // scrambled sobol32 case
+    else if constexpr(std::is_same_v<RocrandPRNGType, rocrand_state_scrambled_sobol32>)
+    {
+        const unsigned int* directions;
+        ROCRAND_CHECK(
+            rocrand_get_direction_vectors32(&directions, ROCRAND_DIRECTION_VECTORS_32_JOEKUO6));
+        rocrand_init(directions, 123456, 654321, host_state);
+    }
+    // sobol64 case
+    else if constexpr(std::is_same_v<RocrandPRNGType, rocrand_state_sobol64>)
+    {
+        const unsigned long long* directions;
+        ROCRAND_CHECK(
+            rocrand_get_direction_vectors64(&directions, ROCRAND_DIRECTION_VECTORS_64_JOEKUO6));
+        rocrand_init(directions, 123456, host_state);
+    }
+    // scrambled sobol64 case
+    else if constexpr(std::is_same_v<RocrandPRNGType, rocrand_state_scrambled_sobol64>)
+    {
+        const unsigned long long* directions;
+        ROCRAND_CHECK(
+            rocrand_get_direction_vectors64(&directions, ROCRAND_DIRECTION_VECTORS_64_JOEKUO6));
+        rocrand_init(directions, 123456, 654321, host_state);
+    }
+    // lfsr113 case
+    else if constexpr(std::is_same_v<RocrandPRNGType, rocrand_state_lfsr113>)
+    {
+        rocrand_init({0xabcd, 0xdabc, 0xcdab, 0xbcda}, 0, 0, host_state);
+    }
+    else
+    {
+        rocrand_init(123456, 654321, 0, host_state);
+    }
+}
+
+using PoissonHostParams = ::testing::Types<rocrand_state_philox4x32_10,
+                                           rocrand_state_mrg31k3p,
+                                           rocrand_state_mrg32k3a,
+                                           rocrand_state_xorwow,
+                                           rocrand_state_sobol32,
+                                           rocrand_state_scrambled_sobol32,
+                                           rocrand_state_sobol64,
+                                           rocrand_state_scrambled_sobol64,
+                                           rocrand_state_lfsr113,
+                                           rocrand_state_threefry2x32_20,
+                                           rocrand_state_threefry2x64_20,
+                                           rocrand_state_threefry4x32_20,
+                                           rocrand_state_threefry4x64_20>;
+
+template<typename T>
+class PoissonHostTest : public ::testing::Test
+{
+public:
+    using rocrand_prng_type = T;
+};
+
+TYPED_TEST_SUITE(PoissonHostTest, PoissonHostParams);
+
+TYPED_TEST(PoissonHostTest, poisson_host)
+{
+    using PrngState = typename TestFixture::rocrand_prng_type;
+
+    constexpr size_t test_size = 50000;
+
+    PrngState state;
+    GetHostRocrandState(&state);
+
+    std::vector<double> all_lambdas = {
+        32,
+        64,
+        1024,
+        2048,
+        4096,
+    };
+
+    std::vector<unsigned int> output(test_size);
+
+    for(const double& lambda : all_lambdas)
+    {
+        double expected_mean    = lambda;
+        double expected_std_dev = std::sqrt(lambda);
+
+        for(size_t i = 0; i < test_size; i++)
+        {
+            output[i] = rocrand_poisson(&state, lambda);
+        }
+        double actual_mean = std::accumulate(output.begin(),
+                                             output.end(),
+                                             (double)0,
+                                             [=](double acc, unsigned int x)
+                                             { return acc + static_cast<double>(x); })
+                             / static_cast<double>(test_size);
+        double actual_std_dev
+            = std::accumulate(output.begin(),
+                              output.end(),
+                              (double)0,
+                              [=](double acc, unsigned int x)
+                              { return acc + std::pow(static_cast<double>(x) - actual_mean, 2); });
+        actual_std_dev = std::sqrt(actual_std_dev / static_cast<double>(test_size - 1));
+
+        double mean_eps    = expected_mean * 0.05;
+        double std_dev_eps = expected_std_dev * 0.05;
+
+        ASSERT_NEAR(expected_mean, actual_mean, mean_eps);
+        ASSERT_NEAR(expected_std_dev, actual_std_dev, std_dev_eps);
+    }
+}
