eb9cca4b80a675afb47333694499cde9ea4ddb0f
Bryant Nelson <bryant.nelson@amd.com>
2025-06-30 15:55:36 -0500

    Getting something working.

--- projects/hipblaslt/tensilelite/Tensile/Source/lib/source/analytical/Hardware.cpp
+++ projects/hipblaslt/tensilelite/Tensile/Source/lib/source/analytical/Hardware.cpp
@@ -95,6 +95,7 @@ namespace TensileLite
                     {MatrixInstruction(16, 16, 128, 8), 32}, // V_MFMA_F32_16X16X128_F8
                     {MatrixInstruction(16, 16, 128, 6), 16}, // V_MFMA_F32_16X16X128_F6
                     {MatrixInstruction(16, 16, 128, 4), 16}, // V_MFMA_F32_16X16X128_F4
+                    {MatrixInstruction(32, 32, 64, 4), 32},  // V_MFMA_F32_32X32X64_F4
 
                 }}};
 
