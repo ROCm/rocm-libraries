55cc950bc198539d035d2231b56377e1f1170434
Chun-Xun Lin <Chun-Xun.Lin@amd.com>
2025-07-10 20:37:29 +0000

    Add RemoveSetCoordinate transformation

--- shared/rocroller/test/unit/AssertTest.cpp
+++ shared/rocroller/test/unit/AssertTest.cpp
@@ -39,6 +39,7 @@
 #include <rocRoller/KernelGraph/KernelGraph.hpp>
 #include <rocRoller/KernelGraph/Transforms/RemoveSetCoordinate.hpp>
 #include <rocRoller/KernelGraph/Visitors.hpp>
+#include <rocRoller/KernelGraph/Transforms/RemoveSetCoordinate.hpp>
 #include <rocRoller/KernelOptions_detail.hpp>
 #include <rocRoller/Utilities/Settings.hpp>
 
