c9754d083b4d8a5759eefd9db92ee8d1ab011798
Chun-Xun Lin <Chun-Xun.Lin@amd.com>
2025-07-14 15:14:18 +0000

    Add verification of input edge type of SetCoordinate

--- shared/rocroller/lib/source/KernelGraph/Transformations/RemoveSetCoordinate.cpp
+++ shared/rocroller/lib/source/KernelGraph/Transformations/RemoveSetCoordinate.cpp
@@ -87,10 +87,10 @@ namespace rocRoller
                 return leaves;
             }
 
-        static void connect(bool const        isSequenceEdge,
+        static void connect(bool const              isSequenceEdge,
                             std::vector<int> const& A,
                             std::vector<int> const& B,
-                            KernelGraph&      kg)
+                            KernelGraph&            kg)
         {
             if(A.empty() or B.empty())
                 return;
@@ -105,6 +105,27 @@ namespace rocRoller
                 addEdges.template operator()<ControlGraph::Sequence>();
             else
                 addEdges.template operator()<ControlGraph::Body>();
+        }
+
+        static bool const verifyInputEdgesAreOfTheSameType(KernelGraph const& kg, int const node)
+        {
+            using GD = rocRoller::Graph::Direction;
+
+            auto const allSequence
+                = std::ranges::all_of(kg.control.getNeighbours<GD::Upstream>(node), [&](int x) {
+                      return std::holds_alternative<CG::Sequence>(kg.control.getEdge(x));
+                  });
+
+            if(not allSequence)
+            {
+                auto const allBody
+                    = std::ranges::all_of(kg.control.getNeighbours<GD::Upstream>(node), [&](int x) {
+                          return std::holds_alternative<CG::Body>(kg.control.getEdge(x));
+                      });
+                AssertFatal(allBody, "A SetCoordinate's input edges are of different types");
+            }
+
+            return allSequence;
         }
 
         static void removeSetCoordinates(KernelGraph& kg)
