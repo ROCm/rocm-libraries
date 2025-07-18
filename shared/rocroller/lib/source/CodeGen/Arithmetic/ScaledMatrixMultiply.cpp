feec2f07b874e07fda282ebed04d532a03021cd6
Scott Quiring <scott.quiring@amd.com>
2025-07-18 20:11:21 +0000

    Address review feedback.

--- shared/rocroller/lib/source/CodeGen/Arithmetic/ScaledMatrixMultiply.cpp
+++ shared/rocroller/lib/source/CodeGen/Arithmetic/ScaledMatrixMultiply.cpp
@@ -127,56 +127,61 @@ namespace rocRoller
             auto typeA = matA->variableType().dataType;
             auto typeB = matB->variableType().dataType;
 
-            std::string mi;
-            std::string aType, bType;
-
-            AssertFatal(arch.HasCapability(GPUCapability::HasMFMA_scale_f8f6f4),
-                        "Scaled Matrix Multiplication is not supported for",
-                        arch.target().toString());
-
-            auto M = miSizes.m;
-            auto N = miSizes.n;
-            auto K = miSizes.k;
-
-            AssertFatal((M == 16 && N == 16 && K == 128) || (M == 32 && N == 32 && K == 64),
-                        "Invalid wavetile {}x{}x{} for scaled MFMA instruction for {}.",
-                        M,
-                        N,
-                        K,
-                        arch.target().toString());
-
-            if(maybeScaleBlockSize)
+            if(arch.HasCapability(GPUCapability::HasMFMA_scale_f8f6f4))
             {
-                auto scaleBlockSize = maybeScaleBlockSize.value();
-                AssertFatal(scaleBlockSize == 32,
-                            fmt::format("Scale block size expected: 32, got: {}, on: {}",
-                                        scaleBlockSize,
-                                        arch.target().toString()));
+                std::string mi;
+                std::string aType, bType;
+
+                auto M = miSizes.m;
+                auto N = miSizes.n;
+                auto K = miSizes.k;
+
+                AssertFatal((M == 16 && N == 16 && K == 128) || (M == 32 && N == 32 && K == 64),
+                            "Invalid wavetile {}x{}x{} for scaled MFMA instruction for {}.",
+                            M,
+                            N,
+                            K,
+                            arch.target().toString());
+
+                if(maybeScaleBlockSize)
+                {
+                    auto scaleBlockSize = maybeScaleBlockSize.value();
+                    AssertFatal(scaleBlockSize == 32,
+                                fmt::format("Scale block size expected: 32, got: {}, on: {}",
+                                            scaleBlockSize,
+                                            arch.target().toString()));
+                }
+
+                mi = concatenate("v_mfma_scale_f32_", M, "x", N, "x", K, "_f8f6f4");
+
+                aType = "cbsz:" + Arithmetic::getModifier(typeA);
+                bType = "blgp:" + Arithmetic::getModifier(typeB);
+
+                AssertFatal(scaleA->getBitOffset() % 8 == 0 && scaleA->getBitOffset() < 32,
+                            ShowValue(scaleA->getBitOffset()));
+
+                AssertFatal(scaleB->getBitOffset() % 8 == 0 && scaleB->getBitOffset() < 32,
+                            ShowValue(scaleB->getBitOffset()));
+
+                auto aScaleByte = scaleA->getBitOffset() / 8;
+                auto bScaleByte = scaleB->getBitOffset() / 8;
+
+                auto [opselLo, opselHi]
+                    = Arithmetic::getOpselModifiers2xByte(aScaleByte, bScaleByte);
+
+                Instruction inst(mi,
+                                 {dest},
+                                 {matA, matB, matC, scaleA, scaleB},
+                                 {opselLo, opselHi, aType, bType},
+                                 "");
+
+                co_yield inst;
             }
-
-            mi = concatenate("v_mfma_scale_f32_", M, "x", N, "x", K, "_f8f6f4");
-
-            aType = "cbsz:" + Arithmetic::getModifier(typeA);
-            bType = "blgp:" + Arithmetic::getModifier(typeB);
-
-            AssertFatal(scaleA->getBitOffset() % 8 == 0 && scaleA->getBitOffset() < 32,
-                        ShowValue(scaleA->getBitOffset()));
-
-            AssertFatal(scaleB->getBitOffset() % 8 == 0 && scaleB->getBitOffset() < 32,
-                        ShowValue(scaleB->getBitOffset()));
-
-            auto aScaleByte = scaleA->getBitOffset() / 8;
-            auto bScaleByte = scaleB->getBitOffset() / 8;
-
-            auto [opselLo, opselHi] = Arithmetic::getOpselModifiers2xByte(aScaleByte, bScaleByte);
-
-            Instruction inst(mi,
-                             {dest},
-                             {matA, matB, matC, scaleA, scaleB},
-                             {opselLo, opselHi, aType, bType},
-                             "");
-
-            co_yield inst;
+            else
+            {
+                Throw<FatalError>("Scaled Matrix Multiplication is not supported for",
+                                  arch.target().toString());
+            }
         }
     }
 }
