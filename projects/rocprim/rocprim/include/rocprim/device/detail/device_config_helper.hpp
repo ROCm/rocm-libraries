93a8b8a3e8393ea5df4e41ac5925402c9c8af9f7
Saiyang Zhang <saiyang@streamhpc.com>
2025-07-22 14:18:24 +0000

    Add to device_adjacent_difference

--- projects/rocprim/rocprim/include/rocprim/device/detail/device_config_helper.hpp
+++ projects/rocprim/rocprim/include/rocprim/device/detail/device_config_helper.hpp
@@ -866,7 +866,7 @@ struct adjacent_difference_config_tag
 
 struct adjacent_difference_config_params
 {
-    kernel_config_params          adjacent_difference_kernel_config;
+    kernel_config_params          kernel_config;
     ::rocprim::block_load_method  block_load_method;
     ::rocprim::block_store_method block_store_method;
 };
