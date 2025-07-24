a1eb88c985d569d41525c7bdea54c90578d364e4
Brad Nemanich <Brad.Nemanich@amd.com>
2025-07-23 22:54:22 -0500

    [rocRoller] Allow more tile sizes

--- shared/rocroller/lib/source/KernelGraph/Utils.cpp
+++ shared/rocroller/lib/source/KernelGraph/Utils.cpp
@@ -1137,7 +1137,10 @@ namespace rocRoller
 
         Expression::ExpressionPtr tileCeilDivide(Expression::ExpressionPtr sdSize, int tileSize)
         {
-            auto tileSizeExpr = Expression::literal(static_cast<uint>(tileSize));
+            auto tileSizeExpr = std::has_single_bit(static_cast<uint>(tileSize)) ?
+                                        Expression::literal(static_cast<uint>(tileSize)) :
+                                        Expression::literal(static_cast<int64_t>(tileSize));
+            
             auto one          = Expression::literal(1u);
 
             return (sdSize + tileSizeExpr - one) / tileSizeExpr;
