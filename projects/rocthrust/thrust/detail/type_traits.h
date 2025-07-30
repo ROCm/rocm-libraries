f29cf7d6ca2ced16e463e4738ee425d33825e48b
Marko Savic <marsavic@amd.com>
2025-07-30 08:23:41 -0400

    Add array type traits (is_unbounded_array, is_bounded_array)

--- projects/rocthrust/thrust/detail/type_traits.h
+++ projects/rocthrust/thrust/detail/type_traits.h
@@ -135,6 +135,21 @@ struct is_non_bool_integral<bool> : public false_type
 template <typename T>
 struct is_non_bool_arithmetic : public ::internal::is_arithmetic<T>
 {};
+
+template <typename T>
+struct is_unbounded_array : public thrust::detail::false_type
+{};
+template <typename T>
+struct is_unbounded_array<T[]> : public thrust::detail::true_type
+{};
+
+template <typename T>
+struct is_bounded_array : public thrust::detail::false_type
+{};
+template <typename T, size_t N>
+struct is_bounded_array<T[N]> : public thrust::detail::true_type
+{};
+
 template <>
 struct is_non_bool_arithmetic<bool> : public false_type
 {};
