c6c1ed2e40e020aea35a41cdc4537eff83dc2d00
yiqialiu <yiqialiu@amd.com>
2025-08-05 15:56:41 +0000

    Fix unit tests

--- shared/rocroller/test/unit/PermLanesTest.cpp
+++ shared/rocroller/test/unit/PermLanesTest.cpp
@@ -126,6 +126,7 @@ namespace PermLanesTest
         kgraph                     = kgraph.transform(updateWavefrontParams);
         kgraph                     = kgraph.transform(std::make_shared<LoadPacked>(context));
         kgraph                     = kgraph.transform(std::make_shared<RemoveSetCoordinate>());
+        kgraph                     = kgraph.transform(std::make_shared<AssignComputeIndex>(context));
 
         context->schedule(k->preamble());
         context->schedule(k->prolog());
