b16f926e883c1b5ddeebc04f4402b4621a417915
Chun-Xun Lin <Chun-Xun.Lin@amd.com>
2025-08-08 19:34:07 +0000

    Fix PythonLint errors

--- shared/rocroller/scripts/lib/rrperf/run.py
+++ shared/rocroller/scripts/lib/rrperf/run.py
@@ -116,6 +116,7 @@ def run_problems(
 
     return result
 
+
 def generate_missing_attr_value(attr, run):
     """Generate value for an option missing in previous rrperf version."""
     match attr:
@@ -125,6 +126,7 @@ def generate_missing_attr_value(attr, run):
             return (wgm_dim, wgm_value)
         case _:
             raise RuntimeError(f"Cannot handle attribuite missing in previous rrperf version: {attr}")
+
 
 def backcast(generator, build_dir):
     """Reconstruct run objects from `generator` into run objects from previous rrperf version."""
@@ -137,7 +139,11 @@ def backcast(generator, build_dir):
         backClass = getattr(module, className, None)
         if backClass is not None:
             backObj = backClass(
-                **{f.name: getattr(run, f.name) if hasattr(run, f.name) else generate_missing_attr_value(f.name, run) for f in fields(backClass)}
+                **{f.name:
+                    getattr(run, f.name)
+                    if hasattr(run, f.name)
+                    else generate_missing_attr_value(f.name, run)
+                    for f in fields(backClass)}
             )
             yield backObj
 
