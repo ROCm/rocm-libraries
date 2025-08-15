a0e2fdc0f78a9c94810ab5ae5293225a5fa16d15
Henderson, Nathan <Nathan.Henderson@amd.com>
2025-08-15 14:54:40 -0600

    Properly set env vars

--- shared/rocroller/.jenkins/common.groovy
+++ shared/rocroller/.jenkins/common.groovy
@@ -78,15 +78,13 @@ def runTestCommand (platform, project)
 {
     String testExclude = platform.jenkinsLabel.contains('compile') ? '-LE GPU' : ''
 
-    def numThreads = 8
-
     def command = """#!/usr/bin/env bash
                 set -ex
                 cd ${project.paths.project_build_prefix}
 
                 pushd build
                 echo Using `nproc` threads for testing.
-                OMP_NUM_THREADS=1 ctest -j ${numThreads} --output-on-failure ${testExclude}
+                OPENBLAS_NUM_THREADS=1 OMP_NUM_THREADS=1 ctest -j `nproc` --output-on-failure ${testExclude}
                 export ROCROLLER_BUILD_DIR="\$(pwd)"
                 popd
                 scripts/rrperf generate --suite generate_gfx950 --arch gfx950
