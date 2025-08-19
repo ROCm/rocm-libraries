d53b5ba7a72e4cf466ac03c6ee158b09a78d0c61
Parth Kumar <Parth.Kumar@amd.com>
2025-08-19 04:18:35 +0000

    Enable stream-k grid sweep in hipblaslt-bench

--- projects/hipblaslt/tensilelite/src/ContractionSolution.cpp
+++ projects/hipblaslt/tensilelite/src/ContractionSolution.cpp
@@ -742,26 +742,13 @@ namespace TensileLite
                 uint32_t skItersPerWG = skTiles * itersPerTile / skGrid;
                 uint32_t skExtraIters = skTiles * itersPerTile % (skGrid);
 
-                if(sizeMapping.customKernelName.empty())
-                {
-                    args.template append<uint32_t>("SKItersPerWG", skItersPerWG);
-                    args.template append<uint32_t>("skGrid",       skGrid);
-                    args.template append<uint32_t>("skTiles",      skTiles);
-                    args.template append<uint32_t>("skExtraIters", skExtraIters);
-                }
-                else
-                {
-                    uint32_t skGridAndTiles = (skGrid << 16) | (skTiles & 0xFFFF);
-                    // safe guard
-                    if(skGrid > 65535 || skTiles > 65535)
-                    {
-                        throw std::runtime_error("Packing skGrid and skTiles exceeds the capacity of a 32-bit register.");
-                    }
+                // Pack skGrid and skTiles into a single uint32_t such that the upper 16 bits
+                // represent skGrid and the lower 16 bits represent skTiles
+                uint32_t skGridAndTiles = (skGrid <<16) | (skTiles & 0xFFFF);
 
-                    args.template append<uint32_t>("SKItersPerWG",   skItersPerWG);
-                    args.template append<uint32_t>("skGridAndTiles", skGridAndTiles);
-                    args.template append<uint32_t>("skExtraIters",   skExtraIters);
-                }
+                args.template append<uint32_t>("SKItersPerWG", skItersPerWG);
+                args.template append<uint32_t>("skGridAndTiles", skGridAndTiles);
+                args.template append<uint32_t>("skExtraIters", skExtraIters);
             }
         }
 
@@ -939,8 +926,8 @@ namespace TensileLite
                / std::ceil(std::ceil(m / mt0) * std::ceil(n / mt1) * gsu / cuCount);
     }
 
-    void ContractionSolution::calculateAutoGSU(Problem const&  problem,
-                                               Hardware const* hardware) const
+    inline void ContractionSolution::calculateAutoGSU(Problem const&  problem,
+                                                      Hardware const* hardware) const
     {
         // if original GSU is not -1
         if(sizeMapping.globalSplitU != -1)
@@ -969,6 +956,44 @@ namespace TensileLite
         uint32_t GSULimit1    = max(1, (uint32_t)std::floor(numCUs / numWGs));
         uint32_t GSULimit2    = max(1, (uint32_t)std::floor((float)K / (float)MT2 / 3.0));
         autoGSU               = min(GSULimit2, max(1, GSULimit1));
+
+        // WorkspaceCheck
+        if(autoGSU > 1)
+        {
+            int    elemC    = sizeMapping.workspaceSizePerElemC * autoGSU;
+            int    elemBias = sizeMapping.workspaceSizePerElemBias * autoGSU;
+            size_t rs       = 0;
+            // 2d reduction
+            if(problem.useGradient() && problem.useBias()
+               && problem.getParams().biasEnum() != rocisa::DataType::None)
+            {
+                if(problem.biasSrc() == ContractionProblemGemm::TENSOR::D && (elemC == 0))
+                    rs += problem.d().totalLogicalElements() * problem.computeTypeElementSize();
+                else if(problem.biasSrc() == ContractionProblemGemm::TENSOR::A)
+                {
+                    rs += M * elemBias;
+                }
+                else if(problem.biasSrc() == ContractionProblemGemm::TENSOR::B)
+                {
+                    rs += N * elemBias;
+                }
+            }
+
+            size_t tiles = problem.getNumTiles(sizeMapping, 1) * B;
+            size_t tileSize = sizeMapping.macroTile.x * sizeMapping.macroTile.y * sizeMapping.workspaceSizePerElemC;
+            size_t bufSize = tiles * tileSize;
+
+            if(problem.groupedGemm())
+            {
+                assert(problem.workspaceSizeGroupedGemm() <= problem.workspaceSize());
+                autoGSU = min(autoGSU,
+                              (problem.workspaceSizeGroupedGemm() - rs)
+                                  / bufSize);
+            }
+            else
+                autoGSU = min(autoGSU,
+                              (problem.workspaceSize() - rs) / bufSize);
+        }
 
         // WorkgroupNumberCheck
 #define MAX_WORKGROUP_NUMBER 16777216
@@ -2993,10 +3018,14 @@ namespace TensileLite
                                           Hardware const& hardware,
                                           size_t          tiles) const
     {
-        size_t skGrid = tiles; // Fallback
         const bool streamKDP = Debug::Instance().useStreamKDataParrallel();
         if(streamKDP)
-            skGrid = tiles;
+            return tiles;
+        
+        if(problem.getParams().skgrid() > 0){
+            // sk grid set via hipblaslt-bench given precedence over ENV FLAG.
+            return static_cast<size_t>(problem.getParams().skgrid());
+        }
 
         // If K==0, run kernel as DP with Alpha=0 to skip main loop and apply beta*c
         size_t z = 1;
@@ -3005,7 +3034,7 @@ namespace TensileLite
             z *= problem.boundSize(i);
         }
         if(z == 0)
-            skGrid = tiles;
+            return tiles;
 
         AMDGPU const* pAMDGPU = dynamic_cast<AMDGPU const*>(&hardware);
 
@@ -3015,7 +3044,7 @@ namespace TensileLite
         // User-specified grid size for Stream-K kernel.
         if(pAMDGPU->skFixedGrid > 0)
         {
-            skGrid = pAMDGPU->skFixedGrid;
+            return pAMDGPU->skFixedGrid;
         }
         else if (pAMDGPU->skDynamicGrid > 0)
         {
@@ -3037,50 +3066,48 @@ namespace TensileLite
             analytical::DataType miDataType = datatypeToAnalyticalDatatype(problem.computeInputType());
             hip::HipAMDGPU const* hipAMDGPU = dynamic_cast<hip::HipAMDGPU const*>(&hardware);
 
-            skGrid = analytical::streamk::select_streamk_grid(x,
-                                                              y,
-                                                              z,
-                                                              batch,
-                                                              problem.transA(),
-                                                              problem.transB(),
-                                                              problem.a().elementBytes() * 8,
-                                                              problem.b().elementBytes() * 8,
-                                                              problem.c().elementBytes() * 8,
-                                                              miDataType,
-                                                              problem.workspaceSize(),
-                                                              sizeMapping.macroTile.x,
-                                                              sizeMapping.macroTile.y,
-                                                              sizeMapping.depthU,
-                                                              sizeMapping.matrixInstruction[0],
-                                                              sizeMapping.matrixInstruction[1],
-                                                              sizeMapping.matrixInstruction[2],
-                                                              sizeMapping.workGroupMapping,
-                                                              sizeMapping.workspaceSizePerElemC,
-                                                              sizeMapping.CUOccupancy,
-                                                              *(hipAMDGPU->analyticalHardware),
-                                                              pAMDGPU->skDynamicGrid);
+            return analytical::streamk::select_streamk_grid(x,
+                                    y,
+                                    z,
+                                    batch,
+                                    problem.transA(),
+                                    problem.transB(),
+                                    problem.a().elementBytes() * 8,
+                                    problem.b().elementBytes() * 8,
+                                    problem.c().elementBytes() * 8,
+                                    miDataType,
+                                    problem.workspaceSize(),
+                                    sizeMapping.macroTile.x,
+                                    sizeMapping.macroTile.y,
+                                    sizeMapping.depthU,
+                                    sizeMapping.matrixInstruction[0],
+                                    sizeMapping.matrixInstruction[1],
+                                    sizeMapping.matrixInstruction[2],
+                                    sizeMapping.workGroupMapping,
+                                    sizeMapping.workspaceSizePerElemC,
+                                    sizeMapping.CUOccupancy,
+                                    *(hipAMDGPU->analyticalHardware),
+                                    pAMDGPU->skDynamicGrid);
         }
         // Limit the CUs Stream-K is launched on either max or the specified,
         // whichever is minimum.
         else if(pAMDGPU->skMaxCUs > 0)
         {
-            skGrid = min(cuCount, pAMDGPU->skMaxCUs);
+            return min(cuCount, pAMDGPU->skMaxCUs);
         }
 
         // Multiply the cuCount with a constant factor (c), and launch
         // c * cuCount number of workgroups for Stream-K.
         else if(pAMDGPU->skGridMultiplier > 1)
         {
-            skGrid = cuCount * pAMDGPU->skGridMultiplier;
+            return cuCount * pAMDGPU->skGridMultiplier;
         }
 
         // If no option is specified, launch exactly cuCount worth of workgroups.
         else
         {
-            skGrid = cuCount;
+            return cuCount;
         }
-
-        return skGrid;
     }
 
     size_t ContractionSolution::partialTileSize(size_t skGrid) const
