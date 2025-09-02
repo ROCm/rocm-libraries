4848af3ea136e18c571b6164fc2d317a96b6a8f3
Braden Stefanuk <121893577+bstefanuk@users.noreply.github.com>
2025-09-02 10:48:39 -0600

    fix: address reviewer comments

--- cmake/modules/disableable_option.cmake
+++ cmake/modules/disableable_option.cmake
@@ -3,14 +3,16 @@
 
 # Helper that honours ROCM_LIBRARIES_DISABLE_ALL and preserves explicit user choices
 # Usage:
-#   disableable_option(<option_name> <doc> <default>)
+#   disableable_option(<option_name> <doc> <default> <disable>)
 #
 # Arguments:
 #   <option_name> - Name of the option to add.
 #   <doc>         - Documentation string for the option.
 #   <default>     - Default value for the option.
-function(disableable_option _option_name _doc _default)
-    if(ROCM_LIBRARIES_DISABLE_ALL)
+#   <disable>     - Whether to disable the option. If the option is already set
+#                   via the CLI, this will be ignored.
+function(disableable_option _option_name _doc _default _disable)
+    if(${_disable})
         option(${_option_name} "${_doc}" OFF)
     else()
         option(${_option_name} "${_doc}" ${_default})
