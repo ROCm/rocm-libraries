5dfda34e524fbd755beb0092eebdd184a3e4bd61
amd-bartgips <bart.gips@amd.com>
2025-09-10 07:34:31 +0000

    fixed calls to new cross-platf utility functions to set env vars

--- projects/miopen/test/gtest/perf_config_HipImplicitGemm3DGroupFwdXdlops.cpp
+++ projects/miopen/test/gtest/perf_config_HipImplicitGemm3DGroupFwdXdlops.cpp
@@ -124,7 +124,7 @@ protected:
 
         // Test override with value 1 (should select index 1 if hardcoded heuristics don't trigger)
         {
-            set_env_var("MIOPEN_DEBUG_3D_CONV_IMPLICIT_GEMM_HIP_FWD_XDLOPS_IDX_OVERRIDE", "1", 1);
+            set_env_var("MIOPEN_DEBUG_3D_CONV_IMPLICIT_GEMM_HIP_FWD_XDLOPS_IDX_OVERRIDE", "1");
 
             Config cfg;
             cfg.HeuristicInit(ctx, problem);
@@ -154,7 +154,7 @@ protected:
         // Test hardcoded heuristics for BF16/FP16 with appropriate conditions
         if(will_use_hardcoded)
         {
-            set_env_var("MIOPEN_DEBUG_3D_CONV_IMPLICIT_GEMM_HIP_FWD_XDLOPS_IDX_OVERRIDE", "0", 1);
+            set_env_var("MIOPEN_DEBUG_3D_CONV_IMPLICIT_GEMM_HIP_FWD_XDLOPS_IDX_OVERRIDE", "0");
 
             Config cfg;
             cfg.HeuristicInit(ctx, problem);
@@ -184,7 +184,7 @@ protected:
 
             // Use a high index that's less likely to trigger hardcoded heuristics
             // but still tests the override functionality
-            set_env_var("MIOPEN_DEBUG_3D_CONV_IMPLICIT_GEMM_HIP_FWD_XDLOPS_IDX_OVERRIDE", "50", 1);
+            set_env_var("MIOPEN_DEBUG_3D_CONV_IMPLICIT_GEMM_HIP_FWD_XDLOPS_IDX_OVERRIDE", "50");
 
             Config cfg_override;
             cfg_override.HeuristicInit(ctx, problem);
