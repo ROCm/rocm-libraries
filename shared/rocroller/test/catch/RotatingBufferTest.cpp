bcec0528e82503b6fb253d4aa55df2df7de5a11a
Mahesh Lakshminarasimhan <Mahesh-Lak@users.noreply.github.com>
2025-09-18 18:11:25 -0600

    Update RotatingBuffer header file path

--- shared/rocroller/test/catch/RotatingBufferTest.cpp
+++ shared/rocroller/test/catch/RotatingBufferTest.cpp
@@ -24,7 +24,7 @@
  *
  *******************************************************************************/
 
-#include "client/include/client/RotatingBuffer.hpp"
+#include "client/RotatingBuffer.hpp"
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
