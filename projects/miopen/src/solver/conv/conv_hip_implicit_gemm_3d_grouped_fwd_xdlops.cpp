e3e09778ee0aa895b38edcff115ea729eb76240e
amd-bartgips <bart.gips@amd.com>
2025-10-01 13:56:50 +0000

    changed default value for kernel_id in HeuristicInit

--- projects/miopen/src/solver/conv/conv_hip_implicit_gemm_3d_grouped_fwd_xdlops.cpp
+++ projects/miopen/src/solver/conv/conv_hip_implicit_gemm_3d_grouped_fwd_xdlops.cpp
@@ -420,7 +420,7 @@ void PerformanceConfigHipImplicitGemm3DGroupFwdXdlops::HeuristicInit(
     const miopen::ExecutionContext& ctx, const ProblemDescription& problem)
 {
     index     = 0;
-    kernel_id = "";
+    kernel_id = valid_kernels[index];
     split_k   = 0;
     // split_k is not used in this solver, but it is required by the AI heuristics interface
 
