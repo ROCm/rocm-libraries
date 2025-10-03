ec0c990497c1eec27255b0a321c32c0b94788fd3
Kerry Wang <kerrywang369@gmail.com>
2025-10-03 18:20:06 +0000

    only disable specific assert op kinds for gfx12

--- shared/rocroller/test/unit/AssertTest.cpp
+++ shared/rocroller/test/unit/AssertTest.cpp
@@ -54,6 +54,13 @@ namespace AssertTest
 {
     class GPU_AssertTest : public GPUContextFixtureParam<std::tuple<AssertOpKind, std::string>>
     {
+    public:
+        bool skipOnGPU(const auto& gpu, AssertOpKind assertOpKind)
+        {
+            return gpu.isRDNA4GPU()
+                   && (assertOpKind == AssertOpKind::MemoryViolation
+                       || assertOpKind == AssertOpKind::STrap);
+        }
     };
 
     TEST_P(GPU_AssertTest, GPU_Assert)
@@ -182,8 +189,7 @@ namespace AssertTest
                 EXPECT_THAT(output(), testing::HasSubstr("AssertOpKind == NoOp"));
             }
 
-            if(isLocalDevice() && assertOpKind != AssertOpKind::MemoryViolation
-               && assertOpKind != AssertOpKind::STrap)
+            if(isLocalDevice() && not skipOnGPU(gpu, assertOpKind))
             {
                 KernelArguments kargs;
 
@@ -308,7 +314,7 @@ namespace AssertTest
                 EXPECT_THAT(output(), testing::HasSubstr("AssertOpKind == NoOp"));
             }
 
-            if(isLocalDevice())
+            if(isLocalDevice() && not skipOnGPU(gpu, assertOpKind))
             {
                 KernelArguments kargs;
 
