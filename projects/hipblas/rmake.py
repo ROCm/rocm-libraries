9564045935cbee544dd1178556220a889883d630
AGonzales-amd <angonzal@amd.com>
2025-10-10 11:51:56 -0400

    use find modules for lapack and blas in hipblas clients

--- projects/hipblas/rmake.py
+++ projects/hipblas/rmake.py
@@ -307,7 +307,6 @@ def config_cmd():
     if args.build_clients:
         cmake_build_dir = cmake_path(build_dir)
         cmake_options.append( f"-DBUILD_CLIENTS_TESTS=ON -DBUILD_CLIENTS_BENCHMARKS=ON -DBUILD_CLIENTS_SAMPLES=ON -DBUILD_DIR={cmake_build_dir} " )
-        cmake_options.append( f"-DLINK_BLIS=ON")
         if args.clients_no_fortran:
             cmake_options.append(f"-DBUILD_FORTRAN_CLIENTS=OFF")
 
