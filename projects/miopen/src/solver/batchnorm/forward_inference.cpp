cf30dd05a693478e1a811718104af7aa8c474f6c
Sai Kubair Kota <sai.kota@streamhpc.com>
2025-10-13 09:43:08 +0000

    solver/batchnorm/forward_inference: updated to use HIP kernels for BatchNormFwdInfer

--- projects/miopen/src/solver/batchnorm/forward_inference.cpp
+++ projects/miopen/src/solver/batchnorm/forward_inference.cpp
@@ -122,12 +122,12 @@ ConvSolution BnFwdInference::GetSolution(const ExecutionContext& context,
         kernel.kernel_name = "MIOpenBatchNormFwdInfer";
         if(problem.GetMode() == miopenBNSpatial)
         { // SPATIAL kernels
-            kernel.kernel_file += "Spatial.cl";
+            kernel.kernel_file += "Spatial.cpp";
             kernel.kernel_name += "SpatialEst";
         }
         else
         { // PER ACTIVATION
-            kernel.kernel_file += "PerAct.cl";
+            kernel.kernel_file += "PerAct.cpp";
             kernel.kernel_name += "PerActivationEst";
         }
 
