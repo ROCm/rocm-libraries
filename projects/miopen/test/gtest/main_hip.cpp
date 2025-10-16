286a9eed5d109f05861be92de31fc87b87355682
Daryl Hawkins <Daryl.Hawkins@amd.com>
2025-10-16 16:24:01 -0400

    Improve comments and logging a bit

--- projects/miopen/test/gtest/main_hip.cpp
+++ projects/miopen/test/gtest/main_hip.cpp
@@ -4,13 +4,17 @@
 #include <gtest/gtest.h>
 #include <hip/hip_runtime_api.h>
 
-// This test event listener ensures that HIP errors are cleaned up after every test
+// This test event listener ensures that HIP errors are cleaned up after every test, and will flag tests that don't
+// clean up their own errors
 class HIPErrorHandler : public testing::EmptyTestEventListener
 {
     void OnTestEnd(const testing::TestInfo& test_info) override
     {
-        EXPECT_EQ(hipExtGetLastError(), hipSuccess) << " hipExtGetLastError returned error code after test";
-        EXPECT_EQ(hipGetLastError(), hipSuccess) << " hipExtGetLastError returned error code after test";
+        auto hipError = hipGetLastError();
+        auto hipExtError = hipExtGetLastError();
+
+        EXPECT_EQ(hipError, hipSuccess) << " hipGetLastError returned error code " << hipError << " after test "  << test_info.test_suite_name() << "." << test_info.name() << ". Error string: " << hipGetErrorString(hipError);
+        EXPECT_EQ(hipExtError, hipSuccess) << " hipExtGetLastError returned error code " << hipExtError << " after test " << test_info.test_suite_name() << "." << test_info.name() << ". Error string: " << hipGetErrorString(hipExtError);
     }
 };
 
