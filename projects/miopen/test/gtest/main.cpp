f67180674327cc1728485b0e6b2ea07f8fab2a95
Daryl Hawkins <Daryl.Hawkins@amd.com>
2025-10-16 14:34:39 -0400

    Added a GTest event handler that cleans up HIP errors after every test

---
+++ projects/miopen/test/gtest/main.cpp
@@ -0,0 +1,25 @@
+// Copyright © Advanced Micro Devices, Inc., or its affiliates.
+// SPDX-License-Identifier:  MIT
+
+#include <gtest/gtest.h>
+#include <hip/hip_runtime_api.h>
+
+// This test event listener ensures that HIP errors are cleaned up after every test
+class HIPErrorHandler : public testing::EmptyTestEventListener
+{
+    void OnTestEnd(const testing::TestInfo& test_info) override
+    {
+        EXPECT_EQ(hipExtGetLastError(), hipSuccess) << " hipExtGetLastError returned error code after test";
+        EXPECT_EQ(hipGetLastError(), hipSuccess) << " hipExtGetLastError returned error code after test";
+    }
+};
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest( &argc, argv );
+
+    testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
+    listeners.Append(new HIPErrorHandler);
+
+    return RUN_ALL_TESTS();
+}
