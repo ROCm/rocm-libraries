d1ceb44dc4a00b0e84e45c42c58a2e771ac12cf6
Daryl Hawkins <Daryl.Hawkins@amd.com>
2025-10-16 14:55:33 -0400

    Cleanup HIP error generated during invalid device ordinal test

--- projects/miopen/test/gtest/handle_hip_device.cpp
+++ projects/miopen/test/gtest/handle_hip_device.cpp
@@ -57,6 +57,10 @@ TEST_F(CPU_HandleHipDevice_NONE, NegativeDeviceIdThrowsWithCorrectMessage)
         std::string error_msg = ex.what();
         EXPECT_NE(error_msg.find("Error setting device"), std::string::npos);
         EXPECT_NE(error_msg.find("-10"), std::string::npos);
+        
+        // Clear any HIP error triggered by asking for an invalid device ordinal
+        hipGetLastError();
+        hipExtGetLastError();
     }
 }
 
