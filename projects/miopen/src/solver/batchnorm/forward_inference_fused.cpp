e8ff63a3c457243a61aca9bb0c98b8808801d8fb
Sai Kubair Kota <sai.kota@streamhpc.com>
2025-10-20 07:36:22 +0000

    solver/batchnorm/forward_inference_fused: determine read unit based on read length for all modes & data types
    We are determining read unit based on read length for all modes & data types for vectorized memory accesses. Previously, read unit determination was only done for spatial mode with non-half data types but I think that this optimization can be applied to all modes & data types.

--- projects/miopen/src/solver/batchnorm/forward_inference_fused.cpp
+++ projects/miopen/src/solver/batchnorm/forward_inference_fused.cpp
@@ -92,11 +92,7 @@ ConvSolution BnFwdInferActivationFused::GetSolution(const FusionContext& /*conte
 
     size_t read_unit = 1;
     size_t read_len  = (mode == miopenBNSpatial) ? h * w : c * h * w;
-
-    if(mode == miopenBNSpatial && input_desc.GetType() != miopenHalf)
-    {
-        read_unit = (read_len % 4 == 0) ? 4 : (read_len % 2 == 0) ? 2 : 1;
-    }
+    read_unit = (read_len % 4 == 0) ? 4 : (read_len % 2 == 0) ? 2 : 1;
 
     size_t xlocalsize = 256;
     size_t xgridsize = read_len / read_unit;
