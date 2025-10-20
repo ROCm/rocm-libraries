d7108a7f6f638676cef63b5990f4eee577a38c7c
Henderson, Nathan <Nathan.Henderson@amd.com>
2025-10-20 22:49:07 +0000

    Update AliasDataFlowTagsTest

--- shared/rocroller/test/catch/AliasDataFlowTagsTest.cpp
+++ shared/rocroller/test/catch/AliasDataFlowTagsTest.cpp
@@ -111,7 +111,7 @@ namespace AliasDataFlowTagsTest
         {
             auto transformAliases = AliasDataFlowTagsDetail::findAliasCandidates(graph);
             CAPTURE(transformAliases);
-            CHECK(transformAliases.size() == 59);
+            CHECK(transformAliases.size() == 40);
         }
 
         SECTION("Try different orders of inputs")
@@ -158,7 +158,7 @@ namespace AliasDataFlowTagsTest
                 auto aliasEdges = graph.coordinates.getEdges<CT::DataFlowEdge>()
                                       .filter(isAliasEdge)
                                       .to<std::vector>();
-                CHECK(aliasEdges.size() == 59);
+                CHECK(aliasEdges.size() == 40);
             }
 
             {
@@ -178,7 +178,7 @@ namespace AliasDataFlowTagsTest
                                           .filter(isAliasEdge)
                                           .filter(isAliasForATiles)
                                           .to<std::vector>();
-                CHECK(aliasEdgesForA.size() == 48);
+                CHECK(aliasEdgesForA.size() == 32);
             }
 
             {
@@ -198,7 +198,7 @@ namespace AliasDataFlowTagsTest
                                           .filter(isAliasEdge)
                                           .filter(isAliasForBTiles)
                                           .to<std::vector>();
-                CHECK(aliasEdgesForB.size() == 11);
+                CHECK(aliasEdgesForB.size() == 8);
             }
         }
     }
