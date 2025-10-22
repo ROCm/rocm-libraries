44b6a73930084ac5878400d7a349155571335a60
geomin12 <geomin12@amd.com>
2025-10-22 11:33:48 -0700

    adding correct cmake flags

--- .github/scripts/therock_matrix.py
+++ .github/scripts/therock_matrix.py
@@ -38,7 +38,7 @@ project_map = {
         "project_to_test": "hipblaslt, rocblas, hipblas",
     },
     "miopen": {
-        "cmake_options": "-DTHEROCK_ENABLE_MIOPEN=ON -DTHEROCK_USE_EXTERNAL_COMPOSABLE_KERNEL=ON -DTHEROCK_COMPOSABLE_KERNEL_SOURCE_DIR=../composable_kernel",
+        "cmake_options": "-DTHEROCK_ENABLE_MIOPEN=ON -DTHEROCK_ENABLE_COMPOSABLE_KERNEL=ON -DTHEROCK_USE_EXTERNAL_COMPOSABLE_KERNEL=ON -DTHEROCK_COMPOSABLE_KERNEL_SOURCE_DIR=../composable_kernel",
         "project_to_test": "miopen",
     },
     "fft": {
