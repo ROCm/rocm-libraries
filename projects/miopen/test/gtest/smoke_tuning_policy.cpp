6e7139181f48ca47afee7d3d5c94be6cbacdea2e
Randy J. Spaulding <rspauldi@amd.com>
2025-10-23 01:54:03 -0700

    check that trying to set invalid does not modify value

--- projects/miopen/test/gtest/smoke_tuning_policy.cpp
+++ projects/miopen/test/gtest/smoke_tuning_policy.cpp
@@ -10,6 +10,21 @@ MIOPEN_LIB_ENV_VAR(MIOPEN_USER_DB_PATH)
 
 class CPU_TuningPolicy_NONE : public ::testing::Test
 {
+    void testSetInvalidValue(const miopenTuningPolicy_t original_policy, int invalid)
+    {
+        auto&& handle = get_handle();
+        miopenTuningPolicy_t test_tuning_policy = miopenTuningPolicy_t::miopenTuningPolicyNone;
+        miopenTuningPolicy_t prev_tuning_policy;
+
+        EXPECT_EQ(miopenSetTuningPolicy(&handle, original_policy), miopenStatusSuccess);
+        EXPECT_EQ(miopenGetTuningPolicy(&handle, &prev_tuning_policy), miopenStatusSuccess);
+        EXPECT_EQ(original_policy, prev_tuning_policy);
+
+        EXPECT_EQ(miopenSetTuningPolicy(&handle, static_cast<miopenTuningPolicy_t>(invalid)),
+                miopenStatusBadParm);
+        EXPECT_EQ(miopenGetTuningPolicy(&handle, &test_tuning_policy), miopenStatusSuccess);
+        EXPECT_EQ(prev_tuning_policy, test_tuning_policy);
+    }
 };
 
 TEST_F(CPU_TuningPolicy_NONE, TestTuningPolicyGetterAndSetterValidValues)
@@ -48,28 +63,10 @@ TEST_F(CPU_TuningPolicy_NONE, TestTuningPolicyGetterAndSetterValidValues)
 
 TEST_F(CPU_TuningPolicy_NONE, TestTuningPolicyGetterAndSetterErrorHandling)
 {
-    auto&& handle = get_handle();
-    miopenTuningPolicy_t test_tuning_policy;
-
-    EXPECT_EQ(miopenSetTuningPolicy(&handle, static_cast<miopenTuningPolicy_t>(-1)),
-              miopenStatusBadParm);
-    EXPECT_EQ(miopenGetTuningPolicy(&handle, &test_tuning_policy), miopenStatusSuccess);
-    EXPECT_EQ(test_tuning_policy, miopenTuningPolicy_t::miopenTuningPolicyNone);
-
-    EXPECT_EQ(miopenSetTuningPolicy(&handle, static_cast<miopenTuningPolicy_t>(0)),
-              miopenStatusBadParm);
-    EXPECT_EQ(miopenGetTuningPolicy(&handle, &test_tuning_policy), miopenStatusSuccess);
-    EXPECT_EQ(test_tuning_policy, miopenTuningPolicy_t::miopenTuningPolicyNone);
-
-    EXPECT_EQ(miopenSetTuningPolicy(&handle, static_cast<miopenTuningPolicy_t>(6)),
-              miopenStatusBadParm);
-    EXPECT_EQ(miopenGetTuningPolicy(&handle, &test_tuning_policy), miopenStatusSuccess);
-    EXPECT_EQ(test_tuning_policy, miopenTuningPolicy_t::miopenTuningPolicyNone);
-
-    EXPECT_EQ(miopenSetTuningPolicy(&handle, static_cast<miopenTuningPolicy_t>(1000)),
-              miopenStatusBadParm);
-    EXPECT_EQ(miopenGetTuningPolicy(&handle, &test_tuning_policy), miopenStatusSuccess);
-    EXPECT_EQ(test_tuning_policy, miopenTuningPolicy_t::miopenTuningPolicyNone);
+    testSetInvalidValue(miopenTuningPolicy_t::miopenTuningPolicyDbUpdate, -1);
+    testSetInvalidValue(miopenTuningPolicy_t::miopenTuningPolicySearch, 0);
+    testSetInvalidValue(miopenTuningPolicy_t::miopenTuningPolicySearchDbUpdate, 6);
+    testSetInvalidValue(miopenTuningPolicy_t::miopenTuningPolicyDbClean, 1000);
 }
 
 TEST_F(CPU_TuningPolicy_NONE, TestNullHandleForTuningPolicy)
