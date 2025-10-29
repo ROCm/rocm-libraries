35aa237649e624220289fdbca26342e247a98c5e
amd-hsivasun <hsivasun@amd.com>
2025-10-29 17:49:01 -0400

    Add rocwmma pipeline ID

--- .github/scripts/azure_resolve_subtree_deps.py
+++ .github/scripts/azure_resolve_subtree_deps.py
@@ -109,6 +109,7 @@ def main(argv=None) -> None:
         "projects/hipsparselt": 309,
         "projects/miopen": 320,
         "shared/origami": 364,
+        "projects/rocwmma": 369,
     }
 
     args = parse_arguments(argv)
