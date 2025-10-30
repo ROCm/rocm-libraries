a449939cd85af5c8315919c7dc710198015e849a
Christopher Erb <christopher.erb@amd.com>
2025-10-30 23:45:15 +0000

    update time to ISO 8601 format

--- projects/miopen/src/logger.cpp
+++ projects/miopen/src/logger.cpp
@@ -126,8 +126,8 @@ inline std::string GetDateTimeMs()
     auto now_t = std::chrono::system_clock::to_time_t(now);
     std::tm local_time = *std::localtime(&now_t);
     std::ostringstream time_s;
-    time_s << std::put_time(&local_time, "%Y-%m-%d %H:%M:%S");
-    time_s << ":"<< std::setfill('0') << std::setw(3) << ms.count();
+    time_s << std::put_time(&local_time, "%Y-%m-%dT%H:%M:%S");
+    time_s << "." << std::setfill('0') << std::setw(3) << ms.count() << "Z";
     return time_s.str();
 }
 
