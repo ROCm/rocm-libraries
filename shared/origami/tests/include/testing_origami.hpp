902688d10c312930098347a1079e7001d670aef3
aliry95amd <ayazdani@amd.com>
2025-10-30 16:17:49 +0000

    Fix origami tests

--- shared/origami/tests/include/testing_origami.hpp
+++ shared/origami/tests/include/testing_origami.hpp
@@ -596,11 +596,13 @@ void BestWGM(const origami::hardware_t& hardware,
              size_t                     MT_N,
              size_t                     MT_K)
 {
+    // Assume no nt
     auto nta = 0;
     auto ntb = 0;
+    // Assume DP
     auto skGrid = (M + MT_M - 1) / MT_M * (N + MT_N - 1) / MT_N;
 
-    auto [best_wgm_large_tile, best_wgmxcc_large_tile] = 
+    auto [best_wgmxcc_large_tile, best_wgm_large_tile] = 
         select_best_wgm(hardware,
                         M,
                         N,
@@ -614,7 +616,7 @@ void BestWGM(const origami::hardware_t& hardware,
                         skGrid,
                         false);
 
-    auto [best_wgm_small_tile, best_wgmxcc_small_tile] = 
+    auto [best_wgmxcc_small_tile, best_wgm_small_tile] = 
         select_best_wgm(hardware,
                         M / 4,
                         N / 4,
@@ -628,7 +630,7 @@ void BestWGM(const origami::hardware_t& hardware,
                         skGrid,
                         false);
 
-    auto [best_wgm_nonsquare_tile, best_wgmxcc_nonsquare_tile] = 
+    auto [best_wgmxcc_nonsquare_tile, best_wgm_nonsquare_tile] = 
         select_best_wgm(hardware,
                         1024,
                         5120,
@@ -642,6 +644,7 @@ void BestWGM(const origami::hardware_t& hardware,
                         skGrid,
                         false);
 
+    EXPECT_EQ(best_wgmxcc_large_tile, best_wgmxcc_small_tile);
     EXPECT_GT(best_wgm_large_tile, best_wgm_small_tile);
     EXPECT_NE(best_wgm_large_tile, best_wgm_nonsquare_tile);
 }
