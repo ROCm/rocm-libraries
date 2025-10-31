1151a38e8ff9e10c9cdf92ecc9a32f6ebb3b8002
Joao P. L. de Carvalho <joao.carvalho@amd.com>
2025-10-31 15:33:33 -0400

    Error out if comparision is not disabled

--- shared/rocroller/.jenkins/common.groovy
+++ shared/rocroller/.jenkins/common.groovy
@@ -215,17 +215,17 @@ def runPerformanceCommand (platform, project)
         sshBlock ->
         def rrperfSuite = platform.jenkinsLabel.contains('gfx12') ? "all_gfx120X" : "all"
 
+        // either a label or a parameter can block comparison to master branch
+        def masterCompare = !(
+            pullRequest.labels.any { it == "ci:no-build-master" || it == "ci:no-build-target" }
+        )
+        if (masterCompare && (params?."Build target branch for comparison" != null))
+        {
+            masterCompare = params."Build target branch for comparison"
+        }
 
         if (env.CHANGE_ID)
         {
-            // either a label or a parameter can block comparison to master branch
-            def masterCompare = !(
-                pullRequest.labels.any { it == "ci:no-build-master" || it == "ci:no-build-target" }
-            )
-            if (masterCompare && (params?."Build target branch for comparison" != null))
-            {
-                masterCompare = params."Build target branch for comparison"
-            }
             String masterCompareCommand
             if (masterCompare)
             {
@@ -348,6 +348,8 @@ def runPerformanceCommand (platform, project)
         {
             def ARCHIVE_LIMIT = "101"
 
+            String masterCompareString = masterCompare ? "1" : "0"
+
             def command = """#!/usr/bin/env bash
                         set -ex
                         cd ${project.paths.project_build_prefix}/
@@ -359,7 +361,12 @@ def runPerformanceCommand (platform, project)
                         if wget ${ARTIFACT_URL_PREFIX}/*zip*/archive.zip; then
                           unzip archive.zip
                         else
-                          echo "WARNING: No lastSuccessfulBuild found at ${ARTIFACT_URL_PREFIX}"
+                          if [ "${masterCompareString}" == "1" ]; then
+                            echo "ERROR: No lastSuccessfulBuild found at ${ARTIFACT_URL_PREFIX}"
+                            exit 1
+                          else
+                            echo "WARNING: No lastSuccessfulBuild found at ${ARTIFACT_URL_PREFIX}"
+                          fi
                         fi
 
                         #Run Performance Test
