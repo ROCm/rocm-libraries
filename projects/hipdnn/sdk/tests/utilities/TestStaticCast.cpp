968a20b519dc9766eff3d568fc10e9bc13b37e50
mousdahl-amd <mitch.ousdahl@amd.com>
2025-11-07 10:14:47 -0800

    Please work on linux

--- projects/hipdnn/sdk/tests/utilities/TestStaticCast.cpp
+++ projects/hipdnn/sdk/tests/utilities/TestStaticCast.cpp
@@ -30,8 +30,8 @@ TEST(TestStaticCast, Correctness)
     testCastTo<hip_bfloat16>(hip_bfloat16());
     testCastToWithFloatIntermediate<hip_bfloat16>(int());
     testCastToWithFloatIntermediate<hip_bfloat16>(0U);
-    testCastToWithFloatIntermediate<hip_bfloat16>(0ULL);
-    testCastToWithFloatIntermediate<hip_bfloat16>(0LL);
+    testCastToWithFloatIntermediate<hip_bfloat16>(uint64_t{0});
+    testCastToWithFloatIntermediate<hip_bfloat16>(int64_t{0});
 
     testCastTo<half>(float());
     testCastToWithFloatIntermediate<half>(double());
@@ -39,8 +39,8 @@ TEST(TestStaticCast, Correctness)
     testCastTo<half>(hip_bfloat16());
     testCastToWithFloatIntermediate<half>(int());
     testCastToWithFloatIntermediate<half>(0U);
-    testCastToWithFloatIntermediate<half>(0ULL);
-    testCastToWithFloatIntermediate<half>(0LL);
+    testCastToWithFloatIntermediate<half>(uint64_t{0});
+    testCastToWithFloatIntermediate<half>(int64_t{0});
 }
 
 }
