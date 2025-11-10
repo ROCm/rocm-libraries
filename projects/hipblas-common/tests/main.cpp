204052c43bdd0cb8992137616dc3a31d763bd887
Eiden Yoshida <47196116+eidenyoshida@users.noreply.github.com>
2025-11-10 13:18:14 -0800

    Update main.cpp

--- projects/hipblas-common/tests/main.cpp
+++ projects/hipblas-common/tests/main.cpp
@@ -21,7 +21,7 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNE-
 // CTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // ########################################################################
-
+// nothing 
 #include "hipblas-common/hipblas-common.h"
 #include <iostream>
 
