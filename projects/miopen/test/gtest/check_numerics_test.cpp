285bbbe5ae4a9cd55a4ced57f80fadc4a67a3842
kokolchin <kokolchin@luxoft.com>
2025-11-20 05:56:07 +0100

    Use SPDF License Identifier. Change include for tensor_holder.hpp. Use descriptive test name, NormalAndAbnormalValues, instead of Test. Move TEST_P macros outside of anonymous namespace.

--- projects/miopen/test/gtest/check_numerics_test.cpp
+++ projects/miopen/test/gtest/check_numerics_test.cpp
@@ -1,28 +1,5 @@
-/*******************************************************************************
- *
- * MIT License
- *
- * Copyright (c) 2017 Advanced Micro Devices, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or associated portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- *******************************************************************************/
+// Copyright © Advanced Micro Devices, Inc., or its affiliates.
+// SPDX-License-Identifier:  MIT
 
 #include <miopen/handle.hpp>
 #include <miopen/check_numerics.hpp>
@@ -34,9 +11,10 @@
 #include <limits>
 #include <vector>
 #include <stdexcept>
+#include <ostream>
 
 #include "get_handle.hpp"
-#include "../tensor_holder.hpp"
+#include "tensor_holder.hpp"
 
 namespace {
 
@@ -159,10 +137,12 @@ void RunAbnormalValueTest(T val)
         std::exception);
 }
 
+} // namespace
+
 // FP32 tests
 using GPU_CheckNumerics_FP32 = GPU_CheckNumerics<float>;
 
-TEST_P(GPU_CheckNumerics_FP32, Test)
+TEST_P(GPU_CheckNumerics_FP32, NormalAndAbnormalValues)
 {
     const auto& test_case = this->GetParam();
     if(test_case.test_type == CheckNumericsTestType::NormalZero ||
@@ -183,7 +163,7 @@ INSTANTIATE_TEST_SUITE_P(Smoke,
 // FP16 tests
 using GPU_CheckNumerics_FP16 = GPU_CheckNumerics<half_float::half>;
 
-TEST_P(GPU_CheckNumerics_FP16, Test)
+TEST_P(GPU_CheckNumerics_FP16, NormalAndAbnormalValues)
 {
     const auto& test_case = this->GetParam();
     if(test_case.test_type == CheckNumericsTestType::NormalZero ||
@@ -204,7 +184,7 @@ INSTANTIATE_TEST_SUITE_P(Smoke,
 // BF16 tests
 using GPU_CheckNumerics_BFP16 = GPU_CheckNumerics<bfloat16>;
 
-TEST_P(GPU_CheckNumerics_BFP16, Test)
+TEST_P(GPU_CheckNumerics_BFP16, NormalAndAbnormalValues)
 {
     const auto& test_case = this->GetParam();
     if(test_case.test_type == CheckNumericsTestType::NormalZero ||
@@ -221,5 +201,3 @@ TEST_P(GPU_CheckNumerics_BFP16, Test)
 INSTANTIATE_TEST_SUITE_P(Smoke,
                          GPU_CheckNumerics_BFP16,
                          testing::ValuesIn(GetCheckNumericsTestCases<bfloat16>()));
-
-} // namespace
