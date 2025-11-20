07fff271f18a58366ecb02001387c1e77ed72667
Tulebaev <stulebae@amd.com>
2025-11-20 09:49:17 +0100

    Fixed lacking header for miopen::StartsWith()

--- projects/miopen/test/tensor_util.hpp
+++ projects/miopen/test/tensor_util.hpp
@@ -28,8 +28,9 @@
 #define GUARD_TENSOR_UTIL_HPP
 
 #include "tensor_holder.hpp"
-
+#include <miopen/stringutils.hpp>
 #include <miopen/filesystem.hpp>
+
 namespace fs = miopen::fs;
 
 // unary operation
