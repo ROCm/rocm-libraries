28ab74cca3d06ef6a7950351aa95581d07063b4f
amd-garydeng <garydeng@amd.com>
2025-11-21 09:51:41 -0700

    correct args

--- projects/rocsolver/.jenkins/debug.groovy
+++ projects/rocsolver/.jenkins/debug.groovy
@@ -43,7 +43,7 @@ def runCI =
         commonGroovy.runPackageCommand(platform, project)
     }
 
-    buildProject(prj, formatCheck, nodes.dockerArray, compileCommand, packageCommand)
+    buildProject(prj, formatCheck, nodes.dockerArray, compileCommand, null, packageCommand)
 
 }
 
