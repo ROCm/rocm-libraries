fd0d07bca6ea959febb93209a11b14fa165ca724
stulebae <stulebae@amd.com>
2025-11-21 15:23:11 +0100

    updating kernel_tuning_net.cpp

--- projects/miopen/test/gtest/kernel_tuning_net.cpp
+++ projects/miopen/test/gtest/kernel_tuning_net.cpp
@@ -290,7 +290,7 @@ protected:
             miopen::conv::IsEnoughWorkspace("GetSolutionsFallback AI", solver_id, ws, &invoke_ctx));
 
         miopen::PerformanceDb db = {
-            miopen::DbKinds::PerfDb, fs::path{"/tmp/nodb.db.txt"}, fs::path{"/tmp/nodb.udb.txt"}
+            miopen::DbKinds::PerfDb, fs::path("/tmp/nodb.db.txt"), fs::path("/tmp/nodb.udb.txt")
         }; // empty db, force heuristic
         miopen::solver::ConvSolution sol =
             solv.FindSolution(ctx, problem, db, {}); // auto tune is not expected here
