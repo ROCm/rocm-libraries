e16b4e6f6fea5a07e84e2869a515b93914e9988a
Ewan Crawford <ewan.crawford@streamhpc.com>
2025-11-24 10:39:42 +0000

    Refactor `iMod` to `iRemquo`.
    Incude comments about workspace assumptions

--- projects/miopen/src/kernels/MIOpenUtilKernels3.cpp
+++ projects/miopen/src/kernels/MIOpenUtilKernels3.cpp
@@ -8,13 +8,22 @@
 
 #include "float_types.h"
 
-inline __device__ unsigned int iMod(unsigned int v, unsigned int u, unsigned int d)
+// Given the value of x & y, computes the value r such that r = x - k*y,
+// where is the quotient `k` is returned as an output parameter.
+// The `k*y` multiplication is done in 24 bits. It is the responsibility of the solver
+// to ensure `x` & `y` don't exceed 2^24, otherwise this code should be updated to
+// avoid UB.
+inline __device__ unsigned int iRemquo(unsigned int x, unsigned int y, unsigned int& k)
 {
-    return v - __mul24(u, d);
+    k = x / y;
+    return x - __mul24(k, y);
 }
 
+static_assert((OUT_CHANNEL_STRIDE % WRITE_UNIT) == 0);
 constexpr unsigned int out_channel_stride_aligned = OUT_CHANNEL_STRIDE / WRITE_UNIT;
-constexpr unsigned int out_stride_aligned         = OUT_STRIDE / WRITE_UNIT;
+
+static_assert((OUT_STRIDE % WRITE_UNIT) == 0);
+constexpr unsigned int out_stride_aligned = OUT_STRIDE / WRITE_UNIT;
 
 extern "C" __global__
 __launch_bounds__(LOCAL_SIZE_X* LOCAL_SIZE_Y) void SubSample(const FLOAT* __restrict in,
@@ -23,14 +32,17 @@ __launch_bounds__(LOCAL_SIZE_X* LOCAL_SIZE_Y) void SubSample(const FLOAT* __rest
     const unsigned int stack_pos = blockIdx.x * LOCAL_SIZE_X + threadIdx.x;
     const unsigned int batch_id  = blockIdx.y * LOCAL_SIZE_Y + threadIdx.y;
 
-    unsigned int map_id  = stack_pos / out_channel_stride_aligned;
-    unsigned int pix_pos = iMod(stack_pos, map_id, out_channel_stride_aligned);
-    unsigned int out_y   = pix_pos / out_stride_aligned;
-    unsigned int out_x   = iMod(pix_pos, out_y, out_stride_aligned) * WRITE_UNIT;
+    unsigned int map_id;
+    unsigned int pix_pos = iRemquo(stack_pos, out_channel_stride_aligned, map_id);
 
+    // Offsets are stored in 32-bit variable, it is the responsibiliy of the solver to ensure
+    // this won't overflow, otherwise this should be updated to 64-bit variable.
+    unsigned int out_y;
+    unsigned int out_x   = iRemquo(pix_pos, out_stride_aligned, out_y) * WRITE_UNIT;
     unsigned int out_off = batch_id * IN_BATCH_STRIDE + stack_pos * WRITE_UNIT;
-    unsigned int in_y    = out_y * FILTER0_STRIDE1;
-    unsigned int in_x    = out_x * FILTER0_STRIDE0;
+
+    unsigned int in_y = out_y * FILTER0_STRIDE1;
+    unsigned int in_x = out_x * FILTER0_STRIDE0;
     unsigned int in_off =
         batch_id * IN0_BATCH_STRIDE + map_id * IN0_CHANNEL_STRIDE + in_y * IN0_STRIDE + in_x;
 
@@ -43,9 +55,17 @@ __launch_bounds__(LOCAL_SIZE_X* LOCAL_SIZE_Y) void SubSample(const FLOAT* __rest
     }
 }
 
+static_assert((IN0_CHANNEL_STRIDE % WRITE_UNIT) == 0);
 constexpr unsigned int in_channel_stride_aligned = IN0_CHANNEL_STRIDE / WRITE_UNIT;
-constexpr unsigned int in_stride_aligned         = IN0_STRIDE / WRITE_UNIT;
 
+static_assert((IN0_STRIDE % WRITE_UNIT) == 0);
+constexpr unsigned int in_stride_aligned = IN0_STRIDE / WRITE_UNIT;
+
+/*
+ *  Upsample kernel requires output to already be zero initalized to avoid undefined values in gaps
+ *  between strides. See zero initalization in miopen/src/conv/invokers/gcn_asm_1x1u_us.cpp with
+ *  TODO comment for finding a way to avoid this pre-initalization to improve performance.
+ */
 extern "C" __global__
 __launch_bounds__(LOCAL_SIZE_X* LOCAL_SIZE_Y) void UpSample(const FLOAT* __restrict in,
                                                             FLOAT* __restrict out)
@@ -53,14 +73,17 @@ __launch_bounds__(LOCAL_SIZE_X* LOCAL_SIZE_Y) void UpSample(const FLOAT* __restr
     const unsigned int stack_pos = blockIdx.x * LOCAL_SIZE_X + threadIdx.x;
     const unsigned int batch_id  = blockIdx.y * LOCAL_SIZE_Y + threadIdx.y;
 
-    unsigned int map_id  = stack_pos / in_channel_stride_aligned;
-    unsigned int pix_pos = iMod(stack_pos, map_id, in_channel_stride_aligned);
-    unsigned int in_y    = pix_pos / in_stride_aligned;
-    unsigned int in_x    = iMod(pix_pos, in_y, in_stride_aligned) * WRITE_UNIT;
+    unsigned int map_id;
+    unsigned int pix_pos = iRemquo(stack_pos, in_channel_stride_aligned, map_id);
 
+    // Offsets are stored in 32-bit variables, it is the responsibiliy of the solver to ensure
+    // this won't overflow, otherwise this should be updated to 64-bit variable.
+    unsigned int in_y   = pix_pos / in_stride_aligned;
+    unsigned int in_x   = iRemquo(pix_pos, in_stride_aligned, in_y) * WRITE_UNIT;
     unsigned int in_off = batch_id * IN_BATCH_STRIDE + stack_pos * WRITE_UNIT;
-    unsigned int out_y  = in_y * FILTER0_STRIDE1;
-    unsigned int out_x  = in_x * FILTER0_STRIDE0;
+
+    unsigned int out_y = in_y * FILTER0_STRIDE1;
+    unsigned int out_x = in_x * FILTER0_STRIDE0;
     unsigned int out_off =
         batch_id * IN0_BATCH_STRIDE + map_id * OUT_CHANNEL_STRIDE + out_y * OUT_STRIDE + out_x;
 
