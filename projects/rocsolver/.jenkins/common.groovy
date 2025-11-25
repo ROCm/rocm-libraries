bc97c74881e6ace77ecbddfc91f083aa9a3a56c9
amd-garydeng <garydeng@amd.com>
2025-11-24 17:04:39 -0700

    remove debug msg and use quotes

--- projects/rocsolver/.jenkins/common.groovy
+++ projects/rocsolver/.jenkins/common.groovy
@@ -27,10 +27,8 @@ def runCompileCommand(platform, project, jobName, boolean sameOrg=false, boolean
                 withSparse = false
             }
         }
-
-        echo (project.buildName)
         // in PRs targeting develop branch build ONLY what CI pipeline will test, unless github gfxall label or debug job
-        if (env.CHANGE_TARGET == "develop" && !pullRequest.labels.contains("gfxall") && !project.buildName.contains('Debug'))
+        if (env.CHANGE_TARGET == "develop" && !pullRequest.labels.contains("gfxall") && !project.buildName.contains("Debug"))
         {
 
             // requires at command execution time ${auxiliary.gfxTargetParser()} to set gfx_var variable
