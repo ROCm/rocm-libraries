18f9209bb27239fa1bdbbcba8b400c2bd61b3c83
Jeremy Hart <jeremy.hart@amd.com>
2025-11-25 17:49:26 +0000

    Add workaround for MIOPEN_FIND_MODE

--- projects/miopen/src/fusion.cpp
+++ projects/miopen/src/fusion.cpp
@@ -52,6 +52,11 @@
     if(x != miopenStatusSuccess) \
         return x;
 
+// Fusion solution selection currently only works with the MIOPEN_FIND_MODE=FAST
+// Until the other modes are properly supported, this forces it down the FAST path, ignoring the set
+// find mode
+#define WORKAROUND_LWPMIOPEN_1882
+
 namespace miopen {
 
 miopenStatus_t ConvBiasActivFusion(const Handle& handle,
@@ -1000,10 +1005,12 @@ miopenStatus_t FusionPlanDescriptor::Compile(const Handle& handle)
     }
 
     {
-        FindMode findMode;
         auto sol = boost::optional<miopenConvSolution_t>{};
+#ifndef WORKAROUND_LWPMIOPEN_1882
+        FindMode findMode;
 
         if(findMode.IsFast(fusion_problem) || findMode.IsHybrid(fusion_problem))
+#endif
         {
             const auto ctx      = FusionContext{handle};
             auto sols           = GetSolutions(ctx, fusion_problem, 1);
@@ -1033,9 +1040,13 @@ miopenStatus_t FusionPlanDescriptor::Compile(const Handle& handle)
                 }
             }
 
+#ifndef WORKAROUND_LWPMIOPEN_1882
             // override the normal find with immed mode with env var
             if(!sols.empty() && (!(findMode.IsHybrid(fusion_problem) && fallback)))
             // || env::enabled(MIOPEN_DEBUG_FORCE_IMMED_MODE_FALLBACK)
+#else
+            if(!sols.empty())
+#endif
             {
                 std::sort(sols.begin(), sols.end(), SolutionTimeComparator());
                 sol = sols.front();
