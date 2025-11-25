574fb40c638d457bf8c1cf73df379a7bc2b5e088
stulebae <stulebae@amd.com>
2025-11-25 23:15:26 +0100

    Fixing clang-format violation
    Fixing clang-format violation in unit_SeqTensorDescriptor.cpp

--- projects/miopen/test/gtest/unit_SeqTensorDescriptor.cpp
+++ projects/miopen/test/gtest/unit_SeqTensorDescriptor.cpp
@@ -11,8 +11,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ *all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -30,45 +30,39 @@
 
 namespace {
 
-struct SeqTensorDescriptorParams
-{
-    SeqTensorDescriptorParams(miopenDataType_t datatype_in, std::vector<size_t>&& lens_in)
-        : datatype(datatype_in), lens(std::move(lens_in))
-    {
-    }
+struct SeqTensorDescriptorParams {
+  SeqTensorDescriptorParams(miopenDataType_t datatype_in,
+                            std::vector<size_t> &&lens_in)
+      : datatype(datatype_in), lens(std::move(lens_in)) {}
 
-    SeqTensorDescriptorParams(miopenDataType_t datatype_in,
-                              std::vector<size_t>&& lens_in,
-                              bool with_padded_seq_layout)
-        : datatype(datatype_in), lens(std::move(lens_in)), padded_seq_layout(with_padded_seq_layout)
-    {
-    }
+  SeqTensorDescriptorParams(miopenDataType_t datatype_in,
+                            std::vector<size_t> &&lens_in,
+                            bool with_padded_seq_layout)
+      : datatype(datatype_in), lens(std::move(lens_in)),
+        padded_seq_layout(with_padded_seq_layout) {}
 
-    miopen::SeqTensorDescriptor GetSeqTensorDescriptor() const
-    {
-        std::vector<unsigned int> layout_default(lens.size());
-        std::iota(layout_default.begin(), layout_default.end(), 0);
-        return {datatype, layout_default, lens, padded_seq_layout};
-    }
+  miopen::SeqTensorDescriptor GetSeqTensorDescriptor() const {
+    std::vector<unsigned int> layout_default(lens.size());
+    std::iota(layout_default.begin(), layout_default.end(), 0);
+    return {datatype, layout_default, lens, padded_seq_layout};
+  }
 
 private:
-    miopenDataType_t datatype;
-    std::vector<size_t> lens;
-    bool padded_seq_layout;
+  miopenDataType_t datatype;
+  std::vector<size_t> lens;
+  bool padded_seq_layout;
 };
 
-struct TestCase
-{
-    SeqTensorDescriptorParams tp;
-    size_t max_sequence_length;
-    size_t total_sequence_length;
-    size_t actual_count_of_sequence;
+struct TestCase {
+  SeqTensorDescriptorParams tp;
+  size_t max_sequence_length;
+  size_t total_sequence_length;
+  size_t actual_count_of_sequence;
 };
 
-auto GetTestCases()
-{
-    return std::vector{
-        // clang-format off
+auto GetTestCases() {
+  return std::vector{
+      // clang-format off
         TestCase{{miopenHalf, {2, 2, 2}}, 2, 4, 2},
         TestCase{{miopenHalf, {2, 2, 2}, true}, 2, 4, 2},
         TestCase{{miopenHalf, {2, 2, 2, 2}}, 2, 4, 2},
@@ -78,20 +72,18 @@ auto GetTestCases()
         TestCase{{miopenHalf, {2, 8, 2, 2}, true}, 8, 16, 2},
         TestCase{{miopenHalf, {2, 16, 8, 2, 2}}, 16, 32, 2},
         TestCase{{miopenHalf, {2, 16, 8, 2, 2}, true}, 16, 32, 2},
-        // clang-format on
-    };
+      // clang-format on
+  };
 }
 
-struct TestTensorSequence : testing::TestWithParam<TestCase>
-{
-    void RunTest()
-    {
-        const auto p  = GetParam();
-        const auto td = p.tp.GetSeqTensorDescriptor();
-        ASSERT_EQ(td.GetTotalSequenceLen(), p.total_sequence_length);
-        ASSERT_EQ(td.GetMaxSequenceLength(), p.max_sequence_length);
-        ASSERT_EQ(td.GetMaxCountOfSequences(), p.actual_count_of_sequence);
-    }
+struct TestTensorSequence : testing::TestWithParam<TestCase> {
+  void RunTest() {
+    const auto p = GetParam();
+    const auto td = p.tp.GetSeqTensorDescriptor();
+    ASSERT_EQ(td.GetTotalSequenceLen(), p.total_sequence_length);
+    ASSERT_EQ(td.GetMaxSequenceLength(), p.max_sequence_length);
+    ASSERT_EQ(td.GetMaxCountOfSequences(), p.actual_count_of_sequence);
+  }
 };
 
 using CPU_TestTensorSequence_FP16 = TestTensorSequence;
@@ -100,7 +92,5 @@ using CPU_TestTensorSequence_FP16 = TestTensorSequence;
 
 TEST_P(CPU_TestTensorSequence_FP16, SeqTensorDescriptor) { RunTest(); };
 
-INSTANTIATE_TEST_SUITE_P(Full,
-                         CPU_TestTensorSequence_FP16,
+INSTANTIATE_TEST_SUITE_P(Full, CPU_TestTensorSequence_FP16,
                          testing::ValuesIn(GetTestCases()));
-
