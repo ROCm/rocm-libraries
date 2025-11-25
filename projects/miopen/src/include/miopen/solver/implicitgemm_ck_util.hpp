1fc1adf900e5151e4ab3c2b45d9c87cab532d5c9
Vsevolod Golovko <gvsevolo@amd.com>
2025-11-25 15:25:13 +0000

    revert changes

--- projects/miopen/src/include/miopen/solver/implicitgemm_ck_util.hpp
+++ projects/miopen/src/include/miopen/solver/implicitgemm_ck_util.hpp
@@ -320,7 +320,6 @@ bool IsCKApplicable(const ProblemDescriptionType& problem)
     const auto args = CKArgsType{problem};
 
     const auto ptrs = DeviceOpType::GetInstances();
-
     return std::any_of(
         ptrs.begin(), ptrs.end(), [&args](auto& ptr) { return args.IsSupportedBy(ptr); });
 }
