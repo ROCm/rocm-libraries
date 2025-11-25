309e4a31d395e22e063fa2a3ce85587e0d6ad722
Tulebaev <stulebae@amd.com>
2025-11-26 00:47:51 +0100

    clang-format unit_SeqTensorDescriptor.cpp

--- projects/miopen/test/gtest/unit_SeqTensorDescriptor.cpp
+++ projects/miopen/test/gtest/unit_SeqTensorDescriptor.cpp
@@ -30,39 +30,45 @@
 
 namespace {
 
-struct SeqTensorDescriptorParams {
-  SeqTensorDescriptorParams(miopenDataType_t datatype_in,
-                            std::vector<size_t> &&lens_in)
-      : datatype(datatype_in), lens(std::move(lens_in)) {}
+struct SeqTensorDescriptorParams
+{
+    SeqTensorDescriptorParams(miopenDataType_t datatype_in, std::vector<size_t>&& lens_in)
+        : datatype(datatype_in), lens(std::move(lens_in))
+    {
+    }
 
-  SeqTensorDescriptorParams(miopenDataType_t datatype_in,
-                            std::vector<size_t> &&lens_in,
-                            bool with_padded_seq_layout)
-      : datatype(datatype_in), lens(std::move(lens_in)),
-        padded_seq_layout(with_padded_seq_layout) {}
+    SeqTensorDescriptorParams(miopenDataType_t datatype_in,
+                              std::vector<size_t>&& lens_in,
+                              bool with_padded_seq_layout)
+        : datatype(datatype_in), lens(std::move(lens_in)), padded_seq_layout(with_padded_seq_layout)
+    {
+    }
 
-  miopen::SeqTensorDescriptor GetSeqTensorDescriptor() const {
-    std::vector<unsigned int> layout_default(lens.size());
-    std::iota(layout_default.begin(), layout_default.end(), 0);
-    return {datatype, layout_default, lens, padded_seq_layout};
-  }
+    miopen::SeqTensorDescriptor GetSeqTensorDescriptor() const
+    {
+        std::vector<unsigned int> layout_default(lens.size());
+        std::iota(layout_default.begin(), layout_default.end(), 0);
+        return {datatype, layout_default, lens, padded_seq_layout};
+    }
 
 private:
-  miopenDataType_t datatype;
-  std::vector<size_t> lens;
-  bool padded_seq_layout;
+    miopenDataType_t datatype;
+    std::vector<size_t> lens;
+    bool padded_seq_layout;
 };
 
-struct TestCase {
-  SeqTensorDescriptorParams tp;
-  size_t max_sequence_length;
-  size_t total_sequence_length;
-  size_t actual_count_of_sequence;
+struct TestCase
+{
+    SeqTensorDescriptorParams tp;
+    size_t max_sequence_length;
+    size_t total_sequence_length;
+    size_t actual_count_of_sequence;
 };
 
-auto GetTestCases() {
-  return std::vector{
-      // clang-format off
+auto GetTestCases()
+{
+    return std::vector{
+        // clang-format off
         TestCase{{miopenHalf, {2, 2, 2}}, 2, 4, 2},
         TestCase{{miopenHalf, {2, 2, 2}, true}, 2, 4, 2},
         TestCase{{miopenHalf, {2, 2, 2, 2}}, 2, 4, 2},
@@ -72,18 +78,20 @@ auto GetTestCases() {
         TestCase{{miopenHalf, {2, 8, 2, 2}, true}, 8, 16, 2},
         TestCase{{miopenHalf, {2, 16, 8, 2, 2}}, 16, 32, 2},
         TestCase{{miopenHalf, {2, 16, 8, 2, 2}, true}, 16, 32, 2},
-      // clang-format on
-  };
+        // clang-format on
+    };
 }
 
-struct TestTensorSequence : testing::TestWithParam<TestCase> {
-  void RunTest() {
-    const auto p = GetParam();
-    const auto td = p.tp.GetSeqTensorDescriptor();
-    ASSERT_EQ(td.GetTotalSequenceLen(), p.total_sequence_length);
-    ASSERT_EQ(td.GetMaxSequenceLength(), p.max_sequence_length);
-    ASSERT_EQ(td.GetMaxCountOfSequences(), p.actual_count_of_sequence);
-  }
+struct TestTensorSequence : testing::TestWithParam<TestCase>
+{
+    void RunTest()
+    {
+        const auto p  = GetParam();
+        const auto td = p.tp.GetSeqTensorDescriptor();
+        ASSERT_EQ(td.GetTotalSequenceLen(), p.total_sequence_length);
+        ASSERT_EQ(td.GetMaxSequenceLength(), p.max_sequence_length);
+        ASSERT_EQ(td.GetMaxCountOfSequences(), p.actual_count_of_sequence);
+    }
 };
 
 using CPU_TestTensorSequence_FP16 = TestTensorSequence;
@@ -92,5 +100,4 @@ using CPU_TestTensorSequence_FP16 = TestTensorSequence;
 
 TEST_P(CPU_TestTensorSequence_FP16, SeqTensorDescriptor) { RunTest(); };
 
-INSTANTIATE_TEST_SUITE_P(Full, CPU_TestTensorSequence_FP16,
-                         testing::ValuesIn(GetTestCases()));
+INSTANTIATE_TEST_SUITE_P(Full, CPU_TestTensorSequence_FP16, testing::ValuesIn(GetTestCases()));
