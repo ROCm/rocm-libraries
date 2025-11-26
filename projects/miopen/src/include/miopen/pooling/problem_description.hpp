946c8ed42e60fd046c28aa0c15a8604291d50739
Sai Kubair Kota <sai.kota@streamhpc.com>
2025-11-26 09:16:09 +0000

    src/include/miopen/pooling/problem_description & & src/pooling.cpp: added requirements for tuning support in pooling
    Added the necessary methods and members in the Pooling ProblemDescription class to enable tuning support. This includes implementing the MakeNetworkConfig method, updating the Visit methods to include input and output shapes, and adding the GetDb friend function for accessing the performance database.

--- projects/miopen/src/include/miopen/pooling/problem_description.hpp
+++ projects/miopen/src/include/miopen/pooling/problem_description.hpp
@@ -29,6 +29,7 @@
 #include <miopen/problem_description_base.hpp>
 #include <miopen/tensor.hpp>
 #include <miopen/pooling.hpp>
+#include <miopen/mlo_internal.hpp>
 
 #include <cassert>
 #include <string>
@@ -45,7 +46,15 @@ enum class Direction
     Backward,
 };
 
-struct ProblemDescription
+struct ProblemDescriptionTag
+{
+};
+
+struct ProblemDescription : ProblemDescriptionTag
+#if MIOPEN_ENABLE_SQLITE
+    ,
+                            SQLiteSerializable<ProblemDescription>
+#endif
 {
     // Forward
     ProblemDescription(const PoolingDescriptor& pooling_,
@@ -102,6 +111,73 @@ struct ProblemDescription
 
     NetworkConfig MakeNetworkConfig() const;
 
+#if MIOPEN_ENABLE_SQLITE
+    template <class Self>
+    static void Visit(Self&& self, std::function<void(int64_t, std::string)> f)
+    {
+        // The column names match the driver command line argument names
+        f(static_cast<uint64_t>(self.direction), "direction");
+        f(static_cast<uint64_t>(self.pooling.GetPaddingMode()), "pad_mode");
+        f(self.pooling.GetStrides().size() > 2 ? static_cast<uint64_t>(self.pooling.GetStrides()[0])
+                                               : 0,
+          "pool_stride_d");
+        f(self.pooling.GetStrides().size() > 2
+              ? static_cast<uint64_t>(self.pooling.GetStrides()[1])
+              : static_cast<uint64_t>(self.pooling.GetStrides()[0]),
+          "pool_stride_h");
+        f(self.pooling.GetStrides().size() > 2
+              ? static_cast<uint64_t>(self.pooling.GetStrides()[2])
+              : static_cast<uint64_t>(self.pooling.GetStrides()[1]),
+          "pool_stride_w");
+        f(self.pooling.GetPads().size() > 2 ? static_cast<uint64_t>(self.pooling.GetPads()[0]) : 0,
+          "pad_d");
+        f(self.pooling.GetPads().size() > 2 ? static_cast<uint64_t>(self.pooling.GetPads()[1])
+                                            : static_cast<uint64_t>(self.pooling.GetPads()[0]),
+          "pad_h");
+        f(self.pooling.GetPads().size() > 2 ? static_cast<uint64_t>(self.pooling.GetPads()[2])
+                                            : static_cast<uint64_t>(self.pooling.GetPads()[1]),
+          "pad_w");
+        f(self.pooling.GetLengths().size() > 2 ? static_cast<uint64_t>(self.pooling.GetLengths()[0])
+                                               : 0,
+          "win_d");
+        f(self.pooling.GetLengths().size() > 2
+              ? static_cast<uint64_t>(self.pooling.GetLengths()[1])
+              : static_cast<uint64_t>(self.pooling.GetLengths()[0]),
+          "win_h");
+        f(self.pooling.GetLengths().size() > 2
+              ? static_cast<uint64_t>(self.pooling.GetLengths()[2])
+              : static_cast<uint64_t>(self.pooling.GetLengths()[1]),
+          "win_w");
+    }
+
+    template <class Self>
+    static void Visit(Self&& self, std::function<void(std::string, std::string)> f)
+    {
+        f(self.GetInShape(), "in_shape");
+        f(self.GetOutShape(), "out_shape");
+        f(self.xDesc.GetLayout_str(), "layout");
+        f(self.GetDirectionStr(), "direction");
+        f(GetDataTypeName(self.xDesc.GetType()), "data_type");
+        f(self.GetModeStr(), "mode");
+    }
+
+    template <class Self, class Visitor>
+    static void VisitAll(Self&& self, const Visitor& f)
+    {
+        Visit(std::forward<Self>(self), [&](int64_t value, std::string name) { f(value, name); });
+        Visit(std::forward<Self>(self),
+              [&](std::string value, std::string name) { f(value, name); });
+    }
+
+    void Serialize(std::ostream& stream) const { stream << MakeNetworkConfig().ToString(); }
+
+    // This declaration marks pooling as a primitive with tuning enabled.
+    // Any tunable solver would be able pick it and fetch a db instance in ExecutePrimitive.
+    // It has to be discoverable via ADL from problem description.
+    friend auto GetDb(const ExecutionContext& context, const ProblemDescriptionTag&)
+        -> PerformanceDb;
+#endif
+
 private:
     Direction direction;
     PoolingDescriptor pooling;
@@ -110,6 +186,61 @@ private:
     TensorDescriptor dxDesc;
     TensorDescriptor dyDesc;
     bool save_index = false;
+    std::size_t GetInBatchSize() const { return xDesc.GetLengths()[0]; }
+    std::size_t GetInChannel() const { return xDesc.GetLengths()[1]; }
+    std::size_t GetInDepth() const { return xDesc.GetNumDims() > 4 ? xDesc.GetLengths()[2] : 0; }
+    std::size_t GetInHeight() const { return xDesc.GetLengths()[xDesc.GetNumDims() - 2]; }
+    std::size_t GetInWidth() const { return xDesc.GetLengths()[xDesc.GetNumDims() - 1]; }
+    std::size_t GetOutBatchSize() const { return yDesc.GetLengths()[0]; }
+    std::size_t GetOutChannel() const { return yDesc.GetLengths()[1]; }
+    std::size_t GetOutDepth() const { return yDesc.GetNumDims() > 4 ? yDesc.GetLengths()[2] : 0; }
+    std::size_t GetOutHeight() const { return yDesc.GetLengths()[yDesc.GetNumDims() - 2]; }
+    std::size_t GetOutWidth() const { return yDesc.GetLengths()[yDesc.GetNumDims() - 1]; }
+    std::string GetInShape() const
+    {
+        std::string shape = "(" + std::to_string(GetInBatchSize());
+        shape += "," + std::to_string(GetInChannel());
+        if(GetInDepth() != 0)
+            shape += "," + std::to_string(GetInDepth());
+        shape += "," + std::to_string(GetInHeight());
+        shape += "," + std::to_string(GetInWidth());
+        shape += ")";
+        return shape;
+    }
+    std::string GetOutShape() const
+    {
+        std::string shape = "(" + std::to_string(GetOutBatchSize());
+        shape += "," + std::to_string(GetOutChannel());
+        if(GetOutDepth() != 0)
+            shape += "," + std::to_string(GetOutDepth());
+        shape += "," + std::to_string(GetOutHeight());
+        shape += "," + std::to_string(GetOutWidth());
+        shape += ")";
+        return shape;
+    }
+    std::string GetDirectionStr() const
+    {
+        std::string s;
+
+        switch(direction)
+        {
+        case Direction::Forward: return "Fwd";
+        case Direction::Backward: return "Bwd";
+        default: MIOPEN_THROW(miopenStatusInvalidValue, "Wrong pooling direction provided");
+        }
+
+        return s;
+    }
+    std::string GetModeStr() const
+    {
+        switch(pooling.GetMode())
+        {
+        case miopenPoolingMax: return "max";
+        case miopenPoolingAverage: return "avg";
+        case miopenPoolingAverageInclusive: return "avg_in";
+        default: MIOPEN_THROW(miopenStatusInvalidValue, "Wrong pooling mode provided");
+        }
+    }
 };
 
 } // namespace pooling
