770308d55a38a30a89c375d0c6dd20e976ec921c
YvanMokwinski <yvan.mokwinski@gmail.com>
2025-11-26 21:39:34 -0700

    clang format

--- projects/rocsparse/library/src/include/rocsparse_array_hash.hpp
+++ projects/rocsparse/library/src/include/rocsparse_array_hash.hpp
@@ -2,8 +2,9 @@
 //
 inline uint32_t rocsparse_array_hash(const void* data, size_t size)
 {
-    void* buffer = malloc(size);
-    std::ignore = hipMemcpy(buffer, data, size, hipMemcpyDefault);
+
+    void* buffer                   = malloc(size);
+    std::ignore                    = hipMemcpy(buffer, data, size, hipMemcpyDefault);
     const unsigned char* byte_data = static_cast<const unsigned char*>(buffer);
     uint32_t             hash      = 0x811C9DC5; // FNV_prime_32
     for(size_t i = 0; i < size; ++i)
