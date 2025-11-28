73357c2150709ea1c40a43a2dc55814cfa7d2abe
milo <milo@streamhpc.com>
2025-11-28 16:04:16 +0000

    Add NHWC/NDHWC pooling tests and fix verification
    - Add gtest suites for NHWC pooling: asymmetric, wide, and 3D variants
    - Fix pooling_common.hpp to use linear indexing for layout-agnostic verification
    - Add stride-based index calculation supporting both NCHW and NHWC layouts
    - Extract pooling3d_driver to pooling3d.hpp for reuse
    - Add in_layout parameter to pooling test driver

--- projects/miopen/test/pooling3d.hpp
+++ projects/miopen/test/pooling3d.hpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2019 Advanced Micro Devices, Inc.
+ * Copyright (c) 2025 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,6 +24,29 @@
  *
  *******************************************************************************/
 
-#include "pooling3d.hpp"
+#include "pooling_common.hpp"
 
-int main(int argc, const char* argv[]) { test_drive<pooling3d_driver>(argc, argv); }
+template <class T>
+struct pooling3d_driver : pooling_driver<T>
+{
+    std::vector<std::vector<int>> get_3d_pooling_input_shapes()
+    {
+        return {{16, 64, 3, 4, 4},
+                {16, 32, 4, 9, 9},
+                {8, 512, 3, 14, 14},
+                {8, 512, 4, 28, 28},
+                {16, 64, 56, 56, 56},
+                {4, 3, 4, 227, 227},
+                {4, 4, 4, 161, 700}};
+    }
+
+    pooling3d_driver() : pooling_driver<T>()
+    {
+        this->add(
+            this->in_shape, "input", this->generate_data_limited(get_3d_pooling_input_shapes(), 4));
+        this->add(this->lens, "lens", this->generate_data({{2, 2, 2}, {3, 3, 3}}));
+        this->add(this->strides, "strides", this->generate_data({{2, 2, 2}, {1, 1, 1}}));
+        this->add(this->pads, "pads", this->generate_data({{0, 0, 0}, {1, 1, 1}}));
+        this->add(this->wsidx, "wsidx", this->generate_data({1}));
+    }
+};
