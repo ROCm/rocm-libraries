0e34ab83be8250b5aa12bc5d3f29b36e56f17923
kokolchin <kokolchin@luxoft.com>
2025-12-01 14:44:25 +0100

    Convert pooling3d test from ctest to Google Test
    - Create test/gtest/pooling3d.cpp with gtest framework
    - Create test/gtest/pooling3d.hpp with pooling3d_driver struct
    - Use testing::ValuesIn() and std::string parameter type for proper test case expansion
    - Support FP32 and FP16 precision
    - Follow same pattern as pooling2d gtest conversion

--- projects/miopen/test/gtest/pooling3d.hpp
+++ projects/miopen/test/gtest/pooling3d.hpp
@@ -24,12 +24,14 @@
  *
  *******************************************************************************/
 
-#include "pooling_common.hpp"
+#include "../pooling_common.hpp"
 
 template <class T>
 struct pooling3d_driver : pooling_driver<T>
 {
-    std::vector<std::vector<int>> get_3d_pooling_input_shapes()
+private:
+    using U = typename std::vector<int>;
+    std::vector<U> get_3d_pooling_input_shapes()
     {
         return {{16, 64, 3, 4, 4},
                 {16, 32, 4, 9, 9},
@@ -40,15 +42,15 @@ struct pooling3d_driver : pooling_driver<T>
                 {4, 4, 4, 161, 700}};
     }
 
+public:
     pooling3d_driver() : pooling_driver<T>()
     {
         this->add(
-            this->in_shape, "input", this->generate_data_limited(get_3d_pooling_input_shapes(), 4));
-        this->add(this->lens, "lens", this->generate_data({{2, 2, 2}, {3, 3, 3}}));
-        this->add(this->strides, "strides", this->generate_data({{2, 2, 2}, {1, 1, 1}}));
-        this->add(this->pads, "pads", this->generate_data({{0, 0, 0}, {1, 1, 1}}));
+            this->in_shape, "input", this->template generate_data_limited<U>(get_3d_pooling_input_shapes(), 4));
+        this->add(this->lens, "lens", this->template generate_data<U>({{2, 2, 2}, {3, 3, 3}}));
+        this->add(this->strides, "strides", this->template generate_data<U>({{2, 2, 2}, {1, 1, 1}}));
+        this->add(this->pads, "pads", this->template generate_data<U>({{0, 0, 0}, {1, 1, 1}}));
         this->add(this->wsidx, "wsidx", this->generate_data({1}));
     }
 };
 
-int main(int argc, const char* argv[]) { test_drive<pooling3d_driver>(argc, argv); }
