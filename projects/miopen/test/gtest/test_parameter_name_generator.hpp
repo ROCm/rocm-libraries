cbc6b5474991e4af05dd9fd7e5b5668663b3310c
Carlo Orrù <carlo.orru@dxc.com>
2025-12-01 19:09:36 +0200

    Explaination has been added for each new test name generation function

--- projects/miopen/test/gtest/test_parameter_name_generator.hpp
+++ projects/miopen/test/gtest/test_parameter_name_generator.hpp
@@ -37,18 +37,21 @@
 
 namespace {
 
+// Concept for printable objets which support the << operator.
 template <typename T>
 concept Printable = requires(std::ostream& os, T t)
 {
     os << t;
 };
 
+// Concept for printable containers, elements of which support the << operator.
 template <typename T>
 concept PrintableElement = requires(std::ostream& os, T t)
 {
     os << t[0];
 };
 
+// Concept for iterable containers.
 template <typename T>
 concept Container = requires(T t)
 {
@@ -65,6 +68,9 @@ concept Container = requires(T t)
     // clang-format on
 };
 
+// Template wrapper around a test parameter.
+// It defines the << operator as required by GTest, and the cast operator that returns the wrapped parameter.
+// If you need to wrap a container, use the 'NamedContainer' template wrapper.
 template <typename T>
 requires Printable<T> && std::is_move_constructible_v<T>
 struct NamedParameter
@@ -87,6 +93,9 @@ struct NamedParameter
     T value{};
 };
 
+// Template wrapper around an iterable container test parameter (like all STL containers).
+// It defines the << operator as required by GTest, and the cast operator that returns the wrapped container.
+// If you need to wrap a parameter which is not a container, use the 'NamedParameter' template wrapper.
 template <typename T>
 requires Container<T> && PrintableElement<T> && std::is_move_constructible_v<T>
 struct NamedContainer
@@ -128,12 +137,39 @@ struct NamedContainer
     std::string separator{};
 };
 
+// Variadic template function that creates a GTest ValueArray of 'NamedParameter' each one with the name and value supplied.
+// The result can be directly fed to the GTest instantiated test suite.
+// 
+// Example:
+// 
+//      testing::Combine(
+//          MakeNamedParameterValues<int>("TestInt1", 1, 2, 3),
+//          MakeNamedParameterValues<int>("TestInt2", 10, 20, 30),
+//          MakeNamedParameterValues<int>("TestInt3", 100, 200, 300)
+//      );
+// 
 template <typename... T>
 static auto MakeNamedParameterValues(const std::string& name, T... values)
 {
     return testing::Values(NamedParameter<T>{name, values}...);
 }
 
+// Variadic template function that creates a GTest ValueArray of 'NamedContainer' each one with the name and value supplied.
+// An optional separator for each value in each containers can be supplied.
+// The result can be directly fed to the GTest instantiated test suite.
+// 
+// Example:
+// 
+//      std::set<std::vector<int>> tensorSizes = ...
+// 
+//      testing::Combine(
+//          MakeNamedParameterCollectionValues<std::vector<int>>("TestTensorSizes", tensorSizes, "x"),
+//          MakeNamedParameterValues<int>("TestInteger", 1, 2, 3)
+//      );
+// 
+// The 'tensorSizes' collection of std::vector<int>'s is turned into a collection of NamedContainer<std::vector<int>>,
+// and then fed into 'testing::Combine()'.
+// 
 template <typename T>
 static auto MakeNamedParameterCollectionValues(const std::string& name,
                                                const std::ranges::range auto& collection,
@@ -151,7 +187,16 @@ static auto MakeNamedParameterCollectionValues(const std::string& name,
     return testing::ValuesIn(v);
 }
 
-static std::string GetRangeAsString(const std::ranges::range auto& r, std::string_view separator)
+// The 'GetRangeAsString()' function Returns the string representation of the input collection, using the user-supplied separator.
+// The returned string meets the GTest requirements for test names: only alphanumeric characters and underscores are allowed.
+// Any dot character (i.e. '.') is turned into a 'p' character, which stands for 'point'.
+//
+// Examples:
+//
+//      GetRangeAsString(std::vector<int>{1, 2, 3, 4}) returns "1_2_3_4"
+//      GetRangeAsString(std::vector<float>{1.1, 2.2, 3.3, 4.4}) returns "1p1_2p2_3p3_4p4"
+//
+static std::string GetRangeAsString(const std::ranges::range auto& r)
 {
     std::string str;
 
@@ -163,7 +208,7 @@ static std::string GetRangeAsString(const std::ranges::range auto& r, std::strin
 
         for(auto it = r.begin() + 1; it != r.end(); ++it)
         {
-            ss << separator << *it;
+            ss << "_" << *it;
         }
 
         str = ss.str();
