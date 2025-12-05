c5d7a9121174cea49e007f12717d91e86c513f64
Carlo Orrù <carlo.orru@dxc.com>
2025-12-05 12:31:16 +0200

    Test modified to adhere to the test naming conventions

--- projects/miopen/test/gtest/deterministic_conv_api.cpp
+++ projects/miopen/test/gtest/deterministic_conv_api.cpp
@@ -24,32 +24,101 @@
  *
  *******************************************************************************/
 
+#include <tuple>
+#include <vector>
+
 #include <miopen/convolution.hpp>
 
 #include "gtest_common.hpp"
+#include "test_parameter_name_generator.hpp"
 
-TEST(DeterministicConvApiTest, Test)
+namespace {
+
+using TestCase = std::tuple<NamedContainer<std::vector<int>>, NamedParameter<int>>;
+
+inline auto GenCases()
 {
-    miopenConvolutionDescriptor_t conv_desc;
-
-    auto status = miopenCreateConvolutionDescriptor(&conv_desc);
-    EXPECT_EQ(status, miopenStatusSuccess);
-
-    status = miopenInitConvolutionDescriptor(conv_desc, miopenConvolutionMode_t::miopenConvolution, 0, 0, 1, 1, 1, 1);
-    EXPECT_EQ(status, miopenStatusSuccess);
-
-    const auto& desc = miopen::deref(conv_desc);
-    EXPECT_EQ(desc.attribute.deterministic.Get(), 0);   // The default value should be false
-    EXPECT_TRUE(!desc.attribute.deterministic);         // Check the bool operator
-
-    const int val = 1;
-    status = miopenSetConvolutionAttribute(conv_desc, MIOPEN_CONVOLUTION_ATTRIB_DETERMINISTIC, val);
-    EXPECT_EQ(status, miopenStatusSuccess);
-    EXPECT_EQ(desc.attribute.deterministic.Get(), 1);
-    EXPECT_TRUE(desc.attribute.deterministic);
-
-    int new_val = -1;
-    status = miopenGetConvolutionAttribute(conv_desc, MIOPEN_CONVOLUTION_ATTRIB_DETERMINISTIC, &new_val);
-    EXPECT_EQ(status, miopenStatusSuccess);
-    EXPECT_EQ(val, new_val);
+    return testing::Combine(
+        testing::Values(NamedContainer<std::vector<int>>{"conv_params", {0, 0, 1, 1, 1, 1}, ", "}),
+        testing::Values(NamedParameter<int>("conv_attr", 1)));
 }
+
+inline auto GetCases()
+{
+    static const auto cases = GenCases();
+    return cases;
+}
+
+} // namespace
+
+struct DeterministicConvApiTest : public testing::TestWithParam<TestCase>
+{
+    void SetUp() override { prng::reset_seed(); }
+
+    void Run()
+    {
+        std::vector<int> conv_params;
+        int conv_attr{};
+
+        std::tie(conv_params, conv_attr) = GetParam();
+
+        miopenConvolutionDescriptor_t conv_desc;
+
+        auto status = miopenCreateConvolutionDescriptor(&conv_desc);
+        EXPECT_EQ(status, miopenStatusSuccess);
+
+        status = miopenInitConvolutionDescriptor(conv_desc,
+                                                 miopenConvolutionMode_t::miopenConvolution,
+                                                 conv_params[0],
+                                                 conv_params[1],
+                                                 conv_params[2],
+                                                 conv_params[3],
+                                                 conv_params[4],
+                                                 conv_params[5]);
+        EXPECT_EQ(status, miopenStatusSuccess);
+
+        const auto& desc = miopen::deref(conv_desc);
+        EXPECT_EQ(desc.attribute.deterministic.Get(), 0); // The default value should be false
+        EXPECT_TRUE(!desc.attribute.deterministic);       // Check the bool operator
+
+        status = miopenSetConvolutionAttribute(
+            conv_desc, MIOPEN_CONVOLUTION_ATTRIB_DETERMINISTIC, conv_attr);
+        EXPECT_EQ(status, miopenStatusSuccess);
+        EXPECT_EQ(desc.attribute.deterministic.Get(), 1);
+        EXPECT_TRUE(desc.attribute.deterministic);
+
+        int new_conv_attr = -1;
+        status            = miopenGetConvolutionAttribute(
+            conv_desc, MIOPEN_CONVOLUTION_ATTRIB_DETERMINISTIC, &new_conv_attr);
+        EXPECT_EQ(status, miopenStatusSuccess);
+        EXPECT_EQ(conv_attr, new_conv_attr);
+    }
+};
+
+struct TestNameGenerator
+{
+    std::string operator()(const auto& info)
+    {
+        const auto& [conv_params, conv_attr] = info.param;
+        std::stringstream ss;
+        std::string str;
+
+        ss << "conv_params_" << GetRangeAsString(conv_params(), "_") << "_conv_attr_" << conv_attr()
+           << "_test_id_" << info.index;
+
+        str = ss.str();
+
+        // Name format only supports letters, numbers and underscores.
+        std::transform(str.begin(), str.end(), str.begin(), [](char c) {
+            return (c == '.') ? 'p' : (std::isalnum(c) ? c : '_');
+        });
+
+        return str;
+    }
+};
+
+using CPU_DeterministicConvApi_NONE = DeterministicConvApiTest;
+
+TEST_P(CPU_DeterministicConvApi_NONE, Test) { Run(); }
+
+INSTANTIATE_TEST_SUITE_P(Smoke, CPU_DeterministicConvApi_NONE, GetCases(), TestNameGenerator{});
