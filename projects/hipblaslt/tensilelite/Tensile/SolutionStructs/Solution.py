ffdb6bf5a32cda6ba396094b53632929b109fd47
b-shi <brianshi@amd.com>
2025-12-08 14:57:28 -0600

    Move check to hasCustomSchedule

--- projects/hipblaslt/tensilelite/Tensile/SolutionStructs/Solution.py
+++ projects/hipblaslt/tensilelite/Tensile/SolutionStructs/Solution.py
@@ -1495,9 +1495,7 @@ class Solution(collections.abc.Mapping):
 
     # Check if CMS is available for this solution
     hasCMS,_ = hasCustomSchedule(state)
-    # List of state params which does not support CMS
-    disableCMS = state["UnrollLoopSwapGlobalReadOrder"]
-    state["UseCustomMainLoopSchedule"] = hasCMS and not disableCMS
+    state["UseCustomMainLoopSchedule"] = hasCMS
 
     # 0: Normal mode. Hardware applies all of the normal data dependency checks
     # 1: Full expert mode (not suppoeted yet). Disable hardware checks against: VA_VDST, VA_SDST, VA_SSRC, VA_VCC, VM_VSRC and SA_SDST.
