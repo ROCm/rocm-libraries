9ca3b1f13abeea1e8941a13e0e4571d0ae35b634
James Newling <james.newling@gmail.com>
2025-12-08 11:39:46 -0600

    remove

--- projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
@@ -205,9 +205,6 @@ def verify_global_reads_not_too_earlySingleCodePath(
             waitIndex += 1
 
         if waveCompleteIndex is None:
-            if context.get("kernel", {}).get("SwapGlobalReadOrder", False):
-                GRX == "GRB" if GRX is "GRA" else "GRA"
-
             return False, (
                 f"Failed to verify that all local reads for {operand} ({LRX}) are complete "
                 f"before the first global read for {operand} is issued. "
