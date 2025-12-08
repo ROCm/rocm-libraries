13952ee9fc1abc853b561613519952aa61c565ae
David Dixon <165835255+davidd-amd@users.noreply.github.com>
2025-12-08 13:02:29 -0700

    Output more debug info on failed tests

--- shared/tensile/.jenkins/common.groovy
+++ shared/tensile/.jenkins/common.groovy
@@ -124,7 +124,18 @@ def runTestCommand(platform, project, jobName, testMark, boolean runHostTest=tru
 
             if ${runHostTest}; then
               pushd build
-              ./TensileTests ${markSkipExtendedTest} --gtest_color=yes
+              echo "Running full test suite..."
+              ./TensileTests --gtest_output=xml:tensile-tests.xml ${markSkipExtendedTest} --gtest_color=yes
+              FAILED_TESTS=$(grep -oP 'testcase name="\K[^"]+(?=".*?status="run"[^>]*?result="failed")?' tensile-tests.xml)
+              if [ -z "$FAILED_TESTS" ]; then
+                  echo "All tests passed. 🚀"
+              else
+                echo "Rerunning failed tests with debug info..."
+                for test in $FAILED_TESTS; do
+                    echo "... $test"
+                    TENSILE_DC=0x8040 ./TensileTests --gtest_output=xml:tensile-tests.xml ${markSkipExtendedTest} --gtest_color=yes
+                done
+              fi
               check_err
               popd
             fi
