b1e16cb3cbdbfb54d984dbcb43043403be21007a
kokolchin <kokolchin@luxoft.com>
2025-12-11 11:54:08 +0100

    Update pooling3d test to comply with GTest development guidelines
    - Change named namespace to anonymous namespace
    - Use TEST_P with INSTANTIATE_TEST_SUITE_P(Full, ...)
    - Add SetUp() override for prng::reset_seed() and device compatibility check
    - Change test classes to inherit from TestWithParam<miopenDataType_t>
    - Move device compatibility check to SetUp() for early skipping
    - Remove duplicate prng::reset_seed() call from RunPooling3dTests()
    - Use testing::ValuesIn() for parameter generation

--- projects/miopen/test/gtest/pooling3d.cpp
+++ projects/miopen/test/gtest/pooling3d.cpp
@@ -1,28 +1,5 @@
-/*******************************************************************************
- *
- * MIT License
- *
- * Copyright (c) 2025 Advanced Micro Devices, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- *******************************************************************************/
+// Copyright © Advanced Micro Devices, Inc., or its affiliates.
+// SPDX-License-Identifier:  MIT
 
 #include <gtest/gtest.h>
 #include "get_handle.hpp"
@@ -31,14 +8,30 @@
 #include "../driver.hpp"
 #include <half/half.hpp>
 
-namespace pooling3d {
+namespace {
 
-class GPU_Pooling3d_FP32 : public testing::Test
+class GPU_Pooling3d_FP32 : public testing::TestWithParam<miopenDataType_t>
 {
+    void SetUp() override
+    {
+        prng::reset_seed();
+        if(!IsTestSupportedByDevice(Gpu::All))
+        {
+            GTEST_SKIP();
+        }
+    }
 };
 
-class GPU_Pooling3d_FP16 : public testing::Test
+class GPU_Pooling3d_FP16 : public testing::TestWithParam<miopenDataType_t>
 {
+    void SetUp() override
+    {
+        prng::reset_seed();
+        if(!IsTestSupportedByDevice(Gpu::All))
+        {
+            GTEST_SKIP();
+        }
+    }
 };
 
 template <typename T>
@@ -56,7 +49,6 @@ void RunPooling3dTests()
         data_args.push_back(&arg);
     }
 
-    prng::reset_seed();
     driver.iteration = 0;
     try
     {
@@ -96,23 +88,18 @@ void Run3dDriver(miopenDataType_t prec)
     }
 };
 
-} // namespace pooling3d
-using namespace pooling3d;
+} // namespace
 
-TEST_F(GPU_Pooling3d_FP32, FloatTest_pooling3d)
+TEST_P(GPU_Pooling3d_FP32, FloatTest_pooling3d)
 {
-    if(!IsTestSupportedByDevice(Gpu::All))
-    {
-        GTEST_SKIP();
-    }
-    Run3dDriver(miopenFloat);
+    Run3dDriver(GetParam());
 }
 
-TEST_F(GPU_Pooling3d_FP16, HalfTest_pooling3d)
+TEST_P(GPU_Pooling3d_FP16, HalfTest_pooling3d)
 {
-    if(!IsTestSupportedByDevice(Gpu::All))
-    {
-        GTEST_SKIP();
-    }
-    Run3dDriver(miopenHalf);
+    Run3dDriver(GetParam());
 }
+
+INSTANTIATE_TEST_SUITE_P(Full, GPU_Pooling3d_FP32, testing::ValuesIn({miopenFloat}));
+
+INSTANTIATE_TEST_SUITE_P(Full, GPU_Pooling3d_FP16, testing::ValuesIn({miopenHalf}));
