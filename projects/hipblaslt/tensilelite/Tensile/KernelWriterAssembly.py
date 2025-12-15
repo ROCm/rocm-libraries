0742851a4bb355b2cf5168d6b4a99eb96c7dba2f
b-shi <brianshi@amd.com>
2025-12-15 04:02:09 -0600

    Move waitcnt before init loop begin label

--- projects/hipblaslt/tensilelite/Tensile/KernelWriterAssembly.py
+++ projects/hipblaslt/tensilelite/Tensile/KernelWriterAssembly.py
@@ -14023,13 +14023,9 @@ class KernelWriterAssembly(KernelWriter):
 
     loopChar = self.states.indexChars[kernel["ProblemType"]["IndicesSummation"][self.states.unrollIdx]]
 
-    # Wait for all LR in pre-loop to complete before we start main loop
-    module.add(SWaitCnt(dscnt=0, vlcnt=-1, vscnt=-1, comment="Wait for all LR in pre-loop to complete"))
-
     if numCodePath == 1:
-      module.add(Label("LoopBegin%s_0"%(loopChar), "" ))
       module.add(MacroInstruction(name="MAINLOOP", args=[0]))
-      module.add(SCBranchSCC0(labelName="label_LoopBegin%s_0"%(loopChar), comment="" ))
+      module.add(SCBranchSCC0(labelName="label_LoopBegin%s"%(loopChar), comment="" ))
       module.add(Label("LoopEnd%s"%(loopChar), "" ))
       return module
 
