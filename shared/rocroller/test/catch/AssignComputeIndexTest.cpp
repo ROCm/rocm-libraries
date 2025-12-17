9de22cd2496633cb2095a9c2cf5f0fd144b0bc45
Caio Salvador Rohwedder <Caio.SalvadorRohwedder@amd.com>
2025-12-17 20:00:56 +0000

    Only check if ComputeIndex nodes are removed in AssignComputeIndexTest

--- shared/rocroller/test/catch/AssignComputeIndexTest.cpp
+++ shared/rocroller/test/catch/AssignComputeIndexTest.cpp
@@ -67,33 +67,6 @@ TEST_CASE("AssignComputeIndex", "[kernel-graph]")
         graph = graph.transform(xform);
     }
 
-    auto verifyAssignComputeIndex = [&graph](int tag) {
-        auto base = graph.mapper.get(
-            tag, Connections::ComputeIndex{Connections::ComputeIndexArgument::BASE});
-        auto offset = graph.mapper.get(
-            tag, Connections::ComputeIndex{Connections::ComputeIndexArgument::OFFSET});
-        auto stride = graph.mapper.get(
-            tag, Connections::ComputeIndex{Connections::ComputeIndexArgument::STRIDE});
-        auto opTag = tag;
-
-        if(base < 0 && offset > 0)
-        {
-            auto candidate = graph.control.getOutputNodeIndices<Sequence>(opTag).to<std::vector>();
-            opTag          = candidate[0];
-            AssertFatal(candidate.size() == 1);
-            auto dest = graph.mapper.get(candidate[0], NaryArgument::DEST);
-            CHECK(dest == offset);
-        }
-
-        if(stride > 0)
-        {
-            auto candidate = graph.control.getOutputNodeIndices<Sequence>(opTag).to<std::vector>();
-            AssertFatal(candidate.size() == 1);
-            auto dest = graph.mapper.get(candidate[0], NaryArgument::DEST);
-            CHECK(dest == stride);
-        }
-    };
-
     // search ComputeIndex operations
     auto isComputeIndexPredicate
         = [&graph](int x) { return graph.control.get<ComputeIndex>(x).has_value(); };
@@ -101,9 +74,6 @@ TEST_CASE("AssignComputeIndex", "[kernel-graph]")
         = graph.control.findNodes(*graph.control.roots().begin(), isComputeIndexPredicate)
               .to<std::vector>();
 
-    // verify the assign operations connect to the correct offset/stride coordinate
-    for(const auto& tag : candidates)
-    {
-        verifyAssignComputeIndex(tag);
-    }
+    // no ComputeIndex operations should be left
+    CHECK(candidates.size() == 0);
 }
