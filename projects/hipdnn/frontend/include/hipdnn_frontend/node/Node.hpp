c1825386c5c78c1fd62cd87c04debabc4c37c92f
Jeremy Hart <jeremy.hart@amd.com>
2025-12-18 17:52:20 +0000

    Remove test code

--- projects/hipdnn/frontend/include/hipdnn_frontend/node/Node.hpp
+++ projects/hipdnn/frontend/include/hipdnn_frontend/node/Node.hpp
@@ -7,7 +7,6 @@
 #include <hipdnn_frontend/attributes/GraphAttributes.hpp>
 #include <hipdnn_frontend/attributes/TensorAttributes.hpp>
 #include <hipdnn_sdk/data_objects/graph_generated.h>
-#include <iostream>
 #include <memory>
 #include <unordered_map>
 #include <unordered_set>
@@ -58,7 +57,6 @@ public:
 
     virtual std::vector<std::shared_ptr<TensorAttributes>> getNodeOutputTensorAttributes() const
     {
-        std::cout << "INode::getNodeOutputTensorAttributes()\n";
         return {};
     }
 
@@ -198,8 +196,6 @@ public:
                     "Node " + self().attributes.name + " does not have a compute_data_type set"};
         }
 
-        std::cout << "outputs.size() = " << this->getNodeOutputTensorAttributes().size() << "\n";
-
         for(const auto& tensorAttr : this->getNodeOutputTensorAttributes())
         {
             HIPDNN_CHECK_ERROR(tensorAttr->validate());
@@ -224,7 +220,6 @@ public:
 
     std::vector<std::shared_ptr<TensorAttributes>> getNodeOutputTensorAttributes() const override
     {
-        std::cout << "BaseNode::getNodeOutputTensorAttributes()\n";
         std::vector<std::shared_ptr<TensorAttributes>> outputAttributes;
         for(auto& tensorAttrPair : self().attributes.outputs)
         {
