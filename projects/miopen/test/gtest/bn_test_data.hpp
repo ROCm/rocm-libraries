ad9ddde08aa05fddc6fd09f35c3bef8d57cdc24d
milo <milo@streamhpc.com>
2025-12-19 13:40:16 +0000

    test: Add V3 API test infrastructure
    Add testBNAPIV3 enum, separate prev/next test buffers, and 86 test
    instantiations across Small2D and Large2D FP32 suites.

--- projects/miopen/test/gtest/bn_test_data.hpp
+++ projects/miopen/test/gtest/bn_test_data.hpp
@@ -498,6 +498,11 @@ struct BNFwdTrainTestData : public BNTestData<XDataType, YDataType, AccDataType,
     tensor<RunSaveDataType> saveVariance;
     tensor<RunSaveDataType> runMean;
     tensor<RunSaveDataType> runVariance;
+    // V3 API: separate prev/next buffers for ping-pong
+    tensor<RunSaveDataType> prevRunMean;
+    tensor<RunSaveDataType> prevRunVariance;
+    tensor<RunSaveDataType> nextRunMean;
+    tensor<RunSaveDataType> nextRunVariance;
 
     tensor<AccDataType> saveMean_ref;
     tensor<AccDataType> saveVariance_ref;
@@ -510,6 +515,11 @@ struct BNFwdTrainTestData : public BNTestData<XDataType, YDataType, AccDataType,
     miopen::Allocator::ManageDataPtr saveVariance_dev;
     miopen::Allocator::ManageDataPtr runMean_dev;
     miopen::Allocator::ManageDataPtr runVariance_dev;
+    // V3 API: separate prev/next device buffers
+    miopen::Allocator::ManageDataPtr prevRunMean_dev;
+    miopen::Allocator::ManageDataPtr prevRunVariance_dev;
+    miopen::Allocator::ManageDataPtr nextRunMean_dev;
+    miopen::Allocator::ManageDataPtr nextRunVariance_dev;
     double epsilon       = 1.0e-5;
     double averageFactor = 0.1;
     float alpha          = 1.0f;
@@ -544,6 +554,19 @@ private:
         runVariance = tensor<RunSaveDataType>{
             BNTestData<XDataType, YDataType, AccDataType, TConfig>::tensor_layout,
             derivedBnDesc.GetLengths()};
+        // V3 API: separate prev/next buffers
+        prevRunMean = tensor<RunSaveDataType>{
+            BNTestData<XDataType, YDataType, AccDataType, TConfig>::tensor_layout,
+            derivedBnDesc.GetLengths()};
+        prevRunVariance = tensor<RunSaveDataType>{
+            BNTestData<XDataType, YDataType, AccDataType, TConfig>::tensor_layout,
+            derivedBnDesc.GetLengths()};
+        nextRunMean = tensor<RunSaveDataType>{
+            BNTestData<XDataType, YDataType, AccDataType, TConfig>::tensor_layout,
+            derivedBnDesc.GetLengths()};
+        nextRunVariance = tensor<RunSaveDataType>{
+            BNTestData<XDataType, YDataType, AccDataType, TConfig>::tensor_layout,
+            derivedBnDesc.GetLengths()};
         // ref
         saveMean_ref = tensor<AccDataType>{
             BNTestData<XDataType, YDataType, AccDataType, TConfig>::tensor_layout,
@@ -568,6 +591,12 @@ private:
             uniform_signed_initializer<RunSaveDataType>(2e-3 /*scale*/, 1000 /*range*/));
         runVariance.generate(
             uniform_signed_initializer<RunSaveDataType>(2e-3 /*scale*/, 1000 /*range*/));
+        // V3 API: initialize prev buffers with same values as runMean/runVariance
+        prevRunMean.generate(
+            uniform_signed_initializer<RunSaveDataType>(2e-3 /*scale*/, 1000 /*range*/));
+        prevRunVariance.generate(
+            uniform_signed_initializer<RunSaveDataType>(2e-3 /*scale*/, 1000 /*range*/));
+        // nextRunMean and nextRunVariance will be output buffers (don't need initialization)
 
         std::transform(saveMean.data.begin(),
                        saveMean.data.end(),
@@ -595,5 +624,11 @@ private:
         saveVariance_dev = handle.Write(saveVariance.data);
         runMean_dev      = handle.Write(runMean.data);
         runVariance_dev  = handle.Write(runVariance.data);
+        // V3 API: write prev buffers to GPU
+        prevRunMean_dev     = handle.Write(prevRunMean.data);
+        prevRunVariance_dev = handle.Write(prevRunVariance.data);
+        // Allocate next buffers (output only, no need to initialize data)
+        nextRunMean_dev     = handle.Create<RunSaveDataType>(prevRunMean.data.size());
+        nextRunVariance_dev = handle.Create<RunSaveDataType>(prevRunVariance.data.size());
     }
 };
