eabd9d8f667ed7aad9bef1ca69523b5d2a3e5bce
Brent Maas <brent.maas@streamhpc.com>
2025-12-19 10:13:20 +0000

    Fix clang-format failure, conflicts with pre-commit

--- projects/miopen/test/rnn_seq_api.hpp
+++ projects/miopen/test/rnn_seq_api.hpp
@@ -147,7 +147,8 @@ struct verify_rnn_api_base
     {
         std::cout << "./bin/MIOpenDriver rnn_seq ";
 
-        std::cout << " -F 0 " << " -m ";
+        std::cout << " -F 0 "
+                  << " -m ";
 
         switch(rnnDesc.rnnMode)
         {
@@ -311,7 +312,7 @@ struct rnn_ref
                           std::vector<T>& workSpace,
                           bool nohx) const = 0;
 
-    virtual ~rnn_ref() {};
+    virtual ~rnn_ref(){};
 };
 
 template <class T>
