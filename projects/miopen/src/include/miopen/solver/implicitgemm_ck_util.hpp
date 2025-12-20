fec320f57e3f955be7480dfbac02a53a2a69c163
Brad Pepers <brad.pepers@amd.com>
2025-12-20 11:32:36 -0700

    Fix issue with looping through split-k values and make it simpler for more explicit

--- projects/miopen/src/include/miopen/solver/implicitgemm_ck_util.hpp
+++ projects/miopen/src/include/miopen/solver/implicitgemm_ck_util.hpp
@@ -54,25 +54,6 @@ namespace solver {
 
 static constexpr int CkSplitkAutoDeduce = -1;
 
-template <int L, int H>
-inline static bool NextCKSplitkValue(int& v)
-{
-    assert((IsTwoPower<L, H>(v) || v == CkSplitkAutoDeduce));
-    if(v == H)
-    {
-        v = CkSplitkAutoDeduce;
-        return false;
-    }
-    if(v == CkSplitkAutoDeduce)
-    {
-        v = L;
-        return true;
-    }
-
-    v *= 2;
-    return false;
-}
-
 #if MIOPEN_BACKEND_HIP && MIOPEN_USE_COMPOSABLEKERNEL
 
 namespace conv {
@@ -335,10 +316,8 @@ size_t GetCKSplitkMaxWorkspaceSize(const ProblemDescriptionType& problem)
     const auto ptrs = DeviceOpType::GetInstances();
     for(auto& ptr : ptrs)
     {
-        // Cycle `split_k` over {1,2,4,...,128} then `CkSplitkAutoDeduce`.
-        // The loop then restarts from 1 for the next conv instance.
-        auto split_k = 1;
-        do
+        // first try powers of 2 split-k from 1..128
+        for(int split_k = 1; split_k <= 128; split_k *= 2)
         {
             if(args.IsSupportedBySplitK(ptr, split_k))
             {
@@ -346,7 +325,15 @@ size_t GetCKSplitkMaxWorkspaceSize(const ProblemDescriptionType& problem)
                 if(workspace_size > max_workspace_size)
                     max_workspace_size = workspace_size;
             }
-        } while(!NextCKSplitkValue<1, 128>(split_k));
+        }
+
+        // and now try the auto deduce split-k
+        if(args.IsSupportedBySplitK(ptr, CkSplitkAutoDeduce))
+        {
+            auto workspace_size = args.GetCKSplitkWorkspaceSize(ptr, CkSplitkAutoDeduce);
+            if(workspace_size > max_workspace_size)
+                    max_workspace_size = workspace_size;
+        }
     }
 
     MIOPEN_LOG_I("Max workspace size reported by CK: " << max_workspace_size);
