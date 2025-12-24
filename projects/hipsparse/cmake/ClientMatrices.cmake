39edb891eab390d2af5ba9a0896091a03c7adb0c
Ibrahim Wani <113864060+ibrahimw1@users.noreply.github.com>
2025-12-24 17:00:01 +0000

    Asan + static build fix 2

--- projects/hipsparse/cmake/ClientMatrices.cmake
+++ projects/hipsparse/cmake/ClientMatrices.cmake
@@ -63,7 +63,7 @@ file(MAKE_DIRECTORY "${PROJECT_BINARY_DIR}")
 file(MAKE_DIRECTORY "${PROJECT_BINARY_DIR}/clients")
 
 if(HIPSPARSE_ENABLE_ASAN)
-  execute_process(COMMAND "${CMAKE_CXX_COMPILER}" "${CONVERT_SOURCE}" -O3 -fsanitize=address -o "${PROJECT_BINARY_DIR}/clients/mtx2csr.exe"
+  execute_process(COMMAND "${CMAKE_CXX_COMPILER}" "${CONVERT_SOURCE}" -O3 -fsanitize=address -static-libasan -o "${PROJECT_BINARY_DIR}/clients/mtx2csr.exe"
     RESULT_VARIABLE STATUS)
 else()
   execute_process(COMMAND "${CMAKE_CXX_COMPILER}" "${CONVERT_SOURCE}" -O3 -o "${PROJECT_BINARY_DIR}/clients/mtx2csr.exe"
