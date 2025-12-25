9bf68f9abad664048eec6805458e1f868d78c216
root <root@smci355-ccs-aus-m11-13.cs-aus.dcgpu>
2025-12-25 08:37:36 +0000

    Revert "Replace 2 movs with 1 s_nop"
    This reverts commit 35633fdcdee2eb20c54360d0899540f387e5856a.

--- projects/hipblaslt/tensilelite/Tensile/Components/LocalRead.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/LocalRead.py
@@ -428,7 +428,8 @@ class LocalReadMFMA(LocalRead):
                                     packCodeT.add(VSubF32(dst=v3t, src0=v3t, src1=vgpr(tmp), comment="end"))
 
                                     if kernel["UseDot2F32XEmulation"]:
-                                        packCodeT.add(SNop(waitState=0, comment=""))
+                                        packCodeT.add(VMovB32(dst=vgpr(tmp), src=0))
+                                        packCodeT.add(VMovB32(dst=vgpr(tmp), src=0))
 
                                     for val in tmpvgpr:
                                         writer.vgprPool.checkIn(val)
