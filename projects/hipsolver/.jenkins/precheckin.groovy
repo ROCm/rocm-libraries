c1724ba00d6634a26f135ddee73b6e4234e2389a
John Robbins <John.Robbins@amd.com>
2026-01-08 09:57:34 -0700

    [hipsolver][jenkins] always set and use common properties

--- projects/hipsolver/.jenkins/precheckin.groovy
+++ projects/hipsolver/.jenkins/precheckin.groovy
@@ -60,8 +60,10 @@ def setupCI(urlJobName, jobNameList, buildCommand, runCI, label)
     jobNameList.each
     {
         jobName, nodeDetails->
-        if (urlJobName == jobName)
+        if (urlJobName == jobName) {
+            properties(auxiliary.addCommonProperties())
             runCI(nodeDetails, jobName, buildCommand, label)
+        }
     }
 
     // For url job names that are not listed by the jobNameList i.e. compute-rocm-dkms-no-npi-1901
