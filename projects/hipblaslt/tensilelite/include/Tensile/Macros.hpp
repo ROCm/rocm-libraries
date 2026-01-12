61a63687080f45fc76da6dee27681fb40ad6f05b
David Dixon <165835255+davidd-amd@users.noreply.github.com>
2026-01-12 09:40:12 -0700

    Fix typo
    Co-authored-by: Braden Stefanuk <121893577+bstefanuk@users.noreply.github.com>

--- projects/hipblaslt/tensilelite/include/Tensile/Macros.hpp
+++ projects/hipblaslt/tensilelite/include/Tensile/Macros.hpp
@@ -7,6 +7,6 @@
 // is no context where we would ever build tensile as a shared library so we are unconditionally
 // defining it as empty and in the build system we define the visibility flags. In the future, if we
 // ever build tensile as a shared library, we will need to use CMakes generate_export_header macro
-// to provide this functionality rather than using an roll-your-own approach.
+// to provide this functionality rather than using a roll-your-own approach.
 
 #define TENSILE_API
