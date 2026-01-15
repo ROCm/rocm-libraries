7068ce80c058ea4e9e70485f8c92ba6e595ef35a
Joao P. L. de Carvalho <joao.carvalho@amd.com>
2026-01-15 19:41:53 +0000

    Delete AddBarrierVisitor in AddPrefetch transform

--- shared/rocroller/lib/source/KernelGraph/Transformations/AddPrefetch.cpp
+++ shared/rocroller/lib/source/KernelGraph/Transformations/AddPrefetch.cpp
@@ -320,28 +320,6 @@ namespace rocRoller
         using namespace AddPrefetchDetail;
 
         /**
-         * Add Barrier transformer.
-         *
-         * Adds Barrier operations for non-prefetched load/store
-         * operations that go through LDS.
-         */
-        struct AddBarrierVisitor
-        {
-            AddBarrierVisitor(ContextPtr context)
-                : m_context(context)
-            {
-            }
-
-            void stage(KernelGraph const&, int);
-            void commit(KernelGraph&);
-
-        private:
-            std::set<int> m_operations;
-
-            ContextPtr m_context;
-        };
-
-        /**
          * Add prefetch transformer.
          */
         struct AddPrefetchVisitor
@@ -386,50 +364,6 @@ namespace rocRoller
             void trackStores(KernelGraph const& graph, int start);
         };
 
-        void AddBarrierVisitor::stage(KernelGraph const& graph, int opTag)
-        {
-            m_operations.insert(opTag);
-        }
-
-        void AddBarrierVisitor::commit(KernelGraph& graph)
-        {
-            for(auto tag : m_operations)
-            {
-                auto maybeStoreLDSTile = graph.control.get<StoreLDSTile>(tag);
-                if(maybeStoreLDSTile)
-                {
-                    auto preBarrier  = graph.control.addElement(Barrier());
-                    auto postBarrier = graph.control.addElement(Barrier());
-
-                    insertBefore(graph, tag, preBarrier, preBarrier);
-                    insertAfter(graph, tag, postBarrier, postBarrier);
-
-                    auto ldsTileTag = graph.mapper.get<LDS>(tag);
-                    // pre-operation barrier to handle loop entry and loop-carried dependencies.
-                    // post-operation barrier to handle forward dependency.
-                    graph.mapper.connect<LDS>(preBarrier, ldsTileTag, 0);
-                    graph.mapper.connect<LDS>(postBarrier, ldsTileTag, 0);
-                }
-                auto maybeLoadTile = graph.control.get<LoadTiled>(tag);
-                if(maybeLoadTile)
-                {
-                    auto tileTag = graph.mapper.get<MacroTile>(tag);
-                    AssertFatal(tileTag != -1);
-                    auto tile = graph.coordinates.get<MacroTile>(tileTag).value();
-                    AssertFatal(tile.memoryType == MemoryType::WAVE_Direct2LDS);
-
-                    auto preBarrier = graph.control.addElement(Barrier());
-
-                    insertBefore(graph, tag, preBarrier, preBarrier);
-
-                    auto ldsTileTag = graph.mapper.get<LDS>(tag);
-                    // pre-operation barrier to handle loop entry and loop-carried dependencies.
-                    // post-operation barrier is currently added by AddPrefetchVisitor.
-                    graph.mapper.connect<LDS>(preBarrier, ldsTileTag, 0);
-                }
-            }
-        }
-
         void AddPrefetchVisitor::trackStores(KernelGraph const& graph, int start)
         {
             for(auto tag : graph.control.depthFirstVisit(start, GD::Downstream))
@@ -462,46 +396,6 @@ namespace rocRoller
 
                 storeHasBarrierAlready = visitor.storeLDSTileOperations();
             }
-
-            auto barrierVisitor = AddBarrierVisitor(m_context);
-            for(auto const& tag : graph.control.getNodes<StoreLDSTile>())
-            {
-                if(!storeHasBarrierAlready.contains(tag))
-                    barrierVisitor.stage(graph, tag);
-            }
-
-            // If we aren't prefetching, we need to put barriers
-            // before Direct2LDS loads as well.  For example, this:
-            //
-            //   for loop:
-            //      buffer_load lds
-            //      s_barrier
-            //      ds_read     <-- an eager wave could wrap around and write into lds
-            //      v_mfma
-            //
-            // needs to be:
-            //
-            //   for loop:
-            //      s_barrier
-            //      buffer_load lds
-            //      s_barrier
-            //      ds_read
-            //      v_mfma
-            if(not m_params->prefetch)
-            {
-                for(auto const& tag : graph.control.getNodes<LoadTiled>())
-                {
-                    auto tileTag = graph.mapper.get<MacroTile>(tag);
-                    if(tileTag == -1)
-                        continue;
-                    auto tile = graph.coordinates.get<MacroTile>(tileTag).value();
-                    if(tile.memoryType != MemoryType::WAVE_Direct2LDS)
-                        continue;
-                    barrierVisitor.stage(graph, tag);
-                }
-            }
-
-            barrierVisitor.commit(graph);
 
             removeRedundantSequenceEdges(graph);
 
