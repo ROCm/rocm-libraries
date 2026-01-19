96b586fa4181eb9c1c7605e313a73d7a6e803b7e
Ibrahim Wani <113864060+ibrahimw1@users.noreply.github.com>
2026-01-19 20:51:17 +0000

    Integrate PR comments

--- shared/origami/tests/CTestTestfile.cmake
+++ shared/origami/tests/CTestTestfile.cmake
@@ -1,27 +1,5 @@
-################################################################################
-#
-# MIT License
-#
-# Copyright 2025 AMD ROCm(TM) Software
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell cop-
-# ies of the Software, and to permit persons to whom the Software is furnished
-# to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IM-
-# PLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNE-
-# CTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-################################################################################
+# Copyright Advanced Micro Devices, Inc., or its affiliates.
+# SPDX-License-Identifier: MIT
 
 # CTestTestfile for installed Origami tests
 # This file is installed to bin/origami/ for TheRock CI testing
@@ -30,8 +8,8 @@
 add_test(origami-tests "../origami-tests")
 
 # Python tests
-add_test(origami_python_test python3 "origami_test.py")
-add_test(origami_python_grid_test python3 "origami_grid_test.py")
+add_test(origami_python_test python "origami_test.py")
+add_test(origami_python_grid_test python "origami_grid_test.py")
 
 # Set environment for Python tests to find liborigami.so and the Python module
 # Paths are relative to bin/origami/ where CTestTestfile.cmake is installed
