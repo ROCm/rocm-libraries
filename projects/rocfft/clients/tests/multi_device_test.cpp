cf9b3f80229652bab4213eeaf1df704667fb489a
regan-amd <raphael.egan@amd.com>
2026-01-21 20:38:33 +0000

    [rocfft-test] enable all placement and transform directions in multi-device unit tests

--- projects/rocfft/clients/tests/multi_device_test.cpp
+++ projects/rocfft/clients/tests/multi_device_test.cpp
@@ -85,30 +85,19 @@ std::vector<fft_params> param_generator_multi_gpu(const SplitType type, const in
 
     for(auto run_callbacks : {false, true})
     {
-        auto params = param_generator_complex(test_prob,
-                                              multi_gpu_sizes,
-                                              precision_range_sp_dp,
-                                              multi_gpu_batch_range,
-                                              stride_generator(stride_range),
-                                              stride_generator(stride_range),
-                                              ioffset_range_zero,
-                                              ooffset_range_zero,
-                                              {fft_placement_inplace, fft_placement_notinplace},
-                                              false,
-                                              run_callbacks);
-        std::copy(params.begin(), params.end(), std::back_inserter(params_single));
-
-        params = param_generator_real(test_prob,
-                                      multi_gpu_sizes,
-                                      precision_range_sp_dp,
-                                      multi_gpu_batch_range,
-                                      stride_generator(stride_range),
-                                      stride_generator(stride_range),
-                                      ioffset_range_zero,
-                                      ooffset_range_zero,
-                                      {fft_placement_notinplace},
-                                      false,
-                                      run_callbacks);
+        auto params = param_generator_base(test_prob,
+                                           trans_type_range_full,
+                                           multi_gpu_sizes,
+                                           precision_range_sp_dp,
+                                           multi_gpu_batch_range,
+                                           generate_types,
+                                           stride_generator(stride_range),
+                                           stride_generator(stride_range),
+                                           ioffset_range_zero,
+                                           ooffset_range_zero,
+                                           place_range,
+                                           false,
+                                           run_callbacks);
         std::copy(params.begin(), params.end(), std::back_inserter(params_single));
     }
 
