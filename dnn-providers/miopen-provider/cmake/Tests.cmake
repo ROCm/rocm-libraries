f4659dd77c4d203bb3c231603e2d1141447fd203
Samuel Reeder <samuel.reeder@amd.com>
2026-01-22 19:23:20 +0000

    Update to duplicate targets

--- dnn-providers/miopen-provider/cmake/Tests.cmake
+++ dnn-providers/miopen-provider/cmake/Tests.cmake
@@ -249,4 +249,11 @@ function(install_miopen_plugin_ctest_files)
             DESTINATION ${MIOPEN_PLUGIN_CTEST_FILE_INSTALL_PATH} RENAME CTestTestfile.cmake
     )
 
+    # Backwards compatibility: create symlink at legacy path for TheRock until submodule is bumped
+    install(CODE "
+        execute_process(COMMAND \${CMAKE_COMMAND} -E create_symlink
+            miopen_plugin
+            \$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/miopen_legacy_plugin)
+    ")
+
 endfunction() # install_miopen_plugin_ctest_files
