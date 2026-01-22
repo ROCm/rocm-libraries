7d7a50a3ef0af42512f49150df29848c95a1d236
Brian Harrison <brian.harrison@amd.com>
2026-01-21 20:16:03 -0500

    Update to link to GH issue for workaround

--- dnn-providers/miopen-provider/integration_tests/IntegrationGpuConvBackwardWeights.cpp
+++ dnn-providers/miopen-provider/integration_tests/IntegrationGpuConvBackwardWeights.cpp
@@ -19,9 +19,9 @@ using namespace hipdnn_test_sdk::utilities;
 using namespace miopen_legacy_plugin::test_utilities;
 using namespace test_conv_common;
 
-// Using 4e-1 for bfloat16 tolerance instead of conv::getToleranceWrw<hip_bfloat16>()
-// due to non-deterministic kernel results from CK leading to a high tolerance requirement for MIOpen tests.
-#define WRW_BFP16_TOLERANCE 4e-1_bf
+// Workaround https://github.com/ROCm/rocm-libraries/issues/4010 by using 4e-1
+// for bfloat16 tolerance instead of conv::getToleranceWrw<hip_bfloat16>()
+#define WORKAROUND_GH_ISSUE_4010 4e-1_bf
 
 namespace
 {
@@ -104,12 +104,12 @@ TEST_P(IntegrationGpuConvWrwDataNcdhwFp32, Correctness)
 
 TEST_P(IntegrationGpuConvWrwDataNchwBfp16, Correctness)
 {
-    runGraphTest(WRW_BFP16_TOLERANCE, TensorLayout::NCHW);
+    runGraphTest(WORKAROUND_GH_ISSUE_4010, TensorLayout::NCHW);
 }
 
 TEST_P(IntegrationGpuConvWrwDataNcdhwBfp16, Correctness)
 {
-    runGraphTest(WRW_BFP16_TOLERANCE, TensorLayout::NCDHW);
+    runGraphTest(WORKAROUND_GH_ISSUE_4010, TensorLayout::NCDHW);
 }
 
 TEST_P(IntegrationGpuConvWrwDataNchwFp16, Correctness)
@@ -134,12 +134,12 @@ TEST_P(IntegrationGpuConvWrwDataNdhwcFp32, Correctness)
 
 TEST_P(IntegrationGpuConvWrwDataNhwcBfp16, Correctness)
 {
-    runGraphTest(WRW_BFP16_TOLERANCE, TensorLayout::NHWC);
+    runGraphTest(WORKAROUND_GH_ISSUE_4010, TensorLayout::NHWC);
 }
 
 TEST_P(IntegrationGpuConvWrwDataNdhwcBfp16, Correctness)
 {
-    runGraphTest(WRW_BFP16_TOLERANCE, TensorLayout::NDHWC);
+    runGraphTest(WORKAROUND_GH_ISSUE_4010, TensorLayout::NDHWC);
 }
 
 TEST_P(IntegrationGpuConvWrwDataNhwcFp16, Correctness)
