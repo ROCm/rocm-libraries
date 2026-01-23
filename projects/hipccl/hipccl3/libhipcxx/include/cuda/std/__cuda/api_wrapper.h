NVIDIA/cccl

Guard inclusion of `cuda_runtime_api` by using a cuda compiler (#2704)

libcudacxx/include/cuda/std/__cuda/api_wrapper.h
@@ -21,30 +21,35 @@
 #  pragma system_header
 #endif // no system header
 
-#if !defined(_CCCL_CUDA_COMPILER_NVCC) && !defined(_CCCL_CUDA_COMPILER_NVHPC)
+#if defined(_CCCL_CUDA_COMPILER_CLANG)
 #  include <cuda_runtime_api.h>
-#endif // !_CCCL_CUDA_COMPILER_NVCC && !_CCCL_CUDA_COMPILER_NVHPC
+#endif // _CCCL_CUDA_COMPILER_CLANG
 
 #include <cuda/std/__exception/cuda_error.h>
 
-#define _CCCL_TRY_CUDA_API(_NAME, _MSG, ...)           \
-  {                                                    \
-    const ::cudaError_t __status = _NAME(__VA_ARGS__); \
-    switch (__status)                                  \
-    {                                                  \
-      case ::cudaSuccess:                              \
-        break;                                         \
-      default:                                         \
-        ::cudaGetLastError();                          \
-        ::cuda::__throw_cuda_error(__status, _MSG);    \
-    }                                                  \
-  }
+#if defined(_CCCL_CUDA_COMPILER)
+#  define _CCCL_TRY_CUDA_API(_NAME, _MSG, ...)           \
+    {                                                    \
+      const ::cudaError_t __status = _NAME(__VA_ARGS__); \
+      switch (__status)                                  \
+      {                                                  \
+        case ::cudaSuccess:                              \
+          break;                                         \
+        default:                                         \
+          ::cudaGetLastError();                          \
+          ::cuda::__throw_cuda_error(__status, _MSG);    \
+      }                                                  \
+    }
 
-#define _CCCL_ASSERT_CUDA_API(_NAME, _MSG, ...)        \
-  {                                                    \
-    const ::cudaError_t __status = _NAME(__VA_ARGS__); \
-    _CCCL_ASSERT(__status == cudaSuccess, _MSG);       \
-    (void) __status;                                   \
-  }
+#  define _CCCL_ASSERT_CUDA_API(_NAME, _MSG, ...)        \
+    {                                                    \
+      const ::cudaError_t __status = _NAME(__VA_ARGS__); \
+      _CCCL_ASSERT(__status == cudaSuccess, _MSG);       \
+      (void) __status;                                   \
+    }
+#else // ^^^ _CCCL_CUDA_COMPILER ^^^ / vvv !_CCCL_CUDA_COMPILER vvv
+#  define _CCCL_TRY_CUDA_API(_NAME, _MSG, ...)
+#  define _CCCL_ASSERT_CUDA_API(_NAME, _MSG, ...)
+#endif // !_CCCL_CUDA_COMPILER
 
 #endif //_CUDA__STD__CUDA_API_WRAPPER_H

ROCm/rocm-libraries

(cherry picked from commit 8bcf5826a117abc504892becb69f48c447e78f0f)

projects/hipccl/hipccl3/libhipcxx/include/cuda/std/__cuda/api_wrapper.h
@@ -38,30 +38,35 @@
 #  pragma system_header
 #endif // no system header
 
-#if !defined(_CCCL_CUDA_COMPILER_NVCC) && !defined(_CCCL_CUDA_COMPILER_NVHPC) && !defined(__HIP_PLATFORM_AMD__) && !defined(__HIPCC_RTC__)
+#if defined(_CCCL_CUDA_COMPILER_CLANG) && !defined(__HIP_PLATFORM_AMD__) && !defined(__HIPCC_RTC__)
 #  include <cuda_runtime_api.h>
-#endif // !_CCCL_CUDA_COMPILER_NVCC && !_CCCL_CUDA_COMPILER_NVHPC
+#endif // _CCCL_CUDA_COMPILER_CLANG
 
 #include <cuda/std/__exception/cuda_error.h>
 
-#define _CCCL_TRY_CUDA_API(_NAME, _MSG, ...)           \
-  {                                                    \
-    const ::hipError_t __status = _NAME(__VA_ARGS__); \
-    switch (__status)                                  \
-    {                                                  \
-      case ::hipSuccess:                              \
-        break;                                         \
-      default:                                         \
-        ::hipGetLastError();                          \
-        ::hip::__throw_cuda_error(__status, _MSG);    \
-    }                                                  \
-  }
+#if defined(_CCCL_CUDA_COMPILER)
+#  define _CCCL_TRY_CUDA_API(_NAME, _MSG, ...)           \
+    {                                                    \
+      const ::hipError_t __status = _NAME(__VA_ARGS__); \
+      switch (__status)                                  \
+      {                                                  \
+        case ::hipSuccess:                              \
+          break;                                         \
+        default:                                         \
+          ::hipGetLastError();                          \
+          ::hip::__throw_cuda_error(__status, _MSG);    \
+      }                                                  \
+    }
 
-#define _CCCL_ASSERT_CUDA_API(_NAME, _MSG, ...)        \
-  {                                                    \
-    const ::hipError_t __status = _NAME(__VA_ARGS__); \
-    _CCCL_ASSERT(__status == cudaSuccess, _MSG);       \
-    (void) __status;                                   \
-  }
+#  define _CCCL_ASSERT_CUDA_API(_NAME, _MSG, ...)        \
+    {                                                    \
+      const ::hipError_t __status = _NAME(__VA_ARGS__); \
+      _CCCL_ASSERT(__status == cudaSuccess, _MSG);       \
+      (void) __status;                                   \
+    }
+#else // ^^^ _CCCL_CUDA_COMPILER ^^^ / vvv !_CCCL_CUDA_COMPILER vvv
+#  define _CCCL_TRY_CUDA_API(_NAME, _MSG, ...)
+#  define _CCCL_ASSERT_CUDA_API(_NAME, _MSG, ...)
+#endif // !_CCCL_CUDA_COMPILER
 
 #endif //_CUDA__STD__CUDA_API_WRAPPER_H