NVIDIA/cccl

Replace `typedef` with `using` in libcu++ (#3368)

libcudacxx/include/cuda/std/__complex/nvbf16.h
@@ -85,8 +85,8 @@ struct __type_to_vector<__nv_bfloat16>
 template <>
 struct __cccl_complex_overload_traits<__nv_bfloat16, false, false>
 {
-  typedef __nv_bfloat16 _ValueType;
-  typedef complex<__nv_bfloat16> _ComplexType;
+  using _ValueType   = __nv_bfloat16;
+  using _ComplexType = complex<__nv_bfloat16>;
 };
 
 template <class _Tp>

ROCm/rocm-libraries

(cherry picked from commit 5fcffaae19707bc28ba33b948c7be2e4d3903883)

projects/hipccl/hipccl3/libhipcxx/include/cuda/std/__complex/nvbf16.h
@@ -106,8 +106,8 @@ struct __type_to_vector<__hip_bfloat16>
 template <>
 struct __cccl_complex_overload_traits<__hip_bfloat16, false, false>
 {
-  typedef __hip_bfloat16 _ValueType;
-  typedef complex<__hip_bfloat16> _ComplexType;
+  using _ValueType   = __hip_bfloat16;
+  using _ComplexType = complex<__hip_bfloat16>;
 };
 
 template <class _Tp>