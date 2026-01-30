32d2a7eb995670a52bcfa4ff36d660ca622fa608
Brian Harrison <brian.harrison@amd.com>
2026-01-30 16:37:06 -0500

    Add versioning to hipDNN SDKs

---
+++ projects/hipdnn/cmake/VersionUtils.cmake
@@ -0,0 +1,48 @@
+# Copyright © Advanced Micro Devices, Inc., or its affiliates.
+# SPDX-License-Identifier:  MIT
+
+# Macro to setup versioning for a component
+# Reads version.json, gets git hash, sets version variables, and calls project()
+macro(hipdnn_setup_version COMPONENT_NAME)
+    string(TOUPPER ${COMPONENT_NAME} COMPONENT_NAME_UPPER)
+
+    # Read version from version.json
+    file(READ "${CMAKE_CURRENT_SOURCE_DIR}/version.json" _version_json)
+    string(JSON ${COMPONENT_NAME_UPPER}_VERSION GET ${_version_json} "${COMPONENT_NAME}_version")
+
+    # Parse version components
+    string(REPLACE "." ";" _version_list ${${COMPONENT_NAME_UPPER}_VERSION})
+    list(GET _version_list 0 ${COMPONENT_NAME_UPPER}_VERSION_MAJOR)
+    list(GET _version_list 1 ${COMPONENT_NAME_UPPER}_VERSION_MINOR)
+    list(GET _version_list 2 ${COMPONENT_NAME_UPPER}_VERSION_PATCH)
+
+    # Get git commit hash for tweak version
+    execute_process(
+        COMMAND git rev-parse --short HEAD
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        OUTPUT_VARIABLE ${COMPONENT_NAME_UPPER}_VERSION_TWEAK
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        ERROR_QUIET
+    )
+    if(NOT ${COMPONENT_NAME_UPPER}_VERSION_TWEAK)
+        set(${COMPONENT_NAME_UPPER}_VERSION_TWEAK "unknown")
+    endif()
+
+    # Full version string
+    set(${COMPONENT_NAME_UPPER}_VERSION_STRING "${${COMPONENT_NAME_UPPER}_VERSION}.${${COMPONENT_NAME_UPPER}_VERSION_TWEAK}")
+
+    # Set project with version
+    project(${COMPONENT_NAME} VERSION ${${COMPONENT_NAME_UPPER}_VERSION} LANGUAGES CXX)
+
+    message(STATUS "${COMPONENT_NAME} version: ${${COMPONENT_NAME_UPPER}_VERSION_STRING}")
+endmacro()
+
+# Function to generate the version header
+macro(hipdnn_generate_version_header COMPONENT_NAME)
+    set(_include_dir "${CMAKE_CURRENT_SOURCE_DIR}/include")
+    configure_file(
+        "${_include_dir}/${COMPONENT_NAME}/version.h.in"
+        "${CMAKE_CURRENT_BINARY_DIR}/include/${COMPONENT_NAME}/version.h"
+        @ONLY
+    )
+endmacro()
