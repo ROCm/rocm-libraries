c74ccfa5b926a22cc96a51d30b65039fe1e11e55
jsandham <jsandham@amd.com>
2026-02-03 22:25:31 +0000

    Clang formatting

--- projects/hipsparse/clients/include/testing_spmv_csr.hpp
+++ projects/hipsparse/clients/include/testing_spmv_csr.hpp
@@ -42,8 +42,8 @@ using namespace hipsparse_test;
 template <typename I, typename J, typename T>
 void testing_spmv_csr_bad_arg(const Arguments& argus)
 {
-#if (!defined(CUDART_VERSION) || CUDART_VERSION > 10010 \
-     || (CUDART_VERSION == 10010 && CUDART_10_1_UPDATE_VERSION == 1))
+#if(!defined(CUDART_VERSION) || CUDART_VERSION > 10010 \
+    || (CUDART_VERSION == 10010 && CUDART_10_1_UPDATE_VERSION == 1))
     int64_t              m         = 100;
     int64_t              n         = 100;
     int64_t              nnz       = 100;
@@ -55,10 +55,10 @@ void testing_spmv_csr_bad_arg(const Arguments& argus)
     hipsparseIndexType_t idxType   = HIPSPARSE_INDEX_32I;
     hipDataType          dataType  = HIP_R_32F;
 
-#if (!defined(CUDART_VERSION))
+#if(!defined(CUDART_VERSION))
     hipsparseSpMVAlg_t alg = HIPSPARSE_MV_ALG_DEFAULT;
 #else
-#if (CUDART_VERSION >= 12000)
+#if(CUDART_VERSION >= 12000)
     hipsparseSpMVAlg_t alg = HIPSPARSE_SPMV_ALG_DEFAULT;
 #elif (CUDART_VERSION >= 10010 && CUDART_VERSION < 12000)
     hipsparseSpMVAlg_t alg = HIPSPARSE_MV_ALG_DEFAULT;
@@ -175,8 +175,8 @@ void testing_spmv_csr_bad_arg(const Arguments& argus)
 template <typename I, typename J, typename T>
 void testing_spmv_csr(Arguments argus)
 {
-#if (!defined(CUDART_VERSION) || CUDART_VERSION > 10010 \
-     || (CUDART_VERSION == 10010 && CUDART_10_1_UPDATE_VERSION == 1))
+#if(!defined(CUDART_VERSION) || CUDART_VERSION > 10010 \
+    || (CUDART_VERSION == 10010 && CUDART_10_1_UPDATE_VERSION == 1))
     J                    m        = argus.M;
     J                    n        = argus.N;
     T                    h_alpha  = make_DataType<T>(argus.alpha);
