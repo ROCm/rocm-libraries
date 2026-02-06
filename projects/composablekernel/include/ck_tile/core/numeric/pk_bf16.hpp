1ce028754f872408c3dcf496603c82b05fb8518d
yingluAMD <Yingmao.Lu@amd.com>
2026-02-06 15:55:58 +0800

    bug fix

--- projects/composablekernel/include/ck_tile/core/numeric/pk_bf16.hpp
+++ projects/composablekernel/include/ck_tile/core/numeric/pk_bf16.hpp
@@ -11,16 +11,18 @@ namespace ck_tile {
 
 // v_cvt_pk_bf16_f32 converts 2 floats to 2 bf16 in one instruction
 // Available on gfx94x (gfx942, gfx950) and later
-#if defined(__gfx94__)
 CK_TILE_DEVICE bf16x2_t cvt_pk_bf16_f32(float a, float b)
 {
+#if defined(__gfx94__)
     bf16x2_t result;
     asm volatile("v_cvt_pk_bf16_f32 %[result], %[a], %[b]"
                  : [result] "=v"(result)
                  : [a] "v"(a), [b] "v"(b));
     return result;
-}
+#else
+    return fp32x2_to_bf16x2(fp32x2_t{a, b});
 #endif
+}
 
 // Packed bf16x2 to fp32x2 conversion using bit operations
 // bf16 to fp32 is just left-shift by 16 bits (padding zeros in mantissa)
