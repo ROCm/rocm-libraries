f7f2aa8e86cdd44b093f221cb42b50c68a003ad8
kokolchin <kokolchin@luxoft.com>
2026-02-09 21:01:10 +0000

    Increa the numbe of tests

--- projects/miopen/test/gtest/conv2d.cpp
+++ projects/miopen/test/gtest/conv2d.cpp
@@ -3,32 +3,60 @@
 
 #include "conv_common_gtest.hpp"
 
-namespace
-{
-auto GenSmallCases()
+namespace {
+
+auto GenFullCases()
 {
     std::vector<miopen::test::conv::conv_test_input> cases{};
-    miopen::test::conv::conv_test_input input{};
-    input.batch_size = 1;
-    input.input_channels = 32;
-    input.output_channels = 64;
-    input.spatial_dim_elements = {28, 28};
-    input.filter_dims = {3, 3};
-    input.pads_strides_dilations = {1, 1, 1, 1, 1, 1};
-    input.trans_output_pads = {0, 0};
-    input.in_layout = "NCHW";
-    input.fil_layout = "NCHW";
-    input.out_layout = "NCHW";
-    input.deterministic = false;
-    input.tensor_vect = 0U;
-    input.vector_length = 1U;
-    // Only valid for int8 input and weights
-    input.output_type = "int32";
-    input.int8_vectorize = false;
-    cases.push_back(input);
+    
+    // Get the standard parameter lists from the header
+    auto batch_sizes = miopen::test::conv::get_batch_sizes();
+    auto spatial_dims = miopen::test::conv::get_2d_spatial_dims();
+    auto filter_dims = miopen::test::conv::get_2d_filter_dims();
+    auto in_channels = miopen::test::conv::get_input_channels();
+    auto out_channels = miopen::test::conv::get_output_channels();
+    auto psd = miopen::test::conv::get_2d_pads_strides_dilations();
 
+    // combinatorial generation with a limit to keep it under control
+    // (Similar to how CTest uses MIOPEN_TEST_LIMIT)
+    size_t count = 0;
+    const size_t limit = 50; // Adjust this to increase/decrease test time
+
+    for (auto b : batch_sizes) {
+        for (auto s : spatial_dims) {
+            for (auto f : filter_dims) {
+                for (auto ic : in_channels) {
+                    for (auto oc : out_channels) {
+                        for (auto p : psd) {
+                            miopen::test::conv::conv_test_input input{};
+                            input.batch_size = b;
+                            input.input_channels = ic;
+                            input.output_channels = oc;
+                            input.spatial_dim_elements = s;
+                            input.filter_dims = f;
+                            input.pads_strides_dilations = p;
+                            input.trans_output_pads = {0, 0};
+                            input.in_layout = "NCHW";
+                            input.fil_layout = "NCHW";
+                            input.out_layout = "NCHW";
+                            input.conv_mode = "CONV";
+                            input.pad_mode = "DEFAULT";
+                            input.deterministic = false;
+                            input.tensor_vect = 0U;
+                            input.vector_length = 1U;
+                            
+                            cases.push_back(input);
+                            
+                            if (++count >= limit) return cases;
+                        }
+                    }
+                }
+            }
+        }
+    }
     return cases;
 }
+
 } // namespace
 
 template <class T>
@@ -40,4 +68,5 @@ using GPU_conv_2d_FP32 = conv2d_test<float>;
 
 TEST_P(GPU_conv_2d_FP32, TestFP32) { Run(); }
 
-INSTANTIATE_TEST_SUITE_P(Smoke, GPU_conv_2d_FP32, ::testing::ValuesIn(GenSmallCases()));
+// Instantiate with the larger dataset
+INSTANTIATE_TEST_SUITE_P(Full, GPU_conv_2d_FP32, ::testing::ValuesIn(GenFullCases()));
