baa0b198067ca9e50823d7593c2fcfd92b639a3c
Borys Petrov <borys@streamhpc.com>
2026-02-10 12:44:35 +0000

    fix(rocRAND): warning: missing HIP_CHECKs in hipgraphs_doc_sample.hpp

--- projects/rocrand/test/hipgraphs_doc_sample.hpp
+++ projects/rocrand/test/hipgraphs_doc_sample.hpp
@@ -2,36 +2,36 @@ size_t        size = 1000;
 float*        data_0;
 unsigned int* data_1;
 
-hipMalloc(&data_0, sizeof(*data_0) * size);
-hipMalloc(&data_1, sizeof(*data_1) * size);
+HIP_CHECK(hipMalloc(&data_0, sizeof(*data_0) * size));
+HIP_CHECK(hipMalloc(&data_1, sizeof(*data_1) * size));
 
 hipGraph_t graph;
-hipGraphCreate(&graph, 0);
+HIP_CHECK(hipGraphCreate(&graph, 0));
 
 hipStream_t stream;
-hipStreamCreateWithFlags(&stream, hipStreamNonBlocking);
+HIP_CHECK(hipStreamCreateWithFlags(&stream, hipStreamNonBlocking));
 
 rocrand_generator generator;
 rocrand_create_generator(&generator, ROCRAND_RNG_PSEUDO_DEFAULT);
 rocrand_set_stream(generator, stream);
 rocrand_initialize_generator(generator);
 
-hipStreamBeginCapture(stream, hipStreamCaptureModeGlobal);
+HIP_CHECK(hipStreamBeginCapture(stream, hipStreamCaptureModeGlobal));
 
 rocrand_generate_normal(generator, data_0, size, 10.0F, 2.0F);
 rocrand_generate_poisson(generator, data_1, size, 3);
 
-hipStreamEndCapture(stream, &graph);
+HIP_CHECK(hipStreamEndCapture(stream, &graph));
 
 hipGraphExec_t instance;
-hipGraphInstantiate(&instance, graph, nullptr, nullptr, 0);
+HIP_CHECK(hipGraphInstantiate(&instance, graph, nullptr, nullptr, 0));
 
-hipGraphLaunch(instance, stream);
-hipStreamSynchronize(stream);
+HIP_CHECK(hipGraphLaunch(instance, stream));
+HIP_CHECK(hipStreamSynchronize(stream));
 
-hipGraphExecDestroy(instance);
+HIP_CHECK(hipGraphExecDestroy(instance));
 rocrand_destroy_generator(generator);
-hipStreamDestroy(stream);
-hipGraphDestroy(graph);
-hipFree(data_1);
-hipFree(data_0);
+HIP_CHECK(hipStreamDestroy(stream));
+HIP_CHECK(hipGraphDestroy(graph));
+HIP_CHECK(hipFree(data_1));
+HIP_CHECK(hipFree(data_0));
