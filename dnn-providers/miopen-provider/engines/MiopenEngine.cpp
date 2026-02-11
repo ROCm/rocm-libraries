567a8fae2b3851f3c8ad37ebcfe62217707e1c92
Evgenii Averin <86725875+averinevg@users.noreply.github.com>
2026-02-11 11:09:57 +0100

    Remove unused headers

--- dnn-providers/miopen-provider/engines/MiopenEngine.cpp
+++ dnn-providers/miopen-provider/engines/MiopenEngine.cpp
@@ -1,9 +1,6 @@
 ﻿// Copyright © Advanced Micro Devices, Inc., or its affiliates.
 // SPDX-License-Identifier:  MIT
 
-#include <limits>
-#include <string>
-
 #include "MiopenEngine.hpp"
 #include "plans/MiopenBatchnormPlanBuilder.hpp"
 
