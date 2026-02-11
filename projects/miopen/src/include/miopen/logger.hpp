050e02ad721e8e0084f82f44297fe5adc97323e1
Christopher Erb <christopher.erb@amd.com>
2026-02-11 11:38:24 -0600

    ci test

--- projects/miopen/src/include/miopen/logger.hpp
+++ projects/miopen/src/include/miopen/logger.hpp
@@ -42,7 +42,7 @@
 #endif
 
 #ifdef _WIN32
-MIOPEN_INTERNALS_EXPORT size_t getpid();
+MIOPEN_INTERNALS_EXPORT int getpid();
 #endif
 
 // See https://github.com/pfultz2/Cloak/wiki/C-Preprocessor-tricks,-tips,-and-idioms
