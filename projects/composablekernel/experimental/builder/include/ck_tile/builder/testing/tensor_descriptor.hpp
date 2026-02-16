2613d66b832b85d176d614c0d877fedf8c1b46d0
John Shumway <jshumway@amd.com>
2026-02-16 22:40:15 +0000

    Fix a couple staging compiler errors in the CK builder code
    This PR fixes two compiler warnings that report as errors:
    
    1. In tensor descriptor, the operator[] accessor needs a [[clang::lifetimebound]] attribute.
    2. In the unit tests for device buffer, there is a test that explicitly checks for an error on a pointer that went out of scope, so it needs a to dispable -Wliretime-safety-permissive in the test code.

--- projects/composablekernel/experimental/builder/include/ck_tile/builder/testing/tensor_descriptor.hpp
+++ projects/composablekernel/experimental/builder/include/ck_tile/builder/testing/tensor_descriptor.hpp
@@ -106,7 +106,7 @@ struct Extent : std::array<size_t, RANK>
     /// @param i The index to index the array with.
     ///
     /// @see std::array::operator[]
-    __device__ __host__ size_t& operator[](size_t i)
+    __device__ __host__ size_t& operator[](size_t i) [[clang::lifetimebound]]
     {
         if constexpr(RANK > 0)
         {
