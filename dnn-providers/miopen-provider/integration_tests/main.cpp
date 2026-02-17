8808b148677e987d508ce94f004d929cf1b44164
Adam Dickin <adam.dickin@amd.com>
2026-02-17 21:13:51 +0000

    remove the fixes for the slow tests as these are resolved better now

--- dnn-providers/miopen-provider/integration_tests/main.cpp
+++ dnn-providers/miopen-provider/integration_tests/main.cpp
@@ -22,16 +22,6 @@ int main(int argc, char** argv)
     hipdnn_data_sdk::logging::registerLoggingCallback(
         hipdnn_test_sdk::utilities::testLoggingCallback);
 
-    auto pluginPath = std::filesystem::weakly_canonical(
-        hipdnn_data_sdk::utilities::getCurrentExecutableDirectory() / PLUGIN_PATH);
-    const std::string pluginPathStr = pluginPath.string();
-    const std::array<const char*, 1> paths = {pluginPathStr.c_str()};
-
-    hipdnnSetEnginePluginPaths_ext(paths.size(), paths.data(), HIPDNN_PLUGIN_LOADING_ABSOLUTE);
-
-    hipdnnHandle_t handle;
-    hipdnnCreate(&handle);
-
     // Register HipErrorHandler to check and clear HIP errors after each test
     testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
     listeners.Append(new hipdnn_test_sdk::utilities::HipErrorHandler);
