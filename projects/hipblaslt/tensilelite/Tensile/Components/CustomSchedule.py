bf00a2b495cd69fd940915f191e50a3b452400cc
Eugene Mezhibovsky <emezhibo@amd.com>
2026-02-18 01:54:48 +0000

    v3, 70.7%

--- projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
@@ -4051,7 +4051,7 @@ def _get_schedule_128x128x64_TF32(kernel, useLDSTr, TLDS):
 
         syncs.add(                                                 48, vlcnt=7, barrier=True, comment="wait for the previous GRs")
 
-        lra1   = [                                                 48,49,50,51,52,53,54,55]
+        lra1   =[[                                                 48,49,50,51,52,53,54,55]]
         lrb1   = [                                                    56,57,  59,60,61,62,63,64]
         syncs.add(                                                          58, dscnt=6, comment="wait for the first two LRAs before packing")
         syncs.add(                                                          63, dscnt=6, comment="wait for the rest of LRAs before packing them")
@@ -4073,18 +4073,16 @@ def _get_schedule_128x128x64_TF32(kernel, useLDSTr, TLDS):
                 6,6,7,7, 8,8, 15,15,16,16]
         
         lra0   = [ 0,0,2,2,4,4,6,6]
-        lrb0   = [                     11,11,13,13,15,15,17,17]
+        lrb0   = [                                       11,11,13,13,15,15,17,17]
         #                wait then read
         syncs.add(             6, dscnt=2, comment="wait for the first 4 LRAs before swapping/packing")
-        syncs.add(                 9, dscnt=2, comment="wait for the rest of LRAs before swapping/packing them")
-        swap_a0= [              7,7,7, 9,9,9, 8,8, 10,10, 8, 10]
-        # pack_a0 =[          11]*12# swap instructions
-        pack_a0 = swap_a0
-        pack_a0+=[                    i+11 for i in offset] # last at 27
+        syncs.add(                     9, dscnt=2, comment="wait for the rest of LRAs before swapping/packing them")
+        pack_a0= [              7,7,7, 9,9,9, 8,8, 10,10, 8, 10] # swap instructions
+        pack_a0+=[                                       i+11 for i in offset] # last at 27
         # because of GR starting at 22, we need barrier at 21, will use that for sync too.
-        syncs.add(                          21, dscnt=0, comment="wait for LRBs before the packing them",
-                                            barrier=True, barrier_comment="barrier before GR")
-        pack_b0= [                                i+28 for i in offset] # last at 44
+        syncs.add(                                                                21, dscnt=0, comment="wait for LRBs before the packing them",
+                                                                                  barrier=True, barrier_comment="barrier before GR")
+        pack_b0= [                                                                  i+28 for i in offset] # last at 44
 
         grinca = [0,1,1,1,2,3,3,3,4]
         grincb = [5,5,5,7,8,9,10,19,19]
@@ -4097,17 +4095,16 @@ def _get_schedule_128x128x64_TF32(kernel, useLDSTr, TLDS):
         grb    = [                                                    53,57,61, 64,69,75,79,84] # one index for two instructions
         num_gr = len(gra) + len(grb)
 
-        syncs.add(                                                 48, vlcnt=7, barrier=True, comment="wait for the previous GRs")
-
-        lra1   = [                                                 48,49,50,51,52,53,54,55]
-        lrb1   = [                                                    56,57,  59,60,61,62,63,64]
-        syncs.add(                                                          58, dscnt=6, comment="wait for the first two LRAs before packing")
-        syncs.add(                                                          63, dscnt=6, comment="wait for the rest of LRAs before packing them")
-        pack_a1 =[                                                            59]*12# swap instructions
-        pack_a1+= [                                                           i+59 for i in offset] # last at 75
-        syncs.add(                                                                                 76, dscnt=0, comment="wait for LRBs before the packing them")
-        pack_b1 =[                                                                                 i+77 for i in offset] # last at 93
-
+        syncs.add(                                                 47, vlcnt=7, barrier=True, comment="wait for the previous GRs")
+        lra1   =[[                                                 48,48,50,50,52,52,54,54],
+                                                                   [49,49,51,51,53,53,54,54]]
+        lrb1   = [                                                                                  59,59,  61,61,63,63,65,65]
+        syncs.add(                                                                   54, dscnt=2, comment="wait for the first two LRAs before packing")
+        syncs.add(                                                                             57, dscnt=0, comment="wait for the rest of LRAs before packing them")
+        pack_a1 =[                                                                    55,55,55, 57,57,57, 55,55, 57,57, 56, 58] # swap instructions
+        pack_a1+= [                                                                                 i+59 for i in offset] # last at 75
+        syncs.add(                                                                                     76, dscnt=0, comment="wait for LRBs before the packing them")
+        pack_b1 =[                                                                                     i+77 for i in offset] # last at 93
 
     else:
         return False, None  
@@ -4128,11 +4125,11 @@ def _get_schedule_128x128x64_TF32(kernel, useLDSTr, TLDS):
         'LWSB':   [lwsb],
         'PackA0': [pack_a0],
         'PackB0': [pack_b0],
-        'LRA1':   [lra1],
+        'LRA1':   lra1,
         'LRB1':   [lrb1],
         'PackB1': [pack_b1],
         'PackA1': [pack_a1],
-        'LCC':    [[n_mfma-2, n_mfma-1]],
+        'LCC':    [[n_mfma-2, n_mfma-2]],
     }
 
     syncCode = syncs.get_code()
