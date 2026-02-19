6ef883c76e41843378aa851010b0e7a6b20d06bf
geomin12 <geomin12@amd.com>
2026-02-19 09:45:45 -0800

    Adding CK as a part of overall workflow

--- .github/scripts/therock_matrix.py
+++ .github/scripts/therock_matrix.py
@@ -7,6 +7,7 @@ import os
 subtree_to_project_map = {
     "dnn-providers/hipblaslt-provider": "hipblaslt-provider",
     "dnn-providers/miopen-provider": "miopen-provider",
+    "projects/composablekernel": "composablekernel",
     "projects/hipblas": "blas",
     "projects/hipblas-common": "blas",
     "projects/hipblaslt": "blas",
@@ -109,6 +110,14 @@ additional_options = {
         ],
         "projects_to_test": ["hipblaslt_plugin"],
         "project_to_add": "blas",
+    },
+    "composablekernel": {
+        "cmake_options": [
+            "-DTHEROCK_ENABLE_COMPOSABLE_KERNEL=ON",
+            "-DTHEROCK_ENABLE_MIOPEN=ON",
+        ],
+        "projects_to_test": ["miopen"],
+        "project_to_add": "miopen",
     },
 }
 
