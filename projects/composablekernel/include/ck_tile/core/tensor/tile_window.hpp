8dd4faff97a0d15db45469b1613036b4df62429a
ThomasNing <thomas.ning@amd.com>
2026-02-19 16:04:00 -0600

    Finish the fix

--- projects/composablekernel/include/ck_tile/core/tensor/tile_window.hpp
+++ projects/composablekernel/include/ck_tile/core/tensor/tile_window.hpp
@@ -627,17 +627,9 @@ struct tile_window_with_static_distribution
                 const auto lds_coord =
                     make_tensor_coordinate(tensor_descriptor, lds_bottom_tensor_thread_idx);
 
-                [[maybe_unused]] constexpr auto IMM_RANGE =
-                    (1 << 12) / sizeof(typename Base::DataType) * Traits::PackedSize;
-                constexpr auto imm_total = lds_ys_offset;
-                constexpr auto imm_valid =
-                    0; // FIX: Avoid inconsistency between lds_ys_offset and dram_ys_offset
-                constexpr auto imm_overflow = imm_total - imm_valid;
-
                 // Calculate SMEM address using base pointer
-                CK_TILE_LDS_ADDR LdsDataType* smem = lds_base_ptr +
-                                                     lds_coord.get_offset() / Traits::PackedSize +
-                                                     imm_overflow / Traits::PackedSize;
+                CK_TILE_LDS_ADDR LdsDataType* smem =
+                    lds_base_ptr + (lds_coord.get_offset() + lds_ys_offset) / Traits::PackedSize;
 
                 const auto dram_ys_offset = [&]() {
                     if constexpr(static_move_ys)
@@ -657,13 +649,21 @@ struct tile_window_with_static_distribution
                         offset + dram_ys_offset,
                         bool_constant<oob_conditional_check>{});
                 else
+                {
+                    using Tsrc =
+                        remove_cvref_t<typename decltype(this->get_bottom_tensor_view())::DataType>;
+                    constexpr index_t imm_max_elems = ((1 << 12) - 1) / sizeof(Tsrc);
+                    constexpr index_t align         = Traits::PackedSize;
+                    constexpr index_t imm_valid     = (dram_ys_offset <= imm_max_elems)
+                                                          ? (dram_ys_offset / align) * align
+                                                          : (imm_max_elems / align) * align;
                     this->get_bottom_tensor_view().template async_get_vectorized_elements<vector_t>(
                         smem,
                         bottom_tensor_thread_coord.get_offset() + offset,
                         dram_ys_offset - imm_valid,
                         number<imm_valid>{},
                         bool_constant<oob_conditional_check>{});
-
+                }
                 // Move thread coordinate if not last access
                 if constexpr(iCoordAccess != (NumAccessPerCoord - 1))
                 {
