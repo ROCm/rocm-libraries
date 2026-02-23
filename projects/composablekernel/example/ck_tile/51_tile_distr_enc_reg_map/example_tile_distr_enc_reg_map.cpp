543c3e584eb438447fa9a808c05bf6681a195866
kiefer <kiefer.van.teutem@streamhpc.com>
2026-02-23 14:18:10 +0000

    Add two more tile distr encoding examples.

--- projects/composablekernel/example/ck_tile/51_tile_distr_enc_reg_map/example_tile_distr_enc_reg_map.cpp
+++ projects/composablekernel/example/ck_tile/51_tile_distr_enc_reg_map/example_tile_distr_enc_reg_map.cpp
@@ -8,10 +8,10 @@ using namespace ck_tile;
 
 int main()
 {
-    // Define distribution encoding
-    // Example RDNA3 V_WMMA_F32_16X16X16_F16 A Matrix (M, K)
-    // L{RM} V{K}
-    using Encoding =
+    // Define some tile distribution encodings and print register mappings.
+
+    printf("Example RDNA3 V_WMMA_F32_16X16X16_F16 A Matrix (M, K)\n L{RM} V{K}\n");
+    TileDistrEncRegMap<
         tile_distribution_encoding<sequence<2>, // R (= Repeat) Lanes 0-15 are duplicated at 16-31
                                    tuple<sequence<16>, sequence<16>>, // H (= Hidden dims = unmerged
                                                                       // dims) for M, K dimension
@@ -19,13 +19,11 @@ int main()
                                    tuple<sequence<0, 0>>, // P minor
                                    sequence<2>,           // Y major (= Yield = Vector items)
                                    sequence<0>            // Y minor
-                                   >;
+                                   >>::print();
 
-    TileDistrEncRegMap<Encoding>::print();
-
-    // Example RDNA3 V_WMMA_F32_16X16X16_F16 C Matrix (M, N)
-    // M{2, 1} L{M1N} V{M2M0} (dummy unmerge to be more similar to other layouts)
-    using Encoding2 =
+    printf("\nExample RDNA3 V_WMMA_F32_16X16X16_F16 C Matrix (M, N)\nM{2, 1} L{M1N} V{M2M0} (dummy "
+           "unmerge to be more similar to other layouts)\n");
+    TileDistrEncRegMap<
         tile_distribution_encoding<sequence<>,                             // R (= Repeat)
                                    tuple<sequence<8, 2, 1>, sequence<16>>, // H (= Hidden dims =
                                                                            // unmerged dims) for M,
@@ -34,9 +32,33 @@ int main()
                                    tuple<sequence<1, 0>>, // P minor
                                    sequence<1, 1>,        // Y major (= Yield = Vector items)
                                    sequence<0, 2>         // Y minor
-                                   >;
+                                   >>::print();
 
-    TileDistrEncRegMap<Encoding2>::print();
+    printf("\nExample CDNA __builtin_amdgcn_mfma_f32_4x4x4f16 A Matrix (M, K) with 16x "
+           "block-hiding in the M dimension\nL{BM} V{K}\n");
+    TileDistrEncRegMap<
+        tile_distribution_encoding<sequence<>,                          // R (= Repeat)
+                                   tuple<sequence<16, 4>, sequence<4>>, // H (= Hidden dims =
+                                                                        // unmerged dims) for M,
+                                                                        // K dimension
+                                   tuple<sequence<1, 1>>, // P major (= Parallelism = lanes)
+                                   tuple<sequence<0, 1>>, // P minor
+                                   sequence<2>,           // Y major (= Yield = Vector items)
+                                   sequence<0>            // Y minor
+                                   >>::print();
+
+    printf("\nExample CDNA __builtin_amdgcn_mfma_f32_4x4x4f16 B Matrix (N, K) with 16x "
+           "block-hiding in the M dimension\nL{BN} V{K}\n");
+    TileDistrEncRegMap<
+        tile_distribution_encoding<sequence<16>,                    // R (= Repeat)
+                                   tuple<sequence<4>, sequence<4>>, // H (= Hidden dims =
+                                                                    // unmerged dims) for N,
+                                                                    // K dimension
+                                   tuple<sequence<0, 1>>, // P major (= Parallelism = lanes)
+                                   tuple<sequence<0, 0>>, // P minor
+                                   sequence<2>,           // Y major (= Yield = Vector items)
+                                   sequence<0>            // Y minor
+                                   >>::print();
 
     return 0;
 }
