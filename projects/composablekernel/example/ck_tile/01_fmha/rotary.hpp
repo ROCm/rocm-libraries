6efae2dad3cb088be88d7a3bc0eee3839913ad79
Brock Hargreaves <brock.hargreaves@amd.com>
2026-02-24 16:37:44 -0500

    Clang format.

--- projects/composablekernel/example/ck_tile/01_fmha/rotary.hpp
+++ projects/composablekernel/example/ck_tile/01_fmha/rotary.hpp
@@ -49,9 +49,7 @@ generate_rotary_cos_sin(ck_tile::index_t seqlen,
     using std::begin, std::end;
 
     ck_tile::HostTensor<float> angle({num_rows, num_cols});
-    std::generate(begin(angle), end(angle), [&] {
-        return generator(random_engine) * 2 * M_PI;
-    });
+    std::generate(begin(angle), end(angle), [&] { return generator(random_engine) * 2 * M_PI; });
 
     ck_tile::HostTensor<DataType> cos({num_rows, num_cols});
     std::transform(begin(angle), end(angle), begin(cos), [](float origin_value) {
