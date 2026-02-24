70ee5b81e1d59fa712d379b8ea2f58a0d4c5940b
Brock Hargreaves <brock.hargreaves@amd.com>
2026-02-23 22:17:49 -0500

    Use a type safe and portable pi.

--- projects/composablekernel/example/ck_tile/01_fmha/rotary.hpp
+++ projects/composablekernel/example/ck_tile/01_fmha/rotary.hpp
@@ -7,7 +7,7 @@
 #include "ck_tile/host/host_tensor.hpp"
 
 #include <cassert>
-#include <cmath>
+#include <numbers>
 #include <functional>
 #include <iterator>
 #include <optional>
@@ -44,7 +44,7 @@ generate_rotary_cos_sin(ck_tile::index_t seqlen,
     using std::begin, std::end;
 
     ck_tile::HostTensor<float> angle({num_rows, num_cols});
-    std::generate(begin(angle), end(angle), [&] { return generator(random_engine) * 2 * M_PI; });
+    std::generate(begin(angle), end(angle), [&] { return generator(random_engine) * 2 * std::numbers::pi_v<float>; });
 
     ck_tile::HostTensor<DataType> cos({num_rows, num_cols});
     std::transform(begin(angle), end(angle), begin(cos), [](float origin_value) {
