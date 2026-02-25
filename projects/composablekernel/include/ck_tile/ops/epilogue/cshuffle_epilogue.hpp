8219caecdad82ca5d4c801b78e2367eb675df3b9
yingluAMD <Yingmao.Lu@amd.com>
2026-02-25 11:31:22 +0800

    bug fix

--- projects/composablekernel/include/ck_tile/ops/epilogue/cshuffle_epilogue.hpp
+++ projects/composablekernel/include/ck_tile/ops/epilogue/cshuffle_epilogue.hpp
@@ -112,7 +112,7 @@ struct CShuffleEpilogue
                            tf32_t,
                            std::conditional_t<std::is_same_v<BDataTypeBuf, pk_int4_t> ||
                                                   std::is_same_v<BDataTypeBuf, pk_fp4_t> ||
-                                                  sizeof(BDataType) < sizeof(ADataTypeBuf),
+                                                  sizeof(BDataTypeBuf) < sizeof(ADataTypeBuf),
                                               ADataTypeBuf,
                                               BDataTypeBuf>>;
 
