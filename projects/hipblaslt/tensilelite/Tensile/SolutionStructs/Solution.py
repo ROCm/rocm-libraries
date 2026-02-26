69e82def77b53c5c76b196ac72b06dc2c15b544f
Hongji Chen <hongjche@amd.com>
2026-02-26 00:19:56 -0500

    revert changes for calcLdsPad

--- projects/hipblaslt/tensilelite/Tensile/SolutionStructs/Solution.py
+++ projects/hipblaslt/tensilelite/Tensile/SolutionStructs/Solution.py
@@ -3276,7 +3276,7 @@ class Solution(collections.abc.Mapping):
             state["MinGRIncPerMfma"] = 3
 
     # calculate ldsPad
-    state["LdsPadA"], state["LdsPadB"], state["LdsPadMetadata"] = calcLdsPad(isaInfoMap)
+    state["LdsPadA"], state["LdsPadB"], state["LdsPadMetadata"] = calcLdsPad(state["LocalReadVectorWidth"], isaInfoMap)
 
     if state["GlobalReadVectorWidthA"] * state["ProblemType"]["MacDataTypeA"].numBytes() == 32 and state["LdsPadA"] == 16 // state["ProblemType"]["MacDataTypeA"].numBytes():
       if auto_LdsBlockSizePerPadA_for_mix:
