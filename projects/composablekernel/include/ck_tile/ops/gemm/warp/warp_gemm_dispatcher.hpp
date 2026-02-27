89a70d1979133669c0f232795b7c46f59b3b5ead
Anton Gorenko <anton@streamhpc.com>
2026-02-27 16:48:45 +0500

    Support fp6 for matrix P
    fp6 x fp4 MFMA has the same performance as fp4 x fp4 but provides much
    higher precision (32 non-negative values vs 8).

--- projects/composablekernel/include/ck_tile/ops/gemm/warp/warp_gemm_dispatcher.hpp
+++ projects/composablekernel/include/ck_tile/ops/gemm/warp/warp_gemm_dispatcher.hpp
@@ -185,6 +185,8 @@ template<typename A, typename B, WGAttrNumAccessEnum I, bool IsScale16>
 struct Dispatcher<A, B, float, 16, 16, 128, true, false, false, I, I, IsScale16, std::enable_if_t<I != EDefault>> { using Type = WarpGemmMfma_f32_16x16x128_f8f6f4_CTransposed<A, B, I>; };
 #endif
 
+template<WGAttrNumAccessEnum I> struct Dispatcher<pk_fp4_t, pk_fp6x16_t, float, 16, 16, 128,  true, false, false, I> { using Type = WarpGemmMfma_f32_16x16x128_fp4_fp6_CTransposed<I>; };
+
 template<> struct Dispatcher<fp8_t, fp8_t, float, 32, 32,  64, false> { using Type = WarpGemmMfma_f32_32x32x64_fp8_fp8<>; };
 template<> struct Dispatcher<fp8_t, bf8_t, float, 32, 32,  64, false> { using Type = WarpGemmMfma_f32_32x32x64_fp8_bf8<>; };
 template<> struct Dispatcher<bf8_t, fp8_t, float, 32, 32,  64, false> { using Type = WarpGemmMfma_f32_32x32x64_bf8_fp8<>; };
