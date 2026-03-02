f9162a4f0583ef5060a50487b786f8d6b9401570
mousdahl-amd <mitch.ousdahl@amd.com>
2026-03-02 22:51:05 +0000

    Initial implementation of KnobSettings lowering via descriptors

--- projects/hipdnn/backend/src/descriptors/DescriptorFactory.cpp
+++ projects/hipdnn/backend/src/descriptors/DescriptorFactory.cpp
@@ -10,6 +10,7 @@
 #include "ExecutionPlanDescriptor.hpp"
 #include "GraphDescriptor.hpp"
 #include "HipdnnException.hpp"
+#include "KnobSettingDescriptor.hpp"
 #include "TensorDescriptor.hpp"
 #include "VariantDescriptor.hpp"
 #include "logging/Logging.hpp"
@@ -52,6 +53,9 @@ void DescriptorFactory::create(hipdnnBackendDescriptorType_t descriptorType,
         break;
     case HIPDNN_BACKEND_OPERATION_CONVOLUTION_FORWARD_DESCRIPTOR:
         privateDesc = std::make_shared<ConvolutionFwdOperationDescriptor>();
+        break;
+    case HIPDNN_BACKEND_KNOB_CHOICE_DESCRIPTOR:
+        privateDesc = std::make_shared<KnobSettingDescriptor>();
         break;
     default:
         throw HipdnnException(HIPDNN_STATUS_NOT_SUPPORTED,
