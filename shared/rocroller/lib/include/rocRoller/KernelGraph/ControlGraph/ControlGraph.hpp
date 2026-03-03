26d65f98154f78daf06a2f889a027dba4ea23a3d
Chun-Xun Lin <Chun-Xun.Lin@amd.com>
2026-03-03 18:50:35 +0000

    Remove commented code

--- shared/rocroller/lib/include/rocRoller/KernelGraph/ControlGraph/ControlGraph.hpp
+++ shared/rocroller/lib/include/rocRoller/KernelGraph/ControlGraph/ControlGraph.hpp
@@ -249,30 +249,15 @@ namespace rocRoller
             }
 
         private:
-            virtual void clearCache(Graph::GraphModification modification) override;
-            void         populateOrderCache() const;
-            void         sortOrderCache() const;
 
             template <CForwardRangeOf<int> Range>
             std::vector<int> populateOrderCacheImpl(Range const& startingNodes) const;
             std::vector<int> populateOrderCacheImpl(int startingNode) const;
 
-            NodeOrdering lookupOrder(CacheOnlyPolicy const, int nodeA, int nodeB) const;
-            NodeOrdering lookupOrder(IgnoreCachePolicy const, int nodeA, int nodeB) const;
-            template <CForwardRangeOf<int> ARange = std::initializer_list<int>,
-                      CForwardRangeOf<int> BRange = std::initializer_list<int>>
-            void writeOrderCache(ARange const& nodesA,
-                                 BRange const& nodesB,
-                                 NodeOrdering  order) const;
-            void writeOrderCache(int nodeA, int nodeB, NodeOrdering order) const;
-            mutable std::unordered_map<int, NodeOrders>       m_orderCache;
-            mutable std::unordered_map<int, std::vector<int>> m_descendentCache;
-            mutable CacheStatus                               m_cacheStatus = CacheStatus::Invalid;
-            mutable bool                                      m_changesRestricted = false;
-
-            //virtual void clearCache(Graph::GraphModification modification) override;
+            virtual void clearCache(Graph::GraphModification modification) override;
+            void         populateOrderCache() const;
             void checkOrderCache() const;
-            //void         populateOrderCache() const;
+            void         sortOrderCache() const;
 
             /**
              * Populates m_orderCache for startingNodes relative to their descendents, and the
@@ -288,27 +273,27 @@ namespace rocRoller
              */
             std::set<int> populateOrderCache(int startingNode) const;
 
-            //NodeOrdering lookupOrder(CacheOnlyPolicy const, int nodeA, int nodeB) const;
-            //NodeOrdering lookupOrder(IgnoreCachePolicy const, int nodeA, int nodeB) const;
+            NodeOrdering lookupOrder(CacheOnlyPolicy const, int nodeA, int nodeB) const;
+            NodeOrdering lookupOrder(IgnoreCachePolicy const, int nodeA, int nodeB) const;
 
-            //void writeOrderCache(int nodeA, int nodeB, NodeOrdering order) const;
+            void writeOrderCache(int nodeA, int nodeB, NodeOrdering order) const;
 
-            //template <CForwardRangeOf<int> ARange = std::initializer_list<int>,
-            //          CForwardRangeOf<int> BRange = std::initializer_list<int>>
-            //void writeOrderCache(ARange const& nodesA,
-            //                     BRange const& nodesB,
-            //                     NodeOrdering  order) const;
+            template <CForwardRangeOf<int> ARange = std::initializer_list<int>,
+                      CForwardRangeOf<int> BRange = std::initializer_list<int>>
+            void writeOrderCache(ARange const& nodesA,
+                                 BRange const& nodesB,
+                                 NodeOrdering  order) const;
 
-            //mutable std::unordered_map<int, std::unordered_map<int, NodeOrdering>> m_orderCache;
+            mutable std::unordered_map<int, NodeOrders>       m_orderCache;
             /**
              * If an entry is present, the value will be the IDs of every descendent from the key,
              * following every kind of edge.
              */
-            //mutable std::unordered_map<int, std::set<int>> m_descendentCache;
+            mutable std::unordered_map<int, std::vector<int>> m_descendentCache;
 
-            //mutable CacheStatus m_cacheStatus = CacheStatus::Invalid;
+            mutable CacheStatus m_cacheStatus = CacheStatus::Invalid;
 
-            //mutable bool m_changesRestricted = false;
+            mutable bool m_changesRestricted = false;
         };
 
         std::string name(ControlGraph::Element const& el);
--- shared/rocroller/lib/source/KernelGraph/ControlGraph/ControlGraph.cpp
+++ shared/rocroller/lib/source/KernelGraph/ControlGraph/ControlGraph.cpp
@@ -12,13 +12,6 @@
 
 namespace rocRoller::KernelGraph::ControlGraph
 {
-    //std::unordered_map<int, std::unordered_map<int, NodeOrdering>> const&
-    //    ControlGraph::nodeOrderTable() const
-    //{
-    //    populateOrderCache();
-    //    return m_orderCache;
-    //}
-
     std::unordered_map<int, std::unordered_map<int, NodeOrdering>>
         ControlGraph::nodeOrderTable() const
     {
@@ -42,57 +35,6 @@ namespace rocRoller::KernelGraph::ControlGraph
         }
         return table;
     }
-
-    //std::string ControlGraph::nodeOrderTableString(std::set<int> const& nodes) const
-    //{
-    //    populateOrderCache();
-
-    //    if(nodes.empty())
-    //    {
-    //        return "Empty order cache.\n";
-    //    }
-
-    //    std::ostringstream msg;
-
-    //    int width = std::ceil(std::log10(static_cast<float>(*nodes.rbegin())));
-    //    width     = std::max(width, 3);
-
-    //    msg << std::setw(width) << " "
-    //        << "\\";
-    //    for(int n : nodes)
-    //        msg << " " << std::setw(width) << n;
-
-    //    for(int i : nodes)
-    //    {
-    //        msg << std::endl << std::setw(width) << i << "|";
-    //        for(int j : nodes)
-    //        {
-    //            if(i == j)
-    //            {
-    //                msg << " ";
-    //                auto oldFill = msg.fill('-');
-    //                msg << std::setw(width) << "-";
-    //                msg.fill(oldFill);
-    //            }
-    //            else
-    //            {
-    //                msg << " " << std::setw(width) << abbrev(lookupOrder(CacheOnly, i, j));
-    //            }
-    //        }
-
-    //        msg << " | " << std::setw(width) << i;
-    //    }
-
-    //    msg << std::endl
-    //        << std::setw(width) << " "
-    //        << "|";
-    //    for(int n : nodes)
-    //        msg << " " << std::setw(width) << n;
-
-    //    msg << std::endl;
-
-    //    return msg.str();
-    //}
 
     std::string ControlGraph::nodeOrderTableString(std::set<int> const& nodes) const
     {
@@ -167,26 +109,6 @@ namespace rocRoller::KernelGraph::ControlGraph
         return nodeOrderTableString(nodes);
     }
 
-    //std::string ControlGraph::nodeOrderTableString() const
-    //{
-    //    populateOrderCache();
-
-    //    TIMER(t, "nodeOrderTable");
-
-    //    std::set<int> nodes;
-
-    //    for(auto const& [node, nodeOrderPairs] : m_orderCache)
-    //    {
-    //        for(auto const pair : nodeOrderPairs)
-    //        {
-    //            nodes.insert(node);
-    //            nodes.insert(pair.first);
-    //        }
-    //    }
-
-    //    return nodeOrderTableString(nodes);
-    //}
-
     void ControlGraph::clearCache(Graph::GraphModification modification)
     {
         Hypergraph<Operation, ControlEdge, false>::clearCache(modification);
@@ -236,32 +158,6 @@ namespace rocRoller::KernelGraph::ControlGraph
             return true;
         }
     }
-
-    //void ControlGraph::sortOrderCache() const
-    //{
-    //    std::unordered_set<int> seen;
-    //    seen.reserve(m_orderCache.size());
-    //    auto check = [&](std::vector<int> const& vec)
-    //    {
-    //       for(auto v: vec)
-    //       {
-    //           auto [_, inserted] = seen.insert(v);
-    //           AssertFatal(inserted, "A duplicate element");
-    //       }
-    //    };
-    //    for(auto& [node, orders] : m_orderCache)
-    //    {
-    //        std::ranges::sort(orders.after);
-    //        std::ranges::sort(orders.before);
-    //        std::ranges::sort(orders.inBody);
-    //        std::ranges::sort(orders.containing);
-    //        //check(orders.after);
-    //        //check(orders.before);
-    //        //check(orders.inBody);
-    //        //check(orders.containing);
-    //        seen.clear();
-    //    }
-    //}
 
     void ControlGraph::sortOrderCache() const
     {
@@ -413,115 +309,6 @@ namespace rocRoller::KernelGraph::ControlGraph
         return allNodes;
     }
 
-    //void ControlGraph::populateOrderCache() const
-    //{
-    //    TIMER(t, "populateOrderCache");
-
-    //    if(m_cacheStatus == CacheStatus::Valid)
-    //        return;
-
-    //    auto r = roots().to<std::set>();
-    //    populateOrderCache(r);
-    //    m_cacheStatus = CacheStatus::Valid;
-
-    //    //
-    //    // m_descendentCache is only used to help build m_orderCache,
-    //    // and it must be cleared after finish building m_orderCache
-    //    // to ensure no stale data being used when building m_orderCache
-    //    // next time.
-    //    //
-    //    m_descendentCache.clear();
-    //}
-
-    //template <CForwardRangeOf<int> Range>
-    //std::set<int> ControlGraph::populateOrderCache(Range const& startingNodes) const
-    //{
-    //    std::set<int> rv;
-
-    //    auto it = startingNodes.begin();
-    //    if(it == startingNodes.end())
-    //        return rv;
-
-    //    rv = populateOrderCache(*it);
-
-    //    for(it++; it != startingNodes.end(); it++)
-    //    {
-    //        auto nodes = populateOrderCache(*it);
-    //        rv.insert(nodes.begin(), nodes.end());
-    //    }
-
-    //    return rv;
-    //}
-
-    //std::set<int> ControlGraph::populateOrderCache(int startingNode) const
-    //{
-    //    auto ccEntry = m_descendentCache.find(startingNode);
-    //    if(ccEntry != m_descendentCache.end())
-    //        return ccEntry->second;
-
-    //    auto addDescendents = [this](Generator<int> nodes) {
-    //        auto theNodes = nodes.to<std::set>();
-
-    //        auto descendents = populateOrderCache(theNodes);
-    //        theNodes.insert(descendents.begin(), descendents.end());
-
-    //        return theNodes;
-    //    };
-
-    //    auto initNodes     = addDescendents(getOutputNodeIndices<Initialize>(startingNode));
-    //    auto bodyNodes     = addDescendents(getOutputNodeIndices<Body>(startingNode));
-    //    auto elseNodes     = addDescendents(getOutputNodeIndices<Else>(startingNode));
-    //    auto incNodes      = addDescendents(getOutputNodeIndices<ForLoopIncrement>(startingNode));
-    //    auto sequenceNodes = addDescendents(getOutputNodeIndices<Sequence>(startingNode));
-
-    //    // {init, body, else, inc} nodes are in the body of the current node
-    //    writeOrderCache({startingNode}, initNodes, NodeOrdering::RightInBodyOfLeft);
-    //    writeOrderCache({startingNode}, bodyNodes, NodeOrdering::RightInBodyOfLeft);
-    //    writeOrderCache({startingNode}, elseNodes, NodeOrdering::RightInBodyOfLeft);
-    //    writeOrderCache({startingNode}, incNodes, NodeOrdering::RightInBodyOfLeft);
-
-    //    // Sequence connected nodes are after the current node
-    //    writeOrderCache({startingNode}, sequenceNodes, NodeOrdering::LeftFirst);
-
-    //    // {body, else, inc, sequence} are after init nodes
-    //    writeOrderCache(initNodes, bodyNodes, NodeOrdering::LeftFirst);
-    //    writeOrderCache(initNodes, elseNodes, NodeOrdering::LeftFirst);
-    //    writeOrderCache(initNodes, incNodes, NodeOrdering::LeftFirst);
-    //    writeOrderCache(initNodes, sequenceNodes, NodeOrdering::LeftFirst);
-
-    //    // {else, inc, sequence} are after body nodes
-    //    writeOrderCache(bodyNodes, elseNodes, NodeOrdering::LeftFirst);
-    //    writeOrderCache(bodyNodes, incNodes, NodeOrdering::LeftFirst);
-    //    writeOrderCache(bodyNodes, sequenceNodes, NodeOrdering::LeftFirst);
-
-    //    // {inc, sequence} are after else nodes
-    //    writeOrderCache(elseNodes, incNodes, NodeOrdering::LeftFirst);
-    //    writeOrderCache(elseNodes, sequenceNodes, NodeOrdering::LeftFirst);
-
-    //    // sequence are after inc nodes.
-    //    writeOrderCache(incNodes, sequenceNodes, NodeOrdering::LeftFirst);
-
-    //    auto allNodes = std::move(sequenceNodes);
-    //    allNodes.insert(bodyNodes.begin(), bodyNodes.end());
-    //    allNodes.insert(elseNodes.begin(), elseNodes.end());
-    //    allNodes.insert(incNodes.begin(), incNodes.end());
-    //    allNodes.insert(initNodes.begin(), initNodes.end());
-
-    //    m_descendentCache[startingNode] = allNodes;
-
-    //    return allNodes;
-    //}
-
-    //template <CForwardRangeOf<int> ARange, CForwardRangeOf<int> BRange>
-    //void ControlGraph::writeOrderCache(ARange const& nodesA,
-    //                                   BRange const& nodesB,
-    //                                   NodeOrdering  order) const
-    //{
-    //    for(int nodeA : nodesA)
-    //        for(int nodeB : nodesB)
-    //            writeOrderCache(nodeA, nodeB, order);
-    //}
-
     template <CForwardRangeOf<int> ARange, CForwardRangeOf<int> BRange>
     void ControlGraph::writeOrderCache(ARange const& nodesA,
                                        BRange const& nodesB,
@@ -585,42 +372,6 @@ namespace rocRoller::KernelGraph::ControlGraph
             break;
         }
     }
-
-    //template <CForwardRangeOf<int> ARange, CForwardRangeOf<int> BRange>
-    //void ControlGraph::writeOrderCache(ARange const& nodesA,
-    //                                   BRange const& nodesB,
-    //                                   NodeOrdering  order) const
-    //{
-    //    for(int nodeA : nodesA)
-    //        for(int nodeB : nodesB)
-    //            writeOrderCache(nodeA, nodeB, order);
-    //}
-
-    //void ControlGraph::writeOrderCache(int nodeA, int nodeB, NodeOrdering order) const
-    //{
-    //    if(nodeA > nodeB)
-    //    {
-    //        writeOrderCache(nodeB, nodeA, opposite(order));
-    //    }
-    //    else
-    //    {
-    //        auto [iter, _ignore] = m_orderCache.try_emplace(nodeA);
-
-    //        if(iter->second.contains(nodeB))
-    //        {
-    //            AssertFatal(iter->second.at(nodeB) == order,
-    //                        "Different kinds of orderings!",
-    //                        ShowValue(nodeA),
-    //                        ShowValue(nodeB),
-    //                        ShowValue(iter->second.at(nodeB)),
-    //                        ShowValue(order));
-    //        }
-    //        else
-    //        {
-    //            iter->second.emplace(nodeB, order);
-    //        }
-    //    }
-    //}
 
     NodeOrdering ControlGraph::lookupOrder(IgnoreCachePolicy const, int nodeA, int nodeB) const
     {
