b6693fdb79e08eb1db9de94ad4aab5d04eb6a7cb
Dmitrii Polomin <dmitriy.polomin@dxc.com>
2026-03-04 12:45:39 +0200

    Updated licence and removed tests that weren't ran for CTest

--- projects/miopen/test/gtest/na_inference.cpp
+++ projects/miopen/test/gtest/na_inference.cpp
@@ -1,28 +1,5 @@
-/*******************************************************************************
- *
- * MIT License
- *
- * Copyright (c) 2025 Advanced Micro Devices, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- *******************************************************************************/
+// Copyright © Advanced Micro Devices, Inc., or its affiliates.
+// SPDX-License-Identifier:  MIT
 
 #include <gtest/gtest.h>
 #include "../fusionHost.hpp"
@@ -380,56 +357,26 @@ struct TestNameGenerator
 
 } // namespace
 
-using GPU_na_fusion_inference_test_I8    = na_fusion_inference_test<int8_t>;
 using GPU_na_fusion_inference_test_FP16  = na_fusion_inference_test<half_float::half>;
-using GPU_na_fusion_inference_test_BFP16 = na_fusion_inference_test<bfloat16>;
 using GPU_na_fusion_inference_test_FP32  = na_fusion_inference_test<float>;
-using GPU_na_fusion_inference_test_FP64  = na_fusion_inference_test<double>;
 
-TEST_P(GPU_na_fusion_inference_test_I8, TestInt8) { Run(); }
 TEST_P(GPU_na_fusion_inference_test_FP16, TestFloat16) { Run(); }
-TEST_P(GPU_na_fusion_inference_test_BFP16, TestBFloat16) { Run(); }
 TEST_P(GPU_na_fusion_inference_test_FP32, TestFloat32) { Run(); }
-TEST_P(GPU_na_fusion_inference_test_FP64, TestFloat64) { Run(); }
 
 INSTANTIATE_TEST_SUITE_P(Smoke,
-                         GPU_na_fusion_inference_test_I8,
-                         GetSmokeCases(),
-                         TestNameGenerator{});
-INSTANTIATE_TEST_SUITE_P(Smoke,
                          GPU_na_fusion_inference_test_FP16,
-                         GetSmokeCases(),
-                         TestNameGenerator{});
-INSTANTIATE_TEST_SUITE_P(Smoke,
-                         GPU_na_fusion_inference_test_BFP16,
                          GetSmokeCases(),
                          TestNameGenerator{});
 INSTANTIATE_TEST_SUITE_P(Smoke,
                          GPU_na_fusion_inference_test_FP32,
                          GetSmokeCases(),
                          TestNameGenerator{});
-INSTANTIATE_TEST_SUITE_P(Smoke,
-                         GPU_na_fusion_inference_test_FP64,
-                         GetSmokeCases(),
-                         TestNameGenerator{});
 
-INSTANTIATE_TEST_SUITE_P(Full,
-                         GPU_na_fusion_inference_test_I8,
-                         GetFullCases(),
-                         TestNameGenerator{});
 INSTANTIATE_TEST_SUITE_P(Full,
                          GPU_na_fusion_inference_test_FP16,
                          GetFullCases(),
                          TestNameGenerator{});
 INSTANTIATE_TEST_SUITE_P(Full,
-                         GPU_na_fusion_inference_test_BFP16,
-                         GetFullCases(),
-                         TestNameGenerator{});
-INSTANTIATE_TEST_SUITE_P(Full,
                          GPU_na_fusion_inference_test_FP32,
-                         GetFullCases(),
-                         TestNameGenerator{});
-INSTANTIATE_TEST_SUITE_P(Full,
-                         GPU_na_fusion_inference_test_FP64,
                          GetFullCases(),
                          TestNameGenerator{});
