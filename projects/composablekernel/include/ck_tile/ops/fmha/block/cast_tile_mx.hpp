e8254e051f8098c41518002353ba46ace6d2bed8
Zoltán Lakatos <zoltan.lakatos@streamhpc.com>
2026-03-06 10:34:38 +0000

    Revert "Replace muls of P and O acc with adjusting P scales"
    This reverts commit e7cc973833947efab98c441f211ed2824a548deb.

--- projects/composablekernel/include/ck_tile/ops/fmha/block/cast_tile_mx.hpp
+++ projects/composablekernel/include/ck_tile/ops/fmha/block/cast_tile_mx.hpp
@@ -12,11 +12,13 @@ template <index_t ScaleGranularity,
           typename DstTensor,
           typename DstScaleTensor,
           typename SrcTensor,
+          typename SrcElementFunc,
           typename ScaleFunc>
 CK_TILE_DEVICE void cast_tile_mx(DstTensor& dst_tensor,
                                  DstScaleTensor& dst_scale_tensor,
                                  const SrcTensor& src_tensor,
-                                 const ScaleFunc& scale_func = identity{})
+                                 const SrcElementFunc& src_element_func = identity{},
+                                 const ScaleFunc& scale_func            = identity{})
 {
     using DstDataType      = remove_cv_t<typename DstTensor::DataType>;
     using DstScaleDataType = remove_cv_t<typename DstScaleTensor::DataType>;
@@ -26,7 +28,11 @@ CK_TILE_DEVICE void cast_tile_mx(DstTensor& dst_tensor,
 
     constexpr index_t size = SrcTensor::get_thread_buffer_size();
 
-    const auto src_thread_buffer = cast_tile<float>(src_tensor).get_thread_buffer();
+    // Scale is calculated using src values before applying src_element_func so max_value is in the
+    // original range (i.e. [0, 1]). Then this scale is used to convert src values after applying
+    // src_element_func which maps [0, 1] to [0, numeric<DstDataType>::max()] so the whole range of
+    // DstDataType is used.
+    const auto src_scaled = tile_elementwise_in(src_element_func, src_tensor).get_thread_buffer();
 
     if constexpr(std::is_same_v<DstDataType, pk_fp4_t>)
     {
@@ -35,7 +41,8 @@ CK_TILE_DEVICE void cast_tile_mx(DstTensor& dst_tensor,
             // (1 lane, 32 per lane for fp4)
             float max_value = 0;
             static_for<0, 32, 1>{}([&](auto j) {
-                max_value = max(max_value, abs(src_thread_buffer[number<i * 32 + j>{}]));
+                const float v = type_convert<float>(src_tensor.get_thread_buffer()[i * 32 + j]);
+                max_value     = max(max_value, abs(v));
             });
 
             static_assert(std::is_same_v<DstScaleDataType, e8m0_t>);
@@ -58,26 +65,26 @@ CK_TILE_DEVICE void cast_tile_mx(DstTensor& dst_tensor,
                 vec_t x;
                 x = __builtin_amdgcn_cvt_scalef32_pk_fp4_f32(
                     x,
-                    src_thread_buffer[number<i * 32 + 8 * j + 0>{}],
-                    src_thread_buffer[number<i * 32 + 8 * j + 1>{}],
+                    src_scaled[number<i * 32 + 8 * j + 0>{}],
+                    src_scaled[number<i * 32 + 8 * j + 1>{}],
                     scale,
                     0); // byte 0
                 x = __builtin_amdgcn_cvt_scalef32_pk_fp4_f32(
                     x,
-                    src_thread_buffer[number<i * 32 + 8 * j + 2>{}],
-                    src_thread_buffer[number<i * 32 + 8 * j + 3>{}],
+                    src_scaled[number<i * 32 + 8 * j + 2>{}],
+                    src_scaled[number<i * 32 + 8 * j + 3>{}],
                     scale,
                     1); // byte 1
                 x = __builtin_amdgcn_cvt_scalef32_pk_fp4_f32(
                     x,
-                    src_thread_buffer[number<i * 32 + 8 * j + 4>{}],
-                    src_thread_buffer[number<i * 32 + 8 * j + 5>{}],
+                    src_scaled[number<i * 32 + 8 * j + 4>{}],
+                    src_scaled[number<i * 32 + 8 * j + 5>{}],
                     scale,
                     2); // byte 2
                 x = __builtin_amdgcn_cvt_scalef32_pk_fp4_f32(
                     x,
-                    src_thread_buffer[number<i * 32 + 8 * j + 6>{}],
-                    src_thread_buffer[number<i * 32 + 8 * j + 7>{}],
+                    src_scaled[number<i * 32 + 8 * j + 6>{}],
+                    src_scaled[number<i * 32 + 8 * j + 7>{}],
                     scale,
                     3); // byte 3
                 dst_tensor.get_thread_buffer().template set_as<vec_t>(number<i * 4 + j>{}, x);
@@ -99,7 +106,8 @@ CK_TILE_DEVICE void cast_tile_mx(DstTensor& dst_tensor,
             // (2 lanes, 16 per lane for fp8/bf8)
             float max_value = 0;
             static_for<0, 16, 1>{}([&](auto j) {
-                max_value = max(max_value, abs(src_thread_buffer[number<i * 16 + j>{}]));
+                const float v = type_convert<float>(src_tensor.get_thread_buffer()[i * 16 + j]);
+                max_value     = max(max_value, abs(v));
             });
             // 2 lanes, 16 values per lane share one scale
             max_value = max(max_value, warp_shuffle(max_value, lane ^ MLane));
@@ -126,14 +134,14 @@ CK_TILE_DEVICE void cast_tile_mx(DstTensor& dst_tensor,
                 {
                     x = __builtin_amdgcn_cvt_scalef32_pk_fp8_f32(
                         x,
-                        src_thread_buffer[number<i * 16 + 4 * j + 0>{}],
-                        src_thread_buffer[number<i * 16 + 4 * j + 1>{}],
+                        src_scaled[number<i * 16 + 4 * j + 0>{}],
+                        src_scaled[number<i * 16 + 4 * j + 1>{}],
                         scale,
                         false); // false -> WORD0
                     x = __builtin_amdgcn_cvt_scalef32_pk_fp8_f32(
                         x,
-                        src_thread_buffer[number<i * 16 + 4 * j + 2>{}],
-                        src_thread_buffer[number<i * 16 + 4 * j + 3>{}],
+                        src_scaled[number<i * 16 + 4 * j + 2>{}],
+                        src_scaled[number<i * 16 + 4 * j + 3>{}],
                         scale,
                         true); // true -> WORD1
                 }
@@ -141,14 +149,14 @@ CK_TILE_DEVICE void cast_tile_mx(DstTensor& dst_tensor,
                 {
                     x = __builtin_amdgcn_cvt_scalef32_pk_bf8_f32(
                         x,
-                        src_thread_buffer[number<i * 16 + 4 * j + 0>{}],
-                        src_thread_buffer[number<i * 16 + 4 * j + 1>{}],
+                        src_scaled[number<i * 16 + 4 * j + 0>{}],
+                        src_scaled[number<i * 16 + 4 * j + 1>{}],
                         scale,
                         false); // false -> WORD0
                     x = __builtin_amdgcn_cvt_scalef32_pk_bf8_f32(
                         x,
-                        src_thread_buffer[number<i * 16 + 4 * j + 2>{}],
-                        src_thread_buffer[number<i * 16 + 4 * j + 3>{}],
+                        src_scaled[number<i * 16 + 4 * j + 2>{}],
+                        src_scaled[number<i * 16 + 4 * j + 3>{}],
                         scale,
                         true); // true -> WORD1
                 }
@@ -174,9 +182,9 @@ CK_TILE_DEVICE void cast_tile_mx(DstTensor& dst_tensor,
             {
                 scale_result = scale;
             }
-            if constexpr(i % 2 == 1)
+            if(i % 2 == 1)
             {
-                dst_scale_tensor.get_thread_buffer()(number<i / 2>{}) =
+                dst_scale_tensor.get_thread_buffer()(i / 2) =
                     type_convert<DstScaleDataType>(scale_result);
             }
         });
