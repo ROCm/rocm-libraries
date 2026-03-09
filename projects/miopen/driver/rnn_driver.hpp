bafb18e12cf223c30380cdded8475144941e36cb
Vasily Maksimov <vasilii.maksimov@gmail.com>
2026-03-09 14:16:47 +0100

    fix Wnrvo for MIOpenDriver target

--- projects/miopen/driver/rnn_driver.hpp
+++ projects/miopen/driver/rnn_driver.hpp
@@ -344,7 +344,7 @@ std::vector<int> RNNDriver<Tgpu, Tref>::GetInputTensorLengthsFromCmdLine()
 
     in_n.push_back(in_h);
 
-    return in_n;
+    return std::move(in_n);
 }
 
 template <typename Tgpu, typename Tref>
