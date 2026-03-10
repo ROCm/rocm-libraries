a05a01fa7967f31fae6a64e904fe98fb72f132ad
John Tyler <jetyler@ualberta.ca>
2026-03-10 12:16:06 -0600

    Revert "Update projects/hipsolver/cmake/get-rocm-cmake.cmake to use Populate instead of MakeAvailable"
    This reverts commit e4b0aa81efc35a7d5cf4b1ed932e3b510d119ef3.

--- projects/hipsolver/cmake/get-rocm-cmake.cmake
+++ projects/hipsolver/cmake/get-rocm-cmake.cmake
@@ -18,8 +18,9 @@ if(NOT ROCmCMakeBuildTools_FOUND)
       rocm-cmake
       GIT_REPOSITORY https://github.com/ROCm/rocm-cmake.git
       GIT_TAG        ${rocm_cmake_tag}
+      SOURCE_SUBDIR "DISABLE ADDING TO BUILD" # We don't really want to consume the build and test targets of ROCm CMake.
     )
-    FetchContent_Populate(rocm-cmake)
+    FetchContent_MakeAvailable(rocm-cmake)
     list(APPEND CMAKE_MODULE_PATH "${rocm-cmake_SOURCE_DIR}/share/rocmcmakebuildtools/cmake")
     find_package(ROCmCMakeBuildTools REQUIRED)
   endif()
