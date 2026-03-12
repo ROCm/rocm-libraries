e6b5b722d739d158ab6adbca65ba3963750a6a84
kokolchin <kokolchin@luxoft.com>
2026-03-12 14:31:28 +0100

    Sync shared conv gtest helpers from part1 for standalone part2 CI

--- projects/miopen/test/gtest/compare_helper.hpp
+++ projects/miopen/test/gtest/compare_helper.hpp
@@ -1,39 +1,17 @@
-/*******************************************************************************
- *
- * MIT License
- *
- * Copyright (c) 2025 Advanced Micro Devices, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- *******************************************************************************/
+// Copyright © Advanced Micro Devices, Inc., or its affiliates.
+// SPDX-License-Identifier: MIT
 
 #pragma once
 
 #include <limits>
 #include <type_traits>
 #include <utility>
+#include <functional>
 #include <half/half.hpp>
 
 #include <iostream>
 
-#include "verify.hpp"
+#include "../verify.hpp"
 #include "../tensor_holder.hpp"
 
 namespace test_helpers {
@@ -117,6 +95,21 @@ auto CompareResults(VerifyT&& verifier, double tolerance = 80.f)
     if(!Compare(cpu_result, gpu_result, tolerance))
     {
         verifier.fail();
+    }
+
+    return std::make_pair(cpu_result, gpu_result);
+}
+
+template <class VerifyT, class OnFailFunc>
+auto CompareResults(VerifyT&& verifier, OnFailFunc&& on_fail, double tolerance = 80.f)
+    -> std::pair<decltype(verifier.cpu()), decltype(verifier.gpu())>
+{
+    const auto cpu_result = verifier.cpu();
+    const auto gpu_result = verifier.gpu();
+    if(!Compare(cpu_result, gpu_result, tolerance))
+    {
+        verifier.fail();
+        std::invoke(std::forward<OnFailFunc>(on_fail));
     }
 
     return std::make_pair(cpu_result, gpu_result);
