22b892aded2da83bbcf55d641f64f773fafc9a5e
Zoltán Lakatos <zoltan.lakatos@streamhpc.com>
2026-03-13 13:47:10 +0000

    fixed LSE errors on fmha mxfp8

--- projects/composablekernel/example/ck_tile/01_fmha/fmha_fwd_runner.hpp
+++ projects/composablekernel/example/ck_tile/01_fmha/fmha_fwd_runner.hpp
@@ -2306,8 +2306,8 @@ fwd_result fmha_fwd_run(mode_enum mode,
                 bool cur_pass = ck_tile::check_err(lse_host_result,
                                                    lse_host_ref,
                                                    "LSE Error: Incorrect results!",
-                                                   1e-4,
-                                                   1e-4,
+                                                   2e-4,
+                                                   2e-4,
                                                    /* allow_infinity_ref = */ true);
 
                 pass &= cur_pass;
