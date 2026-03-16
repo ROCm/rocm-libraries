bb3a101b99bca0d9b0ee7fbd221b33a165c376e9
Zoltán Lakatos <zoltan.lakatos@streamhpc.com>
2026-03-16 08:41:36 +0000

    increase fmha fp8 out threshold

--- projects/composablekernel/example/ck_tile/01_fmha/fmha_fwd_runner.hpp
+++ projects/composablekernel/example/ck_tile/01_fmha/fmha_fwd_runner.hpp
@@ -79,7 +79,7 @@ template <>
 auto get_elimit<FmhaFwdFp8Bf16>(std::string /*init_method*/)
 {
     double rtol = 1e-2;
-    double atol = 1.8e-1;
+    double atol = 1.9e-1;
     return ck_tile::make_tuple(rtol, atol);
 }
 
@@ -87,7 +87,7 @@ template <>
 auto get_elimit<FmhaFwdFp8Fp32>(std::string /*init_method*/)
 {
     double rtol = 1e-2;
-    double atol = 1.8e-1;
+    double atol = 1.9e-1;
     return ck_tile::make_tuple(rtol, atol);
 }
 
@@ -95,7 +95,7 @@ template <>
 auto get_elimit<FmhaFwdMxFp8>(std::string /*init_method*/)
 {
     double rtol = 1e-2;
-    double atol = 1.8e-1;
+    double atol = 1.9e-1;
     return ck_tile::make_tuple(rtol, atol);
 }
 
