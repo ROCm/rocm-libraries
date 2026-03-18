d2fd8b49e6cc82cf82adfa41887a799c53989a17
stulebae <stulebae@amd.com>
2026-03-18 10:35:46 +0100

    Update env.hpp

--- projects/miopen/src/include/miopen/env.hpp
+++ projects/miopen/src/include/miopen/env.hpp
@@ -154,12 +154,12 @@ private:
         static_assert(std::is_same_v<__struct_##_name, ::__struct_##_name>,        \
                       "MIOPEN_DECLARE_ENV* must be used in the global namespace"); \
         using value_type = _type;                                                  \
-        static miopen::env::detail::EnvVar<_type>& ref()                         \
+        static miopen::env::detail::EnvVar<_type>& ref()                           \
         {                                                                          \
-            static miopen::env::detail::EnvVar<_type> var{#_name, __VA_ARGS__};  \
+            static miopen::env::detail::EnvVar<_type> var{#_name, __VA_ARGS__};    \
             return var;                                                            \
         }                                                                          \
-        operator miopen::env::detail::EnvVar<_type>&() const { return ref(); }   \
+        operator miopen::env::detail::EnvVar<_type>&() const { return ref(); }     \
         operator bool() const { return ref().exist(); }                            \
         constexpr std::string_view GetName() const { return #_name; }              \
     } _name;
