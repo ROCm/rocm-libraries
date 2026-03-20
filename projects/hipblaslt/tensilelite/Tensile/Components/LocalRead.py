33aa4606d30effde8999d2b37c036ba6ca4663bf
tomchengchitang <tom.tang@amd.com>
2026-03-20 07:29:39 +0000

    Change the logic the same as develop

--- projects/hipblaslt/tensilelite/Tensile/Components/LocalRead.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/LocalRead.py
@@ -653,32 +653,7 @@ class LocalReadMFMA(LocalRead):
             useDirect32XEmulation = writer.states.a.useDirect32XEmulationThis if tc == "A" else writer.states.b.useDirect32XEmulationThis
         indexTranpose = lrvwTile > 1 and (not useTransposeCode)
 
-        # Change the logic of numSplitMetadata:
-        # Without Cap: numSplitMetadata = max(ceil((blockWidth * 4) // tP["bpeDS"]) - 1, 0) if tP["isM"] else 0
-        # 2nd formula: numSplitMetadata = max(ceil((blockWidth * 4) // (kernel["MIInputPerThread%s"%tc] * tP["bpeDS"])) - 1, 0) if tP["isM"] else 0
-        # With Cap as below is based on the following consideration:
-        # Compare:
-        # lrvwTile  MIInputPerThread    w/ cap             2nd          w/o cap
-        # Metadata      Metadata     numSplitMetadata     ditto         ditto
-        # --------  ----------------  -------------  --------------  -------------
-        #    1            2                0               0              0
-        #    1            4                0               0              0
-        #    2            2                1               0(X)           1
-        #    2            4                1               0(X)           1
-        #    4            2                3               1(X)           3
-        #    4            4                3               0(X)           3
-        #    8            2                3               3              7(->3, otherwise we need 8 registers for 4 elements which is more than 4 registers we have for metadata)
-        #    8            4                3               1              7(->3, ditto)
-        
         numSplitMetadata = max(ceil((blockWidth * 4) // tP["bpeDS"]) - 1, 0) if tP["isM"] else 0
-        # Cap numSplitMetadata to the number of available PackKForMV sgprs
-        if tP["isM"] and numSplitMetadata > 0:
-            if lrvwTile > 2:
-                numSplitMetadata = min(numSplitMetadata, 3)  # PackKForMV0-MV3
-            elif lrvwTile > 1:
-                numSplitMetadata = min(numSplitMetadata, 1)  # PackKForMV0-MV1
-            else:
-                numSplitMetadata = 0
 
         # caculate SMFMA layout
         blocksPerTGroupSMFMA = 1
@@ -1053,6 +1028,8 @@ class LocalReadMFMA(LocalRead):
                                                     vgprOffset = 0
                                                     for rIdx_ in range(0, numReadsPerUnroll*miInputGroup):
                                                         for elementIdx in range(0, numSplitMetadata+1):
+                                                            if elementIdx >= writer.states.bpr:
+                                                                break
                                                             # since the number of input thread is 4, so will alwasy be D0, D1, D2, D3
                                                             packCodeT.add(VPermB32(dst=vgpr("Valu%s_X%u_I%u+%u"%(tc, bufferIdx, iui, vgprIdx+elementIdx)), \
                                                                             src0=vgpr("Valu%s_X%u_I%u_D%u+%u"%(tc, bufferIdx, iui, 1, i+vIdx*numVgpr)), \
@@ -1073,6 +1050,8 @@ class LocalReadMFMA(LocalRead):
                                                     vgprOffset = 0
                                                     for rIdx_ in range(0, numReadsPerUnroll*miInputGroup):
                                                         for elementIdx in range(0, numSplitMetadata+1):
+                                                            if elementIdx >= writer.states.bpr:
+                                                                break
                                                             # since the number of input thread is 2, so will alwasy be D0 and D1
                                                             packCodeT.add(VPermB32(dst=vgpr("Valu%s_X%u_I%u+%u"%(tc, bufferIdx, iui, vgprIdx+elementIdx)), \
                                                                                     src0=vgpr("Valu%s_X%u_I%u_D%u+%u"%(tc, bufferIdx, iui, 1, i+vIdx*numVgpr)), \
@@ -1084,6 +1063,8 @@ class LocalReadMFMA(LocalRead):
                                                     vgprIdx_ = vgprIdx+vIdx*(numSplitMetadata+1)
                                                     packCodeT.add(VMovB32(dst=vgpr("Valu%s_X%u_I%u+%u"%(tc, bufferIdx, iui, vgprIdx_)), src=vgpr("Valu%s_X%u_I%u_D%u+%u"%(tc, bufferIdx, iui, 0, i+vIdx*numVgpr))))
                                                     for elementIdx in range(1, numSplitMetadata+1):
+                                                        if elementIdx >= writer.states.bpr:
+                                                            break
                                                         packCodeT.add(VMovB32(dst=vgpr("Valu%s_X%u_I%u+%u"%(tc, bufferIdx, iui, vgprIdx_ + elementIdx)), src=vgpr("Valu%s_X%u_I%u_D%u+%u"%(tc, bufferIdx, iui, 0, i+vIdx*numVgpr)), \
                                                                             comment="another VGPR storing lshr 8-bit value %d %d" %(vgprIdx, elementIdx)))
                                                         packCodeT.add(VLShiftRightB32(dst=vgpr("Valu%s_X%u_I%u+%u"%(tc, bufferIdx, iui, vgprIdx_+elementIdx)), \
