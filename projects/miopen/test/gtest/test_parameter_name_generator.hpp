f6b92ed4ea2aef7e2391cf8b36fdd1bc4f6b0e92
Tulebaev <stulebae@amd.com>
2026-03-21 19:44:18 +0100

    Fixed clang format violations

--- projects/miopen/test/gtest/test_parameter_name_generator.hpp
+++ projects/miopen/test/gtest/test_parameter_name_generator.hpp
@@ -163,7 +163,7 @@ static auto MakeNamedParameterValues(const std::string& name, T... values)
 //
 template <typename T>
     requires Container<T> && PrintableElement<T> && std::is_move_constructible_v<T>
-    [[maybe_unused]]
+[[maybe_unused]]
 static auto MakeNamedParameterCollectionValues(const std::string& name,
                                                const std::ranges::range auto& collection,
                                                const std::string& separator = " ")
@@ -182,7 +182,7 @@ static auto MakeNamedParameterCollectionValues(const std::string& name,
 
 template <typename T>
     requires NotContainer<T> && Printable<T> && std::is_move_constructible_v<T>
-    [[maybe_unused]]
+[[maybe_unused]]
 static auto MakeNamedParameterCollectionValues(const std::string& name,
                                                const std::ranges::range auto& collection)
 {
