ee3250f4ef6394c94b0fa559778bc45c7f7644ea
kiefer <kiefer.van.teutem@streamhpc.com>
2026-03-23 14:27:00 +0000

    Add support for CTranspose and add more consistency checks

--- projects/composablekernel/include/ck_tile/core/arch/mma/utility/tile_distribution_encoding_calculator.hpp
+++ projects/composablekernel/include/ck_tile/core/arch/mma/utility/tile_distribution_encoding_calculator.hpp
@@ -12,6 +12,7 @@
 #pragma once
 
 #include "ck_tile/core/tensor/tile_distribution.hpp"
+#include "ck_tile/core/arch/mma/utility/tile_distribution_encoding_register_mapper.hpp"
 
 namespace ck_tile::core::arch::mma {
 template <typename MmaOp,
@@ -21,6 +22,7 @@ template <typename MmaOp,
           index_t AttrNumAccessB = MmaOp::kBKNumAccess>
 struct TileDistrEncCalc
 {
+    private:
     static constexpr index_t NumAccessA = std::max(MmaOp::kAKNumAccess, AttrNumAccessA);
     static constexpr index_t NumAccessB = std::max(MmaOp::kBKNumAccess, AttrNumAccessB);
 
@@ -31,12 +33,10 @@ struct TileDistrEncCalc
 
     static_assert(MmaOp::kABKPerLane % NumAccessA == 0);
     static_assert(MmaOp::kABKPerLane % NumAccessB == 0);
-    static_assert(SFactor == 1, "Swizzle not implementeed yet."); // TODO: Implement Swizzle.
-    static_assert(CTranspose == false,
-                  "CTranspose not implemented yet."); // TODO: Implement CTranspose.
+    static_assert(SFactor == 1, "Swizzle not implemented yet."); // TODO: Implement Swizzle.
 
     template <index_t MajorDimSize, index_t Repeat, index_t NumAccess, index_t CompressionRatio = 1>
-    using ABWarpDstrEncoding = tile_distribution_encoding<
+    using ABWarpDstrEnc = tile_distribution_encoding<
         sequence<Repeat>,
         tuple<sequence<MajorDimSize>,
               sequence<NumAccess,
@@ -49,39 +49,60 @@ struct TileDistrEncCalc
 
     static constexpr auto get_cwarp_dstr_encoding()
     {
+        // We unmerge the M and N dimensions in the same way every time.
+        using MSubDims = sequence<MmaOp::kCMBlocks,
+                                  MmaOp::kCMNumAccess,
+                                  MmaOp::kM / MmaOp::kCMBlocks / MmaOp::kCMPerLane,
+                                  MmaOp::kCMPerLane / MmaOp::kCMNumAccess>;
+        using NSubDims = sequence<MmaOp::kCNBlocks, MmaOp::kN / MmaOp::kCNBlocks>;
+
+        // In case of CTranspose, all we do is swap the M and N dimension.
+        using MatDims =
+            std::conditional_t<CTranspose, tuple<NSubDims, MSubDims>, tuple<MSubDims, NSubDims>>;
+        constexpr int MInx = CTranspose ? 2 : 1;
+        constexpr int NInx = CTranspose ? 1 : 2;
+
+        // For MFMA intrinsics with blocks, the block dimensions might be in the Lane dim or in the
+        // Vec dim, so we get different merge orderings.
         if constexpr(MmaOp::CBlockDimInVecDim)
         {
-            return tile_distribution_encoding<
-                sequence<1>,
-                tuple<sequence<MmaOp::kCMBlocks,
-                               MmaOp::kCMNumAccess,
-                               MmaOp::kM / MmaOp::kCMBlocks / MmaOp::kCMPerLane,
-                               MmaOp::kCMPerLane / MmaOp::kCMNumAccess>,
-                      sequence<MmaOp::kCNBlocks, MmaOp::kN / MmaOp::kCNBlocks>>,
-                tuple<sequence<1, 2>>,
-                tuple<sequence<2, 1>>,
-                sequence<1, 2, 1, 1>,
-                sequence<0, 0, 1, 3>>{};
+            return tile_distribution_encoding<sequence<1>,
+                                              MatDims,
+                                              tuple<sequence<MInx, NInx>>,
+                                              tuple<sequence<2, 1>>,
+                                              sequence<MInx, NInx, MInx, MInx>,
+                                              sequence<0, 0, 1, 3>>{};
         }
         else
         {
-            return tile_distribution_encoding<
-                sequence<1>,
-                tuple<sequence<MmaOp::kCMBlocks,
-                               MmaOp::kCMNumAccess,
-                               MmaOp::kM / MmaOp::kCMBlocks / MmaOp::kCMPerLane,
-                               MmaOp::kCMPerLane / MmaOp::kCMNumAccess>,
-                      sequence<MmaOp::kCNBlocks, MmaOp::kN>>,
-                tuple<sequence<1, 2, 1, 2>>,
-                tuple<sequence<0, 0, 2, 1>>,
-                sequence<1, 1>,
-                sequence<1, 3>>{};
+            return tile_distribution_encoding<sequence<1>,
+                                              MatDims,
+                                              tuple<sequence<MInx, NInx, MInx, NInx>>,
+                                              tuple<sequence<0, 0, 2, 1>>,
+                                              sequence<MInx, MInx>,
+                                              sequence<1, 3>>{};
         }
     }
 
-    using AWarpDstrEncoding =
-        ABWarpDstrEncoding<MmaOp::kM, MmaOp::kARepeat, NumAccessA, MmaOp::kCompressionRatio>;
-    using BWarpDstrEncoding = ABWarpDstrEncoding<MmaOp::kN, MmaOp::kBRepeat, NumAccessB>;
+    using AEnc_ = ABWarpDstrEnc<MmaOp::kM, MmaOp::kARepeat, NumAccessA, MmaOp::kCompressionRatio>;
+    using BEnc_ = ABWarpDstrEnc<MmaOp::kN, MmaOp::kBRepeat, NumAccessB>;
+
+    public:
+    // When using CTranspose, the A and B matrices are swapped.
+    using AWarpDstrEncoding = std::conditional_t<CTranspose, BEnc_, AEnc_>;
+    using BWarpDstrEncoding = std::conditional_t<CTranspose, AEnc_, BEnc_>;
     using CWarpDstrEncoding = decltype(get_cwarp_dstr_encoding());
+
+    // Some additional consistency checks
+    static_assert(TileDistrEncRegMap<AWarpDstrEncoding>::num_lanes == MmaOp::WaveSize);
+    static_assert(TileDistrEncRegMap<BWarpDstrEncoding>::num_lanes == MmaOp::WaveSize);
+    static_assert(TileDistrEncRegMap<CWarpDstrEncoding>::num_lanes == MmaOp::WaveSize);
+
+    static_assert(TileDistrEncRegMap<AWarpDstrEncoding>::num_vector_items ==
+                  vector_traits<typename MmaOp::AVecType>::vector_size);
+    static_assert(TileDistrEncRegMap<BWarpDstrEncoding>::num_vector_items ==
+                  vector_traits<typename MmaOp::BVecType>::vector_size);
+    static_assert(TileDistrEncRegMap<CWarpDstrEncoding>::num_vector_items ==
+                  vector_traits<typename MmaOp::CVecType>::vector_size);
 };
 } // namespace ck_tile::core::arch::mma
