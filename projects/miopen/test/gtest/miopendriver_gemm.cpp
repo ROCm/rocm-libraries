2de7c91d1e4deae94c9deb52a0a978460a4f594b
vedenev-amd <mvedenev@amd.com>
2026-03-24 15:07:25 +0000

    disable back the gem test, dispate the it not included in previuse commit by some unclonw reason, live only /2 hip graph test for speed, now all fixed for the windows?

--- projects/miopen/test/gtest/miopendriver_gemm.cpp
+++ projects/miopen/test/gtest/miopendriver_gemm.cpp
@@ -54,13 +54,12 @@ class GPU_MIOpenDriverGemmTest_FP16 : public testing::TestWithParam<std::vector<
 
 void RunMIOpenDriver(const std::vector<TestCase>& testCases)
 {
-    // Temporary: enable Windows testing by commenting out GPU mask check
-    // using e_mask = enabled<Gpu::gfx94X, Gpu::gfx103X, Gpu::gfx110X>;
-    // using d_mask = disabled<Gpu::Default>;
-    // if(!ShouldRunMIOpenDriverTest<d_mask, e_mask>())
-    // {
-    //     GTEST_SKIP();
-    // }
+    using e_mask = enabled<Gpu::gfx94X, Gpu::gfx103X, Gpu::gfx110X>;
+    using d_mask = disabled<Gpu::Default>;
+    if(!ShouldRunMIOpenDriverTest<d_mask, e_mask>())
+    {
+        GTEST_SKIP();
+    }
 
     RunMIOpenDriverTestCommand(testCases);
 };
