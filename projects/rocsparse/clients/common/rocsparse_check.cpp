35c528ad8496845bfb18ac22009def1675639cd2
kliegeois <kimliegeois@ymail.com>
2026-03-24 21:37:43 -0700

    fix years and fix the check_integer cerr for complex

--- projects/rocsparse/clients/common/rocsparse_check.cpp
+++ projects/rocsparse/clients/common/rocsparse_check.cpp
@@ -1,6 +1,6 @@
 /*! \file */
 /* ************************************************************************
- * Copyright (C) 2021-2025 Advanced Micro Devices, Inc. All rights Reserved.
+ * Copyright (C) 2021-2026 Advanced Micro Devices, Inc. All rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -250,7 +250,8 @@ void check_integer(const T* a)
         if(real_part != std::round(real_part) || imag_part != std::round(imag_part))
         {
             std::cerr << "Value is not an integer: (" << real_part << ", " << imag_part << ")"
-                      << " (diffs: " << real_part << ", " << imag_part << ")" << std::endl;
+                      << " (diffs: " << real_part - std::round(real_part) << ", "
+                      << imag_part - std::round(imag_part) << ")" << std::endl;
             exit(EXIT_FAILURE);
         }
     }
