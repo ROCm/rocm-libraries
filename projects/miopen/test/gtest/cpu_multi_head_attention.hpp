be9c2f1f2043e3d206db0be6aa46fcad9848b680
Tulebaev <stulebae@amd.com>
2026-03-26 10:52:14 +0100

    Restored interface in cpu_multi_head_attention.hpp

--- projects/miopen/test/gtest/cpu_multi_head_attention.hpp
+++ projects/miopen/test/gtest/cpu_multi_head_attention.hpp
@@ -101,6 +101,8 @@ protected:
                                                      multi_head_attention, // o_val
                                                      dO_val,
                                                      softmax,
+                                                     attn_max,
+                                                     z_sum,
                                                      dQ_val,
                                                      dK_val,
                                                      dV_val);
