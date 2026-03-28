b1b2ca15d9bbecee9d9012bb4ee45f916641948d
illsilin_amdeng <Illia.Silin@amd.com>
2026-03-27 19:27:14 -0700

    fix clang format

--- projects/composablekernel/test/grouped_gemm/test_grouped_gemm_fixed_nk_bias.cpp
+++ projects/composablekernel/test/grouped_gemm/test_grouped_gemm_fixed_nk_bias.cpp
@@ -18,7 +18,7 @@
 #include <type_traits>
 #include <vector>
 
-static ck::index_t param_mask     = 0xffffff;
+static ck::index_t param_mask = 0xffffff;
 
 using FP32 = float;
 using FP16 = ck::half_t;
@@ -293,7 +293,7 @@ int main(int argc, char** argv)
     if(argc == 1) {}
     else if(argc == 2)
     {
-        param_mask     = strtol(argv[1], nullptr, 0);
+        param_mask = strtol(argv[1], nullptr, 0);
     }
     else
     {
