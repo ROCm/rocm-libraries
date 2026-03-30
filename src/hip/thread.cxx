c3cecf57519ef539e24e1c32c0b9bcd7c65b2139
Mcintosh, Daniel <Daniel.Mcintosh@amd.com>
2026-03-29 21:24:29 -0400

    [thread][libhipcxx] Replace reference to __libcpp_thread_sleep_for with __cccl_thread_sleep_for when CCCL version is 3.0 or newer (#87)
    libhipcxx/libcudacxx renamed the __libcpp_thread_sleep_for API to __cccl_thread_sleep_for
    
    ---------
    
    Co-authored-by: Marko Savic <marsavic@amd.com>

--- src/hip/thread.cxx
+++ src/hip/thread.cxx
@@ -20,7 +20,7 @@
 #include "hip/thread"
 
 #include <hip/atomic>
-// For cuda::std::__libcpp_thread_sleep_for
+// For cuda::std::__cccl_thread_sleep_for / cuda::std::__libcpp_thread_sleep_for(__ns)
 #include <hip/std/atomic>
 
 namespace cuda {
@@ -493,7 +493,11 @@ static __global__ void detachWorkNode(WorkNode_Header *oldWorkNode) {
 
 namespace this_thread {
 _LIBHIPTHREADS_EXPORTED_FROM_ABI __host__ __device__ void sleep_for(cuda::std::chrono::nanoseconds __ns) {
+#if CCCL_VERSION >= 3000000
+    cuda::std::__cccl_thread_sleep_for(__ns);
+#else
     cuda::std::__libcpp_thread_sleep_for(__ns);
+#endif
 }
 
 __device__ hip::thread::id get_id() noexcept {
