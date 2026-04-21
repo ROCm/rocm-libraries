16e9599bf26fde2e338d952f9935b464c380bab5
mousdahl-amd <mitch.ousdahl@amd.com>
2026-04-21 15:03:53 -0700

    Made conv tolerance dim check more forgiving

--- projects/hipdnn/test_sdk/include/hipdnn_test_sdk/utilities/DynamicTolerancesConv.hpp
+++ projects/hipdnn/test_sdk/include/hipdnn_test_sdk/utilities/DynamicTolerancesConv.hpp
@@ -124,7 +124,7 @@ float calculateConvWrwTolerance(double inputMin,
  * @param dyMax The maximum value in the output gradient tensor (dy).
  * @param wMin The minimum value in the filter weights tensor (w).
  * @param wMax The maximum value in the filter weights tensor (w).
- * @param wDims The dimensions of the filter weights tensor (w): [K, C, R, S] or [K, C, D, R, S].
+ * @param wDims The dimensions of the filter weights tensor (w): [K, C, S], [K, C, R, S], or [K, C, D, R, S].
  * @return The calculated tolerance value as float.
  */
 template <typename OutputType, typename InputType, typename ComputeType = float>
@@ -137,10 +137,9 @@ float calculateConvDgradTolerance(
     // Accumulation for input gradients (dx) happens over K (output channels) and Spatial filter dimensions.
     // dx[n, c, h, w] = sum_{k, r, s} dy[n, k, p, q] * w[k, c, r, s]
 
-    if(wDims.empty() || wDims.size() < 4)
+    if(wDims.empty() || wDims.size() < 2)
     {
-        throw std::invalid_argument(
-            "wDims must have at least 4 dimensions for 2D convolution [K, C, R, S].");
+        throw std::invalid_argument("wDims must have at least 2 dimensions (K, C).");
     }
 
     // Number of accumulations = K * (product of spatial dimensions)
