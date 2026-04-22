cbb9a3566755d87cfab587ae4ca2cdb2d9a16f67
Beatriz Navidad Vilches <beatriz@streamhpc.com>
2026-04-22 11:43:15 +0000

    Use [[maybe_unused]] for unused arguments

--- projects/hipcub/benchmark/benchmark_block_scan.cpp
+++ projects/hipcub/benchmark/benchmark_block_scan.cpp
@@ -43,9 +43,8 @@ struct inclusive_scan
 
     template<class T, unsigned int BlockSize, unsigned int ItemsPerThread>
     __device__
-    static void run(const T* input, T* output, const T init)
+    static void run(const T* input, T* output, [[maybe_unused]] const T init)
     {
-        (void)init;
         const unsigned int i = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
 
         T values[ItemsPerThread];
