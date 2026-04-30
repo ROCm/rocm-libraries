bc2f6ddd6804e97f50fae71d2937a5af1c7b8330
Koji Nakajima <knakajim@amd.com>
2026-04-30 15:18:44 +0000

    Small update for gfx950 mx tests + more - enable UseSubtileImpl for all gfx950 non subtile mx tests - skip all gfx950 mxfp8 - use MXScaleFormat=1 as default - set AssertSummationElementMultiple=256 for subtile mxfp4 - fix isSwizzledSubtile in computeLoadSrd

--- projects/hipblaslt/tensilelite/Tensile/SolutionStructs/Solution.py
+++ projects/hipblaslt/tensilelite/Tensile/SolutionStructs/Solution.py
@@ -523,7 +523,8 @@ class Solution(collections.abc.Mapping):
     # set ASEM=minASEMforMX for not TLUA or not TLUB
     # so far, kernel code can support 16, but host code cannot hanlde it
     # TODO: enable 16 (or less)
-    minASEMforMX = 32
+    # TODO: enable less than 256 for Subtile
+    minASEMforMX = 32 if not state["UseSubtileImpl"] else 256
     if (state["ProblemType"]["MXBlockA"] or state["ProblemType"]["MXBlockB"]) and \
        ((not state["ProblemType"]["TLUA"]) or (not state["ProblemType"]["TLUB"])):
       if state["AssertSummationElementMultiple"] % minASEMforMX != 0:
