3e04d55bb6177ea5dcdea530f50fb1fe48bbe8e0
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2026-05-04 16:42:20 -0600

    Fix bump script

--- projects/hipsolver/bump_hipsolver_version.sh
+++ projects/hipsolver/bump_hipsolver_version.sh
@@ -10,7 +10,7 @@ sed -i "s/${OLD_HIPSOLVER_VERSION}/${NEW_HIPSOLVER_VERSION}/g" CMakeLists.txt
 
 # for hipSOLVER library name
 OLD_HIPSOLVER_SOVERSION="1\.1"
-NEW_HIPSOLVER_SOVERSION="1.0"
+NEW_HIPSOLVER_SOVERSION="1.2"
 sed -i "s/${OLD_HIPSOLVER_SOVERSION}/${NEW_HIPSOLVER_SOVERSION}/g" library/CMakeLists.txt
 
 # for rocSOLVER package requirements
