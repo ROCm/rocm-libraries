b4af9b87b948b8937210818165a644ca3134b2e2
Adam Dickin <adam.dickin@amd.com>
2026-05-06 21:08:30 +0000

    fix tidy and test that failed when no devices

--- projects/hipdnn/backend/tests/descriptors/TestEngineConfigDescriptor.cpp
+++ projects/hipdnn/backend/tests/descriptors/TestEngineConfigDescriptor.cpp
@@ -89,6 +89,7 @@ protected:
 
     void SetUp() override
     {
+        SKIP_IF_NO_DEVICES();
         _engineConfigWrapper = createDescriptor<EngineConfigDescriptor>();
         _mockEngineWrapper = createDescriptor<MockEngineDescriptor>();
         _mockEngineBadTypeWrapper = createDescriptor<MockEngineDescriptor>();
