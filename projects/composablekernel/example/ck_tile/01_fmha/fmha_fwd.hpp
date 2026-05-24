d92c41aa633303ed522fa1c9dc0e745c392ebb84
ltqin <letaoqin@amd.com>
2026-05-24 13:36:34 +0000

    [CK Tile] Enable LSE output for fp8bf16 V3 FMHA kernels

--- projects/composablekernel/example/ck_tile/01_fmha/fmha_fwd.hpp
+++ projects/composablekernel/example/ck_tile/01_fmha/fmha_fwd.hpp
@@ -874,7 +874,7 @@ auto fmha_fwd_v3_create_kargs_and_grids(fmha_fwd_args args)
                                          args.q_descale_ptr,
                                          args.k_descale_ptr,
                                          args.v_descale_ptr,
-                                         nullptr, // lse_ptr
+                                         args.lse_ptr,
                                          args.o_ptr,
                                          args.seqstart_q_ptr,
                                          args.seqstart_k_ptr,
@@ -893,7 +893,7 @@ auto fmha_fwd_v3_create_kargs_and_grids(fmha_fwd_args args)
                                          args.nhead_stride_q,
                                          args.nhead_stride_k,
                                          args.nhead_stride_v,
-                                         0, // nhead_stride_lse
+                                         args.nhead_stride_lse,
                                          args.nhead_stride_o,
                                          args.window_size_left,
                                          args.window_size_right,
@@ -910,7 +910,7 @@ auto fmha_fwd_v3_create_kargs_and_grids(fmha_fwd_args args)
                                          args.q_descale_ptr,
                                          args.k_descale_ptr,
                                          args.v_descale_ptr,
-                                         nullptr, // lse_ptr
+                                         args.lse_ptr,
                                          args.o_ptr,
                                          args.seqlen_q,
                                          args.seqlen_k,
@@ -927,12 +927,12 @@ auto fmha_fwd_v3_create_kargs_and_grids(fmha_fwd_args args)
                                          args.nhead_stride_q,
                                          args.nhead_stride_k,
                                          args.nhead_stride_v,
-                                         0, // nhead_stride_lse
+                                         args.nhead_stride_lse,
                                          args.nhead_stride_o,
                                          args.batch_stride_q,
                                          args.batch_stride_k,
                                          args.batch_stride_v,
-                                         0, // batch_stride_lse
+                                         args.batch_stride_lse,
                                          args.batch_stride_o,
                                          args.window_size_left,
                                          args.window_size_right,
