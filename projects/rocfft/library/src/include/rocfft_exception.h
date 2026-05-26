1482bcf3a1ded4bbcddbe08e03a1849e081022e5
regan-amd <raphael.egan@amd.com>
2026-05-26 16:47:52 -0700

    [rocfft] Make rocfft_handle_exception log std::runtime_error's message

--- projects/rocfft/library/src/include/rocfft_exception.h
+++ projects/rocfft/library/src/include/rocfft_exception.h
@@ -21,7 +21,9 @@
 #ifndef ROCFFT_CATCH_H
 #define ROCFFT_CATCH_H
 
+#include "logging.h"
 #include "rocfft/rocfft.h"
+#include <stdexcept>
 
 static inline rocfft_status rocfft_handle_exception() noexcept
 try
@@ -31,6 +33,14 @@ try
 catch(rocfft_status e)
 {
     return e;
+}
+catch(const std::runtime_error& e)
+{
+    if(LOG_TRACE_ENABLED())
+    {
+        (*LogSingleton::GetInstance().GetTraceOS()) << e.what() << std::endl;
+    }
+    return rocfft_status_failure;
 }
 catch(...)
 {
