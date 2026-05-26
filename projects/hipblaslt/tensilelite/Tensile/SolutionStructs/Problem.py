49885a8cf3c6a355a2037af0426eff0940a6f591
George Tseng <george.tseng@amd.com>
2026-05-26 08:21:37 +0000

    Add reuseA/reuseB support to MFMA instruction for precise reuse bit control.

--- projects/hipblaslt/tensilelite/Tensile/SolutionStructs/Problem.py
+++ projects/hipblaslt/tensilelite/Tensile/SolutionStructs/Problem.py
@@ -1150,7 +1150,6 @@ class ProblemType(Mapping):
     state["TLUB"] = strideIdxB < unrollIdxB
     if state["MXBlockB"]:
       state["TLUMXSB"] = state["TLUB"]
-    #state["TLUB"] = True # hack
 
     if printIndexAssignmentInfo:
       print("TLUA:  %s (stridePosA(%d) <? unrollIdxA(%d)" % \
