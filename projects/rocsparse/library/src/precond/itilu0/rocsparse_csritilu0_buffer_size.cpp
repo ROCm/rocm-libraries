01bf3c199aca3c13501cea19072ece11fdc95e5c
YvanMokwinski <yvan.mokwinski@gmail.com>
2026-05-27 16:13:06 -0600

    clang on empty line reported by Kim Liegois.

--- projects/rocsparse/library/src/precond/itilu0/rocsparse_csritilu0_buffer_size.cpp
+++ projects/rocsparse/library/src/precond/itilu0/rocsparse_csritilu0_buffer_size.cpp
@@ -22,7 +22,6 @@
  *
  * ************************************************************************ */
 
-
 #include "rocsparse_csritilu0_buffer_size.hpp"
 #include "internal/precond/rocsparse_csritilu0.h"
 #include "rocsparse_common.hpp"
