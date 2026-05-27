e97c82c6ab0a5aa5cee73db2aca9d4ba2ee112ab
Dmitrii Polomin <dpolomin@amd.com>
2026-05-27 07:21:55 -0400

    Fixed some more inconsistencies

--- projects/miopen/test/gtest/conv3d_test_case.hpp
+++ projects/miopen/test/gtest/conv3d_test_case.hpp
@@ -69,7 +69,7 @@ struct Conv3DTestCase
     std::vector<size_t> GetInput() { return {N, C, img.z, img.y, img.x}; }
     std::vector<size_t> GetWeights()
     {
-        EXPECT_EQUAL(C % G, 0ULL);
+        EXPECT_EQUAL(C % G, 0U);
         return {K, C / G, filter.z, filter.y, filter.x};
     }
 
