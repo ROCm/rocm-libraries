c27066c05d03a8f792a3b30e3835b9ecf97044e2
kiefer <kiefer.van.teutem@streamhpc.com>
2026-05-28 07:09:50 +0000

    Fix for circular include issues caused by warp_gemm_params.hpp including all of core.hpp

--- projects/composablekernel/include/ck_tile/ops/gemm/warp/warp_gemm_params.hpp
+++ projects/composablekernel/include/ck_tile/ops/gemm/warp/warp_gemm_params.hpp
@@ -1,9 +1,7 @@
-// Copyright (c) Advanced Micro Devices, Inc. or its affiliates.
+// Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
 // SPDX-License-Identifier: MIT
 
 #pragma once
-
-#include "ck_tile/core.hpp"
 
 namespace ck_tile {
 
