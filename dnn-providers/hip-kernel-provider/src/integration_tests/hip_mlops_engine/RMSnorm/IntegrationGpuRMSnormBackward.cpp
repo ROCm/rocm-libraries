b145760d84d0afb868eb166b892c4e9b92067aab
Simeon Atanasov <simeon.atanasov@streamhpc.com>
2026-06-01 11:05:51 +0000

    Patch header path in RMSNorm Bwd test

--- dnn-providers/hip-kernel-provider/src/integration_tests/hip_mlops_engine/RMSnorm/IntegrationGpuRMSnormBackward.cpp
+++ dnn-providers/hip-kernel-provider/src/integration_tests/hip_mlops_engine/RMSnorm/IntegrationGpuRMSnormBackward.cpp
@@ -10,7 +10,7 @@
 #include <hipdnn_test_sdk/utilities/TestTolerances.hpp>
 #include <hipdnn_test_sdk/utilities/TestUtilities.hpp>
 
-#include "../IntegrationGraphVerificationHarness.hpp"
+#include "../../IntegrationGraphVerificationHarness.hpp"
 #include "RMSnormCommon.hpp"
 
 using namespace hipdnn_frontend;
