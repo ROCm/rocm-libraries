6190b080fcae1ed5388d8a20b08f859a0ce73c8b
Nick Breed <nick@streamhpc.com>
2026-06-03 13:05:31 +0000

    Add guards around dpp intrinsic for spirv

--- projects/rocprim/rocprim/include/rocprim/intrinsics/warp_shuffle.hpp
+++ projects/rocprim/rocprim/include/rocprim/intrinsics/warp_shuffle.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2025 Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2017-2026 Advanced Micro Devices, Inc. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -94,7 +94,16 @@ T warp_move_dpp(const T& input)
             //       __builtin_amdgcn_update_dpp, hence fail to parse the template altogether. (Except MSVC
             //       because even using /permissive- they somehow still do delayed parsing of the body of
             //       function templates, even though they pinky-swear they don't.)
-            return ::__builtin_amdgcn_mov_dpp(v, dpp_ctrl, row_mask, bank_mask, bound_ctrl);
+            if ROCPRIM_AMDGCN_CONSTEXPR(ROCPRIM_HAS_DPP())
+            {
+                return ::__builtin_amdgcn_mov_dpp(v, dpp_ctrl, row_mask, bank_mask, bound_ctrl);
+            }
+            else
+            {
+                assert(
+                    ROCPRIM_HAS_DPP()); // This function should not be used if dpp is not available.
+                return v;
+            }
         });
 }
 
