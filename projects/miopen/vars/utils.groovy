32fc694e8ec2ecac1f84925ab560649803ea69fb
Randy J. Spaulding <rspauldi@amd.com>
2026-06-10 02:12:32 +0000

    guess trailing whitespace is a thing now

--- projects/miopen/vars/utils.groovy
+++ projects/miopen/vars/utils.groovy
@@ -213,7 +213,7 @@ def getDockerImageName(dockerArgs)
         sh "echo ${env.THEROCK_CANDIDATE_IMAGE} >> ${env.WORKSPACE}/factors.txt"
     }
     def image = "${env.MIOPEN_DOCKER_IMAGE_URL}"
-    // Note: The following files and directories from the CK repo are used to generate a hash for 
+    // Note: The following files and directories from the CK repo are used to generate a hash for
     // the docker image build. To ensure that we rebuild the docker image only when necessary.
     // Add any other files or directories that should trigger a rebuild of the docker image when changed.
     sh """
@@ -728,10 +728,10 @@ def buildHipClangJob(Map conf=[:]){
             withDockerContainer(image: image, args: dockerOpts + " -v=${remote_root}:${remote_root}") {
                 timeout(time: build_timeout, unit:'MINUTES')
                 {
-                    // We set LOGNAME here because under the hood dvc calls Python's getpass.getuser() object to 
+                    // We set LOGNAME here because under the hood dvc calls Python's getpass.getuser() object to
                     // create a unique hash to store its local cache in. When Jenkins runs this Docker container, it
                     // runs as a UID that doesn't have an entry in /etc/passwd within the container. getuser() throws
-                    // an exception if it can't get the user name for the current user's UID, but it will check the 
+                    // an exception if it can't get the user name for the current user's UID, but it will check the
                     // LOGNAME environment variable and use that value if it's available.
                     // https://github.com/iterative/dvc/blob/3915fa26aa7d95d5cbe345e62846bfd82dccbfc7/dvc/repo/__init__.py#L646
                     // https://docs.python.org/3/library/getpass.html#getpass.getuser
