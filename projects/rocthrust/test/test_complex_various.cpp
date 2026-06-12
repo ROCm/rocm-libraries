3ad229c6789b15f4bbb994f036d943185390ffbc
Mátyás Aradi <matyas@streamhpc.com>
2026-06-12 14:39:36 +0200

    Use libhipcxx includes - remove infinity test

--- projects/rocthrust/test/test_complex_various.cpp
+++ projects/rocthrust/test/test_complex_various.cpp
@@ -1,6 +1,6 @@
 /*
  *  Copyright 2008-2013 NVIDIA Corporation
- *  Modifications Copyright© 2019-2025 Advanced Micro Devices, Inc. All rights reserved.
+ *  Modifications Copyright© 2019-2026 Advanced Micro Devices, Inc. All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -152,15 +152,6 @@ void run_trig_tests(
 //                            C99 MATH
 //
 // ===================================================================
-
-TYPED_TEST_SUITE(VariousComplexTest, FloatDouble);
-TYPED_TEST(VariousComplexTest, getInf)
-{
-  using T = typename TestFixture::T;
-
-  T inf = thrust::detail::complex::infinity<T>();
-  ASSERT_TRUE(std::isinf(inf));
-}
 
 #if defined _MSC_VER
 
