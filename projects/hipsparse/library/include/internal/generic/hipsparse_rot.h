15486479dc9b01fc45c4d21fe9b375947e381d1d
jsandham <jsandham@amd.com>
2026-06-12 19:48:37 +0000

    Add cuda 13 tests

--- projects/hipsparse/library/include/internal/generic/hipsparse_rot.h
+++ projects/hipsparse/library/include/internal/generic/hipsparse_rot.h
@@ -82,8 +82,8 @@ extern "C" {
 *  \retval HIPSPARSE_STATUS_INVALID_VALUE \p handle, \p c_coeff, \p s_coeff, \p vecX, or \p vecY is nullptr,
 *          or the vector sizes or data types are incompatible.
 */
-#if(!defined(CUDART_VERSION) || (CUDART_VERSION >= 11000 && CUDART_VERSION < 13000))
-DEPRECATED_CUDA_12000("The routine will be removed in CUDA 13")
+#if(!defined(CUDART_VERSION) || (CUDART_VERSION >= 11000 && CUDART_VERSION < 14000))
+DEPRECATED_CUDA_12000("The routine will be removed in a future CUDA release")
 HIPSPARSE_EXPORT
 hipsparseStatus_t hipsparseRot(hipsparseHandle_t     handle,
                                const void*           c_coeff,
