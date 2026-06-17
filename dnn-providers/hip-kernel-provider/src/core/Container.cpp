e454a33c8ae98732a579c356120c0527effedccc
Zhihui Du <zhdu@amd.com>
2026-06-16 22:34:37 -0700

    hipFlash2 fix: remove spurious kernelCompiler args from HipFlash2FwdPlanBuilder constructor

--- dnn-providers/hip-kernel-provider/src/core/Container.cpp
+++ dnn-providers/hip-kernel-provider/src/core/Container.cpp
@@ -80,8 +80,7 @@ const std::vector<Container::EngineDefinition>& Container::getEngineDefinitions(
              auto engine =
                  std::make_unique<hip_flash2_engine::HipFlash2Engine>(HIP_FLASH2_ENGINE_ID);
              engine->addPlanBuilder(
-                 std::make_unique<hip_flash2_engine::HipFlash2FwdPlanBuilder>(
-                     kernelCompiler, devicePropertyProvider));
+                 std::make_unique<hip_flash2_engine::HipFlash2FwdPlanBuilder>());
              return engine;
          }},
 #endif // HIPDNN_ENGINE_HIP_FLASH2
