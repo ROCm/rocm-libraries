41b6acfdcbbf889093bf4b3ea2ea5c76c3c5711a
Enrico Degregori <enrico@streamhpc.com>
2026-06-18 18:43:28 +0000

    Fix rebase

--- projects/composablekernel/include/ck_tile/ops/gemm/warp/warp_gemm.hpp
+++ projects/composablekernel/include/ck_tile/ops/gemm/warp/warp_gemm.hpp
@@ -548,13 +548,6 @@ using WarpGemmMfma_i32_16x16x32_i8_i8_CTransposed =
     WarpGemmImpl<WarpGemmAttributeMfmaTransposedCDistribution<
         WarpGemmAttributeMfmaImpl_i32_16x16x32_i8<WGAttrCtlEnum::Default_>>>;
 
-template <WGAttrNumAccessEnum AttrNumAccessA = WGAttrNumAccessEnum::Single,
-          WGAttrNumAccessEnum AttrNumAccessB = AttrNumAccessA>
-using WarpGemmMfma_i32_16x16x64_i8_i8 = WarpGemmImpl<
-    WarpGemmAttributeMfma<WarpGemmAttributeMfmaImpl_i32_16x16x64_i8<WGAttrCtlEnum::Default_>,
-                          AttrNumAccessA,
-                          AttrNumAccessB>>;
-
 template <WGAttrNumAccessEnum AttrNumAccess>
 using WarpGemmMfma_i32_16x16x64_i8_i8_CTransposed =
     WarpGemmImpl<WarpGemmAttributeMfmaTransposedCDistribution<
