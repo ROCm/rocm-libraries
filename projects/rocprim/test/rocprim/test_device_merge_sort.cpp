2d6857e48158d4823a0346b29ba295f249751d21
Mahovsky <jmahovsk@amd.com>
2026-06-18 15:37:01 -0400

    remove unused var

--- projects/rocprim/test/rocprim/test_device_merge_sort.cpp
+++ projects/rocprim/test/rocprim/test_device_merge_sort.cpp
@@ -347,8 +347,6 @@ TYPED_TEST(RocprimDeviceSortTests, SortKeyValue)
         unsigned int seed_value = seed_index < random_seeds_count  ? rand() : seeds[seed_index - random_seeds_count];
         SCOPED_TRACE(testing::Message() << "with seed = " << seed_value);
 
-        auto sizes = test_utils::get_sizes(seed_value);
-
         for(size_t size : test_utils::get_sizes(seed_value))
         {
             SCOPED_TRACE(testing::Message() << "with size = " << size);
