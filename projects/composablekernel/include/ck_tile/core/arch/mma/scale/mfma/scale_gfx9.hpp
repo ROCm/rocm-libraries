7f46742b52b8fe7574dac20ed7374605eb4cbbfc
kiefer <kiefer.van.teutem@streamhpc.com>
2026-06-25 08:49:54 +0000

    Add some mixed precision gfx9 scale intrinsics (f8bf8 bf8f8)

--- projects/composablekernel/include/ck_tile/core/arch/mma/scale/mfma/scale_gfx9.hpp
+++ projects/composablekernel/include/ck_tile/core/arch/mma/scale/mfma/scale_gfx9.hpp
@@ -10,8 +10,6 @@
 #include "ck_tile/core/arch/mma/scale/scale_traits.hpp"
 #include "ck_tile/core/config.hpp"
 #include "ck_tile/core/numeric/float8.hpp"
-#include "ck_tile/core/numeric/integer.hpp"
-#include "ck_tile/core/numeric/pk_f6.hpp"
 #include "ck_tile/core/numeric/pk_fp4.hpp"
 #include "ck_tile/core/numeric/vector_type.hpp"
 #include "ck_tile/core/utility/bit_cast.hpp"
@@ -20,19 +18,14 @@
 namespace ck_tile::core::arch::mma {
 
 /**
- * @defgroup scale_mfma_gfx9 Scale MFMA for GFX9
- * @brief Scale specializations of @ref amdgcn_mma for GFX9 family.
+ * @struct amdgcn_mma
+ * @brief Specialization of amdgcn_mma for Scale MFMA on GFX950 targets
  *
- * Template parameters A/B/C denote input/output types,
- * M/N/K are the fragment (MmaTile) sizes,
- * and `enable_if_target_*` restricts the specialization to specific GPU targets.
+ * This specialization implements the Scale MFMA instruction for fp8_t A and B
+ * matrices with fp32_t accumulator, with 16x16x128 block sizes.
  *
- * @tparam CompilerTarget Current compiler target.
- *
- * @sa amdgcn_mma_base for base template parameter documentation.
- * @{
+ * @tparam CompilerTarget Current compiler target
  */
-
 // TODO: c++20 template <amdgcn_target CompilerTarget>
 // TODO: c++20 requires
 
@@ -152,4 +145,349 @@ DEFINE_MMA_SCALE_GFX950_32(pk_fp4_t,    pk_fp4_t,    MMA_SCALE_ARG_F4, MMA_SCALE
 #undef DEFINE_MMA_SCALE_GFX950_32
 // clang-format on
 
+=======
+template <typename CompilerTarget>
+// clang-format off
+struct amdgcn_mma<fp8_t, fp8_t, fp32_t, 16u, 16u, 128u, CompilerTarget, MmaOpFamily::SCALE, enable_if_target_id_t<CompilerTarget, amdgcn_target_id::GFX950>>
+: amdgcn_mma_base<fp8_t, fp8_t, fp32_t, 16u, 16u, 128u, 64u, 32, 2, 1, 2, 1, 4, 1, MfmaOp, MmaOpFamily::SCALE>
+//               | A B C DataTypes    | MNK + WaveSize     |AParams  |BPar |CPar |
+// clang-format on
+{
+    static constexpr const char* instruction_name =
+        "__builtin_amdgcn_mfma_scale_f32_16x16x128_f8f6f4";
+
+    template <typename... Params>
+    CK_TILE_DEVICE static CVecType
+    exec(AVecType const& aVec, BVecType const& bVec, CVecType const& cVec, int scale_A, int scale_B)
+    {
+        using P = WarpGemmParamsParser<Params...>;
+        return {__builtin_amdgcn_mfma_scale_f32_16x16x128_f8f6f4(
+            bit_cast<int32x8_t>(aVec),
+            bit_cast<int32x8_t>(bVec),
+            cVec,
+            scale::detail::ScaleDataTypeToFlag_v<fp8_t>,
+            scale::detail::ScaleDataTypeToFlag_v<fp8_t>,
+            P::op_sel_a,
+            scale_A,
+            P::op_sel_b,
+            scale_B)};
+    }
+};
+
+    static constexpr const char* instruction_name =
+        "__builtin_amdgcn_mfma_scale_f32_16x16x128_f8f6f4";
+
+    template <typename... Params>
+    CK_TILE_DEVICE static CVecType
+    exec(AVecType const& aVec, BVecType const& bVec, CVecType const& cVec, int scale_A, int scale_B)
+    {
+        using P = WarpGemmParamsParser<Params...>;
+        return {__builtin_amdgcn_mfma_scale_f32_16x16x128_f8f6f4(
+            int32x8_t{aVec.data[0], aVec.data[1], aVec.data[2], aVec.data[3], aVec.data[4], aVec.data[5], 0, 0},
+            int32x8_t{bVec.data[0], bVec.data[1], bVec.data[2], bVec.data[3], bVec.data[4], bVec.data[5], 0, 0},
+            cVec,
+            scale::detail::ScaleDataTypeToFlag_v<pk_fp6x16_t>,
+            scale::detail::ScaleDataTypeToFlag_v<pk_fp6x16_t>,
+            P::op_sel_a,
+            scale_A,
+            P::op_sel_b,
+            scale_B)};
+    }
+};
+// clang-format on
+
+/**
+ * @struct amdgcn_mma
+ * @brief Specialization of amdgcn_mma for Scale MFMA on GFX950 targets
+ *
+ * This specialization implements the Scale MFMA instruction for pk_bf6x16_t A and B
+ * matrices with fp32_t accumulator, with 16x16x128 block sizes.
+ *
+ * @tparam CompilerTarget Current compiler target
+ */
+// TODO: c++20 template <amdgcn_target CompilerTarget>
+// TODO: c++20 requires
+template <typename CompilerTarget>
+// clang-format off
+struct amdgcn_mma<pk_bf6x16_t, pk_bf6x16_t, fp32_t, 16u, 16u, 128u, CompilerTarget, MmaOpFamily::SCALE, enable_if_target_id_t<CompilerTarget, amdgcn_target_id::GFX950>>
+: amdgcn_mma_base<pk_bf6x16_t, pk_bf6x16_t, fp32_t, 16u, 16u, 128u, 64u, 32, 1, 1, 1, 1, 4, 1, MfmaOp, MmaOpFamily::SCALE>
+//               | A B C DataTypes                | MNK + WaveSize     |AParams  |BPar |CPar |
+{
+    static constexpr const char* instruction_name =
+        "__builtin_amdgcn_mfma_scale_f32_16x16x128_f8f6f4";
+    
+    template <typename... Params>
+    CK_TILE_DEVICE static CVecType
+    exec(AVecType const& aVec, BVecType const& bVec, CVecType const& cVec, int scale_A, int scale_B)
+    {
+        using P = WarpGemmParamsParser<Params...>;
+        return {__builtin_amdgcn_mfma_scale_f32_16x16x128_f8f6f4(
+            int32x8_t{aVec.data[0], aVec.data[1], aVec.data[2], aVec.data[3], aVec.data[4], aVec.data[5], 0, 0},
+            int32x8_t{bVec.data[0], bVec.data[1], bVec.data[2], bVec.data[3], bVec.data[4], bVec.data[5], 0, 0},
+            cVec,
+            scale::detail::ScaleDataTypeToFlag_v<pk_bf6x16_t>,
+            scale::detail::ScaleDataTypeToFlag_v<pk_bf6x16_t>,
+            P::op_sel_a,
+            scale_A,
+            P::op_sel_b,
+            scale_B)};
+    }
+};
+// clang-format on
+
+/**
+ * @struct amdgcn_mma
+ * @brief Specialization of amdgcn_mma for Scale MFMA on GFX950 targets
+ *
+ * This specialization implements the Scale MFMA instruction for fp8_t A and B
+ * matrices with fp32_t accumulator, with 32x32x64 block sizes.
+ *
+ * @tparam CompilerTarget Current compiler target
+ */
+// TODO: c++20 template <amdgcn_target CompilerTarget>
+// TODO: c++20 requires
+template <typename CompilerTarget>
+// clang-format off
+struct amdgcn_mma<fp8_t, fp8_t, fp32_t, 32u, 32u, 64u, CompilerTarget, MmaOpFamily::SCALE, enable_if_target_id_t<CompilerTarget, amdgcn_target_id::GFX950>>
+: amdgcn_mma_base<fp8_t, fp8_t, fp32_t, 32u, 32u, 64u, 64u, 32, 2, 1, 2, 1, 16, 4, MfmaOp, MmaOpFamily::SCALE>
+//               | A B C DataTypes    | MNK + WaveSize    |AParams  |BPar |CPar  |
+// clang-format on
+{
+    static constexpr const char* instruction_name =
+        "__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4";
+
+    template <typename... Params>
+    CK_TILE_DEVICE static CVecType
+    exec(AVecType const& aVec, BVecType const& bVec, CVecType const& cVec, int scale_A, int scale_B)
+    {
+        using P = WarpGemmParamsParser<Params...>;
+        return {__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4(
+            bit_cast<int32x8_t>(aVec),
+            bit_cast<int32x8_t>(bVec),
+            cVec,
+            scale::detail::ScaleDataTypeToFlag_v<fp8_t>,
+            scale::detail::ScaleDataTypeToFlag_v<fp8_t>,
+            P::op_sel_a,
+            scale_A,
+            P::op_sel_b,
+            scale_B)};
+    }
+};
+
+/**
+ * @struct amdgcn_mma
+ * @brief Specialization of amdgcn_mma for Scale MFMA on GFX950 targets
+ *
+ * This specialization implements the Scale MFMA instruction for bf8_t A and B
+ * matrices with fp32_t accumulator, with 32x32x64 block sizes.
+ *
+ * @tparam CompilerTarget Current compiler target
+ */
+// TODO: c++20 template <amdgcn_target CompilerTarget>
+// TODO: c++20 requires
+template <typename CompilerTarget>
+// clang-format off
+struct amdgcn_mma<bf8_t, bf8_t, fp32_t, 32u, 32u, 64u, CompilerTarget, MmaOpFamily::SCALE, enable_if_target_id_t<CompilerTarget, amdgcn_target_id::GFX950>>
+: amdgcn_mma_base<bf8_t, bf8_t, fp32_t, 32u, 32u, 64u, 64u, 32, 2, 1, 2, 1, 16, 4, MfmaOp, MmaOpFamily::SCALE>
+//               | A B C DataTypes    | MNK + WaveSize    |AParams  |BPar |CPar  |
+// clang-format on
+{
+    static constexpr const char* instruction_name =
+        "__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4";
+
+    template <typename... Params>
+    CK_TILE_DEVICE static CVecType
+    exec(AVecType const& aVec, BVecType const& bVec, CVecType const& cVec, int scale_A, int scale_B)
+    {
+        using P = WarpGemmParamsParser<Params...>;
+        return {__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4(
+            bit_cast<int32x8_t>(aVec),
+            bit_cast<int32x8_t>(bVec),
+            cVec,
+            scale::detail::ScaleDataTypeToFlag_v<bf8_t>,
+            scale::detail::ScaleDataTypeToFlag_v<bf8_t>,
+            P::op_sel_a,
+            scale_A,
+            P::op_sel_b,
+            scale_B)};
+    }
+};
+
+template <typename CompilerTarget>
+// clang-format off
+struct amdgcn_mma<fp8_t, bf8_t, fp32_t, 32u, 32u, 64u, CompilerTarget, MmaOpFamily::SCALE, enable_if_target_id_t<CompilerTarget, amdgcn_target_id::GFX950>>
+: amdgcn_mma_base<fp8_t, bf8_t, fp32_t, 32u, 32u, 64u, 64u, 32, 2, 1, 2, 1, 16, 4, MfmaOp, MmaOpFamily::SCALE>
+//               | A B C DataTypes    | MNK + WaveSize    |AParams  |BPar |CPar  |
+// clang-format on
+{
+    static constexpr const char* instruction_name =
+        "__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4";
+
+    template <typename... Params>
+    CK_TILE_DEVICE static CVecType
+    exec(AVecType const& aVec, BVecType const& bVec, CVecType const& cVec, int scale_A, int scale_B)
+    {
+        using P = WarpGemmParamsParser<Params...>;
+        return {__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4(
+            bit_cast<int32x8_t>(aVec),
+            bit_cast<int32x8_t>(bVec),
+            cVec,
+            scale::detail::ScaleDataTypeToFlag_v<fp8_t>,
+            scale::detail::ScaleDataTypeToFlag_v<bf8_t>,
+            P::op_sel_a,
+            scale_A,
+            P::op_sel_b,
+            scale_B)};
+    }
+};
+
+template <typename CompilerTarget>
+// clang-format off
+struct amdgcn_mma<bf8_t, fp8_t, fp32_t, 32u, 32u, 64u, CompilerTarget, MmaOpFamily::SCALE, enable_if_target_id_t<CompilerTarget, amdgcn_target_id::GFX950>>
+: amdgcn_mma_base<bf8_t, fp8_t, fp32_t, 32u, 32u, 64u, 64u, 32, 2, 1, 2, 1, 16, 4, MfmaOp, MmaOpFamily::SCALE>
+//               | A B C DataTypes    | MNK + WaveSize    |AParams  |BPar |CPar  |
+// clang-format on
+{
+    static constexpr const char* instruction_name =
+        "__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4";
+
+    template <typename... Params>
+    CK_TILE_DEVICE static CVecType
+    exec(AVecType const& aVec, BVecType const& bVec, CVecType const& cVec, int scale_A, int scale_B)
+    {
+        using P = WarpGemmParamsParser<Params...>;
+        return {__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4(
+            bit_cast<int32x8_t>(aVec),
+            bit_cast<int32x8_t>(bVec),
+            cVec,
+            scale::detail::ScaleDataTypeToFlag_v<bf8_t>,
+            scale::detail::ScaleDataTypeToFlag_v<fp8_t>,
+            P::op_sel_a,
+            scale_A,
+            P::op_sel_b,
+            scale_B)};
+    }
+};
+
+/**
+ * @struct amdgcn_mma
+ * @brief Specialization of amdgcn_mma for Scale MFMA on GFX950 targets
+ *
+ * This specialization implements the Scale MFMA instruction for pk_fp4_t A and B
+ * matrices with fp32_t accumulator, with 32x32x64 block sizes.
+ *
+ * @tparam CompilerTarget Current compiler target
+ */
+// TODO: c++20 template <amdgcn_target CompilerTarget>
+// TODO: c++20 requires
+template <typename CompilerTarget>
+// clang-format off
+struct amdgcn_mma<pk_fp4_t, pk_fp4_t, fp32_t, 32u, 32u, 64u, CompilerTarget, MmaOpFamily::SCALE, enable_if_target_id_t<CompilerTarget, amdgcn_target_id::GFX950>> 
+: amdgcn_mma_base<pk_fp4_t, pk_fp4_t, fp32_t, 32u, 32u, 64u, 64u, 32, 1, 1, 1, 1, 16, 4, MfmaOp, MmaOpFamily::SCALE>
+//               | A B C DataTypes          | MNK + WaveSize    |AParams  |BPar |CPar  |
+// clang-format on
+{
+    static constexpr const char* instruction_name =
+        "__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4";
+
+    template <typename... Params>
+    CK_TILE_DEVICE static CVecType
+    exec(AVecType const& aVec, BVecType const& bVec, CVecType const& cVec, int scale_A, int scale_B)
+    {
+        using P         = WarpGemmParamsParser<Params...>;
+        int32x4_t arg_a = bit_cast<int32x4_t>(aVec);
+        int32x4_t arg_b = bit_cast<int32x4_t>(bVec);
+
+        return {__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4(
+            int32x8_t{arg_a[0], arg_a[1], arg_a[2], arg_a[3], 0, 0, 0, 0},
+            int32x8_t{arg_b[0], arg_b[1], arg_b[2], arg_b[3], 0, 0, 0, 0},
+            cVec,
+            scale::detail::ScaleDataTypeToFlag_v<pk_fp4_t>,
+            scale::detail::ScaleDataTypeToFlag_v<pk_fp4_t>,
+            P::op_sel_a,
+            scale_A,
+            P::op_sel_b,
+            scale_B)};
+    }
+};
+
+/**
+ * @struct amdgcn_mma
+ * @brief Specialization of amdgcn_mma for Scale MFMA on GFX950 targets
+ *
+ * This specialization implements the Scale MFMA instruction for pk_fp6x16_t A and B
+ * matrices with fp32_t accumulator, with 32x32x64 block sizes.
+ *
+ * @tparam CompilerTarget Current compiler target
+ */
+// TODO: c++20 template <amdgcn_target CompilerTarget>
+// TODO: c++20 requires
+template <typename CompilerTarget>
+// clang-format off
+struct amdgcn_mma<pk_fp6x16_t, pk_fp6x16_t, fp32_t, 32u, 32u, 64u, CompilerTarget, MmaOpFamily::SCALE, enable_if_target_id_t<CompilerTarget, amdgcn_target_id::GFX950>>
+: amdgcn_mma_base<pk_fp6x16_t, pk_fp6x16_t, fp32_t, 32u, 32u, 64u, 64u, 32, 1, 1, 1, 1, 16, 4, MfmaOp, MmaOpFamily::SCALE>
+//               | A B C DataTypes                | MNK + WaveSize    |AParams  |BPar |CPar  |
+{
+    static constexpr const char* instruction_name =
+        "__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4";
+
+    template <typename... Params>
+    CK_TILE_DEVICE static CVecType
+    exec(AVecType const& aVec, BVecType const& bVec, CVecType const& cVec, int scale_A, int scale_B)
+    {
+        using P = WarpGemmParamsParser<Params...>;
+        return {__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4(
+            int32x8_t{aVec.data[0], aVec.data[1], aVec.data[2], aVec.data[3], aVec.data[4], aVec.data[5], 0, 0},
+            int32x8_t{bVec.data[0], bVec.data[1], bVec.data[2], bVec.data[3], bVec.data[4], bVec.data[5], 0, 0},
+            cVec,
+            scale::detail::ScaleDataTypeToFlag_v<pk_fp6x16_t>,
+            scale::detail::ScaleDataTypeToFlag_v<pk_fp6x16_t>,
+            P::op_sel_a,
+            scale_A,
+            P::op_sel_b,
+            scale_B)};
+    }
+};
+// clang-format on
+
+/**
+ * @struct amdgcn_mma
+ * @brief Specialization of amdgcn_mma for Scale MFMA on GFX950 targets
+ *
+ * This specialization implements the Scale MFMA instruction for pk_bf6x16_t A and B
+ * matrices with fp32_t accumulator, with 32x32x64 block sizes.
+ *
+ * @tparam CompilerTarget Current compiler target
+ */
+// TODO: c++20 template <amdgcn_target CompilerTarget>
+// TODO: c++20 requires
+template <typename CompilerTarget>
+// clang-format off
+struct amdgcn_mma<pk_bf6x16_t, pk_bf6x16_t, fp32_t, 32u, 32u, 64u, CompilerTarget, MmaOpFamily::SCALE, enable_if_target_id_t<CompilerTarget, amdgcn_target_id::GFX950>>
+: amdgcn_mma_base<pk_bf6x16_t, pk_bf6x16_t, fp32_t, 32u, 32u, 64u, 64u, 32, 1, 1, 1, 1, 16, 4, MfmaOp, MmaOpFamily::SCALE>
+//               | A B C DataTypes                | MNK + WaveSize    |AParams  |BPar |CPar  |
+{
+    static constexpr const char* instruction_name =
+        "__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4";
+
+    template <typename... Params>
+    CK_TILE_DEVICE static CVecType
+    exec(AVecType const& aVec, BVecType const& bVec, CVecType const& cVec, int scale_A, int scale_B)
+    {
+        using P = WarpGemmParamsParser<Params...>;
+        return {__builtin_amdgcn_mfma_scale_f32_32x32x64_f8f6f4(
+            int32x8_t{aVec.data[0], aVec.data[1], aVec.data[2], aVec.data[3], aVec.data[4], aVec.data[5], 0, 0},
+            int32x8_t{bVec.data[0], bVec.data[1], bVec.data[2], bVec.data[3], bVec.data[4], bVec.data[5], 0, 0},
+            cVec,
+            scale::detail::ScaleDataTypeToFlag_v<pk_bf6x16_t>,
+            scale::detail::ScaleDataTypeToFlag_v<pk_bf6x16_t>,
+            P::op_sel_a,
+            scale_A,
+            P::op_sel_b,
+            scale_B)};
+    }
+};
+// clang-format on
+
+>>>>>>> 25b558dce0f (Add some mixed precision gfx9 scale intrinsics (f8bf8 bf8f8))
 } // namespace ck_tile::core::arch::mma
