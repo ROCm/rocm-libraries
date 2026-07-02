6a0b4eb52847bf02065a8f462578322bf424b66e
Mátyás Aradi <matyas@streamhpc.com>
2026-07-02 18:20:53 +0200

    Add thrust::strided_iterator and a step for thrust::counting_iterator

--- projects/rocthrust/test/test_counting_iterator.cpp
+++ projects/rocthrust/test/test_counting_iterator.cpp
@@ -21,14 +21,17 @@
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/sort.h>
 
+#include _THRUST_STD_INCLUDE(__algorithm_)
+#include _THRUST_STD_INCLUDE(iterator)
+#include _THRUST_STD_INCLUDE(type_traits)
+
+#include <complex>
 #include <cstdint>
+#include <numeric>
 
 #include "test_param_fixtures.hpp"
 #include "test_real_assertions.hpp"
 #include "test_utils.hpp"
-
-#include _THRUST_STD_INCLUDE(iterator)
-#include _THRUST_STD_INCLUDE(type_traits)
 
 TESTS_DEFINE(CountingIteratorTests, NumericalTestsParams);
 
@@ -328,6 +331,64 @@ TEST(CountingIteratorTests, TestCountingIteratorDifference)
   Iterator last = first + diff;
 
   ASSERT_EQ(diff, last - first);
+}
+
+TEST(CountingIteratorTests, TestCountingIteratorDynamicStride)
+{
+  auto iter = thrust::make_counting_iterator(0, 2);
+  static_assert(sizeof(iter) == 2 * sizeof(int));
+
+  ASSERT_EQ(*iter, 0);
+  iter++;
+  ASSERT_EQ(*iter, 2);
+  iter++;
+  iter++;
+  ASSERT_EQ(*iter, 6);
+  iter += 5;
+  ASSERT_EQ(*iter, 16);
+  iter -= 10;
+  ASSERT_EQ(*iter, -4);
+}
+
+TEST(CountingIteratorTests, TestCountingIteratorStaticStride)
+{
+  auto iter = thrust::make_counting_iterator<2>(0);
+  static_assert(sizeof(decltype(iter)) == sizeof(int));
+
+  ASSERT_EQ(*iter, 0);
+  iter++;
+  ASSERT_EQ(*iter, 2);
+  iter++;
+  iter++;
+  ASSERT_EQ(*iter, 6);
+  iter += 5;
+  ASSERT_EQ(*iter, 16);
+  iter -= 10;
+  ASSERT_EQ(*iter, -4);
+}
+
+TEST(CountingIteratorTests, TestCountingIteratorPointer)
+{
+  int arr[11];
+  std::iota(arr, arr + 11, 0);
+
+  auto iter = thrust::make_counting_iterator(&arr[2]);
+
+  ASSERT_EQ(*iter, &arr[2]);
+  ASSERT_EQ(**iter, 2);
+  iter++;
+  ASSERT_EQ(*iter, &arr[3]);
+  ASSERT_EQ(**iter, 3);
+  iter++;
+  iter++;
+  ASSERT_EQ(*iter, &arr[5]);
+  ASSERT_EQ(**iter, 5);
+  iter += 5;
+  ASSERT_EQ(*iter, &arr[10]);
+  ASSERT_EQ(**iter, 10);
+  iter -= 10;
+  ASSERT_EQ(*iter, &arr[0]);
+  ASSERT_EQ(**iter, 0);
 }
 
 THRUST_DIAG_POP
