281fc43c1281ea9d20e362969edc440ba824768f
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2026-07-03 10:23:56 -0600

    Use ifndef instead of undefine

--- projects/rocfft/library/src/include/library_path.h
+++ projects/rocfft/library/src/include/library_path.h
@@ -25,8 +25,9 @@
 
 #ifdef _WIN32
 #pragma push_macro("WIN32_LEAN_AND_MEAN")
-#undefine WIN32_LEAN_AND_MEAN
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
 #pragma pop_macro("WIN32_LEAN_AND_MEAN")
 #else
