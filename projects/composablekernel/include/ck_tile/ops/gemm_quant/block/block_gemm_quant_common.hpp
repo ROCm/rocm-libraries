78a60467d9dbcb7d6c80d6f5bec1c158bf98a201
Márton Bidlek <marton.bidlek@streamhpc.com>
2026-07-08 09:16:10 +0000

    comment fixes

--- projects/composablekernel/include/ck_tile/ops/gemm_quant/block/block_gemm_quant_common.hpp
+++ projects/composablekernel/include/ck_tile/ops/gemm_quant/block/block_gemm_quant_common.hpp
@@ -207,8 +207,7 @@ struct AQPickerCommon : public BlockGemmQuantBase
                 // to.
                 constexpr index_t m_base_offset_of_c_row =
                     (c_row / WarpGemm::WarpGemmAttribute::Impl::kCM1PerLane) *
-                    (WarpGemm::kCMLane *
-                     WarpGemm::WarpGemmAttribute::Impl::kCM1PerLane);
+                    (WarpGemm::kCMLane * WarpGemm::WarpGemmAttribute::Impl::kCM1PerLane);
 
                 // M offset of each thread within its group (see comment above)
                 index_t m_base_offset_of_lane =
