fa64088b90c760322ada1fcd773be0e0a819ef16
Mark Gates <Mark.Gates@amd.com>
2026-07-13 14:23:52 -0400

    hb2st: add new exception handling

--- projects/rocsolver/library/src/auxiliary/rocauxiliary_sb2st_hb2st.cpp
+++ projects/rocsolver/library/src/auxiliary/rocauxiliary_sb2st_hb2st.cpp
@@ -26,6 +26,7 @@
  * *************************************************************************/
 
 #include "rocauxiliary_sb2st_hb2st.hpp"
+#include "exceptions.hpp"
 
 ROCSOLVER_BEGIN_NAMESPACE
 
@@ -62,6 +63,7 @@ rocblas_status rocsolver_sb2st_hb2st_impl(rocblas_handle handle,
                                           U V,
                                           const I ldv,
                                           T* tau)
+try
 {
     ROCSOLVER_ENTER_TOP("sb2st_hb2st", "-n", n, "--kd", kd, "--ldab", ldab, "--ldv", ldv);
 
@@ -100,6 +102,10 @@ rocblas_status rocsolver_sb2st_hb2st_impl(rocblas_handle handle,
     return rocsolver_sb2st_hb2st_template<false, false, T, I>(
         handle, uplo, n, kd, Aband, shiftA, ldab, strideA, D, strideD, E, strideE, V, ldv, strideV,
         tau, strideTau, batch_count);
+}
+catch(...)
+{
+    return exception2rocblas_status();
 }
 
 ROCSOLVER_END_NAMESPACE
