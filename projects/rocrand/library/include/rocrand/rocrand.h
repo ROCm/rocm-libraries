3de3e0d873241c7845e62c0026d144e9772ddcf4
NguyenNhuDi <zenguyen@amd.com>
2026-07-16 16:15:55 -0400

    added HIP_INCLUDE_HIP_HIP_RUNTIME_API_H guard

--- projects/rocrand/library/include/rocrand/rocrand.h
+++ projects/rocrand/library/include/rocrand/rocrand.h
@@ -45,7 +45,8 @@
 typedef unsigned short __half;
     #endif // if !defined(HIP_INCLUDE_HIP_AMD_DETAIL_HIP_FP16_H)
 
-    #if !defined(HIP_INCLUDE_HIP_AMD_DETAIL_HIP_RUNTIME_H)
+    #if !(defined(HIP_INCLUDE_HIP_AMD_DETAIL_HIP_RUNTIME_H) \
+          || defined(HIP_INCLUDE_HIP_HIP_RUNTIME_API_H))
 struct ihipStream_t;
 typedef struct ihipStream_t* hipStream_t;
     #endif // if !defined(HIP_INCLUDE_HIP_AMD_DETAIL_HIP_RUNTIME_H)
