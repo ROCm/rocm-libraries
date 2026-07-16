7b21bfb4e178ba4980fca64474f22b42de83d3ff
Henderson, Nathan <nathan.henderson@amd.com>
2026-07-15 20:26:53 -0500

    fix(tensilelite): do not bump numSgprStreamK for SkPrefetchPrimed

--- projects/hipblaslt/tensilelite/Tensile/KernelWriter.py
+++ projects/hipblaslt/tensilelite/Tensile/KernelWriter.py
@@ -9439,7 +9439,6 @@ class KernelWriter(metaclass=abc.ABCMeta):
         requiredUnalignedSgprVar.append("StreamKTileID")
       if kernel.get("PrefetchAcrossPersistent"):
         requiredUnalignedSgprVar.append("SkPrefetchPrimed")
-        self.states.numSgprStreamK += 1
       if kernel["StreamKAtomic"] == 0:
         requiredAligned4SgprVar.append("SrdWS")
 
