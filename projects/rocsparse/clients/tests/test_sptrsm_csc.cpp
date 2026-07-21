fbb33c7f6e075178f2fba9c8f93b52333c3a02c9
Alexis Montoison <Alexis.Montoison@amd.com>
2026-07-21 09:35:50 -0600

    Address the comments of Nico

--- projects/rocsparse/clients/tests/test_sptrsm_csc.cpp
+++ projects/rocsparse/clients/tests/test_sptrsm_csc.cpp
@@ -25,6 +25,9 @@
 #include "test.hpp"
 #include "testing_sptrsm_csc.hpp"
 
+// CSC support can be disabled at build time (BUILD_WITH_CSC_TRSM=OFF), in which
+// case the sptrsm_csc tests are not registered.
+#ifdef ROCSPARSE_WITH_CSC_TRSM
 TEST_ROUTINE_WITH_CONFIG(sptrsm_csc,
                          level3,
                          rocsparse_test_config_ijt,
@@ -43,3 +46,4 @@ TEST_ROUTINE_WITH_CONFIG(sptrsm_csc,
                          arg.orderC,
                          arg.matrix,
                          arg.graph_test);
+#endif
