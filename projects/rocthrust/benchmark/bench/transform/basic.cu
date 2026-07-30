7673524e5d74dc9115ebe870ab13b2848eb270d3
Mátyás Aradi <matyas@streamhpc.com>
2026-07-30 09:38:37 +0200

    Use libhipcxx address stability

--- projects/rocthrust/benchmark/bench/transform/basic.cu
+++ projects/rocthrust/benchmark/bench/transform/basic.cu
@@ -33,7 +33,6 @@
 #include <thrust/copy.h>
 #include <thrust/count.h>
 #include <thrust/detail/config/namespace.h>
-#include <thrust/detail/libcxx_wrapper/__functional/address_stability.h>
 #include <thrust/device_vector.h>
 #include <thrust/execution_policy.h>
 #include <thrust/fill.h>
@@ -174,7 +173,7 @@ struct mul
   {
     const T scalar = startScalar;
     thrust::transform(
-      policy, c.begin(), c.end(), b.begin(), ::internal::proclaim_copyable_arguments([=] THRUST_DEVICE(const T& ci) {
+      policy, c.begin(), c.end(), b.begin(), _THRUST_LIBCXX::proclaim_copyable_arguments([=] THRUST_DEVICE(const T& ci) {
         return ci * scalar;
       }));
   }
@@ -195,7 +194,7 @@ struct add
       a.end(),
       b.begin(),
       c.begin(),
-      ::internal::proclaim_copyable_arguments([] THRUST_DEVICE(const T& ai, const T& bi) -> T {
+      _THRUST_LIBCXX::proclaim_copyable_arguments([] THRUST_DEVICE(const T& ai, const T& bi) -> T {
         return ai + bi;
       }));
   }
@@ -217,7 +216,7 @@ struct triad
       b.end(),
       c.begin(),
       a.begin(),
-      ::internal::proclaim_copyable_arguments([=] THRUST_DEVICE(const T& bi, const T& ci) {
+      _THRUST_LIBCXX::proclaim_copyable_arguments([=] THRUST_DEVICE(const T& bi, const T& ci) {
         return bi + scalar * ci;
       }));
   }
@@ -239,7 +238,7 @@ struct nstream
       thrust::make_zip_iterator(a.end(), b.end(), c.end()),
       a.begin(),
       thrust::make_zip_function(
-        ::internal::proclaim_copyable_arguments([=] THRUST_DEVICE(const T& ai, const T& bi, const T& ci) {
+        _THRUST_LIBCXX::proclaim_copyable_arguments([=] THRUST_DEVICE(const T& ai, const T& bi, const T& ci) {
           return ai + bi + scalar * ci;
         })));
   }
