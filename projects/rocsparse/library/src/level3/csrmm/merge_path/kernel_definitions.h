26f471667afba284cd50b41cea64feb060d37c0a
jsandham <jsandham@amd.com>
2026-07-30 18:06:51 +0000

    Clang formatting

--- projects/rocsparse/library/src/level3/csrmm/merge_path/kernel_definitions.h
+++ projects/rocsparse/library/src/level3/csrmm/merge_path/kernel_definitions.h
@@ -38,14 +38,14 @@ namespace rocsparse
               typename B,
               typename C>
     __launch_bounds__(WF_SIZE) __global__
-        void csrmmnt_merge_path_main_kernel(bool conj_A,
-                                            bool conj_B,
-                                            J    ncol_offset,
-                                            J    ncol,
-                                            J    m,
-                                            J    n,
-                                            J    k,
-                                            I    nnz,
+        void csrmmnt_merge_path_main_kernel(bool    conj_A,
+                                            bool    conj_B,
+                                            J       ncol_offset,
+                                            J       ncol,
+                                            J       m,
+                                            J       n,
+                                            J       k,
+                                            I       nnz,
                                             int64_t batch_count,
                                             ROCSPARSE_DEVICE_HOST_SCALAR_PARAMS(T, alpha),
                                             int64_t offsets_batch_stride_A,
@@ -112,13 +112,13 @@ namespace rocsparse
               typename B,
               typename C>
     __launch_bounds__(BLOCKSIZE) __global__
-        void csrmmnt_merge_path_remainder_kernel(bool conj_A,
-                                                 bool conj_B,
-                                                 J    ncol_offset,
-                                                 J    m,
-                                                 J    n,
-                                                 J    k,
-                                                 I    nnz,
+        void csrmmnt_merge_path_remainder_kernel(bool    conj_A,
+                                                 bool    conj_B,
+                                                 J       ncol_offset,
+                                                 J       m,
+                                                 J       n,
+                                                 J       k,
+                                                 I       nnz,
                                                  int64_t batch_count,
                                                  ROCSPARSE_DEVICE_HOST_SCALAR_PARAMS(T, alpha),
                                                  int64_t offsets_batch_stride_A,
@@ -146,27 +146,28 @@ namespace rocsparse
             // Grid-stride loop over the batch dimension (grid y). See main kernel.
             for(int64_t batch = hipBlockIdx_y; batch < batch_count; batch += hipGridDim_y)
             {
-                rocsparse::csrmmnt_merge_path_remainder_device<BLOCKSIZE, WF_SIZE, ITEMS_PER_THREAD>(
-                    conj_A,
-                    conj_B,
-                    ncol_offset,
-                    m,
-                    n,
-                    k,
-                    nnz,
-                    alpha,
-                    load_pointer(csr_row_ptr, batch, offsets_batch_stride_A),
-                    load_pointer(csr_col_ind, batch, columns_values_batch_stride_A),
-                    load_pointer(csr_val, batch, columns_values_batch_stride_A),
-                    coord0,
-                    coord1,
-                    load_pointer(dense_B, batch, batch_stride_B),
-                    ldb,
-                    beta,
-                    load_pointer(dense_C, batch, batch_stride_C),
-                    ldc,
-                    order_C,
-                    idx_base);
+                rocsparse::
+                    csrmmnt_merge_path_remainder_device<BLOCKSIZE, WF_SIZE, ITEMS_PER_THREAD>(
+                        conj_A,
+                        conj_B,
+                        ncol_offset,
+                        m,
+                        n,
+                        k,
+                        nnz,
+                        alpha,
+                        load_pointer(csr_row_ptr, batch, offsets_batch_stride_A),
+                        load_pointer(csr_col_ind, batch, columns_values_batch_stride_A),
+                        load_pointer(csr_val, batch, columns_values_batch_stride_A),
+                        coord0,
+                        coord1,
+                        load_pointer(dense_B, batch, batch_stride_B),
+                        ldb,
+                        beta,
+                        load_pointer(dense_C, batch, batch_stride_C),
+                        ldc,
+                        order_C,
+                        idx_base);
             }
         }
     }
@@ -181,12 +182,12 @@ namespace rocsparse
               typename B,
               typename C>
     __launch_bounds__(BLOCKSIZE) __global__
-        void csrmmnn_merge_path_kernel(bool conj_A,
-                                       bool conj_B,
-                                       J    m,
-                                       J    n,
-                                       J    k,
-                                       I    nnz,
+        void csrmmnn_merge_path_kernel(bool    conj_A,
+                                       bool    conj_B,
+                                       J       m,
+                                       J       n,
+                                       J       k,
+                                       I       nnz,
                                        int64_t batch_count,
                                        ROCSPARSE_DEVICE_HOST_SCALAR_PARAMS(T, alpha),
                                        int64_t offsets_batch_stride_A,
@@ -242,12 +243,12 @@ namespace rocsparse
 #define CSRMMNN_MERGE_PATH_KERNEL(T, I, J, A, B, C, BLOCKSIZE, WFSIZE, ITEM_PER_THREAD) \
     template __launch_bounds__(BLOCKSIZE) __global__ void                               \
         rocsparse::csrmmnn_merge_path_kernel<BLOCKSIZE, WFSIZE, ITEM_PER_THREAD>(       \
-            bool conj_A,                                                                \
-            bool conj_B,                                                                \
-            J    m,                                                                     \
-            J    n,                                                                     \
-            J    k,                                                                     \
-            I    nnz,                                                                   \
+            bool    conj_A,                                                             \
+            bool    conj_B,                                                             \
+            J       m,                                                                  \
+            J       n,                                                                  \
+            J       k,                                                                  \
+            I       nnz,                                                                \
             int64_t batch_count,                                                        \
             ROCSPARSE_DEVICE_HOST_SCALAR_PARAMS(T, alpha),                              \
             int64_t offsets_batch_stride_A,                                             \
@@ -271,14 +272,14 @@ namespace rocsparse
 #define CSRMMNT_MERGE_PATH_MAIN_KERNEL(T, I, J, A, B, C, WFSIZE, ITEM_PER_THREAD, LOOPS) \
     template __launch_bounds__(WFSIZE) __global__ void                                   \
         rocsparse::csrmmnt_merge_path_main_kernel<WFSIZE, ITEM_PER_THREAD, LOOPS>(       \
-            bool conj_A,                                                                 \
-            bool conj_B,                                                                 \
-            J    ncol_offset,                                                            \
-            J    ncol,                                                                   \
-            J    m,                                                                      \
-            J    n,                                                                      \
-            J    k,                                                                      \
-            I    nnz,                                                                    \
+            bool    conj_A,                                                              \
+            bool    conj_B,                                                              \
+            J       ncol_offset,                                                         \
+            J       ncol,                                                                \
+            J       m,                                                                   \
+            J       n,                                                                   \
+            J       k,                                                                   \
+            I       nnz,                                                                 \
             int64_t batch_count,                                                         \
             ROCSPARSE_DEVICE_HOST_SCALAR_PARAMS(T, alpha),                               \
             int64_t offsets_batch_stride_A,                                              \
@@ -302,13 +303,13 @@ namespace rocsparse
 #define CSRMMNT_MERGE_PATH_REMAINDER_KERNEL(T, I, J, A, B, C, BLOCKSIZE, WFSIZE, ITEM_PER_THREAD) \
     template __launch_bounds__(BLOCKSIZE) __global__ void                                         \
         rocsparse::csrmmnt_merge_path_remainder_kernel<BLOCKSIZE, WFSIZE, ITEM_PER_THREAD>(       \
-            bool conj_A,                                                                          \
-            bool conj_B,                                                                          \
-            J    ncol_offset,                                                                     \
-            J    m,                                                                               \
-            J    n,                                                                               \
-            J    k,                                                                               \
-            I    nnz,                                                                             \
+            bool    conj_A,                                                                       \
+            bool    conj_B,                                                                       \
+            J       ncol_offset,                                                                  \
+            J       m,                                                                            \
+            J       n,                                                                            \
+            J       k,                                                                            \
+            I       nnz,                                                                          \
             int64_t batch_count,                                                                  \
             ROCSPARSE_DEVICE_HOST_SCALAR_PARAMS(T, alpha),                                        \
             int64_t offsets_batch_stride_A,                                                       \
