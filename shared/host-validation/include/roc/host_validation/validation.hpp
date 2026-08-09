964a6c8a8475d7a58b9f3a2ab3d124072a24f0b5
James Newling <james.newling@gmail.com>
2026-08-09 13:08:43 -0700

    refactor(host-validation): introduce GEMM invocation API

--- shared/host-validation/include/roc/host_validation/validation.hpp
+++ shared/host-validation/include/roc/host_validation/validation.hpp
@@ -6,7 +6,7 @@
 #include <roc/host_validation/comparison.hpp>
 #include <roc/host_validation/detail/data_generation.hpp>
 #include <roc/host_validation/detail/reference_epilogue.hpp>
-#include <roc/host_validation/detail/reference_gemm.hpp>
 #include <roc/host_validation/detail/reference_reduction.hpp>
 #include <roc/host_validation/detail/structured_sparsity.hpp>
+#include <roc/host_validation/gemm.hpp>
 #include <roc/host_validation/tensor.hpp>
