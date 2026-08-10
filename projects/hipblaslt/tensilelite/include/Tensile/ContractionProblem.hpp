4ff4179f5af0d7bed87128ed95808433ab9cbf57
Alex Brown <alex.brown@amd.com>
2026-08-10 11:37:41 -0500

    Round SM target up to multiple of 32. Add debug variable to allow precise SM targets.
    (cherry picked from commit f4c54316748f638f200280e3c81ecd7249b56b38)

--- projects/hipblaslt/tensilelite/include/Tensile/ContractionProblem.hpp
+++ projects/hipblaslt/tensilelite/include/Tensile/ContractionProblem.hpp
@@ -175,6 +175,12 @@ namespace TensileLite
         void setSmCountTarget(int smCountTarget)
         {
             m_smCountTarget = smCountTarget;
+            const bool preciseSMTarget = Debug::Instance().usePreciseSMTarget();
+            // Round to nearest multiple of 32 if not precise SM target
+            if(!preciseSMTarget)
+            {
+                m_smCountTarget = RoundUpToMultiple(m_smCountTarget, 32);
+            }
         }
 
         int smCountTarget() const
