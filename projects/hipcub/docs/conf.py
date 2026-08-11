ace01167810c3d6583059da07911e40b3b8c0972
{spolifroni-amd} <{sandra.polifroni@amd.com}>
2026-08-11 14:24:06 -0400

    added rocm theme info to the conf.py files

--- projects/hipcub/docs/conf.py
+++ projects/hipcub/docs/conf.py
@@ -34,6 +34,19 @@ external_projects_current_project = "hipcub"
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
 
+# Theme-related settings
+html_theme = "rocm_docs_theme"
+html_theme_options = {
+    "flavor": "rocm",
+    "repository_url": "https://github.com/ROCm/rocm-libraries",
+    "repository_branch": "develop",
+    "path_to_docs": "projects/hipcub/docs",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_source_button": True,
+    "use_download_button": True,
+}
+
 # Suppresses "WARNING: toctree directive not expected with external-toc"
 # Ideally suppression wouldn't be needed; see sphinx-external-toc#36
 suppress_warnings = ["etoc.toctree"]
