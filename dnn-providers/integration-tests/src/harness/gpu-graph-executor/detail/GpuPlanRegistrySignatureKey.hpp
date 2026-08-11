7fc7b1b1ddca6d4f637e5382be6da6aa0fdf5024
Ewan Crawford <ewan.crawford@streamhpc.com>
2026-08-11 11:34:04 +0000

    feat(integraion-tests): Integration pointwise GPU reference
    Allow pointwise integration tests to use the gpu reference kernel
    for validation.
    
    Tested via
    ```sh
    $ ./bin/hipdnn_integration_tests --ta libmiopen_plugin.so --gtest_filter="*Pointwise*" --reference-executor=gpu
    ```
    
    Note that only ReluFwd pointwise opertion is supported by the miopen
    plugin and therefore has integration tests. I decided to not add more
    pointwise integration tests as I couldn't run them against any plugins
    to confirm the tests were well formed.
    
    JIRA ID : ALMIOPEN-1662

--- dnn-providers/integration-tests/src/harness/gpu-graph-executor/detail/GpuPlanRegistrySignatureKey.hpp
+++ dnn-providers/integration-tests/src/harness/gpu-graph-executor/detail/GpuPlanRegistrySignatureKey.hpp
@@ -9,6 +9,7 @@
 
 #include "GpuConvolutionFwdSignatureKey.hpp"
 #include "GpuSdpaFwdSignatureKey.hpp"
+#include "GpuPointwiseSignatureKey.hpp"
 
 namespace hipdnn_integration_tests::gpu_graph_executor::detail
 {
@@ -16,7 +17,8 @@ namespace hipdnn_integration_tests::gpu_graph_executor::detail
 // Variant of all GPU plan signature key types.
 // Add new signature key types here as GPU plans are implemented.
 using GpuPlanRegistrySignatureKey
-    = std::variant<GpuConvolutionFwdSignatureKey, GpuSdpaFwdSignatureKey>;
+    = std::variant<GpuConvolutionFwdSignatureKey, GpuSdpaFwdSignatureKey, GpuPointwiseSignatureKey>;
+
 
 struct GpuPlanRegistrySignatureKeyHash
 {
