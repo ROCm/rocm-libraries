ac5243f8f7a14f23728ae88a239d6f4299c029ee
spolifroni-amd <sandra.polifroni@amd.com>
2026-08-14 16:49:28 -0400

    added a testing doc and testing env vars

--- projects/hipthreads/docs/conf.py
+++ projects/hipthreads/docs/conf.py
@@ -55,6 +55,10 @@ doxygen_project = {
     "path": "doxygen/xml",
 }
 
+article_pages = [
+    {"file": "reference/hipThreads-set-test-timeout", "os": ["linux"]},
+]
+
 cpp_id_attributes = [
     "__device__",
     "__host__",
