b4025dfaa01c91419a09efb8d89d761b9b1c6168
Zach Vincze <zavincze@amd.com>
2026-08-17 11:57:10 -0400

    Add copyright lines

--- projects/rpp/utilities/test/src/tests/image/filter/sobel_filter_test.cpp
+++ projects/rpp/utilities/test/src/tests/image/filter/sobel_filter_test.cpp
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2026 Advanced Micro Devices, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 // sobel_filter GTest: PLN1 only, kernelSize = 3 only. sobel_filter's dstDesc is always
 // single-channel grayscale (c=1, NCHW); a 3-channel input would require an undocumented
 // RGB->grayscale conversion (not independently derivable), and the extended k=5/7 kernels are
