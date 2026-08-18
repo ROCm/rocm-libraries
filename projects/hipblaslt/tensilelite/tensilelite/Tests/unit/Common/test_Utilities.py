5cdb7306bf43762a6f2020cc80e1a217990c3cfc
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2026-08-18 16:33:20 +0000

    2-refactor(tensilelite): update test namespace references (part 1)

--- projects/hipblaslt/tensilelite/tensilelite/Tests/unit/Common/test_Utilities.py
+++ projects/hipblaslt/tensilelite/tensilelite/Tests/unit/Common/test_Utilities.py
@@ -28,9 +28,14 @@ import pytest
 from unittest.mock import mock_open, patch
 from pathlib import Path
 
-from Tensile import __version__
-from Tensile.Common import Utilities as UtilitiesModule
 from Tensile.Common.Utilities import (
+    swizzleGeometry,
+)
+from Tensile.Common.DataType import DataType
+
+from tensilelite import __version__
+from tensilelite.Common import Utilities as UtilitiesModule
+from tensilelite.Common.Utilities import (
     ClientExecutionLock,
     ProgressBar,
     ceilDivide,
@@ -54,11 +59,31 @@ from Tensile.Common.Utilities import (
     setVerbosity,
     state,
     state_key_ordering,
-    swizzleGeometry,
     versionIsCompatible,
     wmmaV3InputVgprLayout,
 )
-from Tensile.Common.DataType import DataType
+
+################################################################################
+#
+# Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+################################################################################
 
 @pytest.fixture
 def mock_openFile():
