22126823d6810c663b6b78a909461cc7c2fa3783
Alex Vasile <48962821+Alex-Vasile@users.noreply.github.com>
2026-08-18 17:30:28 +0000

    2-fix(tensilelite): complete moved package consumer closure

--- projects/hipblaslt/tensilelite/tensilelite/Tests/conftest.py
+++ projects/hipblaslt/tensilelite/tensilelite/Tests/conftest.py
@@ -248,7 +248,7 @@ def pytest_collection_modifyitems(config, items):
 @pytest.fixture
 def useGlobalParameters(tensile_args):
     from tensilelite import Common
-    from tensilelite import tensilelite
+    from tensilelite import Tensile as tensilelite
     import argparse
 
     class gpUpdater:
