17e4d11651607a5b8a8f9e0087499ded5fd658d4
Flavio Teixeira <eng.flavio.teixeira@gmail.com>
2026-08-19 15:04:09 -0600

    - WIP: Fused 2D kernels

--- projects/rocfft/library/src/device/generator/stockham_gen_2d.h
+++ projects/rocfft/library/src/device/generator/stockham_gen_2d.h
@@ -238,12 +238,12 @@ struct StockhamKernelFused2D : public StockhamKernelRR
                                  + " threads per 2d transform",
                              "  (1 2d slab per thread block)"};
 
-        Variable d{"d", "int"};
-        Variable index_along_d{"index_along_d", "size_t"};
-        Variable remaining{"remaining", "size_t"};
-        Variable plength{"plength", "size_t"};
+        Variable d{"d", rtc_index_type(IndexType::_32BIT)};
+        Variable index_along_d{"index_along_d", rtc_index_type(IndexType::_32BIT)};
+        Variable remaining{"remaining", rtc_index_type(IndexType::_32BIT)};
+        Variable plength{"plength", rtc_index_type(IndexType::_32BIT)};
 
-        Variable batch0{"batch0", "size_t"};
+        Variable batch0{"batch0", rtc_index_type(IndexType::_32BIT)};
 
         Variable SB_1ST{"SB_1ST", "const StrideBin"};
         Variable SB_2ND{"SB_2ND", "const StrideBin"};
