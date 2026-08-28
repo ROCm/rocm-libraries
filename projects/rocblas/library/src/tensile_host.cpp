9079a3a9f79efeafb2bac67fd02940a95d18b8e7
Torre Zuk <Torre.Zuk@amd.com>
2026-08-28 16:33:13 -0600

    fix type check

--- projects/rocblas/library/src/tensile_host.cpp
+++ projects/rocblas/library/src/tensile_host.cpp
@@ -1250,7 +1250,7 @@ bool useHipBLASLt(const RocblasContractionProblem<Ti, To, Tc>& prob)
         {
             // gfx950: hipBLASLt is used for all types except complex.
             // TODO remove after complex support is enabled
-            if(rocblas_internal_get_arch(prob.handle) == 950 && rocblas_is_complex_type<Ti>())
+            if(rocblas_internal_get_arch(prob.handle) == 950 && rocblas_is_complex<Ti>())
                 return false; // complex won't default to hipBLASLt
         }
     }
