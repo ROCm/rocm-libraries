93a575a8e122c948d3eed5f8e0faf6e103f35701
James Newling <james.newling@gmail.com>
2026-09-02 16:47:07 -0700

    refactor(rocroller): compute host GEMM directly

--- shared/rocroller/test/unit/GEMMFusion.cpp
+++ shared/rocroller/test/unit/GEMMFusion.cpp
@@ -355,17 +355,15 @@ namespace GEMMDriverTest
             }
 
             // Host result
-            auto referenceProblem
-                = HostNumerics::makeHostReferenceProblem(HostNumerics::hostTensor(descA, hostA),
-                                                         HostNumerics::hostTensor(descB, hostB),
-                                                         HostNumerics::hostTensor(descC, hostC),
-                                                         std::nullopt,
-                                                         std::nullopt,
-                                                         0,
-                                                         alpha,
-                                                         beta);
             auto h_result = HostNumerics::convertHostReference<T>(
-                HostNumerics::computeHostReference(referenceProblem));
+                HostNumerics::computeHostReference(HostNumerics::hostTensor(descA, hostA),
+                                                   HostNumerics::hostTensor(descB, hostB),
+                                                   HostNumerics::hostTensor(descC, hostC),
+                                                   std::nullopt,
+                                                   std::nullopt,
+                                                   0,
+                                                   alpha,
+                                                   beta));
             // Host leaky relu
             for(size_t i = 0; i < M; i++)
             {
