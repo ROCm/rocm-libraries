a956e89e1e3a2914168d0b4eef5d460381dccc58
Enrico Degregori <enrico@streamhpc.com>
2026-09-03 08:31:43 +0000

    Fwd fix for edge cases without changing splitN value

--- projects/composablekernel/include/ck/tensor_operation/operator_transform/transform_conv_fwd_to_gemm.hpp
+++ projects/composablekernel/include/ck/tensor_operation/operator_transform/transform_conv_fwd_to_gemm.hpp
@@ -65,10 +65,10 @@ struct TransformConvFwdToGemm
 
         const IndexType N = a_g_n_c_wis_lengths[I1];
 
-        if(element_space_size >= TwoGB)
+        if(element_space_size > TwoGB)
         {
             // Minimum divisor of N to not exceed 2GB
-            const auto divisor = math::integer_divide_ceil(element_space_size, TwoGB - 1);
+            const auto divisor = math::integer_divide_ceil(element_space_size, TwoGB);
 
             if(divisor <= static_cast<double>(N))
             {
@@ -386,8 +386,10 @@ struct TransformConvFwdToGemm
             I1 + (N_ - I1) * NStrideTensorC_ + (Do_ - I1) * DoStride_ + (Ho_ - I1) * HoStride_ +
             (Wo_ - I1) * WoStride_ + (K_ - I1) * KStrideTensorC_;
 
-        bool is_a_descriptor_smaller_than_2GB = (in_desc_space_size * sizeof(ADataType)) < TwoGB;
-        bool is_c_descriptor_smaller_than_2GB = (out_desc_space_size * sizeof(CDataType)) < TwoGB;
+        bool is_a_descriptor_smaller_than_2GB =
+            (in_desc_space_size * sizeof(ADataType)) <= TwoGB && in_desc_space_size < TwoGB;
+        bool is_c_descriptor_smaller_than_2GB =
+            (out_desc_space_size * sizeof(CDataType)) <= TwoGB && out_desc_space_size < TwoGB;
 
         return is_a_descriptor_smaller_than_2GB && is_c_descriptor_smaller_than_2GB;
     }
