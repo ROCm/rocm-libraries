5a35e80b143f34a690a82bdd3c0719aae16a51a0
Nick Breed <nick@streamhpc.com>
2026-09-10 12:02:47 +0000

    feat(rocprim): add device_scan to kernel tuner

--- projects/rocprim/tuning/run_tuning.py
+++ projects/rocprim/tuning/run_tuning.py
@@ -75,7 +75,8 @@ def get_available_algorithms() -> List[str]:
         "device_select_unique_by_key",
         "device_select_flag",
         "device_select_predicate",
-        "device_select_unique"
+        "device_select_unique",
+        "device_scan"
         # Add new algorithms here
     ]
 
