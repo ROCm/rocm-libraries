0096f8c0a051c5c06396f8c40beb44c4c7a9d2b0
Mahovsky <jmahovsk@amd.com>
2026-09-10 18:27:29 -0400

    fix zero size hipMemAdvice() issue

--- projects/rocthrust/thrust/system/hip/hipstdpar/impl/interpose_allocations_v1.hpp
+++ projects/rocthrust/thrust/system/hip/hipstdpar/impl/interpose_allocations_v1.hpp
@@ -125,6 +125,7 @@ extern "C" {
         auto r = __hipstdpar_hidden_memalign(a, n);
 
         if (!hipstd::__initialised) return r;
+        if (n == 0) return r; // hipMemAdvise rejects zero-length ranges; nothing to advise.
 
         hipDevice_t d{};
         hipGetDevice(&d);
