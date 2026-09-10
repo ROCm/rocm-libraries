971806f9df3be4e45a133e670c3a323b9b89e4cf
Nick Breed <nick@streamhpc.com>
2026-09-10 12:41:12 +0000

    feat(rocprim): add scan_by_key to kernel tuner

--- projects/rocprim/tuning/run_tuning.py
+++ projects/rocprim/tuning/run_tuning.py
@@ -79,7 +79,8 @@ def get_available_algorithms() -> List[str]:
         "device_reduce",
         "device_segmented_reduce",
         "device_reduce_by_key",
-        "device_scan"
+        "device_scan",
+        "device_scan_by_key"
         # Add new algorithms here
     ]
 
