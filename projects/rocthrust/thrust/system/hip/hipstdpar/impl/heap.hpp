cfa9f932dd6ff30b8585b93d606ffdb7bc91a8b2
Stanley Tsang <stanley.tsang@amd.com>
2026-09-11 10:56:30 -0600

    Initial implementation of is_heap, is_heap_until

--- projects/rocthrust/thrust/system/hip/hipstdpar/impl/heap.hpp
+++ projects/rocthrust/thrust/system/hip/hipstdpar/impl/heap.hpp
@@ -40,20 +40,191 @@
 
 #if defined(__HIPSTDPAR__)
 
+#  include <rocprim/rocprim.hpp>
+
+#  include <thrust/execution_policy.h>
+#  include <thrust/find.h>
+#  include <thrust/functional.h>
+#  include <thrust/iterator/counting_iterator.h>
+#  include <thrust/logical.h>
+
+#  include <algorithm>
+#  include <execution>
+#  include <utility>
+
 #  include "hipstd.hpp"
 
-// rocThrust includes
+// rocThrust (and rocPRIM) currently expose no heap primitives at all, so is_heap /
+// is_heap_until are composed here out of the all_of / find_if primitives that rocThrust
+// does provide, by checking the usual "no child compares greater than its parent" invariant
+// for every index in the range.
+namespace thrust
+{
+// BEGIN IS_HEAP
+template <typename RandomIt, typename CompareOp>
+struct __heap_violation
+{
+  RandomIt first;
+  CompareOp compare_op;
 
-// STL includes
+  using difference_type = typename thrust::iterator_difference<RandomIt>::type;
+
+  THRUST_HIP_FUNCTION bool operator()(difference_type i) const
+  {
+    return compare_op(first[(i - 1) / 2], first[i]);
+  }
+};
+
+template <typename RandomIt,
+          typename CompareOp,
+          std::enable_if_t<hipstd::is_offloadable_iterator<RandomIt>() && hipstd::is_offloadable_callable<CompareOp>()>* =
+            nullptr>
+inline bool __is_heap(thrust::hip_rocprim::par_t policy, RandomIt first, RandomIt last, CompareOp compare_op)
+{
+  using difference_type = typename thrust::iterator_difference<RandomIt>::type;
+
+  const difference_type count = thrust::distance(first, last);
+  if (count < 2)
+  {
+    return true;
+  }
+
+  return thrust::none_of(
+    policy,
+    thrust::counting_iterator<difference_type>(1),
+    thrust::counting_iterator<difference_type>(count),
+    __heap_violation<RandomIt, CompareOp>{first, compare_op});
+}
+// END IS_HEAP
+
+// BEGIN IS_HEAP_UNTIL
+template <typename RandomIt,
+          typename CompareOp,
+          std::enable_if_t<hipstd::is_offloadable_iterator<RandomIt>() && hipstd::is_offloadable_callable<CompareOp>()>* =
+            nullptr>
+inline RandomIt __is_heap_until(thrust::hip_rocprim::par_t policy, RandomIt first, RandomIt last, CompareOp compare_op)
+{
+  using difference_type = typename thrust::iterator_difference<RandomIt>::type;
+
+  const difference_type count = thrust::distance(first, last);
+  if (count < 2)
+  {
+    return last;
+  }
+
+  const auto violation = thrust::find_if(
+    policy,
+    thrust::counting_iterator<difference_type>(1),
+    thrust::counting_iterator<difference_type>(count),
+    __heap_violation<RandomIt, CompareOp>{first, compare_op});
+
+  return first + *violation;
+}
+// END IS_HEAP_UNTIL
+} // namespace thrust
 
 namespace std
 {
 // BEGIN IS_HEAP
-// TODO: UNIMPLEMENTED IN THRUST
+template <
+  typename RandomIt,
+  typename CompareOp,
+  enable_if_t<!hipstd::is_offloadable_iterator<RandomIt>() || !hipstd::is_offloadable_callable<CompareOp>()>* = nullptr>
+inline bool is_heap(execution::parallel_unsequenced_policy, RandomIt first, RandomIt last, CompareOp compare_op)
+{
+  if constexpr (!hipstd::is_offloadable_iterator<RandomIt>())
+  {
+    hipstd::unsupported_iterator_category<typename iterator_traits<RandomIt>::iterator_category>();
+  }
+  if constexpr (!hipstd::is_offloadable_callable<CompareOp>())
+  {
+    hipstd::unsupported_callable_type<CompareOp>();
+  }
+
+  return std::is_heap(std::execution::par, first, last, std::move(compare_op));
+}
+
+template <typename RandomIt,
+          typename CompareOp,
+          enable_if_t<hipstd::is_offloadable_iterator<RandomIt>() && hipstd::is_offloadable_callable<CompareOp>()>* =
+            nullptr>
+inline bool is_heap(execution::parallel_unsequenced_policy, RandomIt first, RandomIt last, CompareOp compare_op)
+{
+  ::hipstd::__maybe_bind_globals();
+
+  ::hipstd::warn_if_no_xnack();
+  return ::thrust::__is_heap(::thrust::device, first, last, compare_op);
+}
+
+template <typename RandomIt, enable_if_t<!hipstd::is_offloadable_iterator<RandomIt>()>* = nullptr>
+inline bool is_heap(execution::parallel_unsequenced_policy, RandomIt first, RandomIt last)
+{
+  if constexpr (!hipstd::is_offloadable_iterator<RandomIt>())
+  {
+    hipstd::unsupported_iterator_category<typename iterator_traits<RandomIt>::iterator_category>();
+  }
+
+  return std::is_heap(std::execution::par, first, last);
+}
+
+template <typename RandomIt, enable_if_t<hipstd::is_offloadable_iterator<RandomIt>()>* = nullptr>
+inline bool is_heap(execution::parallel_unsequenced_policy policy, RandomIt first, RandomIt last)
+{
+  ::hipstd::warn_if_no_xnack();
+  using item_type = typename thrust::iterator_value<RandomIt>::type;
+  return std::is_heap(policy, first, last, thrust::less<item_type>());
+}
 // END IS_HEAP
 
 // BEGIN IS_HEAP_UNTIL
-// TODO: UNIMPLEMENTED IN THRUST
+template <
+  typename RandomIt,
+  typename CompareOp,
+  enable_if_t<!hipstd::is_offloadable_iterator<RandomIt>() || !hipstd::is_offloadable_callable<CompareOp>()>* = nullptr>
+inline RandomIt is_heap_until(execution::parallel_unsequenced_policy, RandomIt first, RandomIt last, CompareOp compare_op)
+{
+  if constexpr (!hipstd::is_offloadable_iterator<RandomIt>())
+  {
+    hipstd::unsupported_iterator_category<typename iterator_traits<RandomIt>::iterator_category>();
+  }
+  if constexpr (!hipstd::is_offloadable_callable<CompareOp>())
+  {
+    hipstd::unsupported_callable_type<CompareOp>();
+  }
+
+  return std::is_heap_until(std::execution::par, first, last, std::move(compare_op));
+}
+
+template <typename RandomIt,
+          typename CompareOp,
+          enable_if_t<hipstd::is_offloadable_iterator<RandomIt>() && hipstd::is_offloadable_callable<CompareOp>()>* =
+            nullptr>
+inline RandomIt is_heap_until(execution::parallel_unsequenced_policy, RandomIt first, RandomIt last, CompareOp compare_op)
+{
+  ::hipstd::__maybe_bind_globals();
+
+  ::hipstd::warn_if_no_xnack();
+  return ::thrust::__is_heap_until(::thrust::device, first, last, compare_op);
+}
+
+template <typename RandomIt, enable_if_t<!hipstd::is_offloadable_iterator<RandomIt>()>* = nullptr>
+inline RandomIt is_heap_until(execution::parallel_unsequenced_policy, RandomIt first, RandomIt last)
+{
+  if constexpr (!hipstd::is_offloadable_iterator<RandomIt>())
+  {
+    hipstd::unsupported_iterator_category<typename iterator_traits<RandomIt>::iterator_category>();
+  }
+
+  return std::is_heap_until(std::execution::par, first, last);
+}
+
+template <typename RandomIt, enable_if_t<hipstd::is_offloadable_iterator<RandomIt>()>* = nullptr>
+inline RandomIt is_heap_until(execution::parallel_unsequenced_policy policy, RandomIt first, RandomIt last)
+{
+  ::hipstd::warn_if_no_xnack();
+  using item_type = typename thrust::iterator_value<RandomIt>::type;
+  return std::is_heap_until(policy, first, last, thrust::less<item_type>());
+}
 // END IS_HEAP_UNTIL
 } // namespace std
 #else // __HIPSTDPAR__
