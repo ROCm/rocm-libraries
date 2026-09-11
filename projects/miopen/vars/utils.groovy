22be77027a8011bc795d633d28a520dd9304e159
SreecharanGundaboluAMD <sgundabo@amd.com>
2026-09-11 18:04:21 +0000

    Remove Fp16 Hip Install All gfx115X stage from non-critical nightly

--- projects/miopen/vars/utils.groovy
+++ projects/miopen/vars/utils.groovy
@@ -1637,14 +1637,6 @@ def nonCriticalHWNightlyStages(def pipelineParams, def pipelineEnv, def rocmnode
     }
 
 
-    def fp16Gfx115X = 'Fp16 Hip Install All gfx115X'
-    addStageIf(stages, pipelineParams.TARGET_NAVI35 && pipelineParams.DATATYPE_FP16 && !passedStages.contains(fp16Gfx115X), fp16Gfx115X) {
-        node(rocmnodeFn("strix")) {
-            try {
-                withStageStatus { runBuildAndSingleGtestJobFn(flags: " -DMIOPEN_TEST_GFX115X=On " + Full_test + Fp16_flags + gfx1151_flags, build_timeout_minutes: Build_timeout_minutes, gpu_family: "ci") }
-            } finally { cleanWs() }
-        }
-    }
 
     def fp32Gfx115X = 'Fp32 Hip Install All gfx115X'
     addStageIf(stages, pipelineParams.TARGET_NAVI35 && pipelineParams.DATATYPE_FP32 && !passedStages.contains(fp32Gfx115X), fp32Gfx115X) {
