21aec61e949978762aa6a5e57402a97b9723f5ba
SreecharanGundaboluAMD <sgundabo@amd.com>
2026-09-11 18:15:41 +0000

    Add back gfx1151 to gpu_arch in getDockerImage and buildTheRockDockerImage

--- projects/miopen/vars/utils.groovy
+++ projects/miopen/vars/utils.groovy
@@ -284,7 +284,7 @@ def buildTheRockDockerImage(Map conf=[:])
 
     def cacheRef = "${env.MIOPEN_DOCKER_IMAGE_URL}-ci-docker:therock_cache"
 
-    def gpu_arch = "gfx908;gfx90a;gfx942;gfx950;gfx1101;gfx1201" // multiarch builds
+    def gpu_arch = "gfx908;gfx90a;gfx942;gfx950;gfx1101;gfx1151;gfx1201" // multiarch builds
 
     // Pin TheRock's build_tools (the install_rocm_from_artifacts.py installer) to the ref tracked
     // in the ci-env action, so the tooling that downloads the nightly tarball is reproducible.
@@ -512,7 +512,7 @@ def getDockerImage(Map conf=[:])
     def gpu_arch
     if (gpu_family == "ci")
     {
-        gpu_arch = "gfx908;gfx90a;gfx942;gfx950;gfx1101" // Builds docker image with subset of architectures that CI is run on.
+        gpu_arch = "gfx908;gfx90a;gfx942;gfx950;gfx1101;gfx1151" // Builds docker image with subset of architectures that CI is run on.
     }
     else if (gpu_family == "gfx90X")
     {
