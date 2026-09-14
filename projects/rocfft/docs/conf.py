f3fe5f9a12470a04df41cb64f6a642a5daf02ec2
Peter Park <peter.park@amd.com>
2026-09-14 09:23:41 -0400

    docs(rocfft): enable generating llms.txt

--- projects/rocfft/docs/conf.py
+++ projects/rocfft/docs/conf.py
@@ -39,12 +39,12 @@ html_theme = "rocm_docs_theme"
 html_theme_options = {
     "flavor": "rocm",
     "repository_url": "https://github.com/ROCm/rocm-libraries",
-    "repository_branch": "develop",
     "path_to_docs": "projects/rocfft/docs",
     "use_repository_button": True,
     "use_issues_button": True,
-    "use_source_button": True,
     "use_download_button": True,
 }
+# Generate llms.txt (https://llmstxt.org/)
+rocm_docs_generate_llms = True
 
 extensions = globals().get("extensions", []) + ["sphinxcontrib.datatemplates"]
