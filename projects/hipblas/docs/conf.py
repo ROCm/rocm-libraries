322a402975951d214554da933928ec18ce931a0a
Peter Park <peter.park@amd.com>
2026-09-14 07:15:27 -0400

    docs(conf): enable generating llms.txt

--- projects/hipblas/docs/conf.py
+++ projects/hipblas/docs/conf.py
@@ -18,7 +18,7 @@ left_nav_title = f"hipBLAS {version_number} Documentation"
 # for PDF output on Read the Docs
 project = "hipBLAS Documentation"
 author = "Advanced Micro Devices, Inc."
-copyright = "Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved."
+copyright = "Copyright (c) 2026 Advanced Micro Devices, Inc. All rights reserved."
 version = version_number
 release = version_number
 
@@ -34,3 +34,6 @@ for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
 
 extensions = globals().get("extensions", []) + ["sphinxcontrib.datatemplates"]
+
+# Generate llms.txt (https://llmstxt.org/)
+rocm_docs_generate_llms = True
