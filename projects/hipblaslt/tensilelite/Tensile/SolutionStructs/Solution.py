f1b3ad66d3494d7584b955877fc20cc337e338bb
victorwu <victor.wu2@amd.com>
2026-09-15 02:55:14 -0400

    Revert "fix(tensilelite) Temporarily disable CLS (#10666)"
    This reverts commit 914df70167cd7b8ce68bf280e7f643fd9703cbd0.

--- projects/hipblaslt/tensilelite/Tensile/SolutionStructs/Solution.py
+++ projects/hipblaslt/tensilelite/Tensile/SolutionStructs/Solution.py
@@ -2811,7 +2811,6 @@ class Solution(collections.abc.Mapping):
         return
 
     if state["CompactLoopStore"]:
-      state["CompactLoopStore"] = False
       if not isaInfoMap[isa].asmCaps["HasMovRelsD2B32"]:
         reject(state, printRejectionReason, "This arch does not support CompactLoopStore (no v_movrelsd_2_b32)")
         return
