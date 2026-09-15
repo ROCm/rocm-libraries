b1bc1de4c54c81507cc6205d743108c1b0d57ebe
Alexis Montoison <Alexis.Montoison@amd.com>
2026-09-14 20:22:20 -0600

    Fix a few tests

--- projects/rocsparse/clients/include/testing_sddmm_dispatch.hpp
+++ projects/rocsparse/clients/include/testing_sddmm_dispatch.hpp
@@ -450,28 +450,6 @@ public:
             trans_B = rocsparse_operation_none;
         }
 
-        // buffer size
-        {
-            device_dense_matrix<A> dA;
-            device_dense_matrix<B> dB;
-            rocsparse_local_dnmat  local_A(dA), local_B(dB);
-
-            device_sparse_matrix<C> dC(10, 10, 10, rocsparse_index_base_zero);
-            rocsparse_local_spmat   local_C(dC);
-
-            rocsparse_dnmat_descr mat_A = local_A;
-            rocsparse_dnmat_descr mat_B = local_B;
-            rocsparse_spmat_descr mat_C = local_C;
-
-            alg         = rocsparse_sddmm_alg_dense;
-            temp_buffer = nullptr;
-
-            EXPECT_ROCSPARSE_STATUS(rocsparse_sddmm(PARAMS), rocsparse_status_invalid_pointer);
-
-            alg         = rocsparse_sddmm_alg_default;
-            temp_buffer = (void*)0x4;
-        }
-
 #undef PARAMS
 #undef PARAMS_BUFFER_SIZE
     }
