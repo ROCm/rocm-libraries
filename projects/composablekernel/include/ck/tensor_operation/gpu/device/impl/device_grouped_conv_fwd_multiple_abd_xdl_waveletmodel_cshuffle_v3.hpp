0bf93db18ee65339fc4c3be8713bc8a41d7f177d
Enrico Degregori <enrico@streamhpc.com>
2026-09-18 11:28:39 +0000

    Fix bug forwarding DirectLoad template parameter

--- projects/composablekernel/include/ck/tensor_operation/gpu/device/impl/device_grouped_conv_fwd_multiple_abd_xdl_waveletmodel_cshuffle_v3.hpp
+++ projects/composablekernel/include/ck/tensor_operation/gpu/device/impl/device_grouped_conv_fwd_multiple_abd_xdl_waveletmodel_cshuffle_v3.hpp
@@ -552,7 +552,8 @@ struct DeviceGroupedConvFwdMultipleABD_WaveletModel_Xdl_CShuffle_V3
         CDEBlockTransferClusterLengths_MBlock_MPerBlock_NBlock_NPerBlock,
         CDEBlockTransferScalarPerVector_NPerBlock,
         AComputeDataType,
-        BComputeDataType>;
+        BComputeDataType,
+        DirectLoad>;
     // using GridwiseGemm64 = GridwiseGemmBase<math::max(NXdlPerWave64, 1)>;
     using GridwiseGemm32 = GridwiseGemmBase<NXdlPerWave32>;
 
