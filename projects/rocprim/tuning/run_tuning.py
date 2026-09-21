3bf8c06de3050a417eb3663ab7fc78f21d128b9f
Nick Breed <78807921+NB4444@users.noreply.github.com>
2026-09-21 22:25:52 +0200

    feat(rocprim): add device_reduce algorithms to kernel tuner  (#12095)
    ## Motivation
    
    I have added `device_reduce`, `device_reduce_by_key` and
    `device_segmented_reduce` to kernel tuner.
    JIRA ID : EXSWSTRHPC-322
    
    ## Technical Details
    
    I also added a small change to the script to not let a failed
    compilation (to much LDS usage for example) fully ruin the generation of
    the config, it will just raise a warning. It should be discouraged to
    have failing compilations in the search space (that is what restrictions
    are for), but it should still work with some failing compilations.
    
    ## Test Plan
    
    Run (part) of tuning generate a config and replace it with the current
    one and see if it compiles.
    
    ## Test Result
    
    Was able to run and generate configs, that could compile.
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/TheRock/blob/main/GOVERNANCE.md#pull-requests.

--- projects/rocprim/tuning/run_tuning.py
+++ projects/rocprim/tuning/run_tuning.py
@@ -75,7 +75,10 @@ def get_available_algorithms() -> List[str]:
         "device_select_unique_by_key",
         "device_select_flag",
         "device_select_predicate",
-        "device_select_unique"
+        "device_select_unique",
+        "device_reduce",
+        "device_segmented_reduce",
+        "device_reduce_by_key"
         # Add new algorithms here
     ]
 
