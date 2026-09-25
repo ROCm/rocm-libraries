11589605bec96d2a23647f3ddfd71a0458e71c43
Madhusoodhanan Prabha <amadhuso@ctr2-alola-ctrl-01.amd.com>
2026-09-25 11:09:42 -0400

    Removed unused SGPR usage, removed the KeepBuildTmp parameter from yaml config

--- projects/hipblaslt/tensilelite/Tensile/KernelWriterAssembly.py
+++ projects/hipblaslt/tensilelite/Tensile/KernelWriterAssembly.py
@@ -10942,12 +10942,6 @@ class KernelWriterAssembly(KernelWriter):
         activeMask = "0xFFFFFFFF" if (waveSize == 32) else "0xFFFFFFFFFFFFFFFF"
         SMovBX     = SMovB32 if (waveSize == 32) else SMovB64
         module.add(SMovBX(dst=sgpr(fullExec,sgprCnt), src=activeMask, comment="to restore all threads active"))
-        #bpeVgpr = self.vgprPool.checkOut(1, "bpeVgpr")
-        #module.add(VMovB32(dst=vgpr(bpeVgpr), src=int(tP["bpeGR"]), comment="bpeGR"))
-
-        # can remove this?
-        #zeroVgpr = self.vgprPool.checkOut(1,"zeroVgpr")
-        #module.add(VMovB32(dst=vgpr(zeroVgpr), src=0, comment="zero"))
 
     def globalReadGuardKBody(tP, optParams = None):
       if optParams != None:
@@ -11538,8 +11532,6 @@ class KernelWriterAssembly(KernelWriter):
     # BufferLoad=0 VGPRs are local to each call — always free them
     if not isTr and not kernel["BufferLoad"]:
       self.vgprPool.checkIn(maxAddrVgpr)
-      #self.vgprPool.checkIn(bpeVgpr)
-      #self.vgprPool.checkIn(zeroVgpr)
 
     if doTailOpt == 2:
       return module, loadCnt, vgprList, directToLdsLoads
